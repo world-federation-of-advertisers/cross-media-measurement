495ef5140cb996c940710cb031c7a884599d2459
Jason Frye <fryej@google.com>
2020-04-27 15:40:25 -0600

    Kotlin interfaces for managing computations within a duchy
    What's assumed is the backing storage layer has a place to put
    BLOBs and a relational database with strong consitency to place
    ongoing task metadata and infromation about what computations are
    ready to be picked up by a peasant.
    
    Change-Id: Ia39ecd776b817d5ec389a0b79b62e663c10f170c

---
+++ src/main/kt/org/wfanet/measurement/service/db/ComputationManager.kt
@@ -0,0 +1,148 @@
+package org.wfanet.measurement.service.db
+
+import java.io.IOException
+
+/**
+ * Manages status and state transitions for ongoing computations.
+ *
+ * @param[T] enum of the stages of a computation.
+ */
+class ComputationManager<T: Enum<T>> (
+  private val relationalDatabase: ComputationsRelationalDb<T>,
+  private val blobDatabase: ComputationsBlobDb) {
+
+  /**
+   * Creates a new computation.
+   *
+   * @throws [IOException] upon failure
+   */
+  fun createComputation(globalId: Long, stage: T): ComputationToken<T> {
+    return relationalDatabase.insertComputation(globalId, stage)
+  }
+
+  /**
+   * Returns a [ComputationToken] for the most recent computation for a
+   * [globalId].
+   */
+  fun getToken(globalId: Long): ComputationToken<T>? {
+    return relationalDatabase.getToken(globalId)
+  }
+
+  /**
+   * Transitions the state of an ongoing computation.
+   *
+   * This can be thought of as an atomic transaction, at least on the underlying
+   * [ComputationsRelationalDb]. BLOBs are written first via a
+   * [ComputationsBlobDb], and then the state and work queue is updated in a
+   * single transaction. The new state for the computation is in a clean state
+   * and is thus ready for whatever processing is required, by a worker. This
+   * means all input BLOBs are present at the end of this operation and all of
+   * those BLOBS' references are known for the state. Upon failure, it may be
+   * possible for BLOBs to be written. In such a case, it is up to the caller
+   * to clean up BLOBs if desired.
+   *
+   * Because each task knows the input blobs required to complete it the set of
+   * BLOBs required by the task are all referenced here. Not all of these BLOBs
+   * must be rewritten, as some can be carried forward to the next computation.
+   *
+   * @param[c] The task currently being worked
+   * @param[stateAfter] The state to transition the computation to
+   * @param[blobsToWrite] The input BLOBs for the new state which must be
+   *    written to the [BlobComputationsDb]
+   * @param[blobToCarryForward] The input BLOBs for the new state which already
+   *    exist in [BlobComputationsDb]
+   * @param[blobsRequiredForOutput] The BLOBs which should be written as part of
+   *    this stage, this may be useful when a stage is waiting on inputs from
+   *    multiple other workers.
+   * @param[afterTransition] What to do with the work after transitioning the
+   *    state.
+   *
+   * @throws [IOException] when state state transition fails
+   */
+  fun transitionState(
+    c: ComputationToken<T>,
+    stateAfter: T,
+    blobsToWrite: Map<BlobRef, ByteArray> = mapOf(),
+    blobToCarryForward: Collection<BlobRef> = listOf(),
+    blobsRequiredForOutput: Collection<BlobName> = listOf(),
+    afterTransition: AfterTransition): ComputationToken<T> {
+
+    val refs = HashSet<BlobRef>(blobToCarryForward)
+
+    for ((ref, bytes) in blobsToWrite) {
+      // TODO: Investigate using co-routines to write in parallel.
+      blobDatabase.blockingWrite(ref, bytes)
+      refs.add(ref)
+    }
+    return relationalDatabase.updateComputationState(
+      c, stateAfter, refs, blobsRequiredForOutput, afterTransition)
+  }
+
+  /**
+   * Enqueues a computation into the work queue.
+   *
+   * @throws [IOException] upon failure
+   */
+  fun enqueue(token: ComputationToken<T>) {
+    relationalDatabase.enqueue(token)
+  }
+
+  /**
+   * Pull for available work and claim a task for the worker.
+   *
+   * If the returned value is present, then the task has been claimed for the
+   * worker. When absent, no task was claimed.
+   */
+  fun claimWork(workerId: String): ComputationToken<T>? {
+    return relationalDatabase.claimTask(workerId)
+  }
+
+  /**
+   * Extend the lock time on a computation.
+   *
+   * @throws [IOException] upon failure
+   */
+  fun renewWork(token: ComputationToken<T>) {
+    relationalDatabase.renewTask(token)
+  }
+
+  /**
+   * Reads all the input BLOBs required for a computation task.
+   *
+   * @throws [IOException] upon failure
+   */
+  fun readInputBlobs(c: ComputationToken<T>): Map<BlobRef, ByteArray> {
+    return readBlobReferenceNames(c, BlobDependencyType.INPUT)
+      .map {
+        BlobRef(it.key,
+                checkNotNull(it.value) { "INPUT BLOB $it missing a path." })
+      }
+      // TODO: Read input blobs in parallel
+      .map { it to blobDatabase.read(it) }
+      .toMap()
+  }
+
+  /**
+   * Reads BLOB names for a computation task
+   *
+   * @throws [IOException] upon failure
+   */
+  fun readBlobReferenceNames(
+    token: ComputationToken<T>,
+    dependencyType: BlobDependencyType): Map<BlobName, String?> {
+    return relationalDatabase.readBlobReferenceNames(token, dependencyType = dependencyType)
+  }
+
+  /**
+   * Write a BLOB to blob storage, and then update the reference to it in the
+   * relation data base for a stage.
+   *
+   * @throws [IOException] upon failure
+   */
+  fun writeAndRecordOutputBlob(
+    token: ComputationToken<T>, blobName: BlobRef, blob: ByteArray) {
+
+    blobDatabase.blockingWrite(blobName, blob)
+    relationalDatabase.writeOutputBlobReference(token, blobName)
+  }
+}
---
+++ src/main/kt/org/wfanet/measurement/service/db/StorageAbstractions.kt
@@ -0,0 +1,193 @@
+package org.wfanet.measurement.service.db
+
+/**
+ * The role the worker is playing in an ongoing computation.
+ */
+enum class WorkerRole {
+  /**
+   * The worker is the first worker in the ring of MPC workers. They are
+   * expected to do a few more steps that other workers, such as homomorphic
+   * operations on ciphertexts.
+   */
+  PRIMARY,
+
+  /**
+   * The worker is not the first worker in the ring.
+   */
+  SECONDARY;
+}
+
+/**
+ * Information about a computation.
+ */
+data class ComputationToken<T : Enum<T>>(
+  /** The identifier for the computation used locally. */
+  val localId: Long,
+  /** The identifier for the computation used across all systems. */
+  val globalId: Long,
+  /** The state of the computation when the token was created. */
+  val state: T,
+  /** Name of peasant that owns the lock on the computation. */
+  val owner: String,
+  /** Identifier of the duchy that receives work for this computation. */
+  val nextWorker: String,
+  /** The role this worker is playing for this computation. */
+  val role: WorkerRole,
+  /** The number of previous attempts of this stage for this computation. */
+  val attempt: Long)
+
+/**
+ * Specifies what to do with the lock on a computation after transitioning to a
+ * new stage.
+ *
+ * @see[ComputationsRelationalDb.updateComputationState]
+ */
+enum class AfterTransition {
+  /** Retain and extend the lock for the current owner. */
+  CONTINUE_WORKING,
+
+  /**
+   * Add the computation to the work queue, but in an unclaimed state for some
+   * worker to claim at a later time.
+   */
+  ADD_UNCLAIMED_TO_QUEUE,
+
+  /**
+   * Do not add to the work queue, and release any lock on the computation.
+   * There is no work to be done on the computation at this time.
+   * Examples for when to set this include the computation finished or
+   * input from another source is required before continuing.
+   */
+  DO_NOT_ADD_TO_QUEUE
+}
+
+/**
+ * Relational database for keeping track of stages of a Computation within an
+ * MPC Node.
+ *
+ * The database must have strong consistency guarantees as it is used to
+ * coordinate assignment of work by pulling jobs.
+ */
+interface ComputationsRelationalDb<T : Enum<T>> {
+
+  /**
+   * Inserts a new computation for the global identifier.
+   *
+   * A new local identifier is created and returned in the [ComputationToken].
+   * The computation is not added to the queue.
+   */
+  fun insertComputation(globalId: Long, initialState: T): ComputationToken<T>
+
+  /**
+   * Returns a [ComputationToken] for the most recent computation for a
+   * [globalId].
+   */
+  fun getToken(globalId: Long): ComputationToken<T>?
+
+  /**
+   * Adds a computation to the work queue, saying it can be worked on by a
+   * worker job.
+   *
+   * This will release any ownership and locks associated with the computation.
+   */
+  fun enqueue(token: ComputationToken<T>)
+
+  /**
+   * Query for Computations with tasks ready for processing, and claim one for
+   * an owner.
+   *
+   * @param[ownerId] The identifier of the worker process that will own the
+   *    lock.
+   * @return [ComputationToken] for the work that needs to be completed,
+   *    when this value is null, no work was claimed.
+   */
+  fun claimTask(ownerId: String): ComputationToken<T>?
+
+  /** Extends the time a computation is locked. */
+  fun renewTask(token: ComputationToken<T>)
+
+  /**
+   * Transitions a computation to a new state.
+   *
+   * @param[token] The token for the computation
+   * @param[to] Stage this computation should transition to.
+   * @param[blobInputRefs] References to BLOBs that are inputs to this
+   *    computation stage, all inputs should be written on transition and should
+   *    not change.
+   * @param[blobOutputRefs] Names of BLOBs that are outputs to this computation.
+   *    These are created as part of the computation so they do not have a
+   *    reference to the real storage location.
+   * @param[afterTransition] States what work to do with the computation after
+   *    a successful transition.
+   */
+  fun updateComputationState(
+    token: ComputationToken<T>,
+    to: T,
+    blobInputRefs: Collection<BlobRef>,
+    blobOutputRefs: Collection<BlobName>,
+    afterTransition: AfterTransition
+  ): ComputationToken<T>
+
+  /**
+   * Reads mappings of blob names to paths in blob storage.
+   */
+  fun readBlobReferenceNames(
+    current: ComputationToken<T>,
+    dependencyType: BlobDependencyType = BlobDependencyType.INPUT
+  ): Map<BlobName, String?>
+
+  /**
+   * Writes the reference to a BLOB needed for [BlobDependencyType.OUTPUT] from
+   * a stage.
+   */
+  fun writeOutputBlobReference(c: ComputationToken<T>, blobName: BlobRef)
+}
+
+/**
+ * Name used to identify a BLOB by its purpose across multiple processes.
+ */
+data class BlobName(val name: String)
+
+/**
+ * Reference to a named BLOB's storage location.
+ */
+data class BlobRef(val name: BlobName, val pathToBlob: String) {
+  constructor(name: String, pathToBlob: String) : this(BlobName(name), pathToBlob)
+}
+
+/** BLOBs storage used by a computation. */
+interface ComputationsBlobDb {
+
+  /**
+   * Reads and returns a BLOB from storage
+   */
+  fun read(reference: BlobRef): ByteArray
+
+  /**
+   * Write a BLOB and ensure it is fully written before returning.
+   */
+  fun blockingWrite(blob: BlobRef, bytes: ByteArray)
+
+  /**
+   * Deletes a BLOB
+   */
+  fun delete(reference: String) {}
+}
+
+
+/**
+ * The way in which a stage depends upon a BLOB.
+ */
+enum class BlobDependencyType {
+  /** BLOB is used as an input to the computation stage. */
+  INPUT,
+
+  /** BLOB is an output of the computation stage. */
+  OUTPUT,
+
+  /**
+   * ONLY makes sense when reading blobs from the database. Read all blobs
+   * no mater the type.
+   */
+  ANY;
+}
