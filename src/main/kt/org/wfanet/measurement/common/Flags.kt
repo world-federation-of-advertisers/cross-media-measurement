d2df04e173d611fea07eb5e0e14833fd652f9615
Eli Fox-Epstein <efoxepstein@google.com>
2020-05-08 22:23:39 +0000

    Add Flags library for command-line flag parsing.
    There are a few open-source versions of these but none seem to be
    dominant and this it's so simple, it doesn't seem worth having another
    external dependency.
    
    Change-Id: I60e0c76ae1569a81f22cfedda214801ed20642e6

---
+++ src/main/kt/org/wfanet/measurement/common/Flags.kt
@@ -0,0 +1,116 @@
+/**
+ * Utility library for command-line flags.
+ *
+ * Only flags of the format "--flag_name=flag_value" are supported. This does not support positional
+ * arguments or spaces between the flag name and value.
+ */
+package org.wfanet.measurement.common
+
+/**
+ * Singleton owner of all [Flag] objects.
+ *
+ * Typical usage:
+ *
+ * ```
+ * fun main(args: Array<String>) = runBlocking {
+ *   Args.parse(args.asIterable())
+ *   ... rest of main ...
+ * }
+ * ```
+ */
+object Flags {
+  internal val flags = mutableMapOf<String, Flag<*>>()
+  private val PATTERN = Regex("^--?([a-z_-]+)=(.+)$")
+
+  /**
+   * Parses a list of command-line arguments to set flags.
+   *
+   * Each element of [args] should have the format "--flag_name=flag_value".
+   *
+   * @param[args] a list of args
+   */
+  fun parse(args: Iterable<String>) {
+    args.forEach(this::parse)
+  }
+
+  /**
+   * Parses a command-line argument in the form "--flag_name=flag_value".
+   *
+   * @param[arg] the argument to parse
+   */
+  fun parse(arg: String) {
+    // Group 0 is the full match, 1 is the flag name, 2 is the value.
+    val groups = PATTERN.matchEntire(arg)?.groupValues ?: listOf()
+    when (groups.size) {
+      3 -> setFlag(groups[1], groups[2])
+      else -> throw FlagError("Invalid arg: $arg")
+    }
+  }
+
+  /**
+   * Gets a Flag.
+   *
+   * @param[name] the global name of the flag
+   */
+  operator fun get(name: String) : Flag<*>? = flags[name]
+
+  /**
+   * Clears all flags.
+   *
+   * References to existing flags are still valid, but flags created before [clear] is called will
+   * not be set via [parse] and cannot be retried by [get].
+   */
+  fun clear() {
+    flags.clear()
+  }
+
+  private fun setFlag(name: String, value: String) {
+    when (val maybeFlag = flags[name]) {
+      null -> throw FlagError("Trying to set unknown flag: $name")
+      else -> maybeFlag.parseFrom(value)
+    }
+  }
+}
+
+/**
+ * Wraps a [value] of type [T], along with a string parser for this type.
+ *
+ * @param[T] the wrapped type
+ * @property[name] the unique global name for this value.
+ * @property[default] the initial value of the flag.
+ * @property[parser] a function to translate a string to a [T]
+ */
+class Flag<T>(val name: String, val default: T, private val parser: (String) -> T) {
+  var value: T = default
+
+  init {
+    if (Flags.flags.containsKey(name)) {
+      throw IllegalArgumentException("Flag $name already declared")
+    }
+    Flags.flags[name] = this
+  }
+
+  fun parseFrom(s: String) {
+    value = parser(s)
+  }
+}
+
+fun intFlag(name: String, default: Int): Flag<Int> = Flag(name, default, Integer::decode)
+
+fun longFlag(name: String, default: Long): Flag<Long> = Flag(name, default, java.lang.Long::decode)
+
+fun doubleFlag(name: String, default: Double): Flag<Double> = Flag(name, default, String::toDouble)
+
+fun booleanFlag(name: String, default: Boolean): Flag<Boolean> =
+  Flag(name, default) {
+    when (it) {
+      "1" -> true
+      "0" -> false
+      else -> it.toBoolean()
+    }
+  }
+
+fun stringFlag(name: String, default: String): Flag<String> = Flag(name, default, String::toString)
+
+class FlagError(message: String) : Exception(message)
+
---
+++ src/test/kt/org/wfanet/measurement/common/FlagsTest.kt
@@ -0,0 +1,124 @@
+package org.wfanet.measurement.common
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import kotlin.test.assertFailsWith
+
+@RunWith(JUnit4::class)
+class ArgsTest {
+  @Before
+  fun setup() {
+    Flags.clear()
+  }
+
+  @Test
+  fun `integer flag`() {
+    val flag = intFlag("foo", 100)
+    assertThat(flag.value).isEqualTo(100)
+
+    flag.value = 101
+    assertThat(flag.value).isEqualTo(101)
+
+    flag.parseFrom("102")
+    assertThat(flag.value).isEqualTo(102)
+
+    Flags.parse(listOf("--foo=103"))
+    assertThat(flag.value).isEqualTo(103)
+
+    Flags.parse("-foo=104")
+    assertThat(flag.value).isEqualTo(104)
+
+    Flags.parse("--foo=105")
+    assertThat(flag.value).isEqualTo(105)
+  }
+
+  @Test
+  fun `hex parsing`() {
+    val flag = intFlag("foo", 100)
+
+    flag.parseFrom("0xabc")
+    assertThat(flag.value).isEqualTo(0xabc)
+
+    flag.parseFrom("0xDEf")
+    assertThat(flag.value).isEqualTo(0xdef)
+  }
+
+  @Test
+  fun `boolean flag`() {
+    val flag = booleanFlag("foo", true)
+    assertThat(flag.value).isEqualTo(true)
+
+    flag.parseFrom("false")
+    assertThat(flag.value).isEqualTo(false)
+
+    flag.parseFrom("true")
+    assertThat(flag.value).isEqualTo(true)
+
+    flag.parseFrom("0")
+    assertThat(flag.value).isEqualTo(false)
+
+    flag.parseFrom("1")
+    assertThat(flag.value).isEqualTo(true)
+
+    Flags.parse("-foo=false")
+    assertThat(flag.value).isEqualTo(false)
+  }
+
+  @Test
+  fun `flag lookup`() {
+    val flag = stringFlag("foo", "abc")
+    assertThat(Flags["foo"]).isSameInstanceAs(flag)
+  }
+
+  @Test
+  fun `multiple flags`() {
+    stringFlag("foo", "abc")
+    intFlag("bar", 123)
+    longFlag("baz", 0)
+    booleanFlag("qux", false)
+
+    Flags.parse(listOf(
+      "--foo=def",
+      "-bar=456",
+      "-baz=0xabcdef",
+      "--qux=1"
+    ))
+
+    assertThat(Flags["foo"]!!.value).isEqualTo("def")
+    assertThat(Flags["bar"]!!.value).isEqualTo(456)
+    assertThat(Flags["baz"]!!.value).isEqualTo(0xabcdef)
+    assertThat(Flags["qux"]!!.value).isEqualTo(true)
+  }
+
+  @Test
+  fun `duplicate flag names`() {
+    stringFlag("foo", "abc")
+
+    assertFailsWith<IllegalArgumentException> {
+      stringFlag("foo", "def")
+    }
+  }
+
+  @Test
+  fun `unknown flag`() {
+    assertFailsWith<FlagError> { Flags.parse(listOf("--foo=10")) }
+  }
+
+  @Test
+  fun `bad flag formats`() {
+    stringFlag("foo", "abc")
+    assertFailsWith<FlagError> { Flags.parse("--foo") }
+    assertFailsWith<FlagError> { Flags.parse("=foo") }
+    assertFailsWith<FlagError> { Flags.parse("no flag name") }
+    assertFailsWith<FlagError> { Flags.parse("bar--foo=baz") }
+  }
+
+  @Test
+  fun `bad flag value`() {
+    val f = intFlag("foo", 0)
+    assertFailsWith<IllegalArgumentException> { f.parseFrom("not a valid number") }
+  }
+}
