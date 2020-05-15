8f9861ee5abacdfc9b8ba71e40cfd9a1f49f7b62
Jason Frye <fryej@google.com>
2020-05-15 11:39:57 -0600

    Helper functions to turn protos into Spanner ByteArrays and JSON
    These are to support protos in Spanner
    
    Change-Id: I5d0ecd6f9641833573c7c03e24f441c14fc499b5

---
+++ src/main/kt/org/wfanet/measurement/db/gcp/Protos.kt
@@ -0,0 +1,22 @@
+package org.wfanet.measurement.db.gcp
+
+import com.google.cloud.ByteArray
+import com.google.protobuf.Message
+import com.google.protobuf.MessageOrBuilder
+import com.google.protobuf.Parser
+import com.google.protobuf.util.JsonFormat
+
+/** Converts a protobuf [MessageOrBuilder] into its canonical JSON representation.*/
+fun MessageOrBuilder.toJson(): String {
+  return JsonFormat.printer().omittingInsignificantWhitespace().print(this)
+}
+
+/** Converts a protobuf [Message] to a spanner [ByteArray]. */
+fun Message.toSpannerByteArray(): ByteArray {
+  return ByteArray.copyFrom(this.toByteArray())
+}
+
+/** Parse a spanner [ByteArray] into a protobuf [Message]. */
+fun <T: Message> ByteArray.toProtobufMessage(parser: Parser<T>): T {
+  return parser.parseFrom(this.toByteArray())
+}
