b4a0e1aebfa0f6f0e3d6668da6b8ad5fde0e19db
Eli Fox-Epstein <efoxepstein@google.com>
2020-06-23 18:40:18 +0000

    Clean up GCP Timestamps.kt utility.
    - Remove unused extension functions.
    - Add new useful extension functions (will be used in follow-up CL).
    - Simplify some conversions.
    
    Change-Id: Ie692537d1bed35e3f1b0f7850a821e60473f1f38

--- src/main/kotlin/org/wfanet/measurement/db/gcp/Timestamps.kt
+++ src/main/kotlin/org/wfanet/measurement/db/gcp/Timestamps.kt
@@ -1,21 +1,15 @@
 package org.wfanet.measurement.db.gcp
 
 import com.google.cloud.Timestamp
-import java.sql.Date
 import java.time.Clock
 import java.time.Instant
-import java.util.concurrent.TimeUnit
 import org.wfanet.measurement.common.toInstant
 
 /** Converts a [Timestamp] to milliseconds since the epoch. */
-fun Timestamp.toMillis(): Long = toSqlTimestamp().time
+fun Timestamp.toMillis(): Long = toInstant().toEpochMilli()
 
-/** Converts a [Timestamp] to microseconds since the epoch. */
-fun Timestamp.toMicros(): Long =
-  TimeUnit.SECONDS.toMicros(seconds) + TimeUnit.NANOSECONDS.toMicros(nanos.toLong())
-
-/** Converts a long of milliseconds since the epoch to a [Timestamp]. */
-fun Long.toGcpTimestamp(): Timestamp = Timestamp.of(Date.from(Instant.ofEpochMilli(this)))
+/** Converts a [Timestamp] to an [Instant]. */
+fun Timestamp.toInstant(): Instant = Instant.ofEpochSecond(seconds, nanos.toLong())
 
 /**
  * Converts a [java.time.Instant] to a Spanner Timestamp.
@@ -28,4 +22,4 @@ fun Instant.toGcpTimestamp(): Timestamp = Timestamp.ofTimeSecondsAndNanos(epochS
 fun com.google.protobuf.Timestamp.toGcpTimestamp(): Timestamp = toInstant().toGcpTimestamp()
 
 /** Get the current time of a [Clock] as a GCP [Timestamp]. */
-fun Clock.gcpTimestamp(): Timestamp = Timestamp.ofTimeMicroseconds(millis() * 1000)
+fun Clock.gcpTimestamp(): Timestamp = instant().toGcpTimestamp()
