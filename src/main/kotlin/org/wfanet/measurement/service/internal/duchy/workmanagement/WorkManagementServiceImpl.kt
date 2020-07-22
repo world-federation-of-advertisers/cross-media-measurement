a325cea0ef054b2f9499cd13ef6b9d46571f8589
Jason Frye <fryej@google.com>
2020-07-22 14:49:40 -0600

    Delete the unused WorkManagementService
    This was the service for pushed based starting of computations
    from the kingdom. That is replaced with the Herald.
    
    Change-Id: I0bddc2569557a585585722c7d71b64131b555d9e

--- src/main/kotlin/org/wfanet/measurement/service/internal/duchy/workmanagement/WorkManagementServiceImpl.kt
+++
@@ -1,6 +0,0 @@
-package org.wfanet.measurement.service.internal.duchy.workmanagement
-
-import org.wfanet.measurement.internal.duchy.WorkManagementServiceGrpcKt
-
-class WorkManagementServiceImpl :
-  WorkManagementServiceGrpcKt.WorkManagementServiceCoroutineImplBase()
