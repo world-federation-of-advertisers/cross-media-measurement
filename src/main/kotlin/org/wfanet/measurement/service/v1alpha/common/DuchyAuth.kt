7fba8b54b7f96931b25a9bb3091aa25434a1d3da
Eli Fox-Epstein <efoxepstein@google.com>
2020-08-18 21:39:55 +0000

    Add utilities for passing duchy id in gRPC side channel.
    This CL doesn't handle actual authentication or authorization,
    but it provides a mechanism for passing the Duchy ID in the side channel.
    
    This CL takes no stance on how authz/authn should be performed. We
    simply use the side channel to pass the duchy id as an alternative to
    putting it in the request body because:
    
    - This is a uniform, automatic way to transmit the identity.
    - Duchies can configure this in one line of code with little opportunity
      for error.
    - It avoids polluting the API with extra duchy_id fields.
    
    If, eventually, authz is done through gRPC's credential mechanisms, it
    may require only small changes for this identifier to be secure.
    
    Change-Id: Ic80c9821a84159c40886115552b6d0cf46f34d0f

--- src/main/kotlin/org/wfanet/measurement/service/v1alpha/common/DuchyAuth.kt
+++ src/main/kotlin/org/wfanet/measurement/service/v1alpha/common/DuchyAuth.kt
@@ -1,6 +1,14 @@
 package org.wfanet.measurement.service.v1alpha.common
 
 import io.grpc.Context
+import io.grpc.Contexts
+import io.grpc.Metadata
+import io.grpc.ServerCall
+import io.grpc.ServerCallHandler
+import io.grpc.ServerInterceptor
+import io.grpc.Status
+import io.grpc.stub.AbstractStub
+import io.grpc.stub.MetadataUtils
 
 /**
  * Details about an authenticated Duchy.
@@ -10,6 +18,51 @@ import io.grpc.Context
 data class DuchyAuth(val authenticatedDuchyId: String)
 
 val duchyAuthFromContext: DuchyAuth
-  get() = DUCHY_AUTH_KEY.get()!!
+  get() = requireNotNull(DUCHY_AUTH_CONTEXT_KEY.get())
 
-private val DUCHY_AUTH_KEY: Context.Key<DuchyAuth> = Context.key("duchy_auth")
+private val DUCHY_AUTH_CONTEXT_KEY: Context.Key<DuchyAuth> = Context.key("duchy_auth")
+private val DUCHY_ID_METADATA_KEY = Metadata.Key.of("duchy_id", Metadata.ASCII_STRING_MARSHALLER)
+
+/**
+ * Add an interceptor that sets DuchyAuth in the context.
+ *
+ * Note that this doesn't provide any guarantees that the Duchy is who it claims to be -- that is
+ * still required.
+ *
+ * To install in a server, wrap your Service with:
+ *    ServerInterceptors.interceptForward(yourService, DuchyServerIdentityInterceptor())
+ *
+ * On the client side, use [attachDuchyIdentityHeaders].
+ */
+class DuchyServerIdentityInterceptor : ServerInterceptor {
+  override fun <ReqT, RespT> interceptCall(
+    call: ServerCall<ReqT, RespT>,
+    headers: Metadata,
+    next: ServerCallHandler<ReqT, RespT>
+  ): ServerCall.Listener<ReqT> {
+    val duchyId: String? = headers.get(DUCHY_ID_METADATA_KEY)
+
+    if (duchyId == null) {
+      call.close(
+        Status.UNAUTHENTICATED.withDescription("gRPC metadata missing 'duchy_id' key"),
+        Metadata()
+      )
+      return object : ServerCall.Listener<ReqT>() {}
+    }
+
+    val context = Context.current().withValue(DUCHY_AUTH_CONTEXT_KEY, DuchyAuth(duchyId))
+    return Contexts.interceptCall(context, call, headers, next)
+  }
+}
+
+/**
+ * Sets metadata key "duchy_id" on all outgoing requests.
+ *
+ * Usage:
+ *   val someStub = attachDuchyIdentityHeaders(SomeServiceCoroutineStub(channel), "MyDuchyId")
+ */
+fun <T : AbstractStub<T>> attachDuchyIdentityHeaders(stub: T, duchyId: String): T {
+  val metadata = Metadata()
+  metadata.put(DUCHY_ID_METADATA_KEY, duchyId)
+  return MetadataUtils.attachHeaders(stub, metadata)
+}
