cae33d3c1c5c59ac0f651cc10dfabd8e0d98416a
Mark Fashing <fashing@google.com>
2020-09-04 11:02:35 -0600

    Fix storage fake to support verbose logging.
    Bug: 162265237
    Change-Id: I7e2ab61872bb781e2a915ddcfb87c758b080cde7

--- src/main/kotlin/org/wfanet/measurement/service/testing/storage/Main.kt
+++ src/main/kotlin/org/wfanet/measurement/service/testing/storage/Main.kt
@@ -1,30 +1,19 @@
 package org.wfanet.measurement.service.testing.storage
 
-import kotlin.properties.Delegates
 import org.wfanet.measurement.common.commandLineMain
 import org.wfanet.measurement.service.common.CommonServer
 import picocli.CommandLine
 
-private class FakeStorageServiceFlags {
-  @set:CommandLine.Option(
-    names = ["--port", "-p"],
-    description = ["TCP port for gRPC server."],
-    required = true,
-    defaultValue = "8080"
-  )
-  var port: Int by Delegates.notNull()
-    private set
-}
-
 @CommandLine.Command(
-  name = "gcp_worker_server",
+  name = "fake_storage_server",
   mixinStandardHelpOptions = true,
   showDefaultValues = true
 )
-private fun run(@CommandLine.Mixin fakeStorageServiceFlags: FakeStorageServiceFlags) {
-  CommonServer(
+private fun run(
+  @CommandLine.Mixin commonServerFlags: CommonServer.Flags) {
+  CommonServer.fromFlags(
+    commonServerFlags,
     "FakeStorageService",
-    fakeStorageServiceFlags.port,
     FakeStorageService()
   ).start().blockUntilShutdown()
 }
