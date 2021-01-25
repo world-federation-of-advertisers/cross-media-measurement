9e6d1cb6c69436723f2e27a0779f9c437a0b23fb
Mark Fashing <fashing@google.com>
2021-01-25 15:20:08 -0700

    Changing cross-media-measurement license headers to refer to Cross-Media Measurement Authors
    Change-Id: I468321ff1674f21e48758cb4f33a3b15dfc759eb

--- tools/prow/lint.sh
+++ tools/prow/lint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2020 The Measurement System Authors
+# Copyright 2020 The Cross-Media Measurement Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
