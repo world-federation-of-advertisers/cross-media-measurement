c257ae4f0790d5080875edf80f536c4ae422ee19
Mark Fashing <fashing@google.com>
2021-01-25 15:22:50 -0700

    Changing cross-media-measurement-api license headers to refer to Cross-Media Measurement Authors
    Change-Id: If0ee805eded84cae6dced34d917b80bdf113f65b

--- tools/generate_plantuml_diagrams.sh
+++ tools/generate_plantuml_diagrams.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2020 The Measurement Proto Authors
+# Copyright 2020 The Cross-Media Measurement Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
