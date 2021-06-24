7448d9535d626e5b7c7de13756c2a753bd8e3067
Ugur <uakyol@google.com>
2021-06-24 05:24:36 +0000

    fixed test failure caused by importing new version of proto with the same name

--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/testing/KingdomDataServicesProviderRule.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/testing/KingdomDataServicesProviderRule.kt
@@ -22,6 +22,7 @@ import org.wfanet.measurement.common.testing.ProviderRule
 import org.wfanet.measurement.gcloud.spanner.AsyncDatabaseClient
 import org.wfanet.measurement.gcloud.spanner.testing.SpannerEmulatorDatabaseRule
 import org.wfanet.measurement.kingdom.service.internal.testing.KingdomDataServices
+import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.testing.KINGDOM_SCHEMA
 
 class KingdomDataServicesProviderRule : ProviderRule<KingdomDataServices> {
   private val spannerDatabase = SpannerEmulatorDatabaseRule(KINGDOM_SCHEMA)
