d2df635f2364a3b37ceb41cef304521e595210c8
Yao Wang <64107318+wangyaopw@users.noreply.github.com>
2021-06-24 15:15:56 -0400

    Update the ComputationControlService to use resourceName. (#114)
    The PR also
    * renames MetricValueStore to RequisitionStore.
    * changes ComputationControlService to use requsitionStore instead of
    storageClient directly.
    * deletes the unused GlobalComputation proto.

--- src/main/kotlin/org/wfanet/measurement/duchy/service/system/v1alpha/ComputationControlService.kt
+++ src/main/kotlin/org/wfanet/measurement/duchy/service/system/v1alpha/ComputationControlService.kt
@@ -25,18 +25,26 @@ import org.wfanet.measurement.common.grpc.grpcRequire
 import org.wfanet.measurement.common.grpc.grpcRequireNotNull
 import org.wfanet.measurement.common.identity.DuchyIdentity
 import org.wfanet.measurement.common.identity.duchyIdentityFromContext
+import org.wfanet.measurement.duchy.storage.RequisitionStore
 import org.wfanet.measurement.internal.duchy.AdvanceComputationRequest as AsyncAdvanceComputationRequest
 import org.wfanet.measurement.internal.duchy.AsyncComputationControlGrpcKt.AsyncComputationControlCoroutineStub
 import org.wfanet.measurement.storage.StorageClient
 import org.wfanet.measurement.system.v1alpha.AdvanceComputationRequest
 import org.wfanet.measurement.system.v1alpha.AdvanceComputationResponse
 import org.wfanet.measurement.system.v1alpha.ComputationControlGrpcKt.ComputationControlCoroutineImplBase
+import org.wfanet.measurement.system.v1alpha.ComputationKey
 
 class ComputationControlService(
   private val asyncComputationControlClient: AsyncComputationControlCoroutineStub,
-  private val storageClient: StorageClient,
+  private val requisitionStore: RequisitionStore,
   private val duchyIdentityProvider: () -> DuchyIdentity = ::duchyIdentityFromContext
 ) : ComputationControlCoroutineImplBase() {
+
+  constructor(
+    asyncComputationControlClient: AsyncComputationControlCoroutineStub,
+    storageClient: StorageClient,
+    duchyIdentityProvider: () -> DuchyIdentity = ::duchyIdentityFromContext
+  ) : this(asyncComputationControlClient, RequisitionStore(storageClient), duchyIdentityProvider)
 
   override suspend fun advanceComputation(
     requests: Flow<AdvanceComputationRequest>
@@ -44,12 +52,17 @@ class ComputationControlService(
     grpcRequireNotNull(requests.consumeFirst()) { "Empty request stream" }.use {
       consumed: ConsumedFlowItem<AdvanceComputationRequest> ->
       val header = consumed.item.header
-      val id = header.key.globalComputationId
-      logger.info("[id=$id]: Received request with header $header")
-      grpcRequire(id.isNotEmpty()) { "Cannot advance computation, missing computation ID" }
+      val globalComputationId =
+        grpcRequireNotNull(ComputationKey.fromName(header.name)?.computationId) {
+          "Resource name unspecified or invalid."
+        }
       grpcRequire(consumed.hasRemaining) { "Request stream has no body" }
       if (header.isForAsyncComputation()) {
-        handleAsyncRequest(header, consumed.remaining.map { it.bodyChunk.partialData }, id)
+        handleAsyncRequest(
+          header,
+          consumed.remaining.map { it.bodyChunk.partialData },
+          globalComputationId
+        )
       } else {
         failGrpc { "Synchronous computations are not yet supported." }
       }
@@ -60,20 +73,18 @@ class ComputationControlService(
   /** Write the payload data stream as a blob, and advance the stage via the async service. */
   private suspend fun handleAsyncRequest(
     header: AdvanceComputationRequest.Header,
-    blob: Flow<ByteString>,
+    content: Flow<ByteString>,
     globalId: String
   ) {
-    val sender = duchyIdentityProvider().id
-    val blobKey = generateBlobKey(header, sender)
-    if (storageClient.getBlob(blobKey) == null) {
-      storageClient.createBlob(blobKey, blob)
-    }
+    // TODO: maybe early reject the request, e.g., by checking if a computation with this id
+    //  exists.
+    val blob = requisitionStore.write(content)
     val advanceAsyncComputationRequest =
       AsyncAdvanceComputationRequest.newBuilder()
         .apply {
           globalComputationId = globalId
           computationStage = header.stageExpectingInput()
-          blobPath = blobKey
+          blobPath = blob.blobKey
           dataOrigin = duchyIdentityProvider().id
         }
         .build()
@@ -82,21 +93,5 @@ class ComputationControlService(
 
   companion object {
     private val logger: Logger = Logger.getLogger(this::class.java.name)
-
-    fun generateBlobKey(header: AdvanceComputationRequest.Header, sender: String): String =
-      with(header) {
-        return listOf(
-            "/computations",
-            key.globalComputationId,
-            protocolCase.name,
-            when (protocolCase) {
-              AdvanceComputationRequest.Header.ProtocolCase.LIQUID_LEGIONS_V2 ->
-                liquidLegionsV2.description.name
-              else -> failGrpc { "Unknown protocol $protocolCase" }
-            },
-            sender
-          )
-          .joinToString("/")
-      }
   }
 }
