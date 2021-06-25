311ecde15435d9632bd30aa02beca40fcde48528
Ugur <uakyol@google.com>
2021-06-25 02:17:46 +0000

    rebasing

--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/writers/CreateMeasurementConsumer.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/writers/CreateMeasurementConsumer.kt
@@ -34,7 +34,6 @@ class CreateMeasurementConsumer(private val measurementConsumer: MeasurementCons
     val internalMeasurementConsumerId = idGenerator.generateInternalId()
     val externalMeasurementConsumerId = idGenerator.generateExternalId()
 
-
     Mutation.newInsertBuilder("MeasurementConsumers")
       .set("MeasurementConsumerId")
       .to(internalMeasurementConsumerId.value)
