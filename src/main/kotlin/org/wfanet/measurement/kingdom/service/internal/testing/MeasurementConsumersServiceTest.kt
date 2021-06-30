da0f7f3c87012c88186e656c8c70fe9adddff776
Ugur <uakyol@google.com>
2021-06-30 20:01:17 +0000

    addressing comments

--- src/main/kotlin/org/wfanet/measurement/kingdom/service/internal/testing/MeasurementConsumersServiceTest.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/service/internal/testing/MeasurementConsumersServiceTest.kt
@@ -16,6 +16,7 @@ package org.wfanet.measurement.kingdom.service.internal.testing
 
 import com.google.common.truth.Truth.assertThat
 import com.google.common.truth.extensions.proto.ProtoTruth.assertThat
+import com.google.protobuf.ByteString
 import io.grpc.Status
 import io.grpc.StatusRuntimeException
 import kotlin.test.assertFailsWith
@@ -28,6 +29,7 @@ import org.wfanet.measurement.internal.kingdom.MeasurementConsumer
 import org.wfanet.measurement.internal.kingdom.MeasurementConsumersGrpcKt.MeasurementConsumersCoroutineImplBase
 
 private const val EXTERNAL_MEASUREMENT_CONSUMER_ID = 123L
+private val PREFERRED_CERTIFICATE_DER = ByteString.copyFromUtf8("This is a certificate der.")
 
 @RunWith(JUnit4::class)
 abstract class MeasurementConsumersServiceTest {
@@ -44,15 +46,44 @@ abstract class MeasurementConsumersServiceTest {
         )
       }
 
-    assertThat(exception.status.code).isEqualTo(Status.Code.FAILED_PRECONDITION)
+    assertThat(exception.status.code).isEqualTo(Status.Code.NOT_FOUND)
   }
 
   @Test
   fun `createMeasurementConsumer succeeds`() = runBlocking {
+    val measurementConsumer =
+      MeasurementConsumer.newBuilder()
+        .apply {
+          preferredCertificateBuilder.apply {
+            notValidBeforeBuilder.seconds = 12345
+            notValidAfterBuilder.seconds = 23456
+            detailsBuilder.setX509Der(PREFERRED_CERTIFICATE_DER) }
+          detailsBuilder.apply { apiVersion = "2" }
+        }
+        .build()
     val createdMeasurementConsumer =
-      measurementConsumersService.createMeasurementConsumer(
-        MeasurementConsumer.newBuilder().apply { detailsBuilder.apply { apiVersion = "2" } }.build()
-      )
+      measurementConsumersService.createMeasurementConsumer(measurementConsumer)
+
+    assertThat(createdMeasurementConsumer)
+      .comparingExpectedFieldsOnly()
+      .isEqualTo(measurementConsumer)
+  }
+
+  @Test
+  fun `getMeasurementConsumer succeeds`() = runBlocking {
+
+    val measurementConsumer =
+      MeasurementConsumer.newBuilder()
+        .apply {
+          preferredCertificateBuilder.apply {
+            notValidBeforeBuilder.seconds = 12345
+            notValidAfterBuilder.seconds = 23456
+            detailsBuilder.setX509Der(PREFERRED_CERTIFICATE_DER) }
+          detailsBuilder.apply { apiVersion = "2" }
+        }
+        .build()
+    val createdMeasurementConsumer =
+      measurementConsumersService.createMeasurementConsumer(measurementConsumer)
 
     val measurementConsumerRead =
       measurementConsumersService.getMeasurementConsumer(
