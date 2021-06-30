fca6c31f95877f4c0d888613c00835c287f7fed9
Ugur <uakyol@google.com>
2021-06-30 20:36:39 +0000

    addressed comments

--- src/main/kotlin/org/wfanet/measurement/kingdom/service/internal/testing/MeasurementConsumersServiceTest.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/service/internal/testing/MeasurementConsumersServiceTest.kt
@@ -53,8 +53,11 @@ import org.wfanet.measurement.internal.kingdom.MeasurementConsumersGrpcKt.Measur
 private const val EXTERNAL_MEASUREMENT_CONSUMER_ID = 123L
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 private const val FIXED_GENERATED_INTERNAL_ID = 2345L
 private const val FIXED_GENERATED_EXTERNAL_ID = 6789L
+=======
+>>>>>>> e1417171 (addressed comments)
 private val PUBLIC_KEY = ByteString.copyFromUtf8("This is a  public key.")
 private val PUBLIC_KEY_SIGNATURE = ByteString.copyFromUtf8("This is a  public key signature.")
 private val PREFERRED_CERTIFICATE_DER = ByteString.copyFromUtf8("This is a certificate der.")
@@ -146,6 +149,30 @@ abstract class MeasurementConsumersServiceTest {
   }
 
   @Test
+  fun `createMeasurementConsumer fails for missing fields`() = runBlocking {
+    val measurementConsumer =
+      MeasurementConsumer.newBuilder()
+        .apply {
+          preferredCertificateBuilder.apply {
+            notValidBeforeBuilder.seconds = 12345
+            notValidAfterBuilder.seconds = 23456
+            detailsBuilder.setX509Der(PREFERRED_CERTIFICATE_DER)
+          }
+          detailsBuilder.apply {
+            apiVersion = "2"
+            publicKey = PUBLIC_KEY
+          }
+        }
+        .build()
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        measurementConsumersService.createMeasurementConsumer(measurementConsumer)
+      }
+
+    assertThat(exception.status.code).isEqualTo(Status.Code.INVALID_ARGUMENT)
+  }
+
+  @Test
   fun `createMeasurementConsumer succeeds`() = runBlocking {
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -160,6 +187,9 @@ abstract class MeasurementConsumersServiceTest {
             notValidBeforeBuilder.seconds = 12345
             notValidAfterBuilder.seconds = 23456
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e1417171 (addressed comments)
             detailsBuilder.setX509Der(PREFERRED_CERTIFICATE_DER)
           }
           detailsBuilder.apply {
@@ -167,10 +197,13 @@ abstract class MeasurementConsumersServiceTest {
             publicKey = PUBLIC_KEY
             publicKeySignature = PUBLIC_KEY_SIGNATURE
           }
+<<<<<<< HEAD
 =======
             detailsBuilder.setX509Der(PREFERRED_CERTIFICATE_DER) }
           detailsBuilder.apply { apiVersion = "2" }
 >>>>>>> da0f7f3c (addressing comments)
+=======
+>>>>>>> e1417171 (addressed comments)
         }
         .build()
     val createdMeasurementConsumer =
@@ -200,9 +233,12 @@ abstract class MeasurementConsumersServiceTest {
   @Test
   fun `getMeasurementConsumer succeeds`() = runBlocking {
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 >>>>>>> da0f7f3c (addressing comments)
+=======
+>>>>>>> e1417171 (addressed comments)
     val measurementConsumer =
       MeasurementConsumer.newBuilder()
         .apply {
@@ -210,6 +246,9 @@ abstract class MeasurementConsumersServiceTest {
             notValidBeforeBuilder.seconds = 12345
             notValidAfterBuilder.seconds = 23456
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e1417171 (addressed comments)
             detailsBuilder.setX509Der(PREFERRED_CERTIFICATE_DER)
           }
           detailsBuilder.apply {
@@ -217,10 +256,13 @@ abstract class MeasurementConsumersServiceTest {
             publicKey = PUBLIC_KEY
             publicKeySignature = PUBLIC_KEY_SIGNATURE
           }
+<<<<<<< HEAD
 =======
             detailsBuilder.setX509Der(PREFERRED_CERTIFICATE_DER) }
           detailsBuilder.apply { apiVersion = "2" }
 >>>>>>> da0f7f3c (addressing comments)
+=======
+>>>>>>> e1417171 (addressed comments)
         }
         .build()
     val createdMeasurementConsumer =
