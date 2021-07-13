7bcc03a4c60e4e474565eb1049fa80ac49aae6ff
yunyeng <yunyeng@google.com>
2021-07-13 03:09:51 +0000

    f Add GetExchangeStep classes to retrieve an Exchange Step

--- src/test/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/queries/GetExchangeStepTest.kt
+++ src/test/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/queries/GetExchangeStepTest.kt
@@ -14,9 +14,13 @@
 
 package org.wfanet.measurement.kingdom.deploy.gcloud.spanner.queries
 
+import com.google.cloud.spanner.Statement
+import com.google.cloud.spanner.Struct
+import com.google.cloud.spanner.TimestampBound
 import com.google.common.truth.extensions.proto.ProtoTruth.assertThat
 import com.google.protobuf.ByteString
 import com.google.type.Date
+import java.time.Instant
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
@@ -28,7 +32,6 @@ import org.wfanet.measurement.internal.kingdom.Exchange
 import org.wfanet.measurement.internal.kingdom.ExchangeDetails
 import org.wfanet.measurement.internal.kingdom.ExchangeStep
 import org.wfanet.measurement.internal.kingdom.RecurringExchange
-import org.wfanet.measurement.internal.kingdom.Report.ReportState
 import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.testing.KingdomDatabaseTestBase
 
 private const val DATA_PROVIDER_ID = 1L
@@ -69,10 +72,6 @@ private val DATE3 =
 private val EXCHANGE_DETAILS =
   ExchangeDetails.newBuilder().setAuditTrailHash(ByteString.copyFromUtf8("123")).build()
 
-//private val EXCHANGE_STEP1 = buildExchangeStep(DATE1, Exchange.State.ACTIVE)
-//private val EXCHANGE_STEP2 = buildExchangeStep(DATE2, Exchange.State.FAILED)
-//private val EXCHANGE_STEP3 = buildExchangeStep(DATE3, Exchange.State.ACTIVE)
-
 @RunWith(JUnit4::class)
 class GetExchangeStepTest : KingdomDatabaseTestBase() {
   @Before
@@ -100,40 +99,95 @@ class GetExchangeStepTest : KingdomDatabaseTestBase() {
     insertExchange(RECURRING_EXCHANGE_ID1, DATE1, Exchange.State.ACTIVE, EXCHANGE_DETAILS)
     insertExchange(RECURRING_EXCHANGE_ID2, DATE2, Exchange.State.FAILED, EXCHANGE_DETAILS)
     insertExchange(RECURRING_EXCHANGE_ID1, DATE3, Exchange.State.ACTIVE, EXCHANGE_DETAILS)
+
+    insertExchangeStep(
+      RECURRING_EXCHANGE_ID1,
+      DATE1,
+      1L,
+      ExchangeStep.State.READY,
+      Instant.now().minusSeconds(1000),
+      null,
+      DATA_PROVIDER_ID
+    )
+    insertExchangeStep(
+      RECURRING_EXCHANGE_ID2,
+      DATE2,
+      1L,
+      ExchangeStep.State.BLOCKED,
+      Instant.now(),
+      null,
+      DATA_PROVIDER_ID
+    )
+    insertExchangeStep(
+      RECURRING_EXCHANGE_ID1,
+      DATE3,
+      2L,
+      ExchangeStep.State.READY_FOR_RETRY,
+      Instant.now(),
+      MODEL_PROVIDER_ID,
+      null
+    )
   }
 
-//  @Test
-//  fun `the Exchange Step exists`() = runBlocking {
-//    val report = GetExchangeStepByDataProvider(ExternalId(EXTERNAL_REPORT_ID)).executeSingle(databaseClient.singleUse())
-//
-//    val expected =
-//      ExchangeStep.newBuilder()
-//        .apply {
-//          externalAdvertiserId = EXTERNAL_ADVERTISER_ID
-//          externalReportConfigId = EXTERNAL_REPORT_CONFIG_ID
-//          externalScheduleId = EXTERNAL_SCHEDULE_ID
-//          externalReportId = EXTERNAL_REPORT_ID
-//          state = ReportState.IN_PROGRESS
-//        }
-//        .build()
-//
-//    assertThat(report).comparingExpectedFieldsOnly().isEqualTo(expected)
-//  }
+  private suspend fun readExchangeStepStructs(): List<Struct> =
+    databaseClient
+      .singleUse(TimestampBound.strong())
+      .executeQuery(Statement.of("SELECT * FROM ExchangeSteps"))
+      .toList()
+
+  @Test
+  fun `the Exchange Step with Data Provider exists`() = runBlocking {
+    val exchangeStep =
+      GetExchangeStepByDataProvider(
+          ExternalId(EXTERNAL_DATA_PROVIDER_ID),
+          listOf(ExchangeStep.State.READY, ExchangeStep.State.READY_FOR_RETRY)
+        )
+        .executeSingle(databaseClient.singleUse())
+    val expected =
+      ExchangeStep.newBuilder()
+        .apply {
+          externalRecurringExchangeId = EXTERNAL_RECURRING_EXCHANGE_ID1
+          date = DATE1
+          stepIndex = 1
+          state = ExchangeStep.State.READY
+          externalDataProviderId = EXTERNAL_DATA_PROVIDER_ID
+        }
+        .build()
+
+    assertThat(exchangeStep).comparingExpectedFieldsOnly().isEqualTo(expected)
+  }
+
+  @Test
+  fun `the Exchange Step with Model Provider exists`() = runBlocking {
+    val exchangeStep =
+      GetExchangeStepByModelProvider(
+          ExternalId(EXTERNAL_MODEL_PROVIDER_ID),
+          listOf(ExchangeStep.State.READY_FOR_RETRY)
+        )
+        .executeSingle(databaseClient.singleUse())
+    val expected =
+      ExchangeStep.newBuilder()
+        .apply {
+          externalRecurringExchangeId = EXTERNAL_RECURRING_EXCHANGE_ID1
+          date = DATE3
+          stepIndex = 2
+          state = ExchangeStep.State.READY_FOR_RETRY
+          externalModelProviderId = EXTERNAL_MODEL_PROVIDER_ID
+        }
+        .build()
+
+    assertThat(exchangeStep).comparingExpectedFieldsOnly().isEqualTo(expected)
+  }
 
   @Test
   fun `the Exchange Step does not exist`() = runBlocking {
     val exchangeSteps =
-      GetExchangeStepByModelProvider(ExternalId(EXTERNAL_DATA_PROVIDER_ID), listOf(ExchangeStep.State.READY)).execute(databaseClient.singleUse()).toList()
+      GetExchangeStepByModelProvider(
+          ExternalId(EXTERNAL_DATA_PROVIDER_ID),
+          listOf(ExchangeStep.State.READY_FOR_RETRY)
+        )
+        .execute(databaseClient.singleUse())
+        .toList()
     assertThat(exchangeSteps).isEmpty()
   }
 }
-//
-//private fun buildExchangeStep(state: Exchange.State): ExchangeStep {
-//  return ExchangeStep.newBuilder()
-//    .apply {
-//      this.date = date
-//      this.state = state
-//      this.details = EXCHANGE_DETAILS
-//    }
-//    .build()
-//}
