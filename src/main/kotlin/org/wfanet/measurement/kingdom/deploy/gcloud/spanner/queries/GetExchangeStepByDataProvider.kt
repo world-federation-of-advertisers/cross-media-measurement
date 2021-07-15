4e2a69cf564df90b3ebdd3188750ec23ba5b8b23
Yunus Yen <yunyeng@users.noreply.github.com>
2021-07-15 11:27:58 -0700

    Update GetExchangeStepByDataProvider.kt

--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/queries/GetExchangeStepByDataProvider.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/queries/GetExchangeStepByDataProvider.kt
@@ -17,6 +17,7 @@ package org.wfanet.measurement.kingdom.deploy.gcloud.spanner.queries
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import org.wfanet.measurement.common.identity.ExternalId
+import org.wfanet.measurement.common.numberAsLong
 import org.wfanet.measurement.gcloud.spanner.appendClause
 import org.wfanet.measurement.internal.kingdom.ExchangeStep
 import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.ExchangeStepReader
