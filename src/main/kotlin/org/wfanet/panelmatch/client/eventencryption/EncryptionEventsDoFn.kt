f8fd0ec1f4a8c44ae683e1468bfb2edfe6f4ca74
Julia Morrissey <84739966+jmorrrr@users.noreply.github.com>
2021-07-20 13:49:49 -0400

    Adds EncryptionDoFn (world-federation-of-advertisers/panel-exchange-client#48)
    Creates ApacheBeam Encryption DoFn that takes in a MutableList<KV<ByteString,ByteString>>, packs them into PreprocessedEventRequest protos, encrypts the identifier and event data using several encryption schemes, and unpacks them from PreprocessedEventResponse protos and emits them as KV<ByteString,ByteString> pairs. Testing for this DoFn is also included.

---
+++ src/main/kotlin/org/wfanet/panelmatch/client/eventencryption/EncryptionEventsDoFn.kt
@@ -0,0 +1,52 @@
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.panelmatch.client.eventencryption
+
+import com.google.protobuf.ByteString
+import org.apache.beam.sdk.transforms.DoFn
+import org.apache.beam.sdk.transforms.SerializableFunction
+import org.apache.beam.sdk.values.KV
+import org.wfanet.panelmatch.client.PreprocessEventsRequest
+import org.wfanet.panelmatch.client.PreprocessEventsResponse
+
+/**
+ * Takes in a MutableList<KV<ByteString,ByteString>>, packs them into PreprocessedEventRequest
+ * protos, encrypts the identifier and event data using several encryption schemes, and unpacks them
+ * from PreprocessedEventResponse protos and emits them as KV<ByteString,ByteString> pairs
+ */
+class EncryptionEventsDoFn(
+  private val encryptEvents: SerializableFunction<PreprocessEventsRequest, PreprocessEventsResponse>
+) : DoFn<MutableList<KV<ByteString, ByteString>>, KV<ByteString, ByteString>>() {
+  @ProcessElement
+  fun process(c: ProcessContext) {
+    val list: MutableList<KV<ByteString, ByteString>> = c.element()
+    val request =
+      PreprocessEventsRequest.newBuilder()
+        .apply {
+          for (pair in list) {
+            addUnprocessedEventsBuilder().apply {
+              this.id = pair.key
+              this.data = pair.value
+            }
+          }
+        }
+        .build()
+    val response: PreprocessEventsResponse = encryptEvents.apply(request)
+
+    for (events in response.processedEventsList) {
+      c.output(KV.of(events.encryptedId, events.encryptedData))
+    }
+  }
+}
