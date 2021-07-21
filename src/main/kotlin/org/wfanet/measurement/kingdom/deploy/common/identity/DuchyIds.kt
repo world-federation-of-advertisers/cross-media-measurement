5648e46edc5b895b198760ac9ce8dac0806762b6
Ugur <uakyol@google.com>
2021-07-21 20:24:03 +0000

    addressed comments

--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/common/identity/DuchyIds.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/common/identity/DuchyIds.kt
@@ -29,15 +29,17 @@ object DuchyIds {
     entries = configMessage.duchiesList.map { it.toDuchyIdsEntry() }.toTypedArray()
   }
 
-  /** Returns the internalId for the specified external Duchy ID.
-   *  Matching by iteration is ok because number of duchies are in the order of ~10.
+  /**
+   * Returns the internalId for the specified external Duchy ID. Matching by iteration is ok because
+   * number of duchies are in the order of ~10.
    */
   fun getInternalId(externalDuchyId: String): Long? {
     return entries.firstOrNull { it.externalDuchyId == externalDuchyId }?.internalDuchyId
   }
 
-  /** Returns the externalId for the specified internal Duchy ID.
-   *  Matching by iteration is ok because number of duchies are in the order of ~10.
+  /**
+   * Returns the externalId for the specified internal Duchy ID. Matching by iteration is ok because
+   * number of duchies are in the order of ~10.
    */
   fun getExternalId(internalDuchyId: Long): String? {
     return entries.firstOrNull { it.internalDuchyId == internalDuchyId }?.externalDuchyId
