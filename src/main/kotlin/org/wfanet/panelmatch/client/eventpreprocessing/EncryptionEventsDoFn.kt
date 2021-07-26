820bdbc57598c95df9f519f819ebac3236eebf8e
Julia Morrissey <84739966+jmorrrr@users.noreply.github.com>
2021-07-26 12:43:07 -0400

    Adds Pepper and CryptoKey SerializableFunctions (world-federation-of-advertisers/panel-exchange-client#59)
    Adds calls to and hardcoded implementation and testing for the SerializableFunctions
    cryptokey and pepper

--- src/main/kotlin/org/wfanet/panelmatch/client/eventpreprocessing/EncryptionEventsDoFn.kt
+++ src/main/kotlin/org/wfanet/panelmatch/client/eventpreprocessing/EncryptionEventsDoFn.kt
@@ -27,7 +27,10 @@ import org.wfanet.panelmatch.client.PreprocessEventsResponse
  * from PreprocessedEventResponse protos and emits them as KV<Long,ByteString> pairs
  */
 class EncryptionEventsDoFn(
-  private val encryptEvents: SerializableFunction<PreprocessEventsRequest, PreprocessEventsResponse>
+  private val encryptEvents:
+    SerializableFunction<PreprocessEventsRequest, PreprocessEventsResponse>,
+  private val getPepper: SerializableFunction<Void?, ByteString>,
+  private val getCryptoKey: SerializableFunction<Void?, ByteString>,
 ) : DoFn<MutableList<KV<ByteString, ByteString>>, KV<Long, ByteString>>() {
   @ProcessElement
   fun process(c: ProcessContext) {
@@ -35,10 +38,12 @@ class EncryptionEventsDoFn(
     val request =
       PreprocessEventsRequest.newBuilder()
         .apply {
+          cryptoKey = getCryptoKey.apply(null as Void?)
+          pepper = getPepper.apply(null as Void?)
           for (pair in list) {
             addUnprocessedEventsBuilder().apply {
-              this.id = pair.key
-              this.data = pair.value
+              id = pair.key
+              data = pair.value
             }
           }
         }
