b1c5dfc1c7c25416d0ee65e460aa618a25413fbf
Ugur <uakyol@google.com>
2021-08-01 04:38:50 +0000

    intial commit

---
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/readers/MeasurementReader.kt
@@ -0,0 +1,60 @@
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers
+
+import com.google.cloud.spanner.Struct
+import org.wfanet.measurement.gcloud.spanner.getBytesAsByteString
+import org.wfanet.measurement.gcloud.spanner.getProtoEnum
+import org.wfanet.measurement.gcloud.spanner.getProtoMessage
+import org.wfanet.measurement.internal.kingdom.Certificate
+import org.wfanet.measurement.internal.kingdom.Measurement
+
+class MeasurementReader : SpannerReader<MeasurementReader.Result>() {
+  data class Result(val measurement: Measurement, val measurementId: Long)
+
+  override val baseSql: String =
+    """
+    SELECT
+      Measurements.MeasurementId,
+      Measurements.ExternalMeasurementId,
+      Measurements.MeasurementDetails,
+      Measurements.MeasurementDetailsJson,
+      MeasurementCertificates.ExternalMeasurementCertificateId,
+      Certificates.CertificateId,
+      Certificates.SubjectKeyIdentifier,
+      Certificates.NotValidBefore,
+      Certificates.NotValidAfter,
+      Certificates.RevocationState,
+      Certificates.CertificateDetails
+    FROM Measurements
+    JOIN MeasurementCertificates USING (MeasurementId)
+    JOIN Certificates USING (CertificateId)
+    """.trimIndent()
+
+  override val externalIdColumn: String = "Measurements.ExternalMeasurementId"
+
+  override suspend fun translate(struct: Struct): Result =
+    Result(buildMeasurement(struct), struct.getLong("MeasurementId"))
+
+  private fun buildMeasurement(struct: Struct): Measurement =
+    Measurement.newBuilder()
+      .apply {
+        externalMeasurementId = struct.getLong("ExternalMeasurementId")
+        externalPublicKeyCertificateId = struct.getLong("ExternalMeasurementCertificateId")
+        details = struct.getProtoMessage("MeasurementDetails", Measurement.Details.parser())
+        preferredCertificate = buildCertificate(struct)
+      }
+      .build()
+}
