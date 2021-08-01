3143042558377f4911df326fad2bbf17ddb93511
Ugur <uakyol@google.com>
2021-08-01 06:37:26 +0000

    building

--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/writers/CreateMeasurement.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/writers/CreateMeasurement.kt
@@ -14,64 +14,108 @@
 
 package org.wfanet.measurement.kingdom.deploy.gcloud.spanner.writers
 
-import com.google.cloud.spanner.ErrorCode
-import com.google.cloud.spanner.Mutation
-import com.google.cloud.spanner.SpannerException
+import com.google.cloud.spanner.Value
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.singleOrNull
 import org.wfanet.measurement.common.identity.ExternalId
-import org.wfanet.measurement.common.identity.InternalId
-import org.wfanet.measurement.gcloud.common.toGcloudByteArray
-import org.wfanet.measurement.gcloud.common.toGcloudTimestamp
-import org.wfanet.measurement.gcloud.spanner.AsyncDatabaseClient
+import org.wfanet.measurement.gcloud.spanner.appendClause
 import org.wfanet.measurement.gcloud.spanner.bufferTo
 import org.wfanet.measurement.gcloud.spanner.insertMutation
 import org.wfanet.measurement.gcloud.spanner.set
 import org.wfanet.measurement.gcloud.spanner.setJson
 import org.wfanet.measurement.internal.kingdom.Measurement
 import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.common.KingdomInternalException
-import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.DataProviderReader
 import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.MeasurementConsumerReader
+import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.MeasurementReader
 
 /**
  * Creates a measurement in the database.
  *
  * Throw KingdomInternalException with code CERT_SUBJECT_KEY_ID_ALREADY_EXISTS when executed if
- * subjectKeyIdentifier of [measurement] collides with a measurement already in the database.
  */
 class CreateMeasurement(private val measurement: Measurement) :
   SpannerWriter<Measurement, Measurement>() {
 
-
   override suspend fun TransactionScope.runTransaction(): Measurement {
-    val measurementId = idGenerator.generateInternalId()
-    val externalMapId = idGenerator.generateExternalId()
-    measurement.toInsertMutation(measurementId).bufferTo(transactionContext)
-    return measurement.toBuilder().setExternalMeasurementId(externalMapId.value).build()
+    val measurementConsumerId =
+      MeasurementConsumerReader()
+        .readExternalIdOrNull(
+          transactionContext,
+          ExternalId(measurement.externalMeasurementConsumerId)
+        )
+        ?.measurementConsumerId
+        ?: throw KingdomInternalException(
+          KingdomInternalException.Code.MEASUREMENT_CONSUMER_NOT_FOUND
+        )
+
+    val existingMeasurement = findExistingMeasurement(measurementConsumerId)
+    if (existingMeasurement != null) {
+      return existingMeasurement
+    }
+    // Insert into Requisitions for each EDP
+
+    // Insert into ComputationParticipants for each Duchy
+
+    // Insert this into Measurements
+    return createNewMeasurement(measurementConsumerId)
+  }
+
+  private suspend fun TransactionScope.createNewMeasurement(
+    measurementConsumerId: Long
+  ): Measurement {
+    val internalMeasurementId = idGenerator.generateInternalId()
+    val externalMeasurementId = idGenerator.generateExternalId()
+    val externalComputationId = idGenerator.generateExternalId()
+
+    insertMutation("Measurements") {
+        set("MeasurementId" to internalMeasurementId.value)
+        set("MeasurementConsumerId" to measurementConsumerId)
+        set("ExternalMeasurementId" to externalMeasurementId.value)
+        set("ExternalComputationId" to externalComputationId.value)
+        set("ProvidedMeasurementId" to measurement.providedMeasurementId)
+        set("State" to Measurement.State.PENDING_REQUISITION_PARAMS)
+        set("MeasurementDetails" to measurement.details)
+        setJson("MeasurementDetailsJson" to measurement.details)
+        set("CreateTime" to Value.COMMIT_TIMESTAMP)
+        set("UpdateTime" to Value.COMMIT_TIMESTAMP)
+      }
+      .bufferTo(transactionContext)
+
+    return measurement
+      .toBuilder()
+      .also {
+        it.externalMeasurementId = externalMeasurementId.value
+        it.externalComputationId = externalComputationId.value
+      }
+      .build()
+  }
+
+  private suspend fun TransactionScope.findExistingMeasurement(
+    measurementConsumerId: Long
+  ): Measurement? {
+    val whereClause =
+      """
+      WHERE Measurements.MeasurementConsumerId = @measurement_consumer_id
+        AND Measurements.ProvidedMeasurementId = @provided_measurement_id
+      """.trimIndent()
+
+    return MeasurementReader()
+      .withBuilder {
+        appendClause(whereClause)
+        bind("measurement_consumer_id").to(measurementConsumerId)
+        bind("provided_measurement_id").to(measurement.providedMeasurementId)
+      }
+      .execute(transactionContext)
+      .map { it.measurement }
+      .singleOrNull()
   }
 
   override fun ResultScope<Measurement>.buildResult(): Measurement {
-    return checkNotNull(transactionResult)
-  }
-
-
-  override suspend fun handleSpannerException(e: SpannerException): Measurement? {
-    when (e.errorCode) {
-      ErrorCode.ALREADY_EXISTS ->
-        throw KingdomInternalException(
-          KingdomInternalException.Code.CERT_SUBJECT_KEY_ID_ALREADY_EXISTS
-        )
-      else -> throw e
+    val measurement = checkNotNull(transactionResult)
+    return if (measurement.hasCreateTime()) {
+      measurement
+    } else {
+      measurement.toBuilder().apply { createTime = commitTimestamp.toProto() }.build()
     }
-  }
-}
-
-fun Measurement.toInsertMutation(internalId: InternalId): Mutation {
-  return insertMutation("Measurements") {
-    set("MeasurementId" to internalId.value)
-    set("SubjectKeyIdentifier" to subjectKeyIdentifier.toGcloudByteArray())
-    set("NotValidBefore" to notValidBefore.toGcloudTimestamp())
-    set("NotValidAfter" to notValidAfter.toGcloudTimestamp())
-    set("RevocationState" to revocationState)
-    set("MeasurementDetails" to details)
-    setJson("MeasurementDetailsJson" to details)
   }
 }
--- src/main/kotlin/org/wfanet/measurement/kingdom/service/internal/testing/MeasurementsServiceTest.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/service/internal/testing/MeasurementsServiceTest.kt
@@ -14,6 +14,8 @@
 
 package org.wfanet.measurement.kingdom.service.internal.testing
 
+import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.extensions.proto.ProtoTruth.assertThat
 import com.google.protobuf.ByteString
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
@@ -24,17 +26,28 @@ import org.wfanet.measurement.common.identity.ExternalId
 import org.wfanet.measurement.common.identity.IdGenerator
 import org.wfanet.measurement.common.identity.InternalId
 import org.wfanet.measurement.common.identity.testing.FixedIdGenerator
+import org.wfanet.measurement.internal.kingdom.Measurement
+import org.wfanet.measurement.internal.kingdom.MeasurementConsumer
+import org.wfanet.measurement.internal.kingdom.MeasurementConsumersGrpcKt.MeasurementConsumersCoroutineImplBase
 import org.wfanet.measurement.internal.kingdom.MeasurementsGrpcKt.MeasurementsCoroutineImplBase
 
 private const val EXTERNAL_MEASUREMENT_CONSUMER_ID = 123L
 private const val FIXED_GENERATED_INTERNAL_ID = 2345L
 private const val FIXED_GENERATED_EXTERNAL_ID = 6789L
+private const val PROVIDED_MEASUREMENT_ID = "ProvidedMeasurementId"
 private val PUBLIC_KEY = ByteString.copyFromUtf8("This is a  public key.")
 private val PUBLIC_KEY_SIGNATURE = ByteString.copyFromUtf8("This is a  public key signature.")
-private val PREFERRED_CERTIFICATE_DER = ByteString.copyFromUtf8("This is a certificate der.")
+private val PREFERRED_MC_CERTIFICATE_DER = ByteString.copyFromUtf8("This is a MC certificate der.")
+private val PREFERRED_MC_SUBJECT_KEY_IDENTIFIER =
+  ByteString.copyFromUtf8("This is a MC subject key identifier.")
 
 @RunWith(JUnit4::class)
 abstract class MeasurementsServiceTest<T : MeasurementsCoroutineImplBase> {
+
+  protected data class Services<T>(
+    val measurementsService: T,
+    val measurementConsumersService: MeasurementConsumersCoroutineImplBase
+  )
 
   protected val idGenerator =
     FixedIdGenerator(
@@ -45,26 +58,62 @@ abstract class MeasurementsServiceTest<T : MeasurementsCoroutineImplBase> {
   protected lateinit var measurementsService: T
     private set
 
-  protected abstract fun newService(idGenerator: IdGenerator): T
+  protected lateinit var measurementConsumersService: MeasurementConsumersCoroutineImplBase
+    private set
+
+  protected abstract fun newServices(idGenerator: IdGenerator): Services<T>
 
   @Before
   fun initService() {
-    measurementsService = newService(idGenerator)
+    val services = newServices(idGenerator)
+    measurementConsumersService = services.measurementConsumersService
+    measurementsService = services.measurementsService
   }
 
-  @Test
-  fun `getMeasurement fails for missing Measurement`() = runBlocking {
+  private suspend fun insertMeasurementConsumer(): Long {
+    return measurementConsumersService.createMeasurementConsumer(
+      MeasurementConsumer.newBuilder()
+        .apply {
+              preferredCertificateBuilder.apply {
+                notValidBeforeBuilder.seconds = 12345
+            notValidAfterBuilder.seconds = 23456
+            subjectKeyIdentifier = PREFERRED_MC_SUBJECT_KEY_IDENTIFIER
+            detailsBuilder.setX509Der(PREFERRED_MC_CERTIFICATE_DER)
+              }
+          detailsBuilder.apply {
+                apiVersion = "2"
+            publicKey = PUBLIC_KEY
+            publicKeySignature = PUBLIC_KEY_SIGNATURE
+              }
+            }
+        .build()
+    )
+      .externalMeasurementConsumerId
   }
 
-  @Test
-  fun `createMeasurement fails for missing fields`() = runBlocking {
-  }
+  @Test fun `getMeasurement fails for missing Measurement`() = runBlocking {}
+
+  @Test fun `createMeasurement fails for missing fields`() = runBlocking {}
+
+  @Test fun `createMeasurement succeeds`() = runBlocking {}
 
   @Test
-  fun `createMeasurement succeeds`() = runBlocking {
+  fun `createMeasurement returns already created measurement for the same ProvidedMeasurementId`() =
+      runBlocking {
+    val externalMeasurementConsumerId = insertMeasurementConsumer()
+    val measurement =
+      Measurement.newBuilder()
+        .also {
+          it.detailsBuilder.apiVersion = "v2alpha"
+          it.externalMeasurementConsumerId = externalMeasurementConsumerId
+          it.providedMeasurementId = PROVIDED_MEASUREMENT_ID
+        }
+        .build()
+
+    val createdMeasurement = measurementsService.createMeasurement(measurement)
+    val secondCreateMeasurementAttempt = measurementsService.createMeasurement(measurement)
+    assertThat(secondCreateMeasurementAttempt).isEqualTo(createdMeasurement)
   }
 
-  @Test
-  fun `getMeasurement succeeds`() = runBlocking {
-  }
+  @Test fun `getMeasurement succeeds`() = runBlocking {}
 }
