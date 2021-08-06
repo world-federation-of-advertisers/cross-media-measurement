ce34dc7cd99b27945c550a63caa19625e602283b
yunyeng <yunyeng@google.com>
2021-08-06 17:17:28 +0000

    fix package name

--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/SpannerExchangeStepsService.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/SpannerExchangeStepsService.kt
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.wfanet.measurement.kingdom.service.internal
+package org.wfanet.measurement.kingdom.deploy.gcloud.spanner
 
 import java.time.Clock
 import org.wfanet.measurement.common.grpc.grpcRequire
