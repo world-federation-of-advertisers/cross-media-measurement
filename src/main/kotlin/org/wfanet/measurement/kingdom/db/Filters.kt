17883f57b7afc4bdcaa96c05e30d3836717a9f4c
Yao Wang <wangyaopw@google.com>
2021-08-06 20:48:54 -0400

    Delete legacy kingdom codes.
    The majoritoy of these codes should be deleted.
    Some of them can be updated and reused. However, updating them takes
    long time. It may be easier to delete them and rewrite when needed.

--- src/main/kotlin/org/wfanet/measurement/kingdom/db/Filters.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/db/Filters.kt
@@ -18,92 +18,16 @@ import com.google.type.Date
 import java.time.Instant
 import org.wfanet.measurement.common.AllOfClause
 import org.wfanet.measurement.common.AnyOfClause
-import org.wfanet.measurement.common.GreaterThanClause
 import org.wfanet.measurement.common.LessThanClause
 import org.wfanet.measurement.common.TerminalClause
 import org.wfanet.measurement.common.allOf
 import org.wfanet.measurement.common.identity.ExternalId
 import org.wfanet.measurement.internal.kingdom.ExchangeStep
 import org.wfanet.measurement.internal.kingdom.RecurringExchange
-import org.wfanet.measurement.internal.kingdom.Report.ReportState
-import org.wfanet.measurement.internal.kingdom.Requisition.RequisitionState
-
-typealias StreamRequisitionsFilter = AllOfClause<StreamRequisitionsClause>
-
-typealias StreamReportsFilter = AllOfClause<StreamReportsClause>
 
 typealias StreamRecurringExchangesFilter = AllOfClause<StreamRecurringExchangesClause>
 
 typealias GetExchangeStepFilter = AllOfClause<GetExchangeStepClause>
-
-/**
- * Creates a filter for Requisitions.
- *
- * The list inputs are treated as disjunctions, and all non-null inputs are conjoined.
- *
- * For example,
- *
- * streamRequisitionsFilter(externalDataProviderIds = listOf(ID1, ID2), createdAfter = SOME_TIME)
- *
- * would match each Requisition that matches both these criteria:
- * - it is associated with either ID1 or ID2, and
- * - it was created after SOME_TIME.
- *
- * @param externalDataProviderIds a list of Data Providers
- * @param externalCampaignIds a list of Campaigns
- * @param states a list of [RequisitionState]s
- * @param createdAfter a time after which Requisitions must be created
- */
-fun streamRequisitionsFilter(
-  externalDataProviderIds: List<ExternalId>? = null,
-  externalCampaignIds: List<ExternalId>? = null,
-  states: List<RequisitionState>? = null,
-  createdAfter: Instant? = null
-): StreamRequisitionsFilter =
-  allOf(
-    listOfNotNull(
-      externalDataProviderIds.ifNotNullOrEmpty(StreamRequisitionsClause::ExternalDataProviderId),
-      externalCampaignIds.ifNotNullOrEmpty(StreamRequisitionsClause::ExternalCampaignId),
-      states.ifNotNullOrEmpty(StreamRequisitionsClause::State),
-      createdAfter?.let(StreamRequisitionsClause::CreatedAfter)
-    )
-  )
-
-/**
- * Creates a filter for Reports.
- *
- * The list inputs are treated as disjunctions, and all non-null inputs are conjoined.
- *
- * For example,
- *
- * streamReportsFilter(externalScheduleIds = listOf(ID1, ID2), createdAfter = SOME_TIME)
- *
- * would match each Report that matches both asdasthese criteria:
- * - it is associated with a schedule with external id either ID1 or ID2, and
- * - it was created after SOME_TIME.
- *
- * @param externalAdvertiserIds a list of Advertisers
- * @param externalReportConfigIds a list of Report Configs
- * @param externalScheduleIds a list of ReportConfigSchedules
- * @param states a list of [ReportState]s
- * @param updatedAfter a time after which results must be created
- */
-fun streamReportsFilter(
-  externalAdvertiserIds: List<ExternalId>? = null,
-  externalReportConfigIds: List<ExternalId>? = null,
-  externalScheduleIds: List<ExternalId>? = null,
-  states: List<ReportState>? = null,
-  updatedAfter: Instant? = null
-): StreamReportsFilter =
-  allOf(
-    listOfNotNull(
-      externalAdvertiserIds.ifNotNullOrEmpty(StreamReportsClause::ExternalAdvertiserId),
-      externalReportConfigIds.ifNotNullOrEmpty(StreamReportsClause::ExternalReportConfigId),
-      externalScheduleIds.ifNotNullOrEmpty(StreamReportsClause::ExternalScheduleId),
-      states.ifNotNullOrEmpty(StreamReportsClause::State),
-      updatedAfter.ifNotNullOrEpoch(StreamReportsClause::UpdatedAfter)
-    )
-  )
 
 /**
  * Creates a filter for RecurringExchanges.
@@ -182,55 +106,6 @@ fun getExchangeStepFilter(
       states.ifNotNullOrEmpty(GetExchangeStepClause::State)
     )
   )
-
-/** Base class for Requisition filters. Never directly instantiated. */
-sealed class StreamRequisitionsClause : TerminalClause {
-
-  /** Matching Requisitions must belong to a Data Provider with an external id in [values]. */
-  data class ExternalDataProviderId internal constructor(val values: List<ExternalId>) :
-    StreamRequisitionsClause(), AnyOfClause
-
-  /** Matching Requisitions must belong to a Campaign with an external id in [values]. */
-  data class ExternalCampaignId internal constructor(val values: List<ExternalId>) :
-    StreamRequisitionsClause(), AnyOfClause
-
-  /** Matching Requisitions must have a state among those in [values]. */
-  data class State internal constructor(val values: List<RequisitionState>) :
-    StreamRequisitionsClause(), AnyOfClause
-
-  /** Matching Requisitions must have been created after [value]. */
-  data class CreatedAfter internal constructor(val value: Instant) :
-    StreamRequisitionsClause(), GreaterThanClause
-}
-
-/** Base class for filtering Report streams. Never directly instantiated. */
-sealed class StreamReportsClause : TerminalClause {
-
-  /** Matching Reports must belong to an Advertiser with an external id in [values]. */
-  data class ExternalAdvertiserId internal constructor(val values: List<ExternalId>) :
-    StreamReportsClause(), AnyOfClause
-
-  /** Matching Reports must belong to a ReportConfig with an external id in [values]. */
-  data class ExternalReportConfigId internal constructor(val values: List<ExternalId>) :
-    StreamReportsClause(), AnyOfClause
-
-  /** Matching Reports must belong to ReportConfigSchedule with an external id in [values]. */
-  data class ExternalScheduleId internal constructor(val values: List<ExternalId>) :
-    StreamReportsClause(), AnyOfClause
-
-  /** Matching Reports must have a state among those in [values]. */
-  data class State internal constructor(val values: List<ReportState>) :
-    StreamReportsClause(), AnyOfClause
-
-  /** Matching Reports must have been updated after [value]. */
-  data class UpdatedAfter internal constructor(val value: Instant) :
-    StreamReportsClause(), GreaterThanClause
-}
-
-/** Returns whether the filter acts on a Report's state. This is useful for forcing indexes. */
-fun StreamReportsFilter.hasStateFilter(): Boolean {
-  return clauses.any { it is StreamReportsClause.State }
-}
 
 /** Base class for filtering Exchange streams. Never directly instantiated. */
 sealed class StreamRecurringExchangesClause : TerminalClause {
--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/testing/KingdomDatabaseTestBase.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/testing/KingdomDatabaseTestBase.kt
@@ -18,214 +18,24 @@ import com.google.cloud.ByteArray
 import com.google.cloud.spanner.Mutation
 import com.google.cloud.spanner.Value
 import com.google.type.Date
-import java.time.Instant
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import org.wfanet.measurement.gcloud.common.toCloudDate
-import org.wfanet.measurement.gcloud.common.toGcloudTimestamp
 import org.wfanet.measurement.gcloud.spanner.insertMutation
 import org.wfanet.measurement.gcloud.spanner.set
 import org.wfanet.measurement.gcloud.spanner.testing.UsingSpannerEmulator
-import org.wfanet.measurement.gcloud.spanner.toProtoBytes
-import org.wfanet.measurement.gcloud.spanner.toProtoEnum
-import org.wfanet.measurement.gcloud.spanner.toProtoJson
 import org.wfanet.measurement.internal.kingdom.Exchange
 import org.wfanet.measurement.internal.kingdom.ExchangeDetails
 import org.wfanet.measurement.internal.kingdom.ExchangeStep
 import org.wfanet.measurement.internal.kingdom.RecurringExchange
 import org.wfanet.measurement.internal.kingdom.RecurringExchangeDetails
-import org.wfanet.measurement.internal.kingdom.RepetitionSpec
-import org.wfanet.measurement.internal.kingdom.Report
-import org.wfanet.measurement.internal.kingdom.Report.ReportState
-import org.wfanet.measurement.internal.kingdom.ReportConfig.ReportConfigState
-import org.wfanet.measurement.internal.kingdom.ReportConfigDetails
-import org.wfanet.measurement.internal.kingdom.ReportConfigSchedule
-import org.wfanet.measurement.internal.kingdom.ReportDetails
-import org.wfanet.measurement.internal.kingdom.Requisition
-import org.wfanet.measurement.internal.kingdom.Requisition.RequisitionState
-import org.wfanet.measurement.internal.kingdom.RequisitionDetails
 import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.ExchangeStepReader
 import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.RecurringExchangeReader
-import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.ReportReader
-import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.RequisitionReader
-import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.ScheduleReader
 
-private const val COMBINED_PUBLIC_KEY_RESOURCE_ID = "combined-public-key-1"
-private val REPORT_DETAILS =
-  ReportDetails.newBuilder()
-    .apply { combinedPublicKeyResourceId = COMBINED_PUBLIC_KEY_RESOURCE_ID }
-    .build()
-
-abstract class KingdomDatabaseTestBase : UsingSpannerEmulator(KINGDOM_LEGACY_SCHEMA) {
+// TODO(@yunyeng): Delete once not used.
+abstract class KingdomDatabaseTestBase : UsingSpannerEmulator(KINGDOM_SCHEMA) {
   private suspend fun write(mutation: Mutation) = databaseClient.write(mutation)
-
-  // TODO: add AdvertiserDetails proto as input.
-  protected suspend fun insertAdvertiser(advertiserId: Long, externalAdvertiserId: Long) {
-    write(
-      Mutation.newInsertBuilder("Advertisers")
-        .set("AdvertiserId")
-        .to(advertiserId)
-        .set("ExternalAdvertiserId")
-        .to(externalAdvertiserId)
-        .set("AdvertiserDetails")
-        .to(ByteArray.copyFrom(""))
-        .set("AdvertiserDetailsJson")
-        .to("irrelevant-advertiser-details-json")
-        .build()
-    )
-  }
-
-  protected suspend fun insertReportConfig(
-    advertiserId: Long,
-    reportConfigId: Long,
-    externalReportConfigId: Long,
-    state: ReportConfigState = ReportConfigState.ACTIVE,
-    reportConfigDetails: ReportConfigDetails = ReportConfigDetails.getDefaultInstance(),
-    numRequisitions: Long = 0
-  ) {
-    write(
-      Mutation.newInsertBuilder("ReportConfigs")
-        .set("AdvertiserId")
-        .to(advertiserId)
-        .set("ReportConfigId")
-        .to(reportConfigId)
-        .set("ExternalReportConfigId")
-        .to(externalReportConfigId)
-        .set("NumRequisitions")
-        .to(numRequisitions)
-        .set("State")
-        .toProtoEnum(state)
-        .set("ReportConfigDetails")
-        .toProtoBytes(reportConfigDetails)
-        .set("ReportConfigDetailsJson")
-        .toProtoJson(reportConfigDetails)
-        .build()
-    )
-  }
-
-  protected suspend fun insertReportConfigSchedule(
-    advertiserId: Long,
-    reportConfigId: Long,
-    scheduleId: Long,
-    externalScheduleId: Long,
-    nextReportStartTime: Instant = Instant.EPOCH,
-    repetitionSpec: RepetitionSpec = RepetitionSpec.getDefaultInstance()
-  ) {
-    write(
-      Mutation.newInsertBuilder("ReportConfigSchedules")
-        .set("AdvertiserId")
-        .to(advertiserId)
-        .set("ReportConfigId")
-        .to(reportConfigId)
-        .set("ScheduleId")
-        .to(scheduleId)
-        .set("ExternalScheduleId")
-        .to(externalScheduleId)
-        .set("NextReportStartTime")
-        .to(nextReportStartTime.toGcloudTimestamp())
-        .set("RepetitionSpec")
-        .toProtoBytes(repetitionSpec)
-        .set("RepetitionSpecJson")
-        .toProtoJson(repetitionSpec)
-        .build()
-    )
-  }
-
-  protected suspend fun insertReportConfigCampaign(
-    advertiserId: Long,
-    reportConfigId: Long,
-    dataProviderId: Long,
-    campaignId: Long
-  ) {
-    write(
-      Mutation.newInsertBuilder("ReportConfigCampaigns")
-        .set("AdvertiserId")
-        .to(advertiserId)
-        .set("ReportConfigId")
-        .to(reportConfigId)
-        .set("DataProviderId")
-        .to(dataProviderId)
-        .set("CampaignId")
-        .to(campaignId)
-        .build()
-    )
-  }
-
-  protected suspend fun insertReport(
-    advertiserId: Long,
-    reportConfigId: Long,
-    scheduleId: Long,
-    reportId: Long,
-    externalReportId: Long,
-    state: ReportState,
-    createTime: Instant? = null,
-    updateTime: Instant? = null,
-    windowStartTime: Instant = Instant.EPOCH,
-    windowEndTime: Instant = Instant.EPOCH,
-    reportDetails: ReportDetails = REPORT_DETAILS
-  ) {
-    write(
-      Mutation.newInsertBuilder("Reports")
-        .set("AdvertiserId")
-        .to(advertiserId)
-        .set("ReportConfigId")
-        .to(reportConfigId)
-        .set("ScheduleId")
-        .to(scheduleId)
-        .set("ReportId")
-        .to(reportId)
-        .set("ExternalReportId")
-        .to(externalReportId)
-        .set("CreateTime")
-        .to(createTime?.toGcloudTimestamp() ?: Value.COMMIT_TIMESTAMP)
-        .set("UpdateTime")
-        .to(updateTime?.toGcloudTimestamp() ?: Value.COMMIT_TIMESTAMP)
-        .set("WindowStartTime")
-        .to(windowStartTime.toGcloudTimestamp())
-        .set("WindowEndTime")
-        .to(windowEndTime.toGcloudTimestamp())
-        .set("State")
-        .toProtoEnum(state)
-        .set("ReportDetails")
-        .toProtoBytes(reportDetails)
-        .set("ReportDetailsJson")
-        .toProtoJson(reportDetails)
-        .build()
-    )
-  }
-
-  suspend fun insertReportWithParents(
-    advertiserId: Long,
-    reportConfigId: Long,
-    externalReportConfigId: Long,
-    scheduleId: Long,
-    externalScheduleId: Long,
-    reportId: Long,
-    externalReportId: Long,
-    state: ReportState,
-    createTime: Instant? = null,
-    updateTime: Instant? = null,
-    windowStartTime: Instant = Instant.EPOCH,
-    windowEndTime: Instant = Instant.EPOCH,
-    reportDetails: ReportDetails = ReportDetails.getDefaultInstance()
-  ) {
-    insertReportConfig(advertiserId, reportConfigId, externalReportConfigId)
-    insertReportConfigSchedule(advertiserId, reportConfigId, scheduleId, externalScheduleId)
-    insertReport(
-      advertiserId,
-      reportConfigId,
-      scheduleId,
-      reportId,
-      externalReportId,
-      state,
-      createTime,
-      updateTime,
-      windowStartTime,
-      windowEndTime,
-      reportDetails
-    )
-  }
 
   protected suspend fun insertDataProvider(dataProviderId: Long, externalDataProviderId: Long) {
     write(
@@ -238,105 +48,6 @@ abstract class KingdomDatabaseTestBase : UsingSpannerEmulator(KINGDOM_LEGACY_SCH
         .to(ByteArray.copyFrom(""))
         .set("DataProviderDetailsJson")
         .to("")
-        .build()
-    )
-  }
-
-  protected suspend fun insertCampaign(
-    dataProviderId: Long,
-    campaignId: Long,
-    externalCampaignId: Long,
-    advertiserId: Long,
-    providedCampaignId: String = ""
-  ) {
-    write(
-      Mutation.newInsertBuilder("Campaigns")
-        .set("DataProviderId")
-        .to(dataProviderId)
-        .set("CampaignId")
-        .to(campaignId)
-        .set("ExternalCampaignId")
-        .to(externalCampaignId)
-        .set("AdvertiserId")
-        .to(advertiserId)
-        .set("ProvidedCampaignId")
-        .to(providedCampaignId)
-        .set("CampaignDetails")
-        .to(ByteArray.copyFrom(""))
-        .set("CampaignDetailsJson")
-        .to("")
-        .build()
-    )
-  }
-
-  protected suspend fun insertRequisition(
-    dataProviderId: Long,
-    campaignId: Long,
-    requisitionId: Long,
-    externalRequisitionId: Long,
-    combinedPublicKeyResourceId: String = COMBINED_PUBLIC_KEY_RESOURCE_ID,
-    createTime: Instant = Instant.EPOCH,
-    windowStartTime: Instant = Instant.EPOCH,
-    windowEndTime: Instant = Instant.EPOCH,
-    state: RequisitionState = RequisitionState.UNFULFILLED,
-    duchyId: String? = null,
-    requisitionDetails: RequisitionDetails = RequisitionDetails.getDefaultInstance()
-  ) {
-    write(
-      Mutation.newInsertBuilder("Requisitions")
-        .set("DataProviderId")
-        .to(dataProviderId)
-        .set("CampaignId")
-        .to(campaignId)
-        .set("RequisitionId")
-        .to(requisitionId)
-        .set("ExternalRequisitionId")
-        .to(externalRequisitionId)
-        .set("CombinedPublicKeyResourceId")
-        .to(combinedPublicKeyResourceId)
-        .set("CreateTime")
-        .to(createTime.toGcloudTimestamp())
-        .set("WindowStartTime")
-        .to(windowStartTime.toGcloudTimestamp())
-        .set("WindowEndTime")
-        .to(windowEndTime.toGcloudTimestamp())
-        .set("State")
-        .toProtoEnum(state)
-        .set("DuchyId")
-        .to(duchyId)
-        .set("RequisitionDetails")
-        .toProtoBytes(requisitionDetails)
-        .set("RequisitionDetailsJson")
-        .toProtoJson(requisitionDetails)
-        .build()
-    )
-  }
-
-  protected suspend fun insertReportRequisition(
-    advertiserId: Long,
-    reportConfigId: Long,
-    scheduleId: Long,
-    reportId: Long,
-    dataProviderId: Long,
-    campaignId: Long,
-    requisitionId: Long
-  ) {
-    write(
-      Mutation.newInsertBuilder("ReportRequisitions")
-        .set("AdvertiserId")
-        .to(advertiserId)
-        .set("ReportConfigId")
-        .to(reportConfigId)
-        .set("ScheduleId")
-        .to(scheduleId)
-        .set("ReportId")
-        .to(reportId)
-        .set("DataProviderId")
-        .to(dataProviderId)
-        .set("CampaignId")
-        .to(campaignId)
-        .set("RequisitionId")
-        .to(requisitionId)
         .build()
     )
   }
@@ -410,18 +121,6 @@ abstract class KingdomDatabaseTestBase : UsingSpannerEmulator(KINGDOM_LEGACY_SCH
         set("DataProviderId" to dataProviderId)
       }
     )
-  }
-
-  protected fun readAllReportsInSpanner(): List<Report> = runBlocking {
-    ReportReader().execute(databaseClient.singleUse()).map { it.report }.toList()
-  }
-
-  protected fun readAllSchedulesInSpanner(): List<ReportConfigSchedule> = runBlocking {
-    ScheduleReader().execute(databaseClient.singleUse()).map { it.schedule }.toList()
-  }
-
-  protected fun readAllRequisitionsInSpanner(): List<Requisition> = runBlocking {
-    RequisitionReader().execute(databaseClient.singleUse()).map { it.requisition }.toList()
   }
 
   protected fun readAllRecurringExchangesInSpanner(): List<RecurringExchange> = runBlocking {
