9a32117ebb502867da9ee4b260c6122596247258
enfreck <57185478+enfreck@users.noreply.github.com>
2021-08-10 14:00:30 -0400

    Implement key_generator for client keys (world-federation-of-advertisers/panel-exchange-client#76)
    Implement generating pepper and cryptokeys for client.

---
+++ src/main/cc/wfa/panelmatch/common/crypto/random_bytes_key_generator.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SRC_MAIN_CC_WFA_PANELMATCH_COMMON_CRYPTO_RANDOM_BYTES_KEY_GENERATOR_H_
+#define SRC_MAIN_CC_WFA_PANELMATCH_COMMON_CRYPTO_RANDOM_BYTES_KEY_GENERATOR_H_
+
+#include "absl/status/statusor.h"
+#include "tink/util/secret_data.h"
+
+namespace wfa::panelmatch::common::crypto {
+
+// Random Bytes generator for generating cryptographically secure keys
+class RandomBytesKeyGenerator {
+ public:
+  RandomBytesKeyGenerator() = default;
+  ~RandomBytesKeyGenerator() = default;
+
+  // Generates random bytes
+  absl::StatusOr<::crypto::tink::util::SecretData> GenerateKey(int size) const;
+};
+
+}  // namespace wfa::panelmatch::common::crypto
+
+#endif  // SRC_MAIN_CC_WFA_PANELMATCH_COMMON_CRYPTO_RANDOM_BYTES_KEY_GENERATOR_H_
