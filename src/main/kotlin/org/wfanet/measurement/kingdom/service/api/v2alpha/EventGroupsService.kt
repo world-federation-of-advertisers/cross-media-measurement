56c8909e634409ab5d2bcfdec6c446d5d76b4ed2
Tristan Vuong <tristanvuong@google.com>
2021-08-16 19:56:51 +0000

    Implement v2alpha public event groups service

---
+++ src/main/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/EventGroupsService.kt
@@ -0,0 +1,172 @@
+// Copyright 2020 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.kingdom.service.api.v2alpha
+
+import com.google.protobuf.Timestamp
+import kotlinx.coroutines.flow.toList
+import org.wfanet.measurement.api.v2alpha.CreateEventGroupRequest
+import org.wfanet.measurement.api.v2alpha.DataProviderKey
+import org.wfanet.measurement.api.v2alpha.EventGroup
+import org.wfanet.measurement.api.v2alpha.EventGroupKey
+import org.wfanet.measurement.api.v2alpha.EventGroupsGrpcKt.EventGroupsCoroutineImplBase
+import org.wfanet.measurement.api.v2alpha.GetEventGroupRequest
+import org.wfanet.measurement.api.v2alpha.ListEventGroupsRequest
+import org.wfanet.measurement.api.v2alpha.ListEventGroupsResponse
+import org.wfanet.measurement.api.v2alpha.MeasurementConsumerKey
+import org.wfanet.measurement.common.base64UrlDecode
+import org.wfanet.measurement.common.base64UrlEncode
+import org.wfanet.measurement.common.grpc.grpcRequire
+import org.wfanet.measurement.common.grpc.grpcRequireNotNull
+import org.wfanet.measurement.common.identity.apiIdToExternalId
+import org.wfanet.measurement.common.identity.externalIdToApiId
+import org.wfanet.measurement.internal.kingdom.EventGroup as InternalEventGroup
+import org.wfanet.measurement.internal.kingdom.EventGroupsGrpcKt.EventGroupsCoroutineStub
+import org.wfanet.measurement.internal.kingdom.GetEventGroupRequest as InternalGetEventGroupRequest
+import org.wfanet.measurement.internal.kingdom.StreamEventGroupsRequest
+
+private const val MIN_PAGE_SIZE = 1
+private const val DEFAULT_PAGE_SIZE = 50
+private const val MAX_PAGE_SIZE = 100
+private const val WILDCARD = "-"
+
+class EventGroupsService(private val internalEventGroupsStub: EventGroupsCoroutineStub) :
+  EventGroupsCoroutineImplBase() {
+
+  override suspend fun getEventGroup(request: GetEventGroupRequest): EventGroup {
+    val key =
+      grpcRequireNotNull(EventGroupKey.fromName(request.name)) {
+        "Resource name is either unspecified or invalid"
+      }
+
+    val getRequest = buildInternalGetEventGroupRequest {
+      externalDataProviderId = apiIdToExternalId(key.dataProviderId)
+      externalEventGroupId = apiIdToExternalId(key.eventGroupId)
+    }
+
+    return internalEventGroupsStub.getEventGroup(getRequest).toEventGroup()
+  }
+
+  override suspend fun createEventGroup(request: CreateEventGroupRequest): EventGroup {
+    val parentKey =
+      grpcRequireNotNull(DataProviderKey.fromName(request.parent)) {
+        "Parent is either unspecified or invalid"
+      }
+
+    val measurementConsumerKey =
+      grpcRequireNotNull(MeasurementConsumerKey.fromName(request.eventGroup.measurementConsumer)) {
+        "Measurement consumer is either unspecified or invalid"
+      }
+
+    return internalEventGroupsStub
+      .createEventGroup(request.eventGroup.toInternal(parentKey, measurementConsumerKey))
+      .toEventGroup()
+  }
+
+  override suspend fun listEventGroups(request: ListEventGroupsRequest): ListEventGroupsResponse {
+    grpcRequire(request.pageSize >= 0) { "Page size cannot be less than 0" }
+
+    val parentKey: DataProviderKey =
+      grpcRequireNotNull(DataProviderKey.fromName(request.parent)) {
+        "Parent is either unspecified or invalid"
+      }
+    grpcRequire(
+      (request.filter.measurementConsumersCount > 0 && parentKey.dataProviderId == WILDCARD) ||
+        parentKey.dataProviderId != WILDCARD
+    ) { "Either parent data provider or measurement consumers filter must be provided" }
+
+    val pageSize =
+      when {
+        request.pageSize < MIN_PAGE_SIZE -> DEFAULT_PAGE_SIZE
+        request.pageSize > MAX_PAGE_SIZE -> MAX_PAGE_SIZE
+        else -> request.pageSize
+      }
+
+    val streamRequest = buildStreamEventGroupsRequest {
+      limit = pageSize
+      filterBuilder.apply {
+        if (request.pageToken.isNotBlank()) {
+          createdAfter = Timestamp.parseFrom(request.pageToken.base64UrlDecode())
+        }
+
+        if (parentKey.dataProviderId != WILDCARD) {
+          externalDataProviderId = apiIdToExternalId(parentKey.dataProviderId)
+        }
+
+        addAllExternalMeasurementConsumerIds(
+          request.filter.measurementConsumersList.map { measurementConsumerName ->
+            grpcRequireNotNull(MeasurementConsumerKey.fromName(measurementConsumerName)) {
+              "Measurement consumer name in filter invalid"
+            }
+              .let { key -> apiIdToExternalId(key.measurementConsumerId) }
+          }
+        )
+      }
+    }
+
+    val results: List<InternalEventGroup> =
+      internalEventGroupsStub.streamEventGroups(streamRequest).toList()
+
+    if (results.isEmpty()) {
+      return ListEventGroupsResponse.getDefaultInstance()
+    }
+
+    return ListEventGroupsResponse.newBuilder()
+      .addAllEventGroups(results.map(InternalEventGroup::toEventGroup))
+      .setNextPageToken(results.last().createTime.toByteArray().base64UrlEncode())
+      .build()
+  }
+}
+
+/** Converts an internal [InternalEventGroup] to a public [EventGroup]. */
+private fun InternalEventGroup.toEventGroup(): EventGroup {
+  return buildEventGroup {
+    name =
+      EventGroupKey(
+          externalIdToApiId(externalDataProviderId),
+          externalIdToApiId(externalEventGroupId)
+        )
+        .toName()
+    measurementConsumer =
+      MeasurementConsumerKey(externalIdToApiId(externalMeasurementConsumerId)).toName()
+    eventGroupReferenceId = providedEventGroupId
+  }
+}
+
+/** Converts a public [EventGroup] to an internal [InternalEventGroup]. */
+private fun EventGroup.toInternal(
+  parentKey: DataProviderKey,
+  measurementConsumerKey: MeasurementConsumerKey
+): InternalEventGroup {
+  return buildInternalEventGroup {
+    externalDataProviderId = apiIdToExternalId(parentKey.dataProviderId)
+    externalMeasurementConsumerId = apiIdToExternalId(measurementConsumerKey.measurementConsumerId)
+    providedEventGroupId = eventGroupReferenceId
+  }
+}
+
+internal inline fun buildEventGroup(fill: (@Builder EventGroup.Builder).() -> Unit) =
+  EventGroup.newBuilder().apply(fill).build()
+
+internal inline fun buildInternalEventGroup(
+  fill: (@Builder InternalEventGroup.Builder).() -> Unit
+) = InternalEventGroup.newBuilder().apply(fill).build()
+
+internal inline fun buildInternalGetEventGroupRequest(
+  fill: (@Builder InternalGetEventGroupRequest.Builder).() -> Unit
+) = InternalGetEventGroupRequest.newBuilder().apply(fill).build()
+
+internal inline fun buildStreamEventGroupsRequest(
+  fill: (@Builder StreamEventGroupsRequest.Builder).() -> Unit
+) = StreamEventGroupsRequest.newBuilder().apply(fill).build()
---
+++ src/test/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/EventGroupsServiceTest.kt
@@ -0,0 +1,381 @@
+// Copyright 2020 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.kingdom.service.api.v2alpha
+
+import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.extensions.proto.ProtoTruth.assertThat
+import com.google.protobuf.Timestamp
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import java.time.Instant
+import kotlin.test.assertFailsWith
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.kotlin.UseConstructor
+import org.mockito.kotlin.any
+import org.mockito.kotlin.capture
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.wfanet.measurement.api.v2alpha.CreateEventGroupRequest
+import org.wfanet.measurement.api.v2alpha.DataProviderKey
+import org.wfanet.measurement.api.v2alpha.EventGroup
+import org.wfanet.measurement.api.v2alpha.EventGroupKey
+import org.wfanet.measurement.api.v2alpha.GetEventGroupRequest
+import org.wfanet.measurement.api.v2alpha.ListEventGroupsRequest
+import org.wfanet.measurement.api.v2alpha.ListEventGroupsResponse
+import org.wfanet.measurement.api.v2alpha.MeasurementConsumerKey
+import org.wfanet.measurement.common.base64UrlEncode
+import org.wfanet.measurement.common.grpc.testing.GrpcTestServerRule
+import org.wfanet.measurement.common.identity.apiIdToExternalId
+import org.wfanet.measurement.common.testing.captureFirst
+import org.wfanet.measurement.common.testing.verifyProtoArgument
+import org.wfanet.measurement.common.toProtoTime
+import org.wfanet.measurement.internal.kingdom.EventGroup as InternalEventGroup
+import org.wfanet.measurement.internal.kingdom.EventGroupsGrpcKt.EventGroupsCoroutineImplBase
+import org.wfanet.measurement.internal.kingdom.EventGroupsGrpcKt.EventGroupsCoroutineStub
+import org.wfanet.measurement.internal.kingdom.StreamEventGroupsRequest
+
+private val CREATE_TIME: Timestamp = Instant.ofEpochSecond(123).toProtoTime()
+
+private const val DEFAULT_LIMIT = 50
+
+private const val WILDCARD_NAME = "dataProviders/-"
+
+private const val EVENT_GROUP_NAME = "dataProviders/AAAAAAAAAHs/eventGroups/AAAAAAAAAHs"
+private const val MEASUREMENT_CONSUMER_NAME = "measurementConsumers/AAAAAAAAAHs"
+private const val DATA_PROVIDER_NAME = "dataProviders/AAAAAAAAAHs"
+
+@RunWith(JUnit4::class)
+class EventGroupsServiceTest {
+  private val internalEventGroupsMock: EventGroupsCoroutineImplBase =
+    mock(useConstructor = UseConstructor.parameterless()) {
+      onBlocking { getEventGroup(any()) }.thenReturn(INTERNAL_EVENT_GROUP)
+      onBlocking { createEventGroup(any()) }.thenReturn(INTERNAL_EVENT_GROUP)
+      onBlocking { streamEventGroups(any()) }
+        .thenReturn(flowOf(INTERNAL_EVENT_GROUP, INTERNAL_EVENT_GROUP))
+    }
+
+  @get:Rule val grpcTestServerRule = GrpcTestServerRule { addService(internalEventGroupsMock) }
+
+  private lateinit var service: EventGroupsService
+
+  @Before
+  fun initService() {
+    service = EventGroupsService(EventGroupsCoroutineStub(grpcTestServerRule.channel))
+  }
+
+  @Test
+  fun `getEventGroup returns event group`() {
+    val request = buildGetEventGroupRequest { name = EVENT_GROUP_NAME }
+
+    val result = runBlocking { service.getEventGroup(request) }
+
+    val expected = EVENT_GROUP
+
+    verifyProtoArgument(internalEventGroupsMock, EventGroupsCoroutineImplBase::getEventGroup)
+      .isEqualTo(
+        buildInternalGetEventGroupRequest {
+          val key = EventGroupKey.fromName(EVENT_GROUP.name)
+          externalDataProviderId = apiIdToExternalId(key!!.dataProviderId)
+          externalEventGroupId = apiIdToExternalId(key.eventGroupId)
+        }
+      )
+
+    assertThat(result).isEqualTo(expected)
+  }
+
+  @Test
+  fun `getEventGroup throws INVALID_ARGUMENT when name is missing`() {
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        runBlocking { service.getEventGroup(GetEventGroupRequest.getDefaultInstance()) }
+      }
+    assertThat(exception.status.code).isEqualTo(Status.Code.INVALID_ARGUMENT)
+    assertThat(exception.status.description)
+      .isEqualTo("Resource name is either unspecified or invalid")
+  }
+
+  @Test
+  fun `createEventGroup returns event group`() {
+    val request = buildCreateEventGroupRequest {
+      parent = DATA_PROVIDER_NAME
+      eventGroup = EVENT_GROUP
+    }
+
+    val result = runBlocking { service.createEventGroup(request) }
+
+    val expected = EVENT_GROUP
+
+    verifyProtoArgument(internalEventGroupsMock, EventGroupsCoroutineImplBase::createEventGroup)
+      .isEqualTo(
+        INTERNAL_EVENT_GROUP.rebuild {
+          clearCreateTime()
+          clearExternalEventGroupId()
+        }
+      )
+
+    assertThat(result).isEqualTo(expected)
+  }
+
+  @Test
+  fun `createEventGroup throws INVALID_ARGUMENT when parent is missing`() {
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        runBlocking {
+          service.createEventGroup(buildCreateEventGroupRequest { eventGroup = EVENT_GROUP })
+        }
+      }
+    assertThat(exception.status.code).isEqualTo(Status.Code.INVALID_ARGUMENT)
+    assertThat(exception.status.description).isEqualTo("Parent is either unspecified or invalid")
+  }
+
+  @Test
+  fun `createEventGroup throws INVALID_ARGUMENT when measurement consumer is missing`() {
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        runBlocking {
+          service.createEventGroup(
+            buildCreateEventGroupRequest {
+              parent = DATA_PROVIDER_NAME
+              eventGroup = EVENT_GROUP.rebuild { clearMeasurementConsumer() }
+            }
+          )
+        }
+      }
+    assertThat(exception.status.code).isEqualTo(Status.Code.INVALID_ARGUMENT)
+    assertThat(exception.status.description)
+      .isEqualTo("Measurement consumer is either unspecified or invalid")
+  }
+
+  @Test
+  fun `listEventGroups with parent uses filter with parent`() {
+    val request = buildListEventGroupsRequest { parent = DATA_PROVIDER_NAME }
+
+    val result = runBlocking { service.listEventGroups(request) }
+
+    val expected =
+      ListEventGroupsResponse.newBuilder()
+        .apply {
+          addEventGroups(EVENT_GROUP)
+          addEventGroups(EVENT_GROUP)
+          nextPageToken = CREATE_TIME.toByteArray().base64UrlEncode()
+        }
+        .build()
+
+    val streamEventGroupsRequest =
+      captureFirst<StreamEventGroupsRequest> {
+        verify(internalEventGroupsMock).streamEventGroups(capture())
+      }
+
+    assertThat(streamEventGroupsRequest)
+      .ignoringRepeatedFieldOrder()
+      .isEqualTo(
+        buildStreamEventGroupsRequest {
+          limit = DEFAULT_LIMIT
+          filterBuilder.apply {
+            externalDataProviderId =
+              apiIdToExternalId(DataProviderKey.fromName(DATA_PROVIDER_NAME)!!.dataProviderId)
+          }
+        }
+      )
+
+    assertThat(result).ignoringRepeatedFieldOrder().isEqualTo(expected)
+  }
+
+  @Test
+  fun `listEventGroups with page token uses filter with timestamp from page token`() {
+    val request = buildListEventGroupsRequest {
+      parent = DATA_PROVIDER_NAME
+      pageSize = 2
+      pageToken = CREATE_TIME.toByteArray().base64UrlEncode()
+    }
+
+    val result = runBlocking { service.listEventGroups(request) }
+
+    val expected =
+      ListEventGroupsResponse.newBuilder()
+        .apply {
+          addEventGroups(EVENT_GROUP)
+          addEventGroups(EVENT_GROUP)
+          nextPageToken = CREATE_TIME.toByteArray().base64UrlEncode()
+        }
+        .build()
+
+    val streamEventGroupsRequest =
+      captureFirst<StreamEventGroupsRequest> {
+        verify(internalEventGroupsMock).streamEventGroups(capture())
+      }
+
+    assertThat(streamEventGroupsRequest)
+      .ignoringRepeatedFieldOrder()
+      .isEqualTo(
+        buildStreamEventGroupsRequest {
+          limit = 2
+          filterBuilder.apply {
+            externalDataProviderId =
+              apiIdToExternalId(DataProviderKey.fromName(DATA_PROVIDER_NAME)!!.dataProviderId)
+            createdAfter = CREATE_TIME
+          }
+        }
+      )
+
+    assertThat(result).ignoringRepeatedFieldOrder().isEqualTo(expected)
+  }
+
+  @Test
+  fun `listEventGroups with parent and filter with measurement consumers uses filter with both`() {
+    val request = buildListEventGroupsRequest {
+      parent = DATA_PROVIDER_NAME
+      filterBuilder.apply {
+        addMeasurementConsumers(MEASUREMENT_CONSUMER_NAME)
+        addMeasurementConsumers(MEASUREMENT_CONSUMER_NAME)
+      }
+    }
+
+    val result = runBlocking { service.listEventGroups(request) }
+
+    val expected =
+      ListEventGroupsResponse.newBuilder()
+        .apply {
+          addEventGroups(EVENT_GROUP)
+          addEventGroups(EVENT_GROUP)
+          nextPageToken = CREATE_TIME.toByteArray().base64UrlEncode()
+        }
+        .build()
+
+    val streamEventGroupsRequest =
+      captureFirst<StreamEventGroupsRequest> {
+        verify(internalEventGroupsMock).streamEventGroups(capture())
+      }
+
+    assertThat(streamEventGroupsRequest)
+      .ignoringRepeatedFieldOrder()
+      .isEqualTo(
+        buildStreamEventGroupsRequest {
+          limit = DEFAULT_LIMIT
+          filterBuilder.apply {
+            externalDataProviderId =
+              apiIdToExternalId(DataProviderKey.fromName(DATA_PROVIDER_NAME)!!.dataProviderId)
+            val measurementConsumerId =
+              apiIdToExternalId(
+                MeasurementConsumerKey.fromName(MEASUREMENT_CONSUMER_NAME)!!.measurementConsumerId
+              )
+            addExternalMeasurementConsumerIds(measurementConsumerId)
+            addExternalMeasurementConsumerIds(measurementConsumerId)
+          }
+        }
+      )
+
+    assertThat(result).ignoringRepeatedFieldOrder().isEqualTo(expected)
+  }
+
+  @Test
+  fun `listEventGroups throws INVALID_ARGUMENT when only wildcard parent`() {
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        runBlocking {
+          service.listEventGroups(buildListEventGroupsRequest { parent = WILDCARD_NAME })
+        }
+      }
+    assertThat(exception.status.code).isEqualTo(Status.Code.INVALID_ARGUMENT)
+    assertThat(exception.status.description)
+      .isEqualTo("Either parent data provider or measurement consumers filter must be provided")
+  }
+
+  @Test
+  fun `listRequisitions throws INVALID_ARGUMENT when parent is missing`() {
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        runBlocking { service.listEventGroups(ListEventGroupsRequest.getDefaultInstance()) }
+      }
+    assertThat(exception.status.code).isEqualTo(Status.Code.INVALID_ARGUMENT)
+    assertThat(exception.status.description).isEqualTo("Parent is either unspecified or invalid")
+  }
+
+  @Test
+  fun `listEventGroups throws INVALID_ARGUMENT when measurement consumer in filter is invalid`() {
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        runBlocking {
+          service.listEventGroups(
+            buildListEventGroupsRequest {
+              parent = DATA_PROVIDER_NAME
+              filterBuilder.addMeasurementConsumers("asdf")
+            }
+          )
+        }
+      }
+    assertThat(exception.status.code).isEqualTo(Status.Code.INVALID_ARGUMENT)
+    assertThat(exception.status.description)
+      .isEqualTo("Measurement consumer name in filter invalid")
+  }
+
+  @Test
+  fun `listEventGroups throws INVALID_ARGUMENT when page size is less than 0`() {
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        runBlocking {
+          service.listEventGroups(
+            buildListEventGroupsRequest {
+              parent = DATA_PROVIDER_NAME
+              filterBuilder.apply { pageSize = -1 }
+            }
+          )
+        }
+      }
+    assertThat(exception.status.code).isEqualTo(Status.Code.INVALID_ARGUMENT)
+    assertThat(exception.status.description).isEqualTo("Page size cannot be less than 0")
+  }
+}
+
+private val EVENT_GROUP: EventGroup = buildEventGroup {
+  name = EVENT_GROUP_NAME
+  measurementConsumer = MEASUREMENT_CONSUMER_NAME
+  eventGroupReferenceId = "aaa"
+}
+
+private val INTERNAL_EVENT_GROUP: InternalEventGroup = buildInternalEventGroup {
+  val key = EventGroupKey.fromName(EVENT_GROUP.name)
+  externalDataProviderId = apiIdToExternalId(key!!.dataProviderId)
+  externalEventGroupId = apiIdToExternalId(key.eventGroupId)
+  val measurementConsumerKey = MeasurementConsumerKey.fromName(EVENT_GROUP.measurementConsumer)
+  externalMeasurementConsumerId = apiIdToExternalId(measurementConsumerKey!!.measurementConsumerId)
+  providedEventGroupId = EVENT_GROUP.eventGroupReferenceId
+  createTime = CREATE_TIME
+}
+
+internal inline fun EventGroup.rebuild(fill: (@Builder EventGroup.Builder).() -> Unit) =
+  toBuilder().apply(fill).build()
+
+internal inline fun InternalEventGroup.rebuild(
+  fill: (@Builder InternalEventGroup.Builder).() -> Unit
+) = toBuilder().apply(fill).build()
+
+internal inline fun buildGetEventGroupRequest(
+  fill: (@Builder GetEventGroupRequest.Builder).() -> Unit
+) = GetEventGroupRequest.newBuilder().apply(fill).build()
+
+internal inline fun buildCreateEventGroupRequest(
+  fill: (@Builder CreateEventGroupRequest.Builder).() -> Unit
+) = CreateEventGroupRequest.newBuilder().apply(fill).build()
+
+internal inline fun buildListEventGroupsRequest(
+  fill: (@Builder ListEventGroupsRequest.Builder).() -> Unit
+) = ListEventGroupsRequest.newBuilder().apply(fill).build()
