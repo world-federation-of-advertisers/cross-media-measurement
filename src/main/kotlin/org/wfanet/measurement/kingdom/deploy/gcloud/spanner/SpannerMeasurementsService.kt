b49454a2cc48735cbeb9f4cca38668b4e2a5abca
uakyol <69486838+uakyol@users.noreply.github.com>
2021-08-17 10:56:20 -0700

    Implement Create and Get Methods for Internal Measurements Service - Skeleton (#173)
    Implement Create and Get Methods for Internal Measurements Service
    
    This includes almost complete implementations of : 
    
    CreateMeasurement
    MeasurementReader
    SpannerMeasurementService
    MeasurementServiceTest
    
    These implementations are not complete because they don't include:
    
    Populating all the fields of Requisition to the DB
    Reading all the fields of Requisition and ComputationParticipants from the DB

--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/SpannerMeasurementsService.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/SpannerMeasurementsService.kt
@@ -14,13 +14,19 @@
 
 package org.wfanet.measurement.kingdom.deploy.gcloud.spanner
 
+import io.grpc.Status
 import java.time.Clock
+import org.wfanet.measurement.common.grpc.failGrpc
+import org.wfanet.measurement.common.identity.ExternalId
 import org.wfanet.measurement.common.identity.IdGenerator
 import org.wfanet.measurement.gcloud.spanner.AsyncDatabaseClient
 import org.wfanet.measurement.internal.kingdom.GetMeasurementByComputationIdRequest
 import org.wfanet.measurement.internal.kingdom.GetMeasurementRequest
 import org.wfanet.measurement.internal.kingdom.Measurement
 import org.wfanet.measurement.internal.kingdom.MeasurementsGrpcKt.MeasurementsCoroutineImplBase
+import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.common.KingdomInternalException
+import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.MeasurementReader
+import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.writers.CreateMeasurement
 
 class SpannerMeasurementsService(
   private val clock: Clock,
@@ -28,14 +34,29 @@ class SpannerMeasurementsService(
   private val client: AsyncDatabaseClient
 ) : MeasurementsCoroutineImplBase() {
   override suspend fun createMeasurement(request: Measurement): Measurement {
-    TODO("not implemented yet")
+    try {
+      return CreateMeasurement(request).execute(client, idGenerator, clock)
+    } catch (e: KingdomInternalException) {
+      when (e.code) {
+        KingdomInternalException.Code.MEASUREMENT_CONSUMER_NOT_FOUND ->
+          failGrpc(Status.NOT_FOUND) { "MeasurementConsumer not found" }
+        KingdomInternalException.Code.DATA_PROVIDER_NOT_FOUND ->
+          failGrpc(Status.INVALID_ARGUMENT) { "DataProvider not found" }
+        KingdomInternalException.Code.DUCHY_NOT_FOUND ->
+          failGrpc(Status.INVALID_ARGUMENT) { "Duchy not found" }
+        KingdomInternalException.Code.CERT_SUBJECT_KEY_ID_ALREADY_EXISTS -> throw e
+      }
+    }
   }
   override suspend fun getMeasurement(request: GetMeasurementRequest): Measurement {
-    TODO("not implemented yet")
+    TODO("Not implemented yet.")
   }
   override suspend fun getMeasurementByComputationId(
     request: GetMeasurementByComputationIdRequest
   ): Measurement {
-    TODO("not implemented yet")
+    return MeasurementReader(request.measurementView)
+      .readExternalIdOrNull(client.singleUse(), ExternalId(request.externalComputationId))
+      ?.measurement
+      ?: failGrpc(Status.NOT_FOUND) { "Measurement not found" }
   }
 }
