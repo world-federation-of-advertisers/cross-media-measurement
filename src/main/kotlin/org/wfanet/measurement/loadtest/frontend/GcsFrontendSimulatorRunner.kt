7ca7de0ae21a82c194ee24842de09fe4750f2c3e
Yao Wang <wangyaopw@google.com>
2021-08-17 15:06:06 -0400

    Implement getResult and getEsitimatedResult in the FE simulator.

---
+++ src/main/kotlin/org/wfanet/measurement/loadtest/frontend/GcsFrontendSimulatorRunner.kt
@@ -0,0 +1,37 @@
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.loadtest.frontend
+
+import org.wfanet.measurement.common.commandLineMain
+import org.wfanet.measurement.gcloud.gcs.GcsFromFlags
+import org.wfanet.measurement.gcloud.gcs.GcsStorageClient
+import picocli.CommandLine
+
+@CommandLine.Command(
+  name = "GcsFrontendSimulatorRunner",
+  description = ["Daemon for GcsFrontendSimulatorRunner."],
+  mixinStandardHelpOptions = true,
+  showDefaultValues = true
+)
+class GcsFrontendSimulatorRunner : FrontendSimulatorRunner() {
+  @CommandLine.Mixin private lateinit var gcsFlags: GcsFromFlags.Flags
+
+  override fun run() {
+    val gcs = GcsFromFlags(gcsFlags)
+    run(GcsStorageClient.fromFlags(gcs))
+  }
+}
+
+fun main(args: Array<String>) = commandLineMain(GcsFrontendSimulatorRunner(), args)
