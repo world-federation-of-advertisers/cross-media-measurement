19c8a02e290f9fa8f7be5504721f672ba5980782
Sanjay Vasandani <sanjayvas@google.com>
2021-09-09 11:21:09 -0700

    Implement internal CancelMeasurement method.

--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/writers/CancelMeasurement.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/writers/CancelMeasurement.kt
@@ -14,59 +14,54 @@
 
 package org.wfanet.measurement.kingdom.deploy.gcloud.spanner.writers
 
-import com.google.cloud.spanner.Value
 import org.wfanet.measurement.common.identity.ExternalId
-import org.wfanet.measurement.gcloud.spanner.bufferUpdateMutation
-import org.wfanet.measurement.gcloud.spanner.set
-import org.wfanet.measurement.gcloud.spanner.setJson
 import org.wfanet.measurement.internal.kingdom.Measurement
-import org.wfanet.measurement.internal.kingdom.SetMeasurementResultRequest
 import org.wfanet.measurement.internal.kingdom.copy
 import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.common.KingdomInternalException
 import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.MeasurementReader
 
-private val NEXT_MEASUREMENT_STATE = Measurement.State.SUCCEEDED
-
 /**
- * Sets participant details for a computationPartcipant in the database.
+ * Cancels a [Measurement], transitioning its state to [Measurement.State.CANCELLED].
  *
  * Throws a [KingdomInternalException] on [execute] with the following codes/conditions:
  * * [KingdomInternalException.Code.MEASUREMENT_NOT_FOUND]
+ * * [KingdomInternalException.Code.MEASUREMENT_STATE_ILLEGAL]
  */
-class SetMeasurementResult(private val request: SetMeasurementResultRequest) :
-  SpannerWriter<Measurement, Measurement>() {
-
+class CancelMeasurement(
+  private val externalMeasurementConsumerId: ExternalId,
+  private val externalMeasurementId: ExternalId
+) : SpannerWriter<Measurement, Measurement>() {
   override suspend fun TransactionScope.runTransaction(): Measurement {
-
     val (measurementConsumerId, measurementId, measurement) =
       MeasurementReader(Measurement.View.DEFAULT)
-        .readExternalIdOrNull(transactionContext, ExternalId(request.externalComputationId))
+        .readByExternalIds(transactionContext, externalMeasurementConsumerId, externalMeasurementId)
         ?: throw KingdomInternalException(KingdomInternalException.Code.MEASUREMENT_NOT_FOUND) {
-          "Measurement for external computation ID ${request.externalComputationId} not found"
+          "Measurement with external MeasurementConsumer ID $externalMeasurementConsumerId and " +
+            "external Measurement ID $externalMeasurementId not found"
         }
 
-    val measurementDetails =
-      measurement.details.copy {
-        aggregatorCertificate = request.aggregatorCertificate
-        resultPublicKey = request.resultPublicKey
-        encryptedResult = request.encryptedResult
+    when (val state = measurement.state) {
+      Measurement.State.PENDING_REQUISITION_PARAMS,
+      Measurement.State.PENDING_REQUISITION_FULFILLMENT,
+      Measurement.State.PENDING_PARTICIPANT_CONFIRMATION,
+      Measurement.State.PENDING_COMPUTATION -> {}
+      Measurement.State.SUCCEEDED,
+      Measurement.State.FAILED,
+      Measurement.State.CANCELLED,
+      Measurement.State.STATE_UNSPECIFIED,
+      Measurement.State.UNRECOGNIZED -> {
+        throw KingdomInternalException(KingdomInternalException.Code.MEASUREMENT_STATE_ILLEGAL) {
+          "Unexpected Measurement state $state (${state.number})"
+        }
       }
-
-    transactionContext.bufferUpdateMutation("Measurements") {
-      set("MeasurementConsumerId" to measurementConsumerId)
-      set("MeasurementId" to measurementId)
-      set("UpdateTime" to Value.COMMIT_TIMESTAMP)
-      set("State" to NEXT_MEASUREMENT_STATE)
-      set("MeasurementDetails" to measurementDetails)
-      setJson("MeasurementDetailsJson" to measurementDetails)
     }
 
-    return measurement.copy {
-      state = NEXT_MEASUREMENT_STATE
-      details = measurementDetails
-    }
+    updateMeasurementState(measurementConsumerId, measurementId, Measurement.State.CANCELLED)
+
+    return measurement.copy { this.state = Measurement.State.CANCELLED }
   }
+
   override fun ResultScope<Measurement>.buildResult(): Measurement {
-    return checkNotNull(transactionResult).copy { updateTime = commitTimestamp.toProto() }
+    return checkNotNull(this.transactionResult).copy { updateTime = commitTimestamp.toProto() }
   }
 }
