418aad9cfcbd6228efe4355219319f4a3a7ed0d5
yunyeng <yunyeng@google.com>
2021-10-05 03:02:30 +0000

    Move InProcessKingdom to main folder

---
+++ src/main/kotlin/org/wfanet/measurement/integration/deploy/gcloud/InProcessKingdom.kt
@@ -0,0 +1,37 @@
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.integration.deploy.gcloud
+
+import java.time.Clock
+import org.junit.Rule
+import org.wfanet.measurement.common.identity.IdGenerator
+import org.wfanet.measurement.gcloud.spanner.testing.SpannerEmulatorDatabaseRule
+import org.wfanet.measurement.integration.common.InProcessKingdom
+import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.SpannerDataServices
+import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.testing.KINGDOM_SCHEMA
+
+@get:Rule val spannerDatabase = SpannerEmulatorDatabaseRule(KINGDOM_SCHEMA)
+
+fun buildSpannerInProcessKingdom(
+  idGenerator: IdGenerator,
+  clock: Clock = Clock.systemUTC()
+): InProcessKingdom {
+  return InProcessKingdom(
+    dataServicesProvider = {
+      SpannerDataServices(clock, idGenerator, spannerDatabase.databaseClient)
+    },
+    verboseGrpcLogging = false,
+  )
+}
