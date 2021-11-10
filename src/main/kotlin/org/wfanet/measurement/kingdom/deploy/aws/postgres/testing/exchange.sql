5fb87828b9b18cae118f60371ac4363c3e407028
Dulio Matos <duliomatos1@users.noreply.github.com>
2021-11-10 16:39:10 -0300

    Add pg-embedded dependency to test postgres and run minimal schema validation

---
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/aws/postgres/testing/exchange.sql
@@ -0,0 +1,27 @@
+-- Copyright 2020 The Cross-Media Measurement Authors
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+
+-- minimal port of a table from `src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/exchange.sdl`
+-- for use in testing
+
+CREATE TABLE ModelProviders (
+  PRIMARY KEY (ModelProviderId),
+  ModelProviderId BIGINT NOT NULL,
+
+  ExternalModelProviderId BIGINT NOT NULL
+);
+
+CREATE UNIQUE INDEX ModelProvidersByExternalId
+  ON ModelProviders(ExternalModelProviderId);
