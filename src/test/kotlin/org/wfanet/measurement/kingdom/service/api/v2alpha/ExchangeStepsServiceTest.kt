d9e93d88efb241a9ea50a635eda3e5bd393407f3
yunyeng <yunyeng@google.com>
2021-11-12 19:26:19 +0000

    Implement /ExchangeSteps.listExchangeSteps

--- src/test/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/ExchangeStepsServiceTest.kt
+++ src/test/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/ExchangeStepsServiceTest.kt
@@ -17,11 +17,14 @@ package org.wfanet.measurement.kingdom.service.api.v2alpha
 import com.google.common.truth.Truth.assertThat
 import com.google.common.truth.extensions.proto.ProtoTruth.assertThat
 import com.google.protobuf.ByteString
+import com.google.protobuf.Timestamp
 import com.google.type.date
 import io.grpc.Status
 import io.grpc.StatusRuntimeException
+import java.time.Instant
 import kotlin.test.assertFails
 import kotlin.test.assertFailsWith
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.Test
@@ -30,29 +33,45 @@ import org.junit.runners.JUnit4
 import org.mockito.kotlin.UseConstructor
 import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 import org.wfanet.measurement.api.v2alpha.ClaimReadyExchangeStepRequestKt
 import org.wfanet.measurement.api.v2alpha.ClaimReadyExchangeStepResponse
 import org.wfanet.measurement.api.v2alpha.DataProviderKey
+import org.wfanet.measurement.api.v2alpha.ExchangeKey
 import org.wfanet.measurement.api.v2alpha.ExchangeStep
 import org.wfanet.measurement.api.v2alpha.ExchangeStepAttemptKey
 import org.wfanet.measurement.api.v2alpha.ExchangeStepKey
 import org.wfanet.measurement.api.v2alpha.GetExchangeStepRequest
+import org.wfanet.measurement.api.v2alpha.ListExchangeStepsRequestKt
+import org.wfanet.measurement.api.v2alpha.ListExchangeStepsRequestKt.filter
+import org.wfanet.measurement.api.v2alpha.ListExchangeStepsResponse
 import org.wfanet.measurement.api.v2alpha.ModelProviderKey
 import org.wfanet.measurement.api.v2alpha.claimReadyExchangeStepRequest
 import org.wfanet.measurement.api.v2alpha.claimReadyExchangeStepResponse
 import org.wfanet.measurement.api.v2alpha.exchangeStep
+import org.wfanet.measurement.api.v2alpha.listExchangeStepsRequest
+import org.wfanet.measurement.api.v2alpha.listExchangeStepsResponse
+import org.wfanet.measurement.common.base64UrlEncode
 import org.wfanet.measurement.common.grpc.testing.GrpcTestServerRule
+import org.wfanet.measurement.common.identity.apiIdToExternalId
 import org.wfanet.measurement.common.identity.externalIdToApiId
+import org.wfanet.measurement.common.testing.captureFirst
 import org.wfanet.measurement.common.testing.verifyProtoArgument
+import org.wfanet.measurement.common.toProtoTime
 import org.wfanet.measurement.internal.kingdom.ExchangeStep as InternalExchangeStep
 import org.wfanet.measurement.internal.kingdom.ExchangeStepsGrpcKt.ExchangeStepsCoroutineImplBase as InternalExchangeStepsCoroutineImplBase
 import org.wfanet.measurement.internal.kingdom.ExchangeStepsGrpcKt.ExchangeStepsCoroutineStub as InternalExchangeStepsCoroutineStub
 import org.wfanet.measurement.internal.kingdom.Provider
+import org.wfanet.measurement.internal.kingdom.StreamExchangeStepsRequest
+import org.wfanet.measurement.internal.kingdom.StreamExchangeStepsRequestKt
 import org.wfanet.measurement.internal.kingdom.claimReadyExchangeStepRequest as internalClaimReadyExchangeStepRequest
 import org.wfanet.measurement.internal.kingdom.claimReadyExchangeStepResponse as internalClaimReadyExchangeStepResponse
 import org.wfanet.measurement.internal.kingdom.exchangeStep as internalExchangeStep
 import org.wfanet.measurement.internal.kingdom.provider
+import org.wfanet.measurement.internal.kingdom.streamExchangeStepsRequest
 
+private const val DEFAULT_LIMIT = 50
+private const val EXCHANGE_NAME = "recurringExchanges/AAAAAAAAAHs/exchanges/2021-03-14"
 private const val RECURRING_EXCHANGE_ID = 1L
 private val DATE = date {
   year = 2021
@@ -63,6 +82,7 @@ private const val EXCHANGE_ID = "2021-03-14"
 private const val STEP_INDEX = 123
 private const val ATTEMPT_NUMBER = 5
 private val SERIALIZED_WORKFLOW = ByteString.copyFromUtf8("some-serialized-exchange-workflow")
+private val UPDATE_TIME: Timestamp = Instant.ofEpochSecond(123).toProtoTime()
 
 private val EXCHANGE_STEP = exchangeStep {
   val exchangeStepKey =
@@ -99,6 +119,7 @@ private val INTERNAL_EXCHANGE_STEP: InternalExchangeStep = internalExchangeStep 
   stepIndex = STEP_INDEX
   state = InternalExchangeStep.State.READY_FOR_RETRY
   serializedExchangeWorkflow = SERIALIZED_WORKFLOW
+  updateTime = UPDATE_TIME
 }
 
 @RunWith(JUnit4::class)
@@ -113,6 +134,7 @@ class ExchangeStepsServiceTest {
             attemptNumber = ATTEMPT_NUMBER
           }
         )
+      onBlocking { streamExchangeSteps(any()) }.thenReturn(flowOf(INTERNAL_EXCHANGE_STEP))
     }
 
   @get:Rule val grpcTestServerRule = GrpcTestServerRule { addService(internalService) }
@@ -132,6 +154,12 @@ class ExchangeStepsServiceTest {
     init: ClaimReadyExchangeStepRequestKt.Dsl.() -> Unit
   ): ClaimReadyExchangeStepResponse = runBlocking {
     service.claimReadyExchangeStep(claimReadyExchangeStepRequest(init))
+  }
+
+  private fun listExchangeSteps(
+    init: ListExchangeStepsRequestKt.Dsl.() -> Unit
+  ): ListExchangeStepsResponse = runBlocking {
+    service.listExchangeSteps(listExchangeStepsRequest(init))
   }
 
   @Test
@@ -202,5 +230,82 @@ class ExchangeStepsServiceTest {
     withPrincipal(principal) {
       assertFails { claimReadyExchangeStep { dataProvider = externalIdToApiId(12345L) } }
     }
+  }
+
+  @Test
+  fun `listExchangeSteps unauthenticated`() {
+    val e =
+      assertFailsWith<StatusRuntimeException> {
+        listExchangeSteps { filter = filter { dataProvider = externalIdToApiId(123L) } }
+      }
+    assertThat(e.status.code).isEqualTo(Status.Code.UNAUTHENTICATED)
+  }
+
+  @Test
+  fun `listExchangeSteps with page token uses filter with timestamp from page token`() {
+    val principal = Principal.ModelProvider(ModelProviderKey(externalIdToApiId(12345L)))
+    val provider = provider {
+      type = Provider.Type.MODEL_PROVIDER
+      externalId = 12345L
+    }
+
+    val result =
+      withPrincipal(principal) {
+        listExchangeSteps {
+          parent = EXCHANGE_NAME
+          pageToken = UPDATE_TIME.toByteArray().base64UrlEncode()
+          filter =
+            filter {
+              modelProvider = externalIdToApiId(12345L)
+              states += listOf(ExchangeStep.State.READY, ExchangeStep.State.READY_FOR_RETRY)
+            }
+        }
+      }
+
+    val expected = listExchangeStepsResponse {
+      exchangeStep += EXCHANGE_STEP
+      nextPageToken = UPDATE_TIME.toByteArray().base64UrlEncode()
+    }
+
+    val streamExchangeStepsRequest =
+      captureFirst<StreamExchangeStepsRequest> {
+        verify(internalService).streamExchangeSteps(capture())
+      }
+
+    assertThat(streamExchangeStepsRequest)
+      .ignoringRepeatedFieldOrder()
+      .isEqualTo(
+        streamExchangeStepsRequest {
+          limit = DEFAULT_LIMIT
+          filter =
+            StreamExchangeStepsRequestKt.filter {
+              stepProvider = provider
+              externalRecurringExchangeIds +=
+                apiIdToExternalId(ExchangeKey.fromName(EXCHANGE_NAME)!!.recurringExchangeId)
+              updatedAfter = UPDATE_TIME
+              states +=
+                listOf(InternalExchangeStep.State.READY, InternalExchangeStep.State.READY_FOR_RETRY)
+              dates += DATE
+            }
+        }
+      )
+
+    assertThat(result).ignoringRepeatedFieldOrder().isEqualTo(expected)
+  }
+
+  @Test
+  fun `listExchangeSteps throws INVALID_ARGUMENT when page size is less than 0`() {
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        runBlocking {
+          listExchangeSteps {
+            parent = EXCHANGE_NAME
+            filter = filter { dataProvider = externalIdToApiId(12345L) }
+            pageSize = -1
+          }
+        }
+      }
+    assertThat(exception.status.code).isEqualTo(Status.Code.INVALID_ARGUMENT)
+    assertThat(exception.status.description).isEqualTo("Page size cannot be less than 0")
   }
 }
