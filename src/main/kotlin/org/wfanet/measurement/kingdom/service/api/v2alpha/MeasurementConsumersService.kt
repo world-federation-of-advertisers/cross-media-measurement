eecd62b1ba836eec7bad42aebcb9b0c61b7a5721
Tristan Vuong <85768771+tristanvuong2021@users.noreply.github.com>
2021-12-09 14:26:52 -0800

    Implement public add and remove measurement consumer owners. (#394)

--- src/main/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/MeasurementConsumersService.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/MeasurementConsumersService.kt
@@ -14,15 +14,21 @@
 
 package org.wfanet.measurement.kingdom.service.api.v2alpha
 
+import io.grpc.Status
+import org.wfanet.measurement.api.AccountConstants
 import org.wfanet.measurement.api.Version
+import org.wfanet.measurement.api.v2alpha.AccountKey
+import org.wfanet.measurement.api.v2alpha.AddMeasurementConsumerOwnerRequest
 import org.wfanet.measurement.api.v2alpha.CreateMeasurementConsumerRequest
 import org.wfanet.measurement.api.v2alpha.GetMeasurementConsumerRequest
 import org.wfanet.measurement.api.v2alpha.MeasurementConsumer
 import org.wfanet.measurement.api.v2alpha.MeasurementConsumerCertificateKey
 import org.wfanet.measurement.api.v2alpha.MeasurementConsumerKey
 import org.wfanet.measurement.api.v2alpha.MeasurementConsumersGrpcKt.MeasurementConsumersCoroutineImplBase as MeasurementConsumersCoroutineService
+import org.wfanet.measurement.api.v2alpha.RemoveMeasurementConsumerOwnerRequest
 import org.wfanet.measurement.api.v2alpha.measurementConsumer
 import org.wfanet.measurement.api.v2alpha.signedData
+import org.wfanet.measurement.common.grpc.failGrpc
 import org.wfanet.measurement.common.grpc.grpcRequire
 import org.wfanet.measurement.common.grpc.grpcRequireNotNull
 import org.wfanet.measurement.common.identity.apiIdToExternalId
@@ -30,8 +36,10 @@ import org.wfanet.measurement.common.identity.externalIdToApiId
 import org.wfanet.measurement.internal.kingdom.MeasurementConsumer as InternalMeasurementConsumer
 import org.wfanet.measurement.internal.kingdom.MeasurementConsumerKt.details
 import org.wfanet.measurement.internal.kingdom.MeasurementConsumersGrpcKt.MeasurementConsumersCoroutineStub as InternalMeasurementConsumersCoroutineStub
+import org.wfanet.measurement.internal.kingdom.addMeasurementConsumerOwnerRequest
 import org.wfanet.measurement.internal.kingdom.getMeasurementConsumerRequest
 import org.wfanet.measurement.internal.kingdom.measurementConsumer as internalMeasurementConsumer
+import org.wfanet.measurement.internal.kingdom.removeMeasurementConsumerOwnerRequest
 
 private val API_VERSION = Version.V2_ALPHA
 
@@ -82,6 +90,70 @@ class MeasurementConsumersService(
         }
       )
     return internalResponse.toMeasurementConsumer()
+  }
+
+  override suspend fun addMeasurementConsumerOwner(
+    request: AddMeasurementConsumerOwnerRequest
+  ): MeasurementConsumer {
+    val account =
+      AccountConstants.CONTEXT_ACCOUNT_KEY.get()
+        ?: failGrpc(Status.UNAUTHENTICATED) { "Account credentials are invalid or missing" }
+
+    val measurementConsumerKey: MeasurementConsumerKey =
+      grpcRequireNotNull(MeasurementConsumerKey.fromName(request.name)) {
+        "Resource name unspecified or invalid"
+      }
+
+    val externalMeasurementConsumerId =
+      apiIdToExternalId(measurementConsumerKey.measurementConsumerId)
+
+    if (!account.externalOwnedMeasurementConsumerIdsList.contains(externalMeasurementConsumerId)) {
+      failGrpc(Status.PERMISSION_DENIED) { "Account doesn't own MeasurementConsumer" }
+    }
+
+    val accountKey: AccountKey =
+      grpcRequireNotNull(AccountKey.fromName(request.account)) { "Account unspecified or invalid" }
+
+    val internalAddMeasurementConsumerOwnerRequest = addMeasurementConsumerOwnerRequest {
+      this.externalMeasurementConsumerId = externalMeasurementConsumerId
+      externalAccountId = apiIdToExternalId(accountKey.accountId)
+    }
+
+    return internalClient
+      .addMeasurementConsumerOwner(internalAddMeasurementConsumerOwnerRequest)
+      .toMeasurementConsumer()
+  }
+
+  override suspend fun removeMeasurementConsumerOwner(
+    request: RemoveMeasurementConsumerOwnerRequest
+  ): MeasurementConsumer {
+    val account =
+      AccountConstants.CONTEXT_ACCOUNT_KEY.get()
+        ?: failGrpc(Status.UNAUTHENTICATED) { "Account credentials are invalid or missing" }
+
+    val measurementConsumerKey: MeasurementConsumerKey =
+      grpcRequireNotNull(MeasurementConsumerKey.fromName(request.name)) {
+        "Resource name unspecified or invalid"
+      }
+
+    val externalMeasurementConsumerId =
+      apiIdToExternalId(measurementConsumerKey.measurementConsumerId)
+
+    if (!account.externalOwnedMeasurementConsumerIdsList.contains(externalMeasurementConsumerId)) {
+      failGrpc(Status.PERMISSION_DENIED) { "Account doesn't own MeasurementConsumer" }
+    }
+
+    val accountKey: AccountKey =
+      grpcRequireNotNull(AccountKey.fromName(request.account)) { "Account unspecified or invalid" }
+
+    val internalRemoveMeasurementConsumerOwnerRequest = removeMeasurementConsumerOwnerRequest {
+      this.externalMeasurementConsumerId = externalMeasurementConsumerId
+      externalAccountId = apiIdToExternalId(accountKey.accountId)
+    }
+
+    return internalClient
+      .removeMeasurementConsumerOwner(internalRemoveMeasurementConsumerOwnerRequest)
+      .toMeasurementConsumer()
   }
 }
 
