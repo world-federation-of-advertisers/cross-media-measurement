6b84af702f1e9781910cd55edb183982aff72020
chipingyeh <92889345+chipingyeh@users.noreply.github.com>
2022-02-16 16:32:31 -0500

    Implement EventTemplateTypeRegistry (#437)
    * Implement EventTemplateTypeRegistry
    
    * Squashed commit of the following:
    
    commit 2edcab64e4addefe3d2a7e8f25bafd4ecb34c4ec
    Author: Chiping Yeh <cpy@fb.com>
    Date:   Thu Feb 3 19:09:04 2022 -0500
    
        linting
    
    commit 827e2374e0eeadef20e015010f348fa6cdcfd3d2
    Author: Chiping Yeh <cpy@fb.com>
    Date:   Thu Feb 3 19:05:13 2022 -0500
    
        relinting
    
    commit 3995e98ee413642aae49cc40cf03a754ef3b8fee
    Author: Chiping Yeh <cpy@fb.com>
    Date:   Thu Feb 3 18:57:54 2022 -0500
    
        Linting
    
    commit 97e3086c617d49d6dec0f1c4c4987bfd494985d6
    Author: Chiping Yeh <cpy@fb.com>
    Date:   Thu Feb 3 18:54:35 2022 -0500
    
        Updated BUILD
    
    commit 9e6339d3bf8000f49bd29e09724f9b834210eaca
    Author: Chiping Yeh <cpy@fb.com>
    Date:   Thu Feb 3 18:36:30 2022 -0500
    
        Added lines at end of BUILD files for linter
    
    commit 1f802eecf44b8acf935f3c66687d3df3d1090ff0
    Merge: 7c53bd82 54c95788
    Author: Chiping Yeh <cpy@fb.com>
    Date:   Thu Feb 3 18:29:51 2022 -0500
    
        Merge branch 'chipingyeh-eventfield-typeregistry' of github.com:world-federation-of-advertisers/cross-media-measurement into chipingyeh-eventfield-typeregistry
    
    commit 7c53bd825f802077fc61fb8d172c22381c4dd84a
    Author: Chiping Yeh <cpy@fb.com>
    Date:   Thu Feb 3 18:29:42 2022 -0500
    
        Added license headers
    
    commit 54c95788d2c8bbd2c0260813dfd33b21f3ff8b3a
    Merge: 810ca68c 8739a855
    Author: chipingyeh <92889345+chipingyeh@users.noreply.github.com>
    Date:   Thu Feb 3 18:22:48 2022 -0500
    
        Merge main into chipingyeh-eventfield-typeregistry
    
    commit 8739a8554120476353b93cbe255ad11dcfe79518
    Author: Sanjay Vasandani <sanjayvas@google.com>
    Date:   Thu Feb 3 14:25:36 2022 -0800
    
        Use rules_cue instead of local copy of cue rules. (#434)
    
    * Merge main into chipingyeh-eventfield-typeregistry
    
    * linting
    
    * Merge branch 'chipingyeh-eventfield-typeregistry' of github.com:world-federation-of-advertisers/cross-media-measurement into chipingyeh-eventfield-typeregistry
    
    * Utilized TypeRegistry in place of a map
    
    * Refactored code
    
    * Undo unintended renaming
    
    * Initial BUILD refactoring
    
    * Added proto library
    
    * Fixed merge conflicts
    
    * Removed typecasting
    
    * rebase
    
    * merge conflicts

---
+++ src/test/kotlin/org/wfanet/measurement/api/v2alpha/EventTemplateTypeRegistryTest.kt
@@ -0,0 +1,65 @@
+// Copyright 2020 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.api.v2alpha
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+private const val PACKAGE_NAME = "org.wfanet.measurement.api.v2alpha.event_templates.testing"
+private const val TEMPLATE_PREFIX = "org.wfa.measurement.api.v2alpha.event_templates.testing"
+private const val BANNER_TEMPLATE_NAME = "$TEMPLATE_PREFIX.TestBannerTemplate"
+private const val VIDEO_TEMPLATE_NAME = "$TEMPLATE_PREFIX.TestVideoTemplate"
+
+@RunWith(JUnit4::class)
+class EventTemplateTypeRegistryTest {
+  @Test
+  fun `loadTemplate() template contains correct fields`() {
+    val typeRegistry = EventTemplateTypeRegistry.createRegistryForPackagePrefix(PACKAGE_NAME)
+
+    assertThat(typeRegistry.getDescriptorForType(BANNER_TEMPLATE_NAME)?.fields).isEmpty()
+    assertThat(
+        typeRegistry.getDescriptorForType(VIDEO_TEMPLATE_NAME)?.fields?.map { field -> field.name }
+      )
+      .containsExactly("age", "duration")
+  }
+
+  @Test
+  fun `loadTemplate() template contains correct custom options`() {
+    val typeRegistry = EventTemplateTypeRegistry.createRegistryForPackagePrefix(PACKAGE_NAME)
+
+    assertThat(typeRegistry.getEventTemplateForType(BANNER_TEMPLATE_NAME)?.displayName)
+      .isEqualTo("Banner Ad")
+    assertThat(typeRegistry.getEventTemplateForType(BANNER_TEMPLATE_NAME)?.description)
+      .isEqualTo("A simple Event Template for a banner ad.")
+    assertThat(typeRegistry.getEventTemplateForType(VIDEO_TEMPLATE_NAME)?.displayName)
+      .isEqualTo("Video Ad")
+    assertThat(typeRegistry.getEventTemplateForType(VIDEO_TEMPLATE_NAME)?.description)
+      .isEqualTo("A simple Event Template for a video ad.")
+  }
+
+  @Test
+  fun `loadTemplate() loads subpackages`() {
+    val typeRegistry =
+      EventTemplateTypeRegistry.createRegistryForPackagePrefix("org.wfanet.measurement.api.v2alpha")
+
+    assertThat(typeRegistry.getDescriptorForType(BANNER_TEMPLATE_NAME)).isNotNull()
+    assertThat(typeRegistry.getDescriptorForType(VIDEO_TEMPLATE_NAME)).isNotNull()
+  }
+
+  private fun EventTemplateTypeRegistry.getEventTemplateForType(messageType: String) =
+    getDescriptorForType(messageType)?.options?.getExtension(EventAnnotations.eventTemplate)
+}
