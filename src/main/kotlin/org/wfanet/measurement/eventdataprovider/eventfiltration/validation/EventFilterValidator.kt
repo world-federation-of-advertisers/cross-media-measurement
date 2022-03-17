a33dc48c623eb807c1e4e49406f2cca6fab67335
Dulio Matos <duliomatos1@users.noreply.github.com>
2022-03-17 14:59:38 -0300

    Add event filtering

--- src/main/kotlin/org/wfanet/measurement/eventdataprovider/eventfiltration/validation/EventFilterValidator.kt
+++ src/main/kotlin/org/wfanet/measurement/eventdataprovider/eventfiltration/validation/EventFilterValidator.kt
@@ -15,13 +15,19 @@
 package org.wfanet.measurement.eventdataprovider.eventfiltration.validation
 
 import com.google.api.expr.v1alpha1.Expr
+import com.google.protobuf.Message
+import org.projectnessie.cel.Ast
 import org.projectnessie.cel.Env
+import org.projectnessie.cel.EnvOption
 import org.projectnessie.cel.Issues
+import org.projectnessie.cel.Program
+import org.projectnessie.cel.checker.Decls
+import org.projectnessie.cel.common.types.pb.ProtoTypeRegistry
 
 /**
  * Validates an Event Filtering CEL expression according to Halo rules.
  *
- * Throws a [EventFilterValidationException] on [validate] with the following codes:
+ * Throws a [EventFilterValidationException] on [compile] with the following codes:
  * * [EventFilterValidationException.Code.INVALID_CEL_EXPRESSION]
  * * [EventFilterValidationException.Code.INVALID_VALUE_TYPE]
  * * [EventFilterValidationException.Code.UNSUPPORTED_OPERATION]
@@ -39,33 +45,33 @@ object EventFilterValidator {
     val right = callExpr.argsList[1]
     if (!left.hasIdentExpr() && !left.hasSelectExpr()) {
       throw EventFilterValidationException(
-        EventFilterValidationException.Code.INVALID_VALUE_TYPE,
-        "Operator @in left argument should be a variable",
+          EventFilterValidationException.Code.INVALID_VALUE_TYPE,
+          "Operator @in left argument should be a variable",
       )
     }
     if (!right.hasListExpr()) {
       throw EventFilterValidationException(
-        EventFilterValidationException.Code.INVALID_VALUE_TYPE,
-        "Operator @in right argument should be a list",
+          EventFilterValidationException.Code.INVALID_VALUE_TYPE,
+          "Operator @in right argument should be a list",
       )
     }
     if (right.hasListExpr()) {
       for (element in right.listExpr.elementsList) {
         if (!element.hasConstExpr()) {
           throw EventFilterValidationException(
-            EventFilterValidationException.Code.INVALID_VALUE_TYPE,
-            "Operator @in right argument should be a list of constants or a variable",
+              EventFilterValidationException.Code.INVALID_VALUE_TYPE,
+              "Operator @in right argument should be a list of constants or a variable",
           )
         }
       }
     }
   }
 
-  fun failOnInvalidExpression(issues: Issues) {
+  private fun failOnInvalidExpression(issues: Issues) {
     if (issues.hasIssues()) {
       throw EventFilterValidationException(
-        EventFilterValidationException.Code.INVALID_CEL_EXPRESSION,
-        issues.toString(),
+          EventFilterValidationException.Code.INVALID_CEL_EXPRESSION,
+          issues.toString(),
       )
     }
   }
@@ -73,24 +79,24 @@ object EventFilterValidator {
   private fun failOnListOutsideInOperator(expr: Expr) {
     if (expr.hasListExpr()) {
       throw EventFilterValidationException(
-        EventFilterValidationException.Code.INVALID_VALUE_TYPE,
-        "Lists are only allowed on the right side of a @in operator",
+          EventFilterValidationException.Code.INVALID_VALUE_TYPE,
+          "Lists are only allowed on the right side of a @in operator",
       )
     }
   }
 
   private fun failOnSingleToplevelValue() {
     throw EventFilterValidationException(
-      EventFilterValidationException.Code.EXPRESSION_IS_NOT_CONDITIONAL,
-      "Expression cannot be a single value, should be a conditional",
+        EventFilterValidationException.Code.EXPRESSION_IS_NOT_CONDITIONAL,
+        "Expression cannot be a single value, should be a conditional",
     )
   }
 
   private fun failOnVariableOutsideLeaf(args: List<Expr>) {
     if (hasIndent(args) && hasCallExpr(args)) {
       throw EventFilterValidationException(
-        EventFilterValidationException.Code.FIELD_COMPARISON_OUTSIDE_LEAF,
-        "Field comparison should be done only on the leaf expressions",
+          EventFilterValidationException.Code.FIELD_COMPARISON_OUTSIDE_LEAF,
+          "Field comparison should be done only on the leaf expressions",
       )
     }
   }
@@ -99,8 +105,8 @@ object EventFilterValidator {
     val operator = callExpr.function
     if (LEAF_ONLY_OPERATORS.contains(operator) && hasCallExpr(callExpr.argsList)) {
       throw EventFilterValidationException(
-        EventFilterValidationException.Code.INVALID_OPERATION_OUTSIDE_LEAF,
-        "Operator $operator should only be used on leaf expressions",
+          EventFilterValidationException.Code.INVALID_OPERATION_OUTSIDE_LEAF,
+          "Operator $operator should only be used on leaf expressions",
       )
     }
   }
@@ -108,8 +114,8 @@ object EventFilterValidator {
   private fun failOnNotAllowedOperator(operator: String?) {
     if (!ALLOWED_OPERATORS.contains(operator)) {
       throw EventFilterValidationException(
-        EventFilterValidationException.Code.UNSUPPORTED_OPERATION,
-        "Operator $operator is not allowed",
+          EventFilterValidationException.Code.UNSUPPORTED_OPERATION,
+          "Operator $operator is not allowed",
       )
     }
   }
@@ -133,30 +139,66 @@ object EventFilterValidator {
     }
   }
 
-  fun validate(celExpression: String, env: Env) {
-    val astAndIssues = env.compile(celExpression)
+  fun compile(celExpression: String, env: Env): Ast {
+    val astAndIssues =
+        try {
+          env.compile(celExpression)
+        } catch (e: Exception) {
+          throw EventFilterValidationException(
+              EventFilterValidationException.Code.INVALID_CEL_EXPRESSION,
+              e.message ?: "Compiling expression threw an unexpected exception",
+          )
+        }
     failOnInvalidExpression(astAndIssues.issues)
-    val expr = astAndIssues.ast.expr
+    val ast = astAndIssues.ast
+    val expr = ast.expr
     if (!expr.hasCallExpr()) {
       failOnSingleToplevelValue()
     }
     validateExpr(expr)
+    return ast
+  }
+
+  private fun createEnv(eventMessage: Message): Env {
+    val typeRegistry: ProtoTypeRegistry = ProtoTypeRegistry.newRegistry(eventMessage)
+    val celVariables =
+        eventMessage.descriptorForType.fields.map { field ->
+          val typeName = field.messageType.fullName
+          val defaultValue = eventMessage.getField(field) as? Message
+          checkNotNull(defaultValue) { "eventMessage field should have Message type" }
+          typeRegistry.registerMessage(defaultValue)
+          Decls.newVar(
+              field.name,
+              Decls.newObjectType(typeName),
+          )
+        }
+    return Env.newEnv(
+        EnvOption.customTypeAdapter(typeRegistry),
+        EnvOption.customTypeProvider(typeRegistry),
+        EnvOption.declarations(celVariables),
+    )
+  }
+
+  fun compileProgramWithEventMessage(celExpression: String, eventMessage: Message): Program {
+    val env = createEnv(eventMessage)
+    val ast = compile(celExpression, env)
+    return env.program(ast)
   }
 }
 
 private val LEAF_ONLY_OPERATORS =
-  listOf(
-    "_>_",
-    "_<_",
-    "_!=_",
-    "_==_",
-    "_<=_",
-    "@in",
-  )
+    listOf(
+        "_>_",
+        "_<_",
+        "_!=_",
+        "_==_",
+        "_<=_",
+        "@in",
+    )
 private val BOOLEAN_OPERATORS =
-  listOf(
-    "!_",
-    "_&&_",
-    "_||_",
-  )
+    listOf(
+        "!_",
+        "_&&_",
+        "_||_",
+    )
 private val ALLOWED_OPERATORS = LEAF_ONLY_OPERATORS + BOOLEAN_OPERATORS
