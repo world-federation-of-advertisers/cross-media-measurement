6151743ed0948521a40b6280ab1efbb3076d12c4
Tristan Vuong <tristanvuong@google.com>
2022-03-31 17:46:17 +0000

    Implement internal public keys service.

---
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/writers/UpdatePublicKey.kt
@@ -0,0 +1,105 @@
+// Copyright 2022 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.kingdom.deploy.gcloud.spanner.writers
+
+import kotlinx.coroutines.flow.singleOrNull
+import org.wfanet.measurement.common.identity.ExternalId
+import org.wfanet.measurement.gcloud.spanner.bufferUpdateMutation
+import org.wfanet.measurement.gcloud.spanner.set
+import org.wfanet.measurement.gcloud.spanner.setJson
+import org.wfanet.measurement.internal.kingdom.DataProvider
+import org.wfanet.measurement.internal.kingdom.ErrorCode
+import org.wfanet.measurement.internal.kingdom.MeasurementConsumer
+import org.wfanet.measurement.internal.kingdom.UpdatePublicKeyRequest
+import org.wfanet.measurement.internal.kingdom.copy
+import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.common.KingdomInternalException
+import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.CertificateReader
+import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.DataProviderReader
+import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.MeasurementConsumerReader
+
+/**
+ * Updates the public key details for a [MeasurementConsumer] or a [DataProvider].
+ *
+ * Throws a [KingdomInternalException] on [execute] with the following codes/conditions:
+ * * [ErrorCode.CERTIFICATE_NOT_FOUND]
+ * * [ErrorCode.DATA_PROVIDER_NOT_FOUND]
+ * * [ErrorCode.MEASUREMENT_CONSUMER_NOT_FOUND]
+ */
+class UpdatePublicKey(private val request: UpdatePublicKeyRequest) : SimpleSpannerWriter<Unit>() {
+
+  override suspend fun TransactionScope.runTransaction() {
+    if (request.externalMeasurementConsumerId != 0L) {
+      CertificateReader(CertificateReader.ParentType.MEASUREMENT_CONSUMER)
+        .bindWhereClause(
+          ExternalId(request.externalMeasurementConsumerId),
+          ExternalId(request.externalCertificateId)
+        )
+        .execute(transactionContext)
+        .singleOrNull()
+        ?: throw KingdomInternalException(ErrorCode.CERTIFICATE_NOT_FOUND)
+
+      val measurementConsumerResult =
+        MeasurementConsumerReader()
+          .readByExternalMeasurementConsumerId(
+            transactionContext,
+            ExternalId(request.externalMeasurementConsumerId)
+          )
+          ?: throw KingdomInternalException(ErrorCode.MEASUREMENT_CONSUMER_NOT_FOUND)
+
+      val measurementConsumerDetails =
+        measurementConsumerResult.measurementConsumer.details.copy {
+          apiVersion = request.apiVersion
+          publicKey = request.publicKey
+          publicKeySignature = request.publicKeySignature
+        }
+
+      transactionContext.bufferUpdateMutation("MeasurementConsumers") {
+        set("MeasurementConsumerId" to measurementConsumerResult.measurementConsumerId)
+        set("MeasurementConsumerDetails" to measurementConsumerDetails)
+        setJson("MeasurementConsumerDetailsJson" to measurementConsumerDetails)
+      }
+    } else if (request.externalDataProviderId != 0L) {
+      CertificateReader(CertificateReader.ParentType.DATA_PROVIDER)
+        .bindWhereClause(
+          ExternalId(request.externalDataProviderId),
+          ExternalId(request.externalCertificateId)
+        )
+        .execute(transactionContext)
+        .singleOrNull()
+        ?: throw KingdomInternalException(ErrorCode.CERTIFICATE_NOT_FOUND)
+
+      val dataProviderResult =
+        DataProviderReader()
+          .readByExternalDataProviderId(
+            transactionContext,
+            ExternalId(request.externalDataProviderId)
+          )
+          ?: throw KingdomInternalException(ErrorCode.DATA_PROVIDER_NOT_FOUND)
+
+      val dataProviderDetails =
+        dataProviderResult.dataProvider.details.copy {
+          apiVersion = request.apiVersion
+          publicKey = request.publicKey
+          publicKeySignature = request.publicKeySignature
+        }
+
+      transactionContext.bufferUpdateMutation("DataProviders") {
+        set("DataProviderId" to dataProviderResult.dataProviderId)
+        set("DataProviderDetails" to dataProviderDetails)
+        setJson("DataProviderDetailsJson" to dataProviderDetails)
+      }
+    }
+  }
+}
---
+++ src/main/kotlin/org/wfanet/measurement/kingdom/service/internal/testing/PublicKeysServiceTest.kt
@@ -0,0 +1,204 @@
+// Copyright 2022 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.kingdom.service.internal.testing
+
+import com.google.common.truth.Truth.assertThat
+import com.google.protobuf.ByteString
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import java.time.Clock
+import kotlin.random.Random
+import kotlin.test.assertFailsWith
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.wfanet.measurement.common.identity.IdGenerator
+import org.wfanet.measurement.common.identity.RandomIdGenerator
+import org.wfanet.measurement.internal.kingdom.AccountsGrpcKt.AccountsCoroutineImplBase
+import org.wfanet.measurement.internal.kingdom.CertificatesGrpcKt.CertificatesCoroutineImplBase
+import org.wfanet.measurement.internal.kingdom.DataProvidersGrpcKt.DataProvidersCoroutineImplBase
+import org.wfanet.measurement.internal.kingdom.MeasurementConsumersGrpcKt.MeasurementConsumersCoroutineImplBase
+import org.wfanet.measurement.internal.kingdom.PublicKeysGrpcKt.PublicKeysCoroutineImplBase
+import org.wfanet.measurement.internal.kingdom.getDataProviderRequest
+import org.wfanet.measurement.internal.kingdom.getMeasurementConsumerRequest
+import org.wfanet.measurement.internal.kingdom.updatePublicKeyRequest
+
+private const val RANDOM_SEED = 1
+private const val API_VERSION = "v2alpha"
+
+private val PUBLIC_KEY = ByteString.copyFromUtf8("public key")
+private val PUBLIC_KEY_SIGNATURE = ByteString.copyFromUtf8("public key signature")
+
+@RunWith(JUnit4::class)
+abstract class PublicKeysServiceTest<T : PublicKeysCoroutineImplBase> {
+
+  protected data class Services<T>(
+    val publicKeysService: T,
+    val measurementConsumersService: MeasurementConsumersCoroutineImplBase,
+    val dataProvidersService: DataProvidersCoroutineImplBase,
+    val certificatesService: CertificatesCoroutineImplBase,
+    val accountsService: AccountsCoroutineImplBase,
+  )
+
+  protected val clock: Clock = Clock.systemUTC()
+  protected val idGenerator = RandomIdGenerator(clock, Random(RANDOM_SEED))
+  private val population = Population(clock, idGenerator)
+
+  private lateinit var publicKeysService: T
+
+  protected lateinit var measurementConsumersService: MeasurementConsumersCoroutineImplBase
+    private set
+
+  protected lateinit var dataProvidersService: DataProvidersCoroutineImplBase
+    private set
+
+  protected lateinit var certificatesService: CertificatesCoroutineImplBase
+    private set
+
+  protected lateinit var accountsService: AccountsCoroutineImplBase
+    private set
+
+  protected abstract fun newServices(idGenerator: IdGenerator): Services<T>
+
+  @Before
+  fun initService() {
+    val services = newServices(idGenerator)
+    publicKeysService = services.publicKeysService
+    measurementConsumersService = services.measurementConsumersService
+    dataProvidersService = services.dataProvidersService
+    certificatesService = services.certificatesService
+    accountsService = services.accountsService
+  }
+
+  @Test
+  fun `updatePublicKey updates the public key info for a data provider`() = runBlocking {
+    val dataProvider = population.createDataProvider(dataProvidersService)
+
+    publicKeysService.updatePublicKey(
+      updatePublicKeyRequest {
+        externalDataProviderId = dataProvider.externalDataProviderId
+        externalCertificateId = dataProvider.certificate.externalCertificateId
+        apiVersion = API_VERSION
+        publicKey = dataProvider.details.publicKey.concat(PUBLIC_KEY)
+        publicKeySignature = dataProvider.details.publicKeySignature.concat(PUBLIC_KEY_SIGNATURE)
+      }
+    )
+
+    val updatedDataProvider =
+      dataProvidersService.getDataProvider(
+        getDataProviderRequest { externalDataProviderId = dataProvider.externalDataProviderId }
+      )
+
+    assertThat(dataProvider.details.publicKey).isNotEqualTo(updatedDataProvider.details.publicKey)
+    assertThat(dataProvider.details.publicKeySignature)
+      .isNotEqualTo(updatedDataProvider.details.publicKeySignature)
+  }
+
+  @Test
+  fun `updatePublicKey updates the public key info for a measurement consumer`() = runBlocking {
+    val measurementConsumer =
+      population.createMeasurementConsumer(measurementConsumersService, accountsService)
+
+    publicKeysService.updatePublicKey(
+      updatePublicKeyRequest {
+        externalMeasurementConsumerId = measurementConsumer.externalMeasurementConsumerId
+        externalCertificateId = measurementConsumer.certificate.externalCertificateId
+        apiVersion = API_VERSION
+        publicKey = measurementConsumer.details.publicKey.concat(PUBLIC_KEY)
+        publicKeySignature =
+          measurementConsumer.details.publicKeySignature.concat(PUBLIC_KEY_SIGNATURE)
+      }
+    )
+
+    val updatedMeasurementConsumer =
+      measurementConsumersService.getMeasurementConsumer(
+        getMeasurementConsumerRequest {
+          externalMeasurementConsumerId = measurementConsumer.externalMeasurementConsumerId
+        }
+      )
+
+    assertThat(measurementConsumer.details.publicKey)
+      .isNotEqualTo(updatedMeasurementConsumer.details.publicKey)
+    assertThat(measurementConsumer.details.publicKeySignature)
+      .isNotEqualTo(updatedMeasurementConsumer.details.publicKeySignature)
+  }
+
+  @Test
+  fun `updatePublicKey throws not found when certificate not found`() = runBlocking {
+    val measurementConsumer =
+      population.createMeasurementConsumer(measurementConsumersService, accountsService)
+
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        publicKeysService.updatePublicKey(
+          updatePublicKeyRequest {
+            externalMeasurementConsumerId = measurementConsumer.externalMeasurementConsumerId
+            externalCertificateId = measurementConsumer.certificate.externalCertificateId + 1L
+            apiVersion = API_VERSION
+            publicKey = measurementConsumer.details.publicKey.concat(PUBLIC_KEY)
+            publicKeySignature =
+              measurementConsumer.details.publicKeySignature.concat(PUBLIC_KEY_SIGNATURE)
+          }
+        )
+      }
+
+    assertThat(exception.status.code).isEqualTo(Status.Code.NOT_FOUND)
+  }
+
+  @Test
+  fun `updatePublicKey throws not found when measurement consumer not found`() = runBlocking {
+    val measurementConsumer =
+      population.createMeasurementConsumer(measurementConsumersService, accountsService)
+
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        publicKeysService.updatePublicKey(
+          updatePublicKeyRequest {
+            externalMeasurementConsumerId = measurementConsumer.externalMeasurementConsumerId + 1L
+            externalCertificateId = measurementConsumer.certificate.externalCertificateId
+            apiVersion = API_VERSION
+            publicKey = measurementConsumer.details.publicKey.concat(PUBLIC_KEY)
+            publicKeySignature =
+              measurementConsumer.details.publicKeySignature.concat(PUBLIC_KEY_SIGNATURE)
+          }
+        )
+      }
+
+    assertThat(exception.status.code).isEqualTo(Status.Code.NOT_FOUND)
+  }
+
+  @Test
+  fun `updatePublicKey throws not found when data provider not found`() = runBlocking {
+    val dataProvider = population.createDataProvider(dataProvidersService)
+
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        publicKeysService.updatePublicKey(
+          updatePublicKeyRequest {
+            externalDataProviderId = dataProvider.externalDataProviderId + 1L
+            externalCertificateId = dataProvider.certificate.externalCertificateId
+            apiVersion = API_VERSION
+            publicKey = dataProvider.details.publicKey.concat(PUBLIC_KEY)
+            publicKeySignature =
+              dataProvider.details.publicKeySignature.concat(PUBLIC_KEY_SIGNATURE)
+          }
+        )
+      }
+
+    assertThat(exception.status.code).isEqualTo(Status.Code.NOT_FOUND)
+  }
+}
