fcb054fdc963e3fc557c7909cfc39ee76e3d8fe4
Xavier Negron <xnegron@google.com>
2022-05-06 00:02:00 +0000

    Add graphviz representation to Exchange protos.

--- src/main/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/ExchangesService.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/ExchangesService.kt
@@ -17,8 +17,10 @@ package org.wfanet.measurement.kingdom.service.api.v2alpha
 import io.grpc.Status
 import java.time.LocalDate
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.toList
 import org.wfanet.measurement.api.v2alpha.Exchange
 import org.wfanet.measurement.api.v2alpha.ExchangeKey
+import org.wfanet.measurement.api.v2alpha.ExchangeWorkflow
 import org.wfanet.measurement.api.v2alpha.ExchangesGrpcKt.ExchangesCoroutineImplBase
 import org.wfanet.measurement.api.v2alpha.GetExchangeRequest
 import org.wfanet.measurement.api.v2alpha.GetExchangeRequest.PartyCase
@@ -30,11 +32,19 @@ import org.wfanet.measurement.common.grpc.failGrpc
 import org.wfanet.measurement.common.grpc.grpcRequireNotNull
 import org.wfanet.measurement.common.identity.apiIdToExternalId
 import org.wfanet.measurement.common.toProtoDate
+import org.wfanet.measurement.internal.kingdom.Exchange as InternalExchange
+import org.wfanet.measurement.internal.kingdom.ExchangeStepsGrpcKt.ExchangeStepsCoroutineStub
 import org.wfanet.measurement.internal.kingdom.ExchangesGrpcKt.ExchangesCoroutineStub
+import org.wfanet.measurement.internal.kingdom.RecurringExchange
+import org.wfanet.measurement.internal.kingdom.StreamExchangeStepsRequestKt.filter
 import org.wfanet.measurement.internal.kingdom.getExchangeRequest
+import org.wfanet.measurement.internal.kingdom.streamExchangeStepsRequest
+import org.wfanet.measurement.tools.createGraphViz
 
-class ExchangesService(private val internalExchanges: ExchangesCoroutineStub) :
-  ExchangesCoroutineImplBase() {
+class ExchangesService(
+  private val internalExchanges: ExchangesCoroutineStub,
+  private val internalExchangeSteps: ExchangeStepsCoroutineStub
+) : ExchangesCoroutineImplBase() {
   override suspend fun getExchange(request: GetExchangeRequest): Exchange {
     val provider = validateRequestProvider(getProvider(request))
 
@@ -47,7 +57,21 @@ class ExchangesService(private val internalExchanges: ExchangesCoroutineStub) :
           this.provider = provider
         }
       )
-    return internalExchange.toV2Alpha("")
+    val exchangeSteps =
+      internalExchangeSteps.streamExchangeSteps(
+        streamExchangeStepsRequest {
+          filter { externalRecurringExchangeIds += apiIdToExternalId(key.recurringExchangeId) }
+        }
+      )
+
+    val externalExchangeWorkflow = getExchangeWorkflow(internalExchange)
+    val graphvizString = createGraphViz(externalExchangeWorkflow, exchangeSteps.toList())
+
+    return try {
+      internalExchange.toV2Alpha(graphvizString)
+    } catch (e: Throwable) {
+      failGrpc(Status.INVALID_ARGUMENT) { e.message ?: "Failed to convert InternalExchange" }
+    }
   }
 
   override suspend fun listExchanges(request: ListExchangesRequest): ListExchangesResponse {
@@ -68,4 +92,11 @@ private fun getProvider(request: GetExchangeRequest): String {
         "Caller identity is neither DataProvider nor ModelProvider"
       }
   }
+}
+
+private fun getExchangeWorkflow(internalExchange: InternalExchange): ExchangeWorkflow {
+  val recurringExchange =
+    RecurringExchange.parser().parseFrom(internalExchange.serializedRecurringExchange)
+  val recurringExchangeDetails = recurringExchange.details
+  return ExchangeWorkflow.parser().parseFrom(recurringExchangeDetails.externalExchangeWorkflow)
 }
