54d2b00cbb0d73f7b800b2e86d845444807b380f
Xavier Negron <xnegron@google.com>
2022-05-10 17:51:36 +0000

    Merge remote-tracking branch 'origin/xnegron-graphviz' into xnegron-graphviz
    # Conflicts:
    #	src/main/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/ExchangesService.kt

--- src/test/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/ExchangesServiceTest.kt
+++ src/test/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/ExchangesServiceTest.kt
@@ -75,21 +75,22 @@ private const val EXCHANGE_ID = "2021-03-14"
 
 private val AUDIT_TRAIL_HASH = ByteString.copyFromUtf8("some arbitrary audit_trail_hash")
 
-private const val GRAPHVIZ_REPRESENTATION = "digraph {\n  splines=\"ortho\"\n  " +
-  "Step1 [color=\"blue\", shape=\"box\", label=\"(step_id: " +
-  "\"Step1\"\nparty: DATA_PROVIDER\ninput_labels {\n  key: \"CopyToSharedStorage\"\n  " +
-  "value: \"Step1\"\n}\ninput_labels {\n  key: \"IntersectAndValidate\"\n  " +
-  "value: \"Step2\"\n}\noutput_labels {\n  key: \"PreprocessEvents\"\n  value: " +
-  "\"Step1\"\n}\n, 0).stepId: null\"]\n  Step1 -> Step1\n  Step1 -> Step1\n  Step2 -> Step1\n  " +
-  "Step2 [color=\"blue\", shape=\"box\", label=\"(step_id: \"Step2\"\nparty: " +
-  "DATA_PROVIDER\ninput_labels {\n  key: \"GenerateLookupKeys\"\n  value: " +
-  "\"Step1\"\n}\noutput_labels {\n  key: \"CopyToSharedStorage\"\n  value: \"Step1\"\n}\n, " +
-  "1).stepId: READY\"]\n  Step2 -> Step1\n  Step1 -> Step2\n  Step1 [color=\"blue\", " +
-  "shape=\"egg\", label=\"Step1\"]\n  Step3 [color=\"red\", shape=\"box\", label=\"(step_id: " +
-  "\"Step3\"\nparty: MODEL_PROVIDER\ninput_labels {\n  key: \"InputStep\"\n  value: " +
-  "\"Step1\"\n}\noutput_labels {\n  key: \"GenerateLookupKeys\"\n  value: \"Step1\"\n}\n, " +
-  "2).stepId: READY\"]\n  Step3 -> Step1\n  Step1 -> Step3\n  Step1 [color=\"red\", " +
-  "shape=\"egg\", label=\"Step1\"]\n}\n"
+private const val GRAPHVIZ_REPRESENTATION =
+  "digraph {\n  splines=\"ortho\"\n  " +
+    "Step1 [color=\"blue\", shape=\"box\", label=\"(step_id: " +
+    "\"Step1\"\nparty: DATA_PROVIDER\ninput_labels {\n  key: \"CopyToSharedStorage\"\n  " +
+    "value: \"Step1\"\n}\ninput_labels {\n  key: \"IntersectAndValidate\"\n  " +
+    "value: \"Step2\"\n}\noutput_labels {\n  key: \"PreprocessEvents\"\n  value: " +
+    "\"Step1\"\n}\n, 0).stepId: null\"]\n  Step1 -> Step1\n  Step1 -> Step1\n  Step2 -> Step1\n  " +
+    "Step2 [color=\"blue\", shape=\"box\", label=\"(step_id: \"Step2\"\nparty: " +
+    "DATA_PROVIDER\ninput_labels {\n  key: \"GenerateLookupKeys\"\n  value: " +
+    "\"Step1\"\n}\noutput_labels {\n  key: \"CopyToSharedStorage\"\n  value: \"Step1\"\n}\n, " +
+    "1).stepId: READY\"]\n  Step2 -> Step1\n  Step1 -> Step2\n  Step1 [color=\"blue\", " +
+    "shape=\"egg\", label=\"Step1\"]\n  Step3 [color=\"red\", shape=\"box\", label=\"(step_id: " +
+    "\"Step3\"\nparty: MODEL_PROVIDER\ninput_labels {\n  key: \"InputStep\"\n  value: " +
+    "\"Step1\"\n}\noutput_labels {\n  key: \"GenerateLookupKeys\"\n  value: \"Step1\"\n}\n, " +
+    "2).stepId: READY\"]\n  Step3 -> Step1\n  Step1 -> Step3\n  Step1 [color=\"red\", " +
+    "shape=\"egg\", label=\"Step1\"]\n}\n"
 
 private val INTERNAL_EXCHANGE = internalExchange {
   externalRecurringExchangeId = RECURRING_EXCHANGE_ID
