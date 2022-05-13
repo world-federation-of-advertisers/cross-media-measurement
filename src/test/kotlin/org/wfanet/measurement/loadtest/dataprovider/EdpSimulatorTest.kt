45b7df9924e4421468a240127fadc7975ef29702
Ugur <uakyol@google.com>
2022-05-13 04:55:00 +0000

    fixed test

--- src/test/kotlin/org/wfanet/measurement/loadtest/dataprovider/EdpSimulatorTest.kt
+++ src/test/kotlin/org/wfanet/measurement/loadtest/dataprovider/EdpSimulatorTest.kt
@@ -347,10 +347,12 @@ class EdpSimulatorTest {
       )
 
       // All the Buckets are only charged once, so all entries should have a repetition count of 1.
-      backingStore.ledger.forEach { assertThat(it.repetitionCount).isEqualTo(1) }
+      backingStore.balanceLedger.values
+        .flatMap { it.values }
+        .forEach { assertThat(it.repetitionCount).isEqualTo(1) }
 
       // The list of all the charged privacy bucket groups should be correct based on the filter.
-      assertThat(backingStore.ledger.map { it.privacyBucketGroup })
+      assertThat(backingStore.balanceLedger.keys)
         .containsExactly(
           PrivacyBucketGroup(
             MC_NAME,
