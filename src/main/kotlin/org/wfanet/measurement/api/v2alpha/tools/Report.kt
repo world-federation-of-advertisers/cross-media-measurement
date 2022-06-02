e03605ca5e934bfb36f1f8f25ac15bdd2105e47d
renjiez <renjiez@google.com>
2022-06-02 17:44:25 +0000

    Add Reporting CLI tool to create and list reporting_set

---
+++ src/main/kotlin/org/wfanet/measurement/api/v2alpha/tools/Report.kt
@@ -0,0 +1,188 @@
+// Copyright 2022 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.api.v2alpha.tools
+
+import io.grpc.ManagedChannel
+import java.time.Duration
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import org.wfanet.measurement.common.commandLineMain
+import org.wfanet.measurement.common.crypto.SigningCerts
+import org.wfanet.measurement.common.grpc.TlsFlags
+import org.wfanet.measurement.common.grpc.buildMutualTlsChannel
+import org.wfanet.measurement.common.grpc.withShutdownTimeout
+import org.wfanet.measurement.reporting.v1alpha.ReportingSetsGrpcKt.ReportingSetsCoroutineStub
+import org.wfanet.measurement.reporting.v1alpha.createReportingSetRequest
+import org.wfanet.measurement.reporting.v1alpha.listReportingSetsRequest
+import org.wfanet.measurement.reporting.v1alpha.reportingSet
+import picocli.CommandLine
+
+private class ReportingApiFlags {
+  @CommandLine.Option(
+    names = ["--reporting-server-api-target"],
+    description = ["gRPC target (authority) of the reporting server's public API"],
+    required = true,
+  )
+  lateinit var apiTarget: String
+    private set
+
+  @CommandLine.Option(
+    names = ["--reporting-server-api-cert-host"],
+    description =
+      [
+        "Expected hostname (DNS-ID) in the reporting server's TLS certificate.",
+        "This overrides derivation of the TLS DNS-ID from --reporting-server-api-target.",
+      ],
+    required = false,
+  )
+  var apiCertHost: String? = null
+    private set
+}
+
+@CommandLine.Command(name = "create-reporting-set", description = ["Creates a reporting set"])
+class CreateReportingSetCommand : Runnable {
+  @CommandLine.ParentCommand private lateinit var parent: Report
+
+  @CommandLine.Option(
+    names = ["--measurement-consumer"],
+    description = ["API resource name of the Measurement Consumer"],
+    required = true,
+  )
+  private lateinit var measurementConsumerName: String
+
+  @CommandLine.Option(
+    names = ["--event-groups"],
+    arity = "1..*",
+    description = ["List of EventGroup's API resource names"],
+    required = true,
+  )
+  private lateinit var eventGroups: List<String>
+
+  @CommandLine.Option(
+    names = ["--filter"],
+    description = ["CEL filter predicate that applies to all `event_groups`"],
+    required = false,
+    defaultValue = ""
+  )
+  private lateinit var filterExpression: String
+
+  @CommandLine.Option(
+    names = ["--display-name"],
+    description = ["Human-readable name for display purposes"],
+    required = false,
+    defaultValue = ""
+  )
+  private lateinit var displayNameInput: String
+
+  override fun run() {
+    val reportingSetStub = ReportingSetsCoroutineStub(parent.channel)
+
+    val reportingSet =
+      runBlocking(Dispatchers.IO) {
+        reportingSetStub.createReportingSet(
+          createReportingSetRequest {
+            parent = measurementConsumerName
+            reportingSet = reportingSet {
+              eventGroups += this@CreateReportingSetCommand.eventGroups
+              if (filterExpression.isNotEmpty()) {
+                filter = filterExpression
+              }
+              if (displayNameInput.isNotEmpty()) displayName = displayNameInput
+            }
+          }
+        )
+      }
+    print(reportingSet)
+  }
+}
+
+@CommandLine.Command(name = "list-reporting-sets", description = ["List reporting sets"])
+class ListReportingSetsCommand : Runnable {
+  @CommandLine.ParentCommand private lateinit var parent: Report
+
+  @CommandLine.Option(
+    names = ["--measurement-consumer"],
+    description = ["API resource name of the Measurement Consumer"],
+    required = true,
+  )
+  private lateinit var measurementConsumerName: String
+
+  override fun run() {
+    val reportingSetStub = ReportingSetsCoroutineStub(parent.channel)
+
+    val response =
+      runBlocking(Dispatchers.IO) {
+        reportingSetStub.listReportingSets(
+          listReportingSetsRequest { parent = measurementConsumerName }
+        )
+      }
+
+    val reportingSets = response.reportingSetsList
+    print(reportingSets)
+  }
+}
+
+@CommandLine.Command(name = "create-report", description = ["Create a set operation report"])
+class CreateReportCommand : Runnable {
+  override fun run() {}
+}
+
+@CommandLine.Command(name = "list-reports", description = ["List set operation reports"])
+class ListReportsCommand : Runnable {
+  override fun run() {}
+}
+
+@CommandLine.Command(name = "get-report", description = ["Get a set operation report"])
+class GetReportCommand : Runnable {
+  override fun run() {}
+}
+
+@CommandLine.Command(
+  name = "SimpleReport",
+  description = ["Simple report from Kingdom"],
+  sortOptions = false,
+  subcommands =
+    [
+      CommandLine.HelpCommand::class,
+      CreateReportingSetCommand::class,
+      ListReportingSetsCommand::class,
+      CreateReportCommand::class,
+      ListReportsCommand::class,
+      GetReportCommand::class,
+    ]
+)
+class Report : Runnable {
+  @CommandLine.Mixin private lateinit var tlsFlags: TlsFlags
+  @CommandLine.Mixin private lateinit var apiFlags: ReportingApiFlags
+
+  val channel: ManagedChannel by lazy {
+    val clientCerts =
+      SigningCerts.fromPemFiles(
+        certificateFile = tlsFlags.certFile,
+        privateKeyFile = tlsFlags.privateKeyFile,
+        trustedCertCollectionFile = tlsFlags.certCollectionFile
+      )
+    buildMutualTlsChannel(apiFlags.apiTarget, clientCerts, apiFlags.apiCertHost)
+      .withShutdownTimeout(Duration.ofSeconds(1))
+  }
+  override fun run() {}
+}
+
+/**
+ * Create, List and Get reporting set or report.
+ *
+ * Use the `help` command to see usage details.
+ */
+fun main(args: Array<String>) = commandLineMain(Report(), args)
---
+++ src/test/kotlin/org/wfanet/measurement/api/v2alpha/tools/ReportTest.kt
@@ -0,0 +1,170 @@
+// Copyright 2022 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.api.v2alpha.tools
+
+import io.grpc.ServerServiceDefinition
+import io.netty.handler.ssl.ClientAuth
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.concurrent.TimeUnit.SECONDS
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.kotlin.any
+import org.wfanet.measurement.common.crypto.SigningCerts
+import org.wfanet.measurement.common.getRuntimePath
+import org.wfanet.measurement.common.grpc.CommonServer
+import org.wfanet.measurement.common.grpc.testing.mockService
+import org.wfanet.measurement.common.testing.verifyProtoArgument
+import org.wfanet.measurement.reporting.v1alpha.ReportingSetsGrpcKt.ReportingSetsCoroutineImplBase
+import org.wfanet.measurement.reporting.v1alpha.createReportingSetRequest
+import org.wfanet.measurement.reporting.v1alpha.listReportingSetsRequest
+import org.wfanet.measurement.reporting.v1alpha.listReportingSetsResponse
+import org.wfanet.measurement.reporting.v1alpha.reportingSet
+import picocli.CommandLine
+
+private const val HOST = "localhost"
+private const val PORT = 15789
+private val SECRETS_DIR: Path =
+  getRuntimePath(
+    Paths.get(
+      "wfa_measurement_system",
+      "src",
+      "main",
+      "k8s",
+      "testing",
+      "secretfiles",
+    )
+  )!!
+
+private const val MEASUREMENT_CONSUMER_NAME = "measurementConsumers/1"
+private const val EVENT_GROUP_NAME_1 = "dataProviders/1/eventGroups/1"
+private const val EVENT_GROUP_NAME_2 = "dataProviders/1/eventGroups/2"
+private const val EVENT_GROUP_NAME_3 = "dataProviders/2/eventGroups/1"
+
+private val REPORTING_SET = reportingSet {}
+private val LIST_REPORTING_SETS_RESPONSE = listReportingSetsResponse {
+  reportingSets += reportingSet {
+    name = "$MEASUREMENT_CONSUMER_NAME/reportingSets/1"
+    eventGroups += listOf(EVENT_GROUP_NAME_1, EVENT_GROUP_NAME_2, EVENT_GROUP_NAME_3)
+    filter = "some.filter1"
+    displayName = "test-reporting-set1"
+  }
+  reportingSets += reportingSet {
+    name = "$MEASUREMENT_CONSUMER_NAME/reportingSets/2"
+    eventGroups += listOf(EVENT_GROUP_NAME_1)
+    filter = "some.filter2"
+    displayName = "test-reporting-set2"
+  }
+}
+
+@RunWith(JUnit4::class)
+class ReportTest {
+  private val reportingSetsServiceMock: ReportingSetsCoroutineImplBase =
+    mockService() {
+      onBlocking { createReportingSet(any()) }.thenReturn(REPORTING_SET)
+      onBlocking { listReportingSets(any()) }.thenReturn(LIST_REPORTING_SETS_RESPONSE)
+    }
+
+  private lateinit var server: CommonServer
+  @Before
+  fun initServer() {
+    val services: List<ServerServiceDefinition> =
+      listOf(
+        reportingSetsServiceMock.bindService(),
+      )
+
+    val serverCerts =
+      SigningCerts.fromPemFiles(
+        certificateFile = SECRETS_DIR.resolve("kingdom_tls.pem").toFile(),
+        privateKeyFile = SECRETS_DIR.resolve("kingdom_tls.key").toFile(),
+        trustedCertCollectionFile = SECRETS_DIR.resolve("mc_root.pem").toFile(),
+      )
+
+    server =
+      CommonServer.fromParameters(
+        PORT,
+        true,
+        serverCerts,
+        ClientAuth.REQUIRE,
+        "kingdom-test",
+        services
+      )
+    server.start()
+  }
+
+  @After
+  fun shutdownServer() {
+    server.server.shutdown()
+    server.server.awaitTermination(1, SECONDS)
+  }
+
+  @Test
+  fun `Create reporting_set call api with valid CreateReportingSetRequest`() {
+    val args =
+      arrayOf(
+        "--tls-cert-file=$SECRETS_DIR/mc_tls.pem",
+        "--tls-key-file=$SECRETS_DIR/mc_tls.key",
+        "--cert-collection-file=$SECRETS_DIR/kingdom_root.pem",
+        "--reporting-server-api-target=$HOST:$PORT",
+        "create-reporting-set",
+        "--measurement-consumer=$MEASUREMENT_CONSUMER_NAME",
+        "--event-groups",
+        "dataProviders/1/eventGroups/1",
+        "dataProviders/1/eventGroups/2",
+        "dataProviders/2/eventGroups/1",
+        "--filter=some.filter",
+        "--display-name=test-reporting-set",
+      )
+
+    CommandLine(Report()).execute(*args)
+
+    verifyProtoArgument(
+        reportingSetsServiceMock,
+        ReportingSetsCoroutineImplBase::createReportingSet
+      )
+      .comparingExpectedFieldsOnly()
+      .isEqualTo(
+        createReportingSetRequest {
+          parent = MEASUREMENT_CONSUMER_NAME
+          reportingSet = reportingSet {
+            eventGroups += listOf(EVENT_GROUP_NAME_1, EVENT_GROUP_NAME_2, EVENT_GROUP_NAME_3)
+            filter = "some.filter"
+            displayName = "test-reporting-set"
+          }
+        }
+      )
+  }
+
+  @Test
+  fun `List reporting_sets call api with valid ListReportingSetRequest`() {
+    val args =
+      arrayOf(
+        "--tls-cert-file=$SECRETS_DIR/mc_tls.pem",
+        "--tls-key-file=$SECRETS_DIR/mc_tls.key",
+        "--cert-collection-file=$SECRETS_DIR/kingdom_root.pem",
+        "--reporting-server-api-target=$HOST:$PORT",
+        "list-reporting-sets",
+        "--measurement-consumer=$MEASUREMENT_CONSUMER_NAME",
+      )
+    CommandLine(Report()).execute(*args)
+
+    verifyProtoArgument(reportingSetsServiceMock, ReportingSetsCoroutineImplBase::listReportingSets)
+      .comparingExpectedFieldsOnly()
+      .isEqualTo(listReportingSetsRequest { parent = MEASUREMENT_CONSUMER_NAME })
+  }
+}
