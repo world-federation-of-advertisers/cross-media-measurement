45c24bc1fce9cc829a7c81dbdd639d8c34707988
Rieman Li <riemanli@google.com>
2022-06-07 21:33:02 +0000

    Overriding createReportingSet function.

--- src/main/kotlin/org/wfanet/measurement/reporting/service/api.v1alpha/ReportingSetsService.kt
+++ src/main/kotlin/org/wfanet/measurement/reporting/service/api.v1alpha/ReportingSetsService.kt
@@ -1,4 +1,37 @@
 package org.wfanet.measurement.reporting.service.api.v1alpha
 
-class ReportingSetsService {
+import org.wfanet.measurement.api.v2alpha.MeasurementConsumerKey
+import org.wfanet.measurement.api.v2alpha.principalFromCurrentContext
+import org.wfanet.measurement.common.grpc.grpcRequire
+import org.wfanet.measurement.common.grpc.grpcRequireNotNull
+import org.wfanet.measurement.internal.reporting.ReportingSetsGrpcKt.ReportingSetsCoroutineStub
+import org.wfanet.measurement.reporting.v1alpha.CreateReportingSetRequest
+import org.wfanet.measurement.reporting.v1alpha.ReportingSet
+import org.wfanet.measurement.reporting.v1alpha.ReportingSetsGrpcKt.ReportingSetsCoroutineImplBase
+
+private const val MIN_PAGE_SIZE = 1
+private const val DEFAULT_PAGE_SIZE = 50
+private const val MAX_PAGE_SIZE = 100
+
+class ReportingSetsService(private val internalReportingSetsStub: ReportingSetsCoroutineStub):
+  ReportingSetsCoroutineImplBase() {
+  override suspend fun createReportingSet(request: CreateReportingSetRequest): ReportingSet {
+    val parentKey = grpcRequireNotNull(MeasurementConsumerKey.fromName(request.parent)) {
+      "Parent is either unspecified or invalid"
+    }
+
+    val principal = principalFromCurrentContext
+
+    grpcRequire(request.parent == principal.resourceKey.toName()) {
+      "Cannot create ReportingSet for another MeasurementConsumer."
+    }
+
+    grpcRequire(request.hasReportingSet()) {
+      ""
+    }
+
+
+
+    return super.createReportingSet(request)
+  }
 }
