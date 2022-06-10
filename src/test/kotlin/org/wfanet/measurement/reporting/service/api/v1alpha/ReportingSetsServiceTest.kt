f4a4cd82e0bc14a43da9cd27526ea4401d683a62
Rieman Li <riemanli@google.com>
2022-06-10 16:24:45 +0000

    Adding unit tests for ReportingSetsService.

---
+++ src/test/kotlin/org/wfanet/measurement/reporting/service/api/v1alpha/ReportingSetsServiceTest.kt
@@ -0,0 +1,9 @@
+package org.wfanet.measurement.reporting.service.api.v1alpha
+
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class ReportingSetsServiceTest {
+
+}
