1b35675bd2f0da297c53626a713a6f8a01e721c2
Rieman Li <riemanli@google.com>
2022-07-28 21:03:46 +0000

    Move TextprotoFilePrincipalLookup.kt to measurement/api/v2alpha.

--- src/main/kotlin/org/wfanet/measurement/duchy/deploy/common/server/RequisitionFulfillmentServer.kt
+++ src/main/kotlin/org/wfanet/measurement/duchy/deploy/common/server/RequisitionFulfillmentServer.kt
@@ -19,7 +19,7 @@ import org.wfanet.measurement.api.v2alpha.withPrincipalsFromX509AuthorityKeyIden
 import org.wfanet.measurement.common.crypto.SigningCerts
 import org.wfanet.measurement.common.grpc.CommonServer
 import org.wfanet.measurement.common.grpc.buildMutualTlsChannel
-import org.wfanet.measurement.common.identity.TextprotoFilePrincipalLookup
+import org.wfanet.measurement.api.v2alpha.TextprotoFilePrincipalLookup
 import org.wfanet.measurement.common.identity.withDuchyId
 import org.wfanet.measurement.duchy.deploy.common.CommonDuchyFlags
 import org.wfanet.measurement.duchy.deploy.common.ComputationsServiceFlags
