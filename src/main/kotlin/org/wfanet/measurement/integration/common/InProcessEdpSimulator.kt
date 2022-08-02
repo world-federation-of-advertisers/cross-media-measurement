4ee7440faf919fc8292eb0f83baf097c3ee49023
Joe Corilla <jcorilla@google.com>
2022-08-02 19:20:33 +0000

    Update copyright comments and rollback randomeventquery change

--- src/main/kotlin/org/wfanet/measurement/integration/common/InProcessEdpSimulator.kt
+++ src/main/kotlin/org/wfanet/measurement/integration/common/InProcessEdpSimulator.kt
@@ -26,14 +26,6 @@ import org.wfanet.measurement.api.v2alpha.CertificatesGrpcKt.CertificatesCorouti
 import org.wfanet.measurement.api.v2alpha.EventGroupsGrpcKt.EventGroupsCoroutineStub
 import org.wfanet.measurement.api.v2alpha.RequisitionFulfillmentGrpcKt.RequisitionFulfillmentCoroutineStub
 import org.wfanet.measurement.api.v2alpha.RequisitionsGrpcKt.RequisitionsCoroutineStub
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestEvent
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestPrivacyBudgetTemplate
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestPrivacyBudgetTemplateKt.gender
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestVideoTemplate
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestVideoTemplateKt.ageRange
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.testEvent
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.testPrivacyBudgetTemplate
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.testVideoTemplate
 import org.wfanet.measurement.common.identity.withPrincipalName
 import org.wfanet.measurement.common.throttler.MinimumIntervalThrottler
 import org.wfanet.measurement.eventdataprovider.privacybudgetmanagement.InMemoryBackingStore
@@ -42,7 +34,8 @@ import org.wfanet.measurement.eventdataprovider.privacybudgetmanagement.PrivacyB
 import org.wfanet.measurement.eventdataprovider.privacybudgetmanagement.testing.TestPrivacyBucketMapper
 import org.wfanet.measurement.loadtest.dataprovider.EdpData
 import org.wfanet.measurement.loadtest.dataprovider.EdpSimulator
-import org.wfanet.measurement.loadtest.dataprovider.FilterEventQuery
+import org.wfanet.measurement.loadtest.dataprovider.RandomEventQuery
+import org.wfanet.measurement.loadtest.dataprovider.SketchGenerationParams
 import org.wfanet.measurement.loadtest.storage.SketchStore
 import org.wfanet.measurement.storage.StorageClient
 
@@ -71,28 +64,6 @@ class InProcessEdpSimulator(
     edpJob =
       backgroundScope.launch {
         val edpData = createEdpData(displayName, edpName)
-        // TestEvent defined to match default filter for simulators set in Configs.kt
-        val defaultTestEvent = testEvent {
-          this.privacyBudget = testPrivacyBudgetTemplate {
-            gender = gender { value = TestPrivacyBudgetTemplate.Gender.Value.GENDER_FEMALE }
-          }
-          this.videoAd = testVideoTemplate {
-            age = ageRange { value = TestVideoTemplate.AgeRange.Value.AGE_18_TO_24 }
-          }
-        }
-
-        val firstExposureEvents = (1..1000).associateWith { defaultTestEvent }
-        val secondExposureEvents = (1..100).associateWith { defaultTestEvent }
-        val thirdExposureEvents = (1..1).associateWith { defaultTestEvent }
-
-        val events: Map<Int, List<TestEvent>> =
-          (firstExposureEvents.keys.asSequence()).associateWith {
-            listOfNotNull(
-              firstExposureEvents[it],
-              secondExposureEvents[it],
-              thirdExposureEvents[it]
-            )
-          }
 
         EdpSimulator(
             edpData = edpData,
@@ -102,7 +73,8 @@ class InProcessEdpSimulator(
             requisitionsStub = requisitionsClient.withPrincipalName(edpData.name),
             requisitionFulfillmentStub = requisitionFulfillmentClient,
             sketchStore = SketchStore(storageClient),
-            eventQuery = FilterEventQuery(events),
+            eventQuery =
+              RandomEventQuery(SketchGenerationParams(reach = 1000, universeSize = 10_000)),
             throttler = MinimumIntervalThrottler(Clock.systemUTC(), Duration.ofMillis(1000)),
             eventTemplateNames = eventTemplateNames,
             PrivacyBudgetManager(
