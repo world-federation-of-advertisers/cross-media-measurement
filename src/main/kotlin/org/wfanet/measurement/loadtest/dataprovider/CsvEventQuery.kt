a2b428d7688aa4b77d64f1ab7eb7b4e4344fb74c
alberthsuu <alberthsuu@google.com>
2022-08-15 19:54:20 +0000

    Merge event template and event filter code. Working but need to fix privacyBudget event template

--- src/main/kotlin/org/wfanet/measurement/loadtest/dataprovider/CsvEventQuery.kt
+++ src/main/kotlin/org/wfanet/measurement/loadtest/dataprovider/CsvEventQuery.kt
@@ -14,20 +14,39 @@
 
 package org.wfanet.measurement.loadtest.dataprovider
 
+import com.google.protobuf.Message
 import com.opencsv.CSVReaderBuilder
 import java.io.FileReader
 import java.nio.file.Paths
 import java.util.logging.Logger
 import org.wfanet.measurement.api.v2alpha.RequisitionSpec.EventFilter
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestBannerTemplate.Gender as BannerGender
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestBannerTemplateKt.gender as bannerGender
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestEvent
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestPrivacyBudgetTemplate.AgeRange as PrivacyAgeRange
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestPrivacyBudgetTemplate.Gender as PrivacyGender
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestPrivacyBudgetTemplateKt.ageRange as privacyAgeRange
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestPrivacyBudgetTemplateKt.gender as privacyGender
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestVideoTemplate.AgeRange as VideoAgeRange
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestVideoTemplateKt.ageRange as videoAgeRange
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.testBannerTemplate
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.testEvent
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.testPrivacyBudgetTemplate
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.testVideoTemplate
 import org.wfanet.measurement.common.getRuntimePath
+import org.wfanet.measurement.eventdataprovider.eventfiltration.EventFilters
 
 /** Fulfill the query with VIDs imported from CSV file. */
 class CsvEventQuery(
   private val edpDisplayName: String,
 ) : EventQuery() {
   private val edpIdIndex = 0
+  private val sexIndex = 2
+  private val ageGroupIndex = 3
   private val vidIndex = 7
-  private var allFilteredByEdpVids: MutableList<Long> = mutableListOf()
+
+  private val vidsList: MutableList<Int> = mutableListOf()
+  private val eventsList: MutableList<TestEvent> = mutableListOf()
 
   /** Import VIDs from CSV file. */
   init {
@@ -46,7 +65,7 @@ class CsvEventQuery(
         "data",
       )
     // Update fileName to match the CSV file you want to use
-    val fileName = "synthetic-labelled-events-small.csv"
+    val fileName = "synthetic-labelled-events.csv"
     // val fileName = "benchmark_data_large.csv"
     val fileRuntimePath = getRuntimePath(directoryPath.resolve(fileName)).toString()
     logger.info("Reading data from CSV file...")
@@ -58,7 +77,9 @@ class CsvEventQuery(
         var row = reader.readNext()
         while (row != null) {
           if (row[edpIdIndex] == edpDisplayName.last().toString()) {
-            allFilteredByEdpVids.add(row[vidIndex].toLong())
+            val csvEventMap = mapOf("Sex" to row[sexIndex], "Age_Group" to row[ageGroupIndex])
+            vidsList.add(row[vidIndex].toInt())
+            eventsList.add(csvEntryToTestEvent(csvEventMap))
           }
 
           row = reader.readNext()
@@ -68,54 +89,9 @@ class CsvEventQuery(
     logger.info("Finished reading data from CSV file")
   }
 
-  override fun getUserVirtualIds(eventFilter: EventFilter): Sequence<Long> {
-    logger.info("Querying VIDs from CsvEventQuery...")
-
-    return allFilteredByEdpVids.asSequence()
-  }
-
-  companion object {
-    private val logger: Logger = Logger.getLogger(this::class.java.name)
-/**
- * Copyright 2022 The Cross-Media Measurement Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- * ```
- *      http://www.apache.org/licenses/LICENSE-2.0
- * ```
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
-package org.wfanet.measurement.loadtest.dataprovider
-
-import com.google.protobuf.Message
-import org.wfanet.measurement.api.v2alpha.RequisitionSpec.EventFilter
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestBannerTemplate.Gender as BannerGender
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestBannerTemplateKt.gender as bannerGender
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestEvent
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestPrivacyBudgetTemplate.AgeRange as PrivacyAgeRange
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestPrivacyBudgetTemplate.Gender as PrivacyGender
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestPrivacyBudgetTemplateKt.ageRange as privacyAgeRange
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestPrivacyBudgetTemplateKt.gender as privacyGender
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestVideoTemplate.AgeRange as VideoAgeRange
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestVideoTemplateKt.ageRange as videoAgeRange
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.testBannerTemplate
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.testEvent
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.testPrivacyBudgetTemplate
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.testVideoTemplate
-import org.wfanet.measurement.eventdataprovider.eventfiltration.EventFilters
-
-/** Fulfill the query with data pulled in from csv source. */
-class CsvEventQuery : EventQuery() {
-
-  private val eventsList: MutableList<TestEvent> = mutableListOf()
-  private val vidsList: MutableList<Int> = mutableListOf()
-
   /** Generates Ids by applying filter on events */
   override fun getUserVirtualIds(eventFilter: EventFilter): Sequence<Long> {
+    logger.info("Querying and filtering VIDs from CsvEventQuery...")
     val program =
       EventFilters.compileProgram(
         eventFilter.expression,
@@ -132,7 +108,6 @@ class CsvEventQuery : EventQuery() {
   }
 
   private fun csvEntryToTestEvent(event: Map<String, Any>): TestEvent {
-
     return testEvent {
       this.privacyBudget = testPrivacyBudgetTemplate {
         when (event["Sex"]) {
@@ -169,5 +144,9 @@ class CsvEventQuery : EventQuery() {
       this.vidsList.add(it["VID"] as Int)
       this.eventsList.add(csvEntryToTestEvent(it))
     }
+  }
+
+  companion object {
+    private val logger: Logger = Logger.getLogger(this::class.java.name)
   }
 }
