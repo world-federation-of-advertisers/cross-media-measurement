72169334fbdcedb397447eeac2ec69cece335ce0
Sandip Samantaray <10330291+sandip80@users.noreply.github.com>
2022-08-17 15:24:42 -0700

    Add AWS CA (world-federation-of-advertisers/panel-exchange-client#352)
    * Add AWS CA
    
    Co-authored-by: Sandip Samantaray <sandip.samantaray@bytedance.com>

---
+++ src/main/kotlin/org/wfanet/panelmatch/common/certificates/aws/CertificateAuthority.kt
@@ -0,0 +1,118 @@
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.panelmatch.common.certificates.aws
+
+import java.security.KeyPair
+import java.security.PrivateKey
+import java.security.cert.X509Certificate
+import org.wfanet.measurement.common.crypto.generateKeyPair
+import org.wfanet.measurement.common.crypto.readCertificate
+import org.wfanet.panelmatch.common.certificates.CertificateAuthority
+import org.wfanet.panelmatch.common.certificates.generateCsrFromPrivateKey
+import software.amazon.awssdk.core.SdkBytes
+import software.amazon.awssdk.services.acmpca.model.ASN1Subject
+import software.amazon.awssdk.services.acmpca.model.ApiPassthrough
+import software.amazon.awssdk.services.acmpca.model.ExtendedKeyUsage
+import software.amazon.awssdk.services.acmpca.model.ExtendedKeyUsageType
+import software.amazon.awssdk.services.acmpca.model.Extensions
+import software.amazon.awssdk.services.acmpca.model.GeneralName
+import software.amazon.awssdk.services.acmpca.model.GetCertificateRequest
+import software.amazon.awssdk.services.acmpca.model.IssueCertificateRequest
+import software.amazon.awssdk.services.acmpca.model.KeyUsage
+import software.amazon.awssdk.services.acmpca.model.SigningAlgorithm
+import software.amazon.awssdk.services.acmpca.model.Validity
+
+/**
+ * Defines the template ARN used by AWS private CA to issue certificate.
+ *
+ * More information:
+ * https://docs.aws.amazon.com/acm-pca/latest/userguide/UsingTemplates.html#BlankSubordinateCACertificate_PathLen0_APIPassthrough
+ */
+const val AWS_CERTIFICATE_TEMPLATE_ARN =
+  "arn:aws:acm-pca:::template/BlankSubordinateCACertificate_PathLen0_APIPassthrough/V1"
+
+val AWS_CERTIFICATE_SIGNING_ALGORITHM = SigningAlgorithm.SHA256_WITHECDSA
+
+class CertificateAuthority(
+  private val context: CertificateAuthority.Context,
+  private val certificateAuthorityArn: String,
+  private val client: CreateCertificateClient,
+  private val generateKeyPair: () -> KeyPair = { generateKeyPair("EC") }
+) : CertificateAuthority {
+
+  private val certificateParams =
+    ApiPassthrough.builder()
+      .extensions(
+        Extensions.builder()
+          .keyUsage(
+            KeyUsage.builder()
+              .digitalSignature(true)
+              .nonRepudiation(true)
+              .keyEncipherment(true)
+              .build()
+          )
+          .extendedKeyUsage(
+            ExtendedKeyUsage.builder()
+              .extendedKeyUsageType(ExtendedKeyUsageType.SERVER_AUTH)
+              .build()
+          )
+          .subjectAlternativeNames(GeneralName.builder().dnsName(context.dnsName).build())
+          .build()
+      )
+      .subject(
+        ASN1Subject.builder()
+          .commonName(context.commonName)
+          .organization(context.organization)
+          .build()
+      )
+      .build()
+
+  private val certificateLifetime =
+    Validity.builder().value(context.validDays.toLong()).type("DAYS").build()
+
+  override suspend fun generateX509CertificateAndPrivateKey(): Pair<X509Certificate, PrivateKey> {
+    val keyPair: KeyPair = generateKeyPair()
+    val privateKey: PrivateKey = keyPair.private
+
+    val issueRequest =
+      IssueCertificateRequest.builder()
+        .templateArn(AWS_CERTIFICATE_TEMPLATE_ARN)
+        .apiPassthrough(certificateParams)
+        .certificateAuthorityArn(certificateAuthorityArn)
+        .csr(
+          SdkBytes.fromByteArray(
+            generateCsrFromPrivateKey(privateKey, context.commonName, context.organization)
+              .toByteArray()
+          )
+        )
+        .signingAlgorithm(AWS_CERTIFICATE_SIGNING_ALGORITHM)
+        .validity(certificateLifetime)
+        .build()
+
+    val issueResponse = client.issueCertificate(issueRequest)
+
+    val certificateArn = issueResponse.certificateArn()
+
+    val getRequest =
+      GetCertificateRequest.builder()
+        .certificateArn(certificateArn)
+        .certificateAuthorityArn(certificateAuthorityArn)
+        .build()
+
+    val getResponse = client.getCertificate(getRequest)
+
+    return readCertificate(getResponse.certificate().byteInputStream()) to privateKey
+  }
+}
---
+++ src/test/kotlin/org/wfanet/panelmatch/common/certificates/aws/CertificateAuthorityTest.kt
@@ -0,0 +1,140 @@
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.panelmatch.common.certificates.aws
+
+import com.google.common.truth.Truth
+import com.google.protobuf.kotlin.toByteStringUtf8
+import java.security.KeyPair
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.kotlin.any
+import org.mockito.kotlin.argumentCaptor
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import org.wfanet.measurement.common.crypto.readCertificate
+import org.wfanet.measurement.common.crypto.readPrivateKey
+import org.wfanet.measurement.common.crypto.sign
+import org.wfanet.measurement.common.crypto.testing.FIXED_CA_CERT_PEM_FILE
+import org.wfanet.measurement.common.crypto.verifySignature
+import org.wfanet.panelmatch.common.certificates.CertificateAuthority
+import software.amazon.awssdk.services.acmpca.model.ASN1Subject
+import software.amazon.awssdk.services.acmpca.model.ApiPassthrough
+import software.amazon.awssdk.services.acmpca.model.ExtendedKeyUsage
+import software.amazon.awssdk.services.acmpca.model.ExtendedKeyUsageType
+import software.amazon.awssdk.services.acmpca.model.Extensions
+import software.amazon.awssdk.services.acmpca.model.GeneralName
+import software.amazon.awssdk.services.acmpca.model.GetCertificateRequest
+import software.amazon.awssdk.services.acmpca.model.GetCertificateResponse
+import software.amazon.awssdk.services.acmpca.model.IssueCertificateRequest
+import software.amazon.awssdk.services.acmpca.model.IssueCertificateResponse
+import software.amazon.awssdk.services.acmpca.model.KeyUsage
+import software.amazon.awssdk.services.acmpca.model.Validity
+
+private val CONTEXT =
+  CertificateAuthority.Context(
+    commonName = "some-common-name",
+    organization = "some-org-name",
+    dnsName = "some-domain-name",
+    validDays = 5,
+  )
+
+private const val CERTIFICATE_AUTHORITY_ARN = "some-ca-arn"
+private const val CERTIFICATE_ARN = "some-cert-arn"
+private val ROOT_X509 by lazy { readCertificate(FIXED_CA_CERT_PEM_FILE) }
+private val ROOT_PUBLIC_KEY by lazy { ROOT_X509.publicKey }
+private val ROOT_PRIVATE_KEY_FILE by lazy { FIXED_CA_CERT_PEM_FILE.resolveSibling("ca.key") }
+private val CERTIFICATE_LIFETIME =
+  Validity.builder().value(CONTEXT.validDays.toLong()).type("DAYS").build()
+
+@RunWith(JUnit4::class)
+class CertificateAuthorityTest {
+  @Test
+  fun generateX509CertificateAndPrivateKey() = runBlocking {
+    val mockCreateCertificateClient: CreateCertificateClient = mock<CreateCertificateClient>()
+
+    val expectedCertificateParams =
+      ApiPassthrough.builder()
+        .extensions(
+          Extensions.builder()
+            .keyUsage(
+              KeyUsage.builder()
+                .digitalSignature(true)
+                .nonRepudiation(true)
+                .keyEncipherment(true)
+                .build()
+            )
+            .extendedKeyUsage(
+              ExtendedKeyUsage.builder()
+                .extendedKeyUsageType(ExtendedKeyUsageType.SERVER_AUTH)
+                .build()
+            )
+            .subjectAlternativeNames(GeneralName.builder().dnsName(CONTEXT.dnsName).build())
+            .build()
+        )
+        .subject(
+          ASN1Subject.builder()
+            .commonName(CONTEXT.commonName)
+            .organization(CONTEXT.organization)
+            .build()
+        )
+        .build()
+
+    val expectedGetCertificateRequest =
+      GetCertificateRequest.builder()
+        .certificateArn(CERTIFICATE_ARN)
+        .certificateAuthorityArn(CERTIFICATE_AUTHORITY_ARN)
+        .build()
+
+    whenever(mockCreateCertificateClient.issueCertificate(any()))
+      .thenReturn(IssueCertificateResponse.builder().certificateArn(CERTIFICATE_ARN).build())
+
+    whenever(mockCreateCertificateClient.getCertificate(any()))
+      .thenReturn(
+        GetCertificateResponse.builder().certificate(FIXED_CA_CERT_PEM_FILE.readText()).build()
+      )
+
+    val certificateAuthority =
+      CertificateAuthority(
+        CONTEXT,
+        CERTIFICATE_AUTHORITY_ARN,
+        mockCreateCertificateClient,
+        generateKeyPair = { KeyPair(ROOT_PUBLIC_KEY, readPrivateKey(ROOT_PRIVATE_KEY_FILE, "ec")) }
+      )
+
+    val (x509, privateKey) = certificateAuthority.generateX509CertificateAndPrivateKey()
+
+    argumentCaptor<IssueCertificateRequest> {
+      verify(mockCreateCertificateClient).issueCertificate(capture())
+      Truth.assertThat(firstValue.templateArn()).isEqualTo(AWS_CERTIFICATE_TEMPLATE_ARN)
+      Truth.assertThat(firstValue.apiPassthrough()).isEqualTo(expectedCertificateParams)
+      Truth.assertThat(firstValue.certificateAuthorityArn()).isEqualTo(CERTIFICATE_AUTHORITY_ARN)
+      Truth.assertThat(firstValue.signingAlgorithm()).isEqualTo(AWS_CERTIFICATE_SIGNING_ALGORITHM)
+      Truth.assertThat(firstValue.validity()).isEqualTo(CERTIFICATE_LIFETIME)
+    }
+
+    argumentCaptor<GetCertificateRequest> {
+      verify(mockCreateCertificateClient).getCertificate(capture())
+      Truth.assertThat(firstValue).isEqualTo(expectedGetCertificateRequest)
+    }
+
+    val data = "some-data-to-be-signed".toByteStringUtf8()
+    val signature = privateKey.sign(x509, data)
+
+    Truth.assertThat(x509.verifySignature(data, signature)).isTrue()
+  }
+}
