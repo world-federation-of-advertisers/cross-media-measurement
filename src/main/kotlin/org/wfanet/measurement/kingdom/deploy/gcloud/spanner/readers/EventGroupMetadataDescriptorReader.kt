996247632e64febce6a42a4a4d07b29fb20acd7a
Sanjay Vasandani <sanjayvas@google.com>
2022-12-12 12:40:15 -0800

    Support request_id field in CreateEventGroupMetadataDescriptorRequest.
    This allows for idempotent create operations.

--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/readers/EventGroupMetadataDescriptorReader.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/readers/EventGroupMetadataDescriptorReader.kt
@@ -23,19 +23,7 @@ import org.wfanet.measurement.gcloud.spanner.appendClause
 import org.wfanet.measurement.gcloud.spanner.bind
 import org.wfanet.measurement.gcloud.spanner.getProtoMessage
 import org.wfanet.measurement.internal.kingdom.EventGroupMetadataDescriptor
-
-private val BASE_SQL =
-  """
-    SELECT
-      EventGroupMetadataDescriptors.DataProviderId,
-      EventGroupMetadataDescriptors.EventGroupMetadataDescriptorId,
-      EventGroupMetadataDescriptors.ExternalEventGroupMetadataDescriptorId,
-      EventGroupMetadataDescriptors.DescriptorDetails,
-      DataProviders.ExternalDataProviderId
-    FROM EventGroupMetadataDescriptors
-    JOIN DataProviders USING (DataProviderId)
-    """
-    .trimIndent()
+import org.wfanet.measurement.internal.kingdom.eventGroupMetadataDescriptor
 
 class EventGroupMetadataDescriptorReader :
   BaseSpannerReader<EventGroupMetadataDescriptorReader.Result>() {
@@ -74,47 +62,88 @@ class EventGroupMetadataDescriptorReader :
     readContext: AsyncDatabaseClient.ReadContext,
     externalDataProviderId: Long,
     externalDescriptorId: Long,
-  ): EventGroupMetadataDescriptorReader.Result? {
-    val externalDataProviderIdParam = "externalDataProviderId"
-    val externalDescriptorIdParam = "externalEventGroupMetadataDescriptorId"
-
+  ): Result? {
     return fillStatementBuilder {
         appendClause(
           """
           WHERE
-            ExternalDataProviderId = @$externalDataProviderIdParam
-            AND ExternalEventGroupMetadataDescriptorId = @$externalDescriptorIdParam
+            ExternalDataProviderId = @$EXTERNAL_DATA_PROVIDER_ID_PARAM
+            AND ExternalEventGroupMetadataDescriptorId = @$EXTERNAL_EVENT_GROUP_METADATA_DESCRIPTOR_ID_PARAM
           """
             .trimIndent()
         )
-        bind(externalDataProviderIdParam to externalDataProviderId)
-        bind(externalDescriptorIdParam to externalDescriptorId)
+        bind(EXTERNAL_DATA_PROVIDER_ID_PARAM to externalDataProviderId)
+        bind(EXTERNAL_EVENT_GROUP_METADATA_DESCRIPTOR_ID_PARAM to externalDescriptorId)
         appendClause("LIMIT 1")
       }
       .execute(readContext)
       .singleOrNull()
   }
 
-  override suspend fun translate(struct: Struct): EventGroupMetadataDescriptorReader.Result =
-    EventGroupMetadataDescriptorReader.Result(
+  suspend fun readByIdempotencyKey(
+    readContext: AsyncDatabaseClient.ReadContext,
+    dataProviderId: InternalId,
+    idempotencyKey: String
+  ): Result? {
+    return fillStatementBuilder {
+        appendClause(
+          """
+          WHERE
+            DataProviderId = @$DATA_PROVIDER_ID_PARAM
+            AND IdempotencyKey = @$IDEMPOTENCY_KEY_PARAM
+          """
+            .trimIndent()
+        )
+        bind(DATA_PROVIDER_ID_PARAM to dataProviderId)
+        bind(IDEMPOTENCY_KEY_PARAM to idempotencyKey)
+
+        appendClause("LIMIT 1")
+      }
+      .execute(readContext)
+      .singleOrNull()
+  }
+
+  override suspend fun translate(struct: Struct): Result =
+    Result(
       buildEventGroupMetadataDescriptor(struct),
       InternalId(struct.getLong("EventGroupMetadataDescriptorId")),
       InternalId(struct.getLong("DataProviderId"))
     )
 
-  private fun buildEventGroupMetadataDescriptor(struct: Struct): EventGroupMetadataDescriptor =
-    EventGroupMetadataDescriptor.newBuilder()
-      .apply {
-        externalDataProviderId = struct.getLong("ExternalDataProviderId")
-        externalEventGroupMetadataDescriptorId =
-          struct.getLong("ExternalEventGroupMetadataDescriptorId")
-        if (!struct.isNull("DescriptorDetails")) {
-          details =
-            struct.getProtoMessage(
-              "DescriptorDetails",
-              EventGroupMetadataDescriptor.Details.parser()
-            )
-        }
+  private fun buildEventGroupMetadataDescriptor(struct: Struct): EventGroupMetadataDescriptor {
+    return eventGroupMetadataDescriptor {
+      externalDataProviderId = struct.getLong("ExternalDataProviderId")
+      externalEventGroupMetadataDescriptorId =
+        struct.getLong("ExternalEventGroupMetadataDescriptorId")
+      if (!struct.isNull("IdempotencyKey")) {
+        idempotencyKey = struct.getString("IdempotencyKey")
       }
-      .build()
+      if (!struct.isNull("DescriptorDetails")) {
+        details =
+          struct.getProtoMessage("DescriptorDetails", EventGroupMetadataDescriptor.Details.parser())
+      }
+    }
+  }
+
+  companion object {
+    private const val IDEMPOTENCY_KEY_PARAM = "idempotencyKey"
+    private const val DATA_PROVIDER_ID_PARAM = "dataProviderId"
+    private const val EXTERNAL_DATA_PROVIDER_ID_PARAM = "externalDataProviderId"
+    private const val EXTERNAL_EVENT_GROUP_METADATA_DESCRIPTOR_ID_PARAM =
+      "externalEventGroupMetadataDescriptorId"
+
+    private val BASE_SQL =
+      """
+      SELECT
+        EventGroupMetadataDescriptors.DataProviderId,
+        EventGroupMetadataDescriptors.EventGroupMetadataDescriptorId,
+        EventGroupMetadataDescriptors.ExternalEventGroupMetadataDescriptorId,
+        EventGroupMetadataDescriptors.IdempotencyKey,
+        EventGroupMetadataDescriptors.DescriptorDetails,
+        DataProviders.ExternalDataProviderId
+      FROM EventGroupMetadataDescriptors
+      JOIN DataProviders USING (DataProviderId)
+      """
+        .trimIndent()
+  }
 }
