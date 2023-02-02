44f7344d31a474505f70ae32cf24a95b2bab7f1e
Rieman Li <riemanli@google.com>
2023-02-02 20:35:16 +0000

    Add details to MetricCalculationMetrics to store serialized groups of cross grouping predicates.

--- src/main/resources/reporting/postgres/create-v2alpha-reporting-schema.sql
+++ src/main/resources/reporting/postgres/create-v2alpha-reporting-schema.sql
@@ -416,6 +416,11 @@ CREATE TABLE MetricCalculationMetrics (
   MeasurementConsumerId bigint NOT NULL,
   MetricCalculationId bigint NOT NULL,
   MetricId bigint NOT NULL,
+
+  -- Serialized org.wfanet.measurement.internal.reporting.Report.MetricCalculation.Details
+  -- protobuf message.
+  MetricCalculationMetricsDetails bytea NOT NULL,
+
   PRIMARY KEY(MeasurementConsumerId, MetricCalculationId, MetricId),
   FOREIGN KEY(MeasurementConsumerId)
     REFERENCES MeasurementConsumers(MeasurementConsumerId),
