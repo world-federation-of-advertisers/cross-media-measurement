5158b80da44a1f1745a2a82b4a7451c6a1fe55be
Rieman Li <riemanli@google.com>
2023-02-09 21:41:36 +0000

    Add dependencies to metrics service.

--- src/main/kotlin/org/wfanet/measurement/reporting/service/api/v2alpha/MetricsService.kt
+++ src/main/kotlin/org/wfanet/measurement/reporting/service/api/v2alpha/MetricsService.kt
@@ -14,5 +14,171 @@
 
 package org.wfanet.measurement.reporting.service.api.v2alpha
 
-class MetricsService {
+import com.google.protobuf.ByteString
+import com.google.protobuf.Duration
+import com.google.protobuf.duration
+import com.google.protobuf.util.Durations
+import io.grpc.Status
+import io.grpc.StatusException
+import java.security.PrivateKey
+import java.security.SecureRandom
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import org.wfanet.measurement.api.v2alpha.CertificatesGrpcKt.CertificatesCoroutineStub
+import org.wfanet.measurement.api.v2alpha.CreateMeasurementRequest
+import org.wfanet.measurement.api.v2alpha.DataProviderKey
+import org.wfanet.measurement.api.v2alpha.DataProvidersGrpcKt.DataProvidersCoroutineStub
+import org.wfanet.measurement.api.v2alpha.EncryptionPublicKey
+import org.wfanet.measurement.api.v2alpha.Measurement
+import org.wfanet.measurement.api.v2alpha.Measurement.DataProviderEntry
+import org.wfanet.measurement.api.v2alpha.MeasurementConsumer
+import org.wfanet.measurement.api.v2alpha.MeasurementConsumerKey
+import org.wfanet.measurement.api.v2alpha.MeasurementKey
+import org.wfanet.measurement.api.v2alpha.MeasurementKt.DataProviderEntryKt.value as dataProviderEntryValue
+import org.wfanet.measurement.api.v2alpha.MeasurementKt.dataProviderEntry
+import org.wfanet.measurement.api.v2alpha.MeasurementSpec
+import org.wfanet.measurement.api.v2alpha.MeasurementSpec.VidSamplingInterval
+import org.wfanet.measurement.api.v2alpha.MeasurementSpecKt
+import org.wfanet.measurement.api.v2alpha.MeasurementSpecKt.vidSamplingInterval
+import org.wfanet.measurement.api.v2alpha.MeasurementsGrpcKt.MeasurementsCoroutineStub
+import org.wfanet.measurement.api.v2alpha.RequisitionSpec.EventGroupEntry
+import org.wfanet.measurement.api.v2alpha.RequisitionSpecKt
+import org.wfanet.measurement.api.v2alpha.RequisitionSpecKt.eventGroupEntry
+import org.wfanet.measurement.api.v2alpha.TimeInterval as MeasurementTimeInterval
+import org.wfanet.measurement.api.v2alpha.createMeasurementRequest
+import org.wfanet.measurement.api.v2alpha.differentialPrivacyParams
+import org.wfanet.measurement.api.v2alpha.getCertificateRequest
+import org.wfanet.measurement.api.v2alpha.getDataProviderRequest
+import org.wfanet.measurement.api.v2alpha.getMeasurementRequest
+import org.wfanet.measurement.api.v2alpha.measurement
+import org.wfanet.measurement.api.v2alpha.measurementSpec
+import org.wfanet.measurement.api.v2alpha.requisitionSpec
+import org.wfanet.measurement.api.v2alpha.timeInterval as measurementTimeInterval
+import org.wfanet.measurement.api.withAuthenticationKey
+import org.wfanet.measurement.common.crypto.PrivateKeyHandle
+import org.wfanet.measurement.common.crypto.SigningKeyHandle
+import org.wfanet.measurement.common.crypto.hashSha256
+import org.wfanet.measurement.common.crypto.readCertificate
+import org.wfanet.measurement.common.grpc.failGrpc
+import org.wfanet.measurement.common.grpc.grpcRequireNotNull
+import org.wfanet.measurement.common.identity.apiIdToExternalId
+import org.wfanet.measurement.consent.client.measurementconsumer.decryptResult
+import org.wfanet.measurement.consent.client.measurementconsumer.encryptRequisitionSpec
+import org.wfanet.measurement.consent.client.measurementconsumer.signMeasurementSpec
+import org.wfanet.measurement.consent.client.measurementconsumer.signRequisitionSpec
+import org.wfanet.measurement.consent.client.measurementconsumer.verifyResult
+import org.wfanet.measurement.internal.reporting.Measurement as InternalMeasurement
+import org.wfanet.measurement.internal.reporting.Measurement.Result as InternalMeasurementResult
+import org.wfanet.measurement.internal.reporting.MeasurementKt.ResultKt.frequency as internalFrequency
+import org.wfanet.measurement.internal.reporting.MeasurementKt.ResultKt.impression as internalImpression
+import org.wfanet.measurement.internal.reporting.MeasurementKt.ResultKt.reach as internalReach
+import org.wfanet.measurement.internal.reporting.MeasurementKt.ResultKt.watchDuration as internalWatchDuration
+import org.wfanet.measurement.internal.reporting.MeasurementKt.failure as internalFailure
+import org.wfanet.measurement.internal.reporting.MeasurementKt.result as internalMeasurementResult
+import org.wfanet.measurement.internal.reporting.MeasurementsGrpcKt.MeasurementsCoroutineStub as InternalMeasurementsCoroutineStub
+import org.wfanet.measurement.internal.reporting.Metric.Details as InternalMetricDetails
+import org.wfanet.measurement.internal.reporting.Metric.Details.MetricTypeCase as InternalMetricTypeCase
+import org.wfanet.measurement.internal.reporting.ReportingSet as InternalReportingSet
+import org.wfanet.measurement.internal.reporting.ReportingSetsGrpcKt.ReportingSetsCoroutineStub as InternalReportingSetsCoroutineStub
+import org.wfanet.measurement.internal.reporting.SetMeasurementResultRequest as SetInternalMeasurementResultRequest
+import org.wfanet.measurement.internal.reporting.getReportingSetRequest
+import org.wfanet.measurement.internal.reporting.measurement as internalMeasurement
+import org.wfanet.measurement.internal.reporting.setMeasurementFailureRequest as setInternalMeasurementFailureRequest
+import org.wfanet.measurement.internal.reporting.setMeasurementResultRequest as setInternalMeasurementResultRequest
+import org.wfanet.measurement.reporting.v1alpha.TimeInterval
+
+private const val MIN_PAGE_SIZE = 1
+private const val DEFAULT_PAGE_SIZE = 50
+private const val MAX_PAGE_SIZE = 1000
+
+private const val NUMBER_VID_BUCKETS = 300
+private const val REACH_ONLY_VID_SAMPLING_WIDTH = 3.0f / NUMBER_VID_BUCKETS
+private const val NUMBER_REACH_ONLY_BUCKETS = 16
+private val REACH_ONLY_VID_SAMPLING_START_LIST =
+  (0 until NUMBER_REACH_ONLY_BUCKETS).map { it * REACH_ONLY_VID_SAMPLING_WIDTH }
+private const val REACH_ONLY_REACH_EPSILON = 0.0041
+private const val REACH_ONLY_FREQUENCY_EPSILON = 0.0001
+private const val REACH_ONLY_MAXIMUM_FREQUENCY_PER_USER = 1
+
+private const val REACH_FREQUENCY_VID_SAMPLING_WIDTH = 5.0f / NUMBER_VID_BUCKETS
+private const val NUMBER_REACH_FREQUENCY_BUCKETS = 19
+private val REACH_FREQUENCY_VID_SAMPLING_START_LIST =
+  (0 until NUMBER_REACH_FREQUENCY_BUCKETS).map {
+    REACH_ONLY_VID_SAMPLING_START_LIST.last() +
+      REACH_ONLY_VID_SAMPLING_WIDTH +
+      it * REACH_FREQUENCY_VID_SAMPLING_WIDTH
+  }
+private const val REACH_FREQUENCY_REACH_EPSILON = 0.0033
+private const val REACH_FREQUENCY_FREQUENCY_EPSILON = 0.115
+
+private const val IMPRESSION_VID_SAMPLING_WIDTH = 62.0f / NUMBER_VID_BUCKETS
+private const val NUMBER_IMPRESSION_BUCKETS = 1
+private val IMPRESSION_VID_SAMPLING_START_LIST =
+  (0 until NUMBER_IMPRESSION_BUCKETS).map {
+    REACH_FREQUENCY_VID_SAMPLING_START_LIST.last() +
+      REACH_FREQUENCY_VID_SAMPLING_WIDTH +
+      it * IMPRESSION_VID_SAMPLING_WIDTH
+  }
+private const val IMPRESSION_EPSILON = 0.0011
+
+private const val WATCH_DURATION_VID_SAMPLING_WIDTH = 95.0f / NUMBER_VID_BUCKETS
+private const val NUMBER_WATCH_DURATION_BUCKETS = 1
+private val WATCH_DURATION_VID_SAMPLING_START_LIST =
+  (0 until NUMBER_WATCH_DURATION_BUCKETS).map {
+    IMPRESSION_VID_SAMPLING_START_LIST.last() +
+      IMPRESSION_VID_SAMPLING_WIDTH +
+      it * WATCH_DURATION_VID_SAMPLING_WIDTH
+  }
+private const val WATCH_DURATION_EPSILON = 0.001
+
+private const val DIFFERENTIAL_PRIVACY_DELTA = 1e-12
+
+private val REACH_ONLY_MEASUREMENT_SPEC =
+  MeasurementSpecKt.reachAndFrequency {
+    reachPrivacyParams = differentialPrivacyParams {
+      epsilon = REACH_ONLY_REACH_EPSILON
+      delta = DIFFERENTIAL_PRIVACY_DELTA
+    }
+    frequencyPrivacyParams = differentialPrivacyParams {
+      epsilon = REACH_ONLY_FREQUENCY_EPSILON
+      delta = DIFFERENTIAL_PRIVACY_DELTA
+    }
+    maximumFrequencyPerUser = REACH_ONLY_MAXIMUM_FREQUENCY_PER_USER
+  }
+
+data class MeasurementInfo(
+  val measurementConsumerReferenceId: String,
+  val idempotencyKey: String,
+  val eventGroupFilters: Map<String, String>,
+)
+
+data class SigningConfig(
+  val signingCertificateName: String,
+  val signingCertificateDer: ByteString,
+  val signingPrivateKey: PrivateKey,
+)
+
+data class WeightedMeasurementInfo(
+  val reportingMeasurementId: String,
+  val weightedMeasurement: WeightedMeasurement,
+  val timeInterval: TimeInterval,
+  var kingdomMeasurementId: String? = null,
+)
+
+data class SetOperationResult(
+  val weightedMeasurementInfoList: List<WeightedMeasurementInfo>,
+  val internalMetricDetails: InternalMetricDetails,
+)
+class MetricsService(
+  private val internalReportingSetsStub: InternalReportingSetsCoroutineStub,
+  private val internalMeasurementsStub: InternalMeasurementsCoroutineStub,
+  private val dataProvidersStub: DataProvidersCoroutineStub,
+  private val measurementsStub: MeasurementsCoroutineStub,
+  private val certificateStub: CertificatesCoroutineStub,
+  private val measurementConsumer: MeasurementConsumer,
+  private val apiAuthenticationKey: String,
+  private val secureRandom: SecureRandom,
+) {
+
+  
 }
