d3ca353d67e9f5185e2f06c117d766b6820c8682
Joe Corilla <10494069+jcorilla@users.noreply.github.com>
2023-02-14 18:29:19 +0000

    Fix linter comments

--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/writers/DeleteEventGroup.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/writers/DeleteEventGroup.kt
@@ -3,9 +3,11 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
+ *
  * ```
  *      http://www.apache.org/licenses/LICENSE-2.0
  * ```
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -29,6 +31,7 @@ import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.EventGroupRe
  * Soft Delete [EventGroup] in the database.
  *
  * Throws a subclass of [KingdomInternalException] on [execute].
+ *
  * @throws [EventGroupNotFoundException] EventGroup not found
  * @throws [EventGroupInvalidArgsException] DataProvider ids mismatch
  */
@@ -64,7 +67,7 @@ class DeleteEventGroup(private val eventGroup: EventGroup) :
     }
 
     return internalEventGroupResult.eventGroup.copy {
-      this.externalMeasurementConsumerCertificateId= 0L
+      this.externalMeasurementConsumerCertificateId = 0L
       this.details = EventGroup.Details.getDefaultInstance()
       this.state = EventGroup.State.DELETED
     }
