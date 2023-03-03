ea4c8f21a216c6600915b6b2ad1dccc4123719a7
wfa-mdharmalingam <mohanraj.dharmalingam@zealsols.com>
2023-03-03 16:58:47 +0530

    Initial Kingdom code

--- src/main/terraform/kingdom/gcloud/main.tf
+++ src/main/terraform/kingdom/gcloud/main.tf
@@ -12,19 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-/*
-  This is a terraform provider that will tie the terraform with the GCP account that we have.
-  The JSON mentioned here should never be pushed with the code.
-*/
 provider "google" {
   project = var.project
 }
-
-
-#terraform {
-# backend "gcs" {
-#  }
-#}
-
-
-
