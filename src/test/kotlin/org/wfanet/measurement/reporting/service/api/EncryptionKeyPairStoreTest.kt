4ebf68fd4d7da5427f71a8c205005ab22d108af6
Rieman Li <riemanli@google.com>
2023-03-08 18:05:33 +0000

    Move EncryptionKeyPairStore up a level and shared across versions.

--- src/test/kotlin/org/wfanet/measurement/reporting/service/api/EncryptionKeyPairStoreTest.kt
+++ src/test/kotlin/org/wfanet/measurement/reporting/service/api/EncryptionKeyPairStoreTest.kt
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.wfanet.measurement.reporting.service.api.v2alpha
+package org.wfanet.measurement.reporting.service.api
 
 import com.google.common.truth.Truth
 import com.google.protobuf.ByteString
