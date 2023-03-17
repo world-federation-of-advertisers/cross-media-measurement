6f88800fa4a923024d5e14c7324c22e107461f92
wfa-siyengar <santosh.iyengar@zealsols.com>
2023-03-17 11:44:12 +0530

    Upgraded the provider

--- src/main/terraform/duchy/gcloud/main.tf
+++ src/main/terraform/duchy/gcloud/main.tf
@@ -17,6 +17,11 @@ provider "google" {
 }
 
 terraform {
+  required_providers {
+    google = {
+      version = "~> 3.90.0"
+    }
+  }
   backend "gcs" {
     bucket  = "terraform-state-halo-cmm"
     prefix  = "wfa"
