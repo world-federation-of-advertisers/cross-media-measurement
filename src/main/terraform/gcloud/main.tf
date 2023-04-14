7ff99eefc045f31e2269181906e38aaf85bc4bdd
wfa-siyengar <santosh.iyengar@zealsols.com>
2023-04-14 14:59:32 +0530

    Testing Kingdom

--- src/main/terraform/gcloud/main.tf
+++ src/main/terraform/gcloud/main.tf
@@ -30,24 +30,3 @@ module "kingdom" {
   project = var.project
   service_account = var.service_account
 }
-
-module "worker-1" {
-  source = "./duchy"
-  env = var.env
-  project = var.project
-  component = "worker1"
-}
-
-module "worker-2" {
-  source = "./duchy"
-  env = var.env
-  project = var.project
-  component = "worker2"
-}
-
-module "worker-3" {
-  source = "./duchy"
-  env = var.env
-  project = var.project
-  component = "worker-3"
-}
