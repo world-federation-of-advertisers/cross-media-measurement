02770f2c6555d87cb213cdc886f586453ca0404d
wfa-siyengar <santosh.iyengar@zealsols.com>
2023-04-14 14:48:20 +0530

    Local changed to var

--- src/main/terraform/gcloud/variables.tf
+++ src/main/terraform/gcloud/variables.tf
@@ -12,6 +12,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+locals {
+  env = var.env
+  project = var.project
+  component = "kingdom"
+  zone = "us-central1"
+
+  # e.g. Prefix will look like dev-halo-kingdom
+  prefix = "${local.env}-${local.component}"
+
+  spanner_db = {
+    deletion_protection = false
+    version_retention_period = "3d"
+    num_nodes    = 1
+  }
+
+  kingdom = {
+    # configured as per the document.
+    name = "${local.prefix}-gke-cluster"
+    cluster_node_count = 2 # TODO(wfa-dharmalingam): change it 3, once the quota increased
+    machine_type = "e2-highcpu-2"
+    min_node_count = 2 # TODO(wfa-dharmalingam): change it 3, once the quota increased
+    max_node_count = 6
+    auto_scaling = true
+    database_encryption_state = "DECRYPTED" # TODO(wfa-dharmalingam): Set the state to ENCRYPTED state - (Required) ENCRYPTED or DECRYPTED
+    encryption_key = "" #"TODO(wfa-dharmalingam): Set the value to "projects/${local.project}/locations/${local.zone}/keyRings/test-key-ring/cryptoKeys/k8s-secret"
+  }
+  autoscaling_resource_limits = local.kingdom.auto_scaling ? concat([{
+    resource_type = "cpu"
+    minimum       = 1
+    maximum       = 8
+  }, {
+    resource_type = "memory"
+    minimum       = 2
+    maximum       = 16
+  }]): []
+
+  kms = {
+    ring_name = "test-key-ring"
+  }
+}
+
 variable "project" {
   type = string
   default = "halo-cmm-dev"
