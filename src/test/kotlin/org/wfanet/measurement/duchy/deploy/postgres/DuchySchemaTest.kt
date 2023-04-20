5fa806de7a64e64dbf156d60d3acc8cedc66a630
Yuhong Wang <wyuhong@amazon.com>
2023-04-20 17:38:04 +0000

    lint

--- src/test/kotlin/org/wfanet/measurement/duchy/deploy/postgres/DuchySchemaTest.kt
+++ src/test/kotlin/org/wfanet/measurement/duchy/deploy/postgres/DuchySchemaTest.kt
@@ -15,7 +15,6 @@
 package org.wfanet.measurement.duchy.deploy.postgres
 
 import com.google.common.truth.Truth.assertThat
-
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
@@ -26,25 +25,36 @@ import org.wfanet.measurement.common.db.r2dbc.boundStatement
 import org.wfanet.measurement.common.db.r2dbc.postgres.testing.EmbeddedPostgresDatabaseProvider
 import org.wfanet.measurement.duchy.deploy.postgres.testing.Schemata.DUCHY_CHANGELOG_PATH
 
-
 @RunWith(JUnit4::class)
 class DuchySchemaTest {
   val databaseClient = EmbeddedPostgresDatabaseProvider(DUCHY_CHANGELOG_PATH).createNewDatabase()
 
   private fun translate(row: ResultRow): String = row["table_name"]
 
-  @Test fun `database is created`() {}
+  @Test
+  fun `database is created`() {
+  }
 
   @Test
   fun `all duchy tables are created`(): Unit = runBlocking {
     val query = "SELECT table_name FROM information_schema.tables;"
-    val duchyTableNames = listOf(
-      "computations", "requisitions", "computationstages", "computationblobreferences",
-      "computationstageattempts", "computationstats", "heraldcontinuationtokens"
+    val duchyTableNames =
+      listOf(
+        "computations",
+        "requisitions",
+        "computationstages",
+        "computationblobreferences",
+        "computationstageattempts",
+        "computationstats",
+        "heraldcontinuationtokens"
       )
 
     val resList = mutableListOf<String>()
-    databaseClient.readTransaction().executeQuery(boundStatement(query)).consume(::translate).toList(resList)
+    databaseClient
+      .readTransaction()
+      .executeQuery(boundStatement(query))
+      .consume(::translate)
+      .toList(resList)
 
     assertThat(resList).containsAtLeastElementsIn(duchyTableNames)
   }
