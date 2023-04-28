44fff514e69476589daebc24a08b543a6f4d7997
marcopremier <marcopremier@meta.com>
2023-04-28 12:49:06 +0200

    Add license to new files. Add ModelReleaseReader

--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/SpannerModelReleasesService.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/SpannerModelReleasesService.kt
@@ -1,9 +1,32 @@
+/*
+ * Copyright 2023 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wfanet.measurement.kingdom.deploy.gcloud.spanner
 
+import io.grpc.Status
+import kotlinx.coroutines.flow.Flow
+import org.wfanet.measurement.common.grpc.failGrpc
+import org.wfanet.measurement.common.identity.ExternalId
 import org.wfanet.measurement.common.identity.IdGenerator
 import org.wfanet.measurement.gcloud.spanner.AsyncDatabaseClient
+import org.wfanet.measurement.internal.kingdom.GetModelReleaseRequest
 import org.wfanet.measurement.internal.kingdom.ModelRelease
 import org.wfanet.measurement.internal.kingdom.ModelReleasesGrpcKt.ModelReleasesCoroutineImplBase
+import org.wfanet.measurement.internal.kingdom.StreamModelReleasesRequest
+import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.ModelReleaseReader
 import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.writers.CreateModelRelease
 
 class SpannerModelReleasesService(
@@ -13,5 +36,16 @@ class SpannerModelReleasesService(
 
   override suspend fun createModelRelease(request: ModelRelease): ModelRelease {
     return CreateModelRelease(request).execute(client, idGenerator)
+  }
+
+  override suspend fun getModelRelease(request: GetModelReleaseRequest): ModelRelease {
+    return ModelReleaseReader()
+      .readByExternalModelReleaseId(client.singleUse(), ExternalId(request.externalModelReleaseId))
+      ?.modelRelease
+      ?: failGrpc(Status.NOT_FOUND) { "ModelRelease not found" }
+  }
+
+  override fun streamModelReleases(request: StreamModelReleasesRequest): Flow<ModelRelease> {
+    return super.streamModelReleases(request)
   }
 }
