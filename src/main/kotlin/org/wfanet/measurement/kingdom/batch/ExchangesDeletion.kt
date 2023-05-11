7a07c70b9bdaf61b354a2327a45857e493139c97
Joe Corilla <10494069+jcorilla@users.noreply.github.com>
2023-05-11 23:53:19 +0000

    Add cronjob configs and classes to enable exchanges retention policy

---
+++ src/main/kotlin/org/wfanet/measurement/kingdom/batch/ExchangesDeletion.kt
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2023 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.kingdom.batch
+
+import io.opentelemetry.api.GlobalOpenTelemetry
+import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.api.metrics.LongCounter
+import io.opentelemetry.api.metrics.Meter
+import java.time.LocalDate
+import java.time.ZoneOffset
+import java.util.logging.Logger
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+import org.wfanet.measurement.common.toProtoDate
+import org.wfanet.measurement.internal.kingdom.DeleteExchangeRequest
+import org.wfanet.measurement.internal.kingdom.Exchange
+import org.wfanet.measurement.internal.kingdom.ExchangesGrpcKt
+import org.wfanet.measurement.internal.kingdom.StreamExchangesRequestKt
+import org.wfanet.measurement.internal.kingdom.batchDeleteExchangesRequest
+import org.wfanet.measurement.internal.kingdom.deleteExchangeRequest
+import org.wfanet.measurement.internal.kingdom.streamExchangesRequest
+
+private val MAX_BATCH_DELETE = 1000
+
+class ExchangesDeletion(
+  private val exchangesService: ExchangesGrpcKt.ExchangesCoroutineStub,
+  private val daysToLive: Long,
+  private val dryRun: Boolean = false,
+  private val openTelemetry: OpenTelemetry = GlobalOpenTelemetry.get()
+) {
+  private val meter: Meter = openTelemetry.getMeter(ExchangesDeletion::class.java.name)
+  private val exchangeDeletionCounter: LongCounter =
+    meter
+      .counterBuilder("exchanges_deletion_total")
+      .setDescription("Total number of exchanges deleted under retention policy")
+      .build()
+
+  fun run() {
+    val currentDate = LocalDate.now(ZoneOffset.UTC)
+    runBlocking {
+      val exchangesToDelete: List<Exchange> =
+        exchangesService
+          .streamExchanges(
+            streamExchangesRequest {
+              filter = StreamExchangesRequestKt.filter {
+                dateBefore = currentDate.minusDays(daysToLive).toProtoDate()
+              }
+            }
+          )
+          .toList()
+
+      if (dryRun) {
+        logger.info { "Exchanges that would have been deleted $exchangesToDelete" }
+      } else {
+        for (batchExchangesToDelete in exchangesToDelete.chunked(MAX_BATCH_DELETE)) {
+          val deleteRequests: List<DeleteExchangeRequest> =
+            batchExchangesToDelete.map {
+              deleteExchangeRequest {
+                externalRecurringExchangeId = it.externalRecurringExchangeId
+                date = it.date
+              }
+            }
+          exchangesService.batchDeleteExchanges(
+            batchDeleteExchangesRequest { requests += deleteRequests }
+          )
+          exchangeDeletionCounter.add(deleteRequests.size.toLong())
+        }
+      }
+    }
+  }
+  companion object {
+    private val logger: Logger = Logger.getLogger(this::class.java.name)
+  }
+}
---
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/common/job/ExchangesDeletionJob.kt
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2023 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.kingdom.deploy.common.job
+
+import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.api.common.Attributes
+import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter
+import io.opentelemetry.sdk.OpenTelemetrySdk
+import io.opentelemetry.sdk.metrics.SdkMeterProvider
+import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader
+import io.opentelemetry.sdk.resources.Resource
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes
+import java.time.Duration
+import kotlin.properties.Delegates
+import org.wfanet.measurement.common.commandLineMain
+import org.wfanet.measurement.common.crypto.SigningCerts
+import org.wfanet.measurement.common.grpc.TlsFlags
+import org.wfanet.measurement.common.grpc.buildMutualTlsChannel
+import org.wfanet.measurement.common.grpc.withDefaultDeadline
+import org.wfanet.measurement.common.grpc.withVerboseLogging
+import org.wfanet.measurement.internal.kingdom.ExchangesGrpcKt.ExchangesCoroutineStub
+import org.wfanet.measurement.kingdom.batch.ExchangesDeletion
+import org.wfanet.measurement.kingdom.deploy.common.server.KingdomApiServerFlags
+import picocli.CommandLine
+
+private class ExchangesRetentionFlags {
+  @CommandLine.Mixin
+  lateinit var tlsFlags: TlsFlags
+    private set
+
+  @CommandLine.Mixin
+  lateinit var kingdomApiServerFlags: KingdomApiServerFlags
+    private set
+
+  @set:CommandLine.Option(
+    names = ["--days-to-live"],
+    defaultValue = "100",
+    description =
+    [
+      "Days to live for an Exchange since its scheduled date. An Exchange won't live longer than" +
+        "this duration."
+    ],
+  )
+  var exchangesDaysToLive by Delegates.notNull<Long>()
+    private set
+
+  @CommandLine.Option(
+    names = ["--otel-exporter-otlp-endpoint"],
+    description = ["Endpoint for OpenTelemetry Collector."],
+    required = true,
+  )
+  lateinit var otelExporterOtlpEndpoint: String
+    private set
+
+  @CommandLine.Option(
+    names = ["--otel-service-name"],
+    description = ["Service name to label cronjob metrics with."],
+    required = true,
+  )
+  lateinit var otelServiceName: String
+    private set
+
+  @set:CommandLine.Option(
+    names = ["--dry-run"],
+    description =
+    [
+      "Option to print the Exchanges that would be deleted instead of performing the deletion."
+    ],
+    required = false,
+    defaultValue = "false"
+  )
+  var dryRun by Delegates.notNull<Boolean>()
+    private set
+}
+
+@CommandLine.Command(
+  name = "ExchangeDeletionJob",
+  mixinStandardHelpOptions = true,
+  showDefaultValues = true
+)
+private fun run(@CommandLine.Mixin flags: ExchangesRetentionFlags) {
+  val clientCerts =
+    SigningCerts.fromPemFiles(
+      certificateFile = flags.tlsFlags.certFile,
+      privateKeyFile = flags.tlsFlags.privateKeyFile,
+      trustedCertCollectionFile = flags.tlsFlags.certCollectionFile
+    )
+
+  val channel =
+    buildMutualTlsChannel(
+      flags.kingdomApiServerFlags.internalApiFlags.target,
+      clientCerts,
+      flags.kingdomApiServerFlags.internalApiFlags.certHost
+    )
+      .withVerboseLogging(flags.kingdomApiServerFlags.debugVerboseGrpcClientLogging)
+      .withDefaultDeadline(flags.kingdomApiServerFlags.internalApiFlags.defaultDeadlineDuration)
+
+  val internalExchangesClient = ExchangesCoroutineStub(channel)
+
+  val otlpEndpoint = flags.otelExporterOtlpEndpoint
+  val otelServiceName = flags.otelServiceName
+  val resource: Resource =
+    Resource.getDefault()
+      .merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, otelServiceName)))
+  val meterProvider =
+    SdkMeterProvider.builder()
+      .setResource(resource)
+      .registerMetricReader(
+        PeriodicMetricReader.builder(
+          OtlpGrpcMetricExporter.builder()
+            .setTimeout(Duration.ofSeconds(30L))
+            .setEndpoint(otlpEndpoint)
+            .build()
+        )
+          .setInterval(Duration.ofSeconds(60L))
+          .build()
+      )
+      .build()
+  val openTelemetry: OpenTelemetry =
+    OpenTelemetrySdk.builder().setMeterProvider(meterProvider).build()
+
+  val exchangesDeletion =
+    ExchangesDeletion(
+      internalExchangesClient,
+      flags.exchangesDaysToLive,
+      flags.dryRun,
+      openTelemetry
+    )
+  exchangesDeletion.run()
+}
+
+fun main(args: Array<String>) = commandLineMain(::run, args)
