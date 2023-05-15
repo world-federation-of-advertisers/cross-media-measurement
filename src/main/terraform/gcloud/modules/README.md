545a8164f89c9683b8ea4d0b8edfbb0d12c89f0c
Sanjay Vasandani <sanjayvas@google.com>
2023-05-15 10:07:26 -0700

    Rewrite Terraform configuration to be more modular.

---
+++ src/main/terraform/gcloud/modules/README.md
@@ -0,0 +1,3 @@
+# Google Cloud Terraform Modules
+
+Any module with a README file may be reused externally.
---
+++ src/main/terraform/gcloud/modules/common/outputs.tf
@@ -0,0 +1,23 @@
+# Copyright 2023 The Cross-Media Measurement Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+output "cluster_service_account" {
+  description = "`google_service_account` for GKE clusters."
+  value       = google_service_account.gke_cluster
+}
+
+output "cluster_secret_key" {
+  description = "`google_kms_crypto_key` for Kubernetes cluster secrets."
+  value       = google_kms_crypto_key.k8s
+}
