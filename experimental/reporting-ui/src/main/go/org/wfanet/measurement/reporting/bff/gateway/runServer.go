2deaaf3e468c13abdf9c28938d623d1df6526ff0
Brandon Domen <bdomen@google.com>
2023-05-23 20:51:54 +0000

    Adding GRPC for the Reporting BFF

---
+++ experimental/reporting-ui/src/main/go/org/wfanet/measurement/reporting/bff/gateway/runServer.go
@@ -0,0 +1,91 @@
+// Copyright 2023 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gateway
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/golang/glog"
+	gwruntime "github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+)
+
+// Endpoint describes a gRPC endpoint
+type Endpoint struct {
+	Addr string
+}
+
+// Options is a set of options to be passed to Run
+type Options struct {
+	// Addr is the address to listen
+	Addr string
+
+	// GRPCServer defines an endpoint of a gRPC service
+	GRPCServer Endpoint
+
+	// Mux is a list of options to be passed to the gRPC-Gateway multiplexer
+	Mux []gwruntime.ServeMuxOption
+
+	// File location of the TLS cert for the gRPC service
+	TlsCertPath string
+
+	TlsKeyPath string
+}
+
+// Run starts a HTTP server and blocks while running if successful.
+// The server will be shutdown when "ctx" is canceled.
+func Run(ctx context.Context, opts Options) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	conn, err := dial(ctx, opts.GRPCServer.Addr, opts.TlsCertPath)
+	if err != nil {
+		return err
+	}
+	go func() {
+		<-ctx.Done()
+		if err := conn.Close(); err != nil {
+			glog.Errorf("Failed to close a client connection to the gRPC server: %v", err)
+		}
+	}()
+
+	mux := http.NewServeMux()
+
+	gw, err := newGateway(ctx, conn, opts.Mux)
+	if err != nil {
+		return err
+	}
+	mux.Handle("/", gw)
+
+	s := &http.Server{
+		Addr:    opts.Addr,
+		Handler: allowCORS(mux),
+	}
+	go func() {
+		<-ctx.Done()
+		glog.Infof("Shutting down the http server")
+		if err := s.Shutdown(context.Background()); err != nil {
+			glog.Errorf("Failed to shutdown http server: %v", err)
+		}
+	}()
+
+	glog.Infof("Starting listening at %s", opts.Addr)
+	if err := s.ListenAndServeTLS(opts.TlsCertPath, opts.TlsKeyPath); err != http.ErrServerClosed {
+		glog.Errorf("Failed to listen and serve: %v", err)
+		return err
+	}
+	return nil
+}
