07ed3b765d3d753c9fcb18fbdbb92b8ddc4176f8
marcopremier <marcoprem@gmail.com>
2023-05-25 11:37:14 +0200

    model outage internal api (#1010)
    Co-authored-by: marcopremier <marcopremier@meta.com>

--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/common/KingdomInternalException.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/common/KingdomInternalException.kt
@@ -28,6 +28,7 @@ import org.wfanet.measurement.internal.kingdom.ErrorCode
 import org.wfanet.measurement.internal.kingdom.EventGroup
 import org.wfanet.measurement.internal.kingdom.Measurement
 import org.wfanet.measurement.internal.kingdom.ModelLine
+import org.wfanet.measurement.internal.kingdom.ModelOutage
 import org.wfanet.measurement.internal.kingdom.Requisition
 
 sealed class KingdomInternalException : Exception {
@@ -595,6 +596,59 @@ class ExchangeStepNotFoundException(
         "external_recurring_exchange_id" to externalRecurringExchangeId.value.toString(),
         "date" to date.toString(),
         "step_index" to stepIndex.toString(),
+      )
+}
+
+class ModelOutageNotFoundException(
+  val externalModelProviderId: ExternalId,
+  val externalModelSuiteId: ExternalId,
+  val externalModelLineId: ExternalId,
+  val externalModelOutageId: ExternalId,
+  provideDescription: () -> String = { "ModelOutage not found" }
+) : KingdomInternalException(ErrorCode.MODEL_OUTAGE_NOT_FOUND, provideDescription) {
+  override val context
+    get() =
+      mapOf(
+        "external_model_provider_id" to externalModelProviderId.toString(),
+        "external_model_suite_id" to externalModelSuiteId.toString(),
+        "external_model_line_id" to externalModelLineId.toString(),
+        "external_model_outage_id" to externalModelOutageId.toString()
+      )
+}
+
+class ModelOutageStateIllegalException(
+  val externalModelProviderId: ExternalId,
+  val externalModelSuiteId: ExternalId,
+  val externalModelLineId: ExternalId,
+  val externalModelOutageId: ExternalId,
+  val state: ModelOutage.State,
+  provideDescription: () -> String = { "ModelOutage state illegal" }
+) : KingdomInternalException(ErrorCode.MODEL_OUTAGE_STATE_ILLEGAL, provideDescription) {
+  override val context
+    get() =
+      mapOf(
+        "external_model_provider_id" to externalModelProviderId.toString(),
+        "external_model_suite_id" to externalModelSuiteId.toString(),
+        "external_model_line_id" to externalModelLineId.toString(),
+        "external_model_outage_id" to externalModelOutageId.toString(),
+        "model_outage_state" to state.toString()
+      )
+}
+
+class ModelOutageInvalidArgsException(
+  val externalModelProviderId: ExternalId,
+  val externalModelSuiteId: ExternalId,
+  val externalModelLineId: ExternalId,
+  val externalModelOutageId: ExternalId? = null,
+  provideDescription: () -> String = { "ModelOutage invalid outage interval arguments" }
+) : KingdomInternalException(ErrorCode.MODEL_OUTAGE_INVALID_ARGS, provideDescription) {
+  override val context
+    get() =
+      mapOf(
+        "external_model_provider_id" to externalModelProviderId.toString(),
+        "external_model_suite_id" to externalModelSuiteId.toString(),
+        "external_model_line_id" to externalModelLineId.toString(),
+        "external_model_outage_id" to externalModelOutageId.toString()
       )
 }
 
