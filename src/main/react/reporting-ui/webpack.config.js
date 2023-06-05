1f46f05cf50c1dfe1b92c981742134017bb2d2c3
Brandon Domen <bdomen@google.com>
2023-06-05 20:35:12 +0000

    adding react hello world and bazel configuration

---
+++ src/main/react/reporting-ui/webpack.config.js
@@ -0,0 +1,50 @@
+// Copyright 2023 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
+    modules: ['node_modules'],
+  },
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'index.js',
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  devServer: {
+    static: path.resolve(__dirname, './public'),
+  },
+};
