22300891ec78311a48780db36d73bd6454861601
Sanjay Vasandani <sanjayvas@google.com>
2023-06-07 10:49:29 -0700

    Add missing NetworkPolicy resources for EDP simulators.
    When adding any NetworkPolicy to the cluster, all traffic not covered by the policy appears to be blocked by default. This can be triggered by enabling monitoring.
    
    This also simplifies the metrics deployment guide for GKE.

--- src/main/terraform/gcloud/modules/common/main.tf
+++ src/main/terraform/gcloud/modules/common/main.tf
@@ -48,14 +48,3 @@ resource "google_project_iam_member" "node_service_account" {
   role    = "roles/container.nodeServiceAccount"
   member  = google_service_account.gke_cluster.member
 }
-
-resource "google_service_account" "gmp_monitoring" {
-  account_id  = "gmp-monitoring"
-  description = "Google Managed Prometheus monitoring"
-}
-
-resource "google_project_iam_member" "monitoring_viewer" {
-  project = data.google_project.project.name
-  role    = "roles/monitoring.viewer"
-  member  = google_service_account.gmp_monitoring.member
-}
