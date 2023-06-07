cfa92cadbd6d22c8de789935c2f69e801a565fb4
Yuhong Wang <wyuhong@amazon.com>
2023-06-07 14:38:58 +0000

    lint

--- src/test/kotlin/org/wfanet/measurement/duchy/deploy/postgres/PostgresContinuationTokenServiceTest.kt
+++ src/test/kotlin/org/wfanet/measurement/duchy/deploy/postgres/PostgresContinuationTokenServiceTest.kt
@@ -14,18 +14,18 @@
 
 package org.wfanet.measurement.duchy.deploy.postgres
 
+import java.time.Clock
+import kotlin.random.Random
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import org.wfanet.measurement.common.db.r2dbc.postgres.testing.EmbeddedPostgresDatabaseProvider
 import org.wfanet.measurement.common.identity.RandomIdGenerator
 import org.wfanet.measurement.duchy.deploy.postgres.testing.Schemata.DUCHY_CHANGELOG_PATH
 import org.wfanet.measurement.duchy.service.internal.testing.ContinuationTokensServiceTest
-import java.time.Clock
-import kotlin.random.Random
 
 @RunWith(JUnit4::class)
 class PostgresContinuationTokenServiceTest :
-  ContinuationTokensServiceTest<PostgresContinuationTokenService>() {
+    ContinuationTokensServiceTest<PostgresContinuationTokenService>() {
   override fun newService(): PostgresContinuationTokenService {
     val client = EmbeddedPostgresDatabaseProvider(DUCHY_CHANGELOG_PATH).createNewDatabase()
     val idGenerator = RandomIdGenerator(Clock.systemUTC(), Random(1))
