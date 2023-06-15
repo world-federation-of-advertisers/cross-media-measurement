a9e893a3de20d1127e4cde9d8654bf4e39ee5830
YuhongWang-Amazon <123598495+YuhongWang-Amazon@users.noreply.github.com>
2023-06-15 11:47:00 -0400

    Implement postgres continuation token service (#1059)
    This implements the Postgres continuation token service and its dependent Postgres readers/writers.

---
+++ src/main/kotlin/org/wfanet/measurement/duchy/deploy/postgres/PostgresContinuationTokensService.kt
@@ -0,0 +1,57 @@
+// Copyright 2023 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.duchy.deploy.postgres
+
+import io.grpc.Status
+import org.wfanet.measurement.common.db.r2dbc.DatabaseClient
+import org.wfanet.measurement.common.identity.IdGenerator
+import org.wfanet.measurement.duchy.deploy.postgres.readers.ContinuationTokenReader
+import org.wfanet.measurement.duchy.deploy.postgres.writers.SetContinuationToken
+import org.wfanet.measurement.duchy.service.internal.ContinuationTokenInvalidException
+import org.wfanet.measurement.duchy.service.internal.ContinuationTokenMalformedException
+import org.wfanet.measurement.internal.duchy.ContinuationTokensGrpcKt.ContinuationTokensCoroutineImplBase
+import org.wfanet.measurement.internal.duchy.GetContinuationTokenRequest
+import org.wfanet.measurement.internal.duchy.GetContinuationTokenResponse
+import org.wfanet.measurement.internal.duchy.SetContinuationTokenRequest
+import org.wfanet.measurement.internal.duchy.SetContinuationTokenResponse
+import org.wfanet.measurement.internal.duchy.getContinuationTokenResponse
+
+class PostgresContinuationTokensService(
+  private val client: DatabaseClient,
+  private val idGenerator: IdGenerator,
+) : ContinuationTokensCoroutineImplBase() {
+
+  override suspend fun getContinuationToken(
+    request: GetContinuationTokenRequest
+  ): GetContinuationTokenResponse {
+    val result: ContinuationTokenReader.Result =
+      ContinuationTokenReader().getContinuationToken(client.singleUse())
+        ?: return GetContinuationTokenResponse.getDefaultInstance()
+    return getContinuationTokenResponse { token = result.continuationToken }
+  }
+
+  override suspend fun setContinuationToken(
+    request: SetContinuationTokenRequest
+  ): SetContinuationTokenResponse {
+    try {
+      SetContinuationToken(request.token).execute(client, idGenerator)
+    } catch (e: ContinuationTokenInvalidException) {
+      throw e.asStatusRuntimeException(Status.Code.FAILED_PRECONDITION)
+    } catch (e: ContinuationTokenMalformedException) {
+      throw e.asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    }
+    return SetContinuationTokenResponse.getDefaultInstance()
+  }
+}
---
+++ src/main/kotlin/org/wfanet/measurement/duchy/deploy/postgres/writers/SetContinutationToken.kt
@@ -0,0 +1,85 @@
+// Copyright 2023 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.measurement.duchy.deploy.postgres.writers
+
+import com.google.protobuf.InvalidProtocolBufferException
+import com.google.protobuf.util.Timestamps
+import java.time.Instant
+import org.wfanet.measurement.common.base64UrlDecode
+import org.wfanet.measurement.common.db.r2dbc.boundStatement
+import org.wfanet.measurement.common.db.r2dbc.postgres.PostgresWriter
+import org.wfanet.measurement.duchy.deploy.postgres.readers.ContinuationTokenReader
+import org.wfanet.measurement.duchy.service.internal.ContinuationTokenInvalidException
+import org.wfanet.measurement.duchy.service.internal.ContinuationTokenMalformedException
+import org.wfanet.measurement.duchy.service.internal.DuchyInternalException
+import org.wfanet.measurement.system.v1alpha.StreamActiveComputationsContinuationToken
+
+/**
+ * [PostgresWriter] for setting continuation tokens.
+ *
+ * Throws a subclass of [DuchyInternalException] on [execute]:
+ * * [ContinuationTokenMalformedException] when the new token is malformed
+ * * [ContinuationTokenInvalidException] when the new token is invalid
+ */
+class SetContinuationToken(private val continuationToken: String) : PostgresWriter<Unit>() {
+  private fun decodeContinuationToken(token: String): StreamActiveComputationsContinuationToken =
+    try {
+      StreamActiveComputationsContinuationToken.parseFrom(token.base64UrlDecode())
+    } catch (e: InvalidProtocolBufferException) {
+      throw ContinuationTokenMalformedException(
+        continuationToken,
+        "ContinuationToken is malformed."
+      )
+    }
+
+  override suspend fun TransactionScope.runTransaction() {
+    val statement =
+      boundStatement(
+        """
+      INSERT INTO HeraldContinuationTokens (Presence, ContinuationToken, UpdateTime)
+        VALUES ($1, $2, $3)
+      ON CONFLICT (Presence)
+      DO
+        UPDATE SET ContinuationToken = EXCLUDED.ContinuationToken, UpdateTime = EXCLUDED.UpdateTime;
+      """
+      ) {
+        bind("$1", true)
+        bind("$2", continuationToken)
+        bind("$3", Instant.now())
+      }
+
+    transactionContext.run {
+      val newContinuationToken = decodeContinuationToken(continuationToken)
+      val oldContinuationToken =
+        ContinuationTokenReader().getContinuationToken(transactionContext)?.continuationToken?.let {
+          decodeContinuationToken(it)
+        }
+
+      if (
+        oldContinuationToken != null &&
+          Timestamps.compare(
+            newContinuationToken.updateTimeSince,
+            oldContinuationToken.updateTimeSince
+          ) < 0
+      ) {
+        throw ContinuationTokenInvalidException(
+          continuationToken,
+          "ContinuationToken to set cannot have older timestamp."
+        )
+      }
+      transactionContext.executeStatement(statement)
+    }
+  }
+}
