eefd211ebd16c7ebac21bf104df01b9aeefaace6
Brandon Domen <bdomen@google.com>
2023-06-20 17:24:42 +0000

    fixing...

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-module.exports = {
-    paths: function (paths, env) {
-        paths.appPublic = path.resolve(__dirname, 'static');
-        paths.appHtml = path.resolve(__dirname, 'static/index.html');
-        return paths;
-    }
-}
