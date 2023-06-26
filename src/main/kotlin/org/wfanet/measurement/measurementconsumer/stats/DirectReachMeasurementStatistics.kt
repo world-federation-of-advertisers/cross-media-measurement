64a4e410031ded29a37af0a1dde5086769e6bc48
Rieman Li <riemanli@google.com>
2023-06-26 00:31:18 +0000

    Add measurement statistics.

---
+++ src/main/kotlin/org/wfanet/measurement/measurementconsumer/stats/DirectReachMeasurementStatistics.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2023 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.measurementconsumer.stats
+
+/** Computes statistics for direct reach measurements. */
+class DirectReachMeasurementStatistics : DirectMeasurementStatistics(), ReachMeasurementStatistics {
+  /** Outputs the variance of the given [reach]. */
+  override fun variance(reach: Int, params: ReachMeasurementParams): Double {
+    return calculateScalarMeasurementVariance(
+      reach.toDouble(),
+      params.vidSamplingIntervalWidth,
+      params.dpParams,
+      1.0
+    )
+  }
+
+  /**
+   * Outputs the covariance of the given two reaches.
+   *
+   * Precisely, computes the covariance between any two reach measurements when at least one reach
+   * is a direct measurement.
+   */
+  override fun covariance(
+    thisReach: Int,
+    thatReach: Int,
+    unionReach: Int,
+    thisSamplingWidth: Double,
+    thatSamplingWidth: Double,
+    unionSamplingWidth: Double
+  ): Double {
+    val overlapReach = thisReach + thatReach - unionReach
+    val overlapSamplingWidth = thisSamplingWidth + thatSamplingWidth - unionSamplingWidth
+    return overlapReach * (overlapSamplingWidth / thisSamplingWidth / thatSamplingWidth - 1)
+  }
+}
---
+++ src/main/kotlin/org/wfanet/measurement/measurementconsumer/stats/FrequencyMeasurementStatistics.kt
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2023 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.measurementconsumer.stats
+
+interface FrequencyMeasurementStatistics {
+
+  val reachMeasurementStatistics: ReachMeasurementStatistics
+
+  /** Outputs [FrequencyVariances] of the given reach-frequency measurement. */
+  fun variances(
+    totalReach: Int,
+    relativeFrequencyDistribution: Map<Int, Double>,
+    params: FrequencyMeasurementParams
+  ): FrequencyVariances {
+    var suffixSum = 0.0
+
+    val kPlusRelativeFrequencyDistribution: Map<Int, Double> =
+    // There is no estimate of zero-frequency reach
+    (params.maximumFrequency downTo 1).associateWith { frequency ->
+        suffixSum += relativeFrequencyDistribution.getOrDefault(frequency, 0.0)
+        suffixSum
+      }
+
+    val reachVariance =
+      reachMeasurementStatistics.variance(
+        totalReach,
+        ReachMeasurementParams(params.vidSamplingIntervalWidth, params.reachDpParams)
+      )
+
+    val relativeVariances: Map<Int, Double> =
+      (1..params.maximumFrequency).associateWith { frequency ->
+        frequencyRelativeVariance(
+          totalReach,
+          relativeFrequencyDistribution.getValue(frequency),
+          params,
+          1
+        )
+      }
+
+    val kPlusRelativeVariances: Map<Int, Double> =
+      (1..params.maximumFrequency).associateWith { frequency ->
+        frequencyRelativeVariance(
+          totalReach,
+          relativeFrequencyDistribution.getValue(frequency),
+          params,
+          params.maximumFrequency - frequency + 1
+        )
+      }
+
+    val countVariances: Map<Int, Double> =
+      (1..params.maximumFrequency).associateWith { frequency ->
+        frequencyCountVariance(
+          totalReach,
+          reachVariance,
+          relativeFrequencyDistribution.getValue(frequency),
+          relativeVariances.getValue(frequency)
+        )
+      }
+
+    val kPlusCountVariances: Map<Int, Double> =
+      (1..params.maximumFrequency).associateWith { frequency ->
+        frequencyCountVariance(
+          totalReach,
+          reachVariance,
+          kPlusRelativeFrequencyDistribution.getValue(frequency),
+          kPlusRelativeVariances.getValue(frequency)
+        )
+      }
+
+    return FrequencyVariances(
+      relativeVariances,
+      kPlusRelativeVariances,
+      countVariances,
+      kPlusCountVariances
+    )
+  }
+
+  /**
+   * Outputs the variance of the given [reachRatio] at a certain frequency.
+   *
+   * Different types of frequency histograms have different values of [multiplier].
+   */
+  fun frequencyRelativeVariance(
+    totalReach: Int,
+    reachRatio: Double,
+    frequencyMeasurementParams: FrequencyMeasurementParams,
+    multiplier: Int
+  ): Double
+
+  /**
+   * Outputs the variance of the given reach count at a certain frequency.
+   *
+   * Reach count = [totalReach] * [reachRatio]
+   */
+  fun frequencyCountVariance(
+    totalReach: Int,
+    totalReachVariance: Double,
+    reachRatio: Double,
+    reachRatioVariance: Double,
+  ): Double
+}
