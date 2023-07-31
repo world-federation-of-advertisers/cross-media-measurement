045b437b246c6129408c2fe9db91657a24d2fea1
bdomen-ggl <108941520+bdomen-ggl@users.noreply.github.com>
2023-07-31 13:50:15 +0000

    adding UI assets (#1141)
    Adding assets for Reporting UI - font and icons.

---
+++ experimental/reporting-ui/src/main/react/reporting-ui/library/assets/icons/add.tsx
@@ -0,0 +1,30 @@
+// Copyright 2023 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react';
+
+export const AddIcon = () => {
+  return (
+    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clipPath="url(#clip0_281_24976)">
+        <path d="M23 31V25H17V23H23V17H25V23H31V25H25V31H23Z" fill="#4A454E" />
+      </g>
+      <defs>
+        <clipPath id="clip0_281_24976">
+          <rect width="48" height="48" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
---
+++ experimental/reporting-ui/src/main/react/reporting-ui/library/assets/icons/menu.tsx
@@ -0,0 +1,25 @@
+// Copyright 2023 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react';
+
+export const MenuIcon = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g id="menu_24px">
+            <path id="icon" fillRule="evenodd" clipRule="evenodd" d="M3 8V6H21V8H3ZM3 13H21V11H3V13ZM3 18H21V16H3V18Z" fill="#4A454E"/>
+        </g>
+    </svg>
+  );
+};
