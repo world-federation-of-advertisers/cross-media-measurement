6926b1e0fb089b83157a7b0edfa03b339f23a19e
Brandon Domen <bdomen@google.com>
2023-08-08 16:11:16 +0000

    moving file structure

--- experimental/reporting-ui/src/main/react/reporting-ui/webpack.config.js
+++ experimental/reporting-ui/src/main/react/reporting-ui/webpack.config.js
@@ -16,7 +16,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.tsx'),
+  entry: path.resolve(__dirname, './index.tsx'),
   module: {
     rules: [
       {
