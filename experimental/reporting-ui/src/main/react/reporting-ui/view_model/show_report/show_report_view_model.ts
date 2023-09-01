176651b19f0fd25e3b940773105e3b2e815dd75f
Brandon Domen <bdomen@google.com>
2023-09-01 16:53:36 +0000

    tweaks

--- experimental/reporting-ui/src/main/react/reporting-ui/view_model/show_report/show_report_view_model.ts
+++ experimental/reporting-ui/src/main/react/reporting-ui/view_model/show_report/show_report_view_model.ts
@@ -1,3 +1,17 @@
+// Copyright 2023 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import {useState} from 'react';
 import { Report, ReportState } from '../../model/reporting';
 import { ShowReportRepository } from '../../model/show_report/show_report_repository';
@@ -8,7 +22,11 @@ type ShowReport = {
   status: ReportState,
 }
 
-const handleShowReport = (report: Report) => {
+const handleShowReport = (report: Report|undefined) => {
+  if (!report) {
+    return null;
+  }
+
   return {
     id: report.id,
     name: report.name,
@@ -37,12 +55,8 @@ export const ShowReportViewModel = () => {
       tempErrors.push(response.reason);
       setErrors(errors);
     } else {
-      if (response.value) {
-        const result = handleShowReport(response.value);
-        setReport(result);
-      } else {
-        setReport(null);
-      }
+      const result = handleShowReport(response.value);
+      setReport(result);
     }
 
     setLoading(false);
