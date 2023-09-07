abb1d8b7c275b1aee41cba8e6eb5f09ed18e08eb
bdomen-ggl <108941520+bdomen-ggl@users.noreply.github.com>
2023-09-07 21:33:58 +0000

    Fix broken jest tests. (#1204)
    Didn't update a `require` path when moving files around. Fixing this so
    the react tests pass.

--- experimental/reporting-ui/src/main/react/reporting-ui/jest.config.js
+++ experimental/reporting-ui/src/main/react/reporting-ui/jest.config.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const baseConfig = require('../../../test/react/jest/jest_base.config');
+const baseConfig = require('../testing/jest/jest_base.config');
 
 module.exports = {
     ...baseConfig,
