7e5a68be1e5201c6f8822a2fc743bccd26171539
marcopremier <marcopremier@meta.com>
2023-09-15 14:07:17 +0200

    Add ForwardedStorageExchangeWorkflowDaemon

---
+++ src/main/kotlin/org/wfanet/panelmatch/client/storage/forwarded/ForwardedStorageFactory.kt
@@ -0,0 +1,51 @@
+// Copyright 2021 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.panelmatch.client.storage.forwarded
+
+import java.io.File
+import org.wfanet.measurement.common.crypto.readCertificateCollection
+import org.wfanet.measurement.common.grpc.buildTlsChannel
+import org.wfanet.measurement.internal.testing.ForwardedStorageGrpcKt
+import org.wfanet.measurement.storage.StorageClient
+import org.wfanet.measurement.storage.forwarded.ForwardedStorageClient
+import org.wfanet.panelmatch.client.storage.StorageDetails
+import org.wfanet.panelmatch.common.ExchangeDateKey
+import org.wfanet.panelmatch.common.storage.StorageFactory
+import org.wfanet.panelmatch.client.storage.StorageDetails.ForwardedStorage
+import org.wfanet.panelmatch.common.storage.withPrefix
+
+class ForwardedStorageFactory(
+  private val storageDetails: StorageDetails,
+  private val exchangeDateKey: ExchangeDateKey
+) : StorageFactory {
+
+  override fun build(): StorageClient {
+
+    val forwardedStorage = storageDetails.custom.details.unpack(ForwardedStorage::class.java)
+
+
+    return ForwardedStorageClient(
+      ForwardedStorageGrpcKt.ForwardedStorageCoroutineStub(
+        buildTlsChannel(
+          forwardedStorage.target,
+          readCertificateCollection(requireNotNull(File(forwardedStorage.certCollectionPath))),
+          forwardedStorage.forwardedStorageCertHost
+        )
+      )
+    ).withPrefix(exchangeDateKey.path)
+  }
+
+}
+
