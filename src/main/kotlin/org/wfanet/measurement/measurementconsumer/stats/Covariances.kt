b869acc555895d9b0c38a801beef710e7cbf2254
Rieman Li <riemanli@google.com>
2023-09-25 18:43:18 +0000

    Refactor MeasurementStatistics

--- src/main/kotlin/org/wfanet/measurement/measurementconsumer/stats/Covariances.kt
+++ src/main/kotlin/org/wfanet/measurement/measurementconsumer/stats/Covariances.kt
@@ -16,6 +16,11 @@
 
 package org.wfanet.measurement.measurementconsumer.stats
 
+class MeasurementSketchParamsNotMatchCovarianceException(
+  message: String? = null,
+  cause: Throwable? = null
+) : Exception(message, cause)
+
 /** Functions to compute different covariances. */
 object Covariances {
   /**
@@ -25,17 +30,20 @@ object Covariances {
    * Precisely, computes the covariance between any two reach measurements when at least one reach
    * is computed using deterministic count distinct methodology.
    */
-  fun computeDeterministicCovariance(reachCovarianceParams: ReachCovarianceParams): Double {
+  fun computeDeterministicCovariance(
+    reachMeasurementCovarianceParams: ReachMeasurementCovarianceParams
+  ): Double {
     val overlapReach =
-      reachCovarianceParams.reach + reachCovarianceParams.otherReach -
-        reachCovarianceParams.unionReach
+      reachMeasurementCovarianceParams.reach + reachMeasurementCovarianceParams.otherReach -
+        reachMeasurementCovarianceParams.unionReach
     val overlapSamplingWidth =
-      reachCovarianceParams.samplingWidth + reachCovarianceParams.otherSamplingWidth -
-        reachCovarianceParams.unionSamplingWidth
+      reachMeasurementCovarianceParams.samplingWidth +
+        reachMeasurementCovarianceParams.otherSamplingWidth -
+        reachMeasurementCovarianceParams.unionSamplingWidth
     return overlapReach *
       (overlapSamplingWidth /
-        reachCovarianceParams.samplingWidth /
-        reachCovarianceParams.otherSamplingWidth - 1.0)
+        reachMeasurementCovarianceParams.samplingWidth /
+        reachMeasurementCovarianceParams.otherSamplingWidth - 1.0)
   }
 
   /**
@@ -47,20 +55,21 @@ object Covariances {
    */
   fun computeLiquidLegionsCovariance(
     sketchParams: LiquidLegionsSketchParams,
-    reachCovarianceParams: ReachCovarianceParams,
+    reachMeasurementCovarianceParams: ReachMeasurementCovarianceParams,
   ): Double {
     return LiquidLegions.inflatedReachCovariance(
       sketchParams = sketchParams,
-      reach = reachCovarianceParams.reach,
-      otherReach = reachCovarianceParams.otherReach,
+      reach = reachMeasurementCovarianceParams.reach,
+      otherReach = reachMeasurementCovarianceParams.otherReach,
       overlapReach =
-        reachCovarianceParams.reach + reachCovarianceParams.otherReach -
-          reachCovarianceParams.unionReach,
-      samplingWidth = reachCovarianceParams.samplingWidth,
-      otherSamplingWidth = reachCovarianceParams.otherSamplingWidth,
+        reachMeasurementCovarianceParams.reach + reachMeasurementCovarianceParams.otherReach -
+          reachMeasurementCovarianceParams.unionReach,
+      samplingWidth = reachMeasurementCovarianceParams.samplingWidth,
+      otherSamplingWidth = reachMeasurementCovarianceParams.otherSamplingWidth,
       overlapSamplingWidth =
-        reachCovarianceParams.samplingWidth + reachCovarianceParams.otherSamplingWidth -
-          reachCovarianceParams.unionSamplingWidth,
+        reachMeasurementCovarianceParams.samplingWidth +
+          reachMeasurementCovarianceParams.otherSamplingWidth -
+          reachMeasurementCovarianceParams.unionSamplingWidth,
       inflation = 0.0
     )
   }
