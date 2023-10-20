1433581680695c4d0d7c8f40a2430babd6812e01
Phi <lephi@google.com>
2023-10-20 14:32:04 -0400

    Fix the bug that outputs 0 reach if the execution phase crashes right after writing to the output blob. (#1290)
    In the execution phase of reach only protocols, the aggregator first
    initializes the reach to 0. It then runs an existingOutputOr block to
    compute the next token. If the output blob is empty, it updates the
    reach and writes stuff to the output blob. Otherwise, it skips the
    block, reuses the cache results and sends the reach to the kingdom.
    
    There is a problem when the output blob exists, but the aggregator
    crashes before sending the reach to the kingdom. As the existingOutputOr
    block is skipped, the reach will not be updated and will stay at the
    initialized value (i.e. 0). As a result, the kingdom will receive 0.

--- src/test/kotlin/org/wfanet/measurement/duchy/daemon/mill/liquidlegionsv2/ReachOnlyLiquidLegionsV2MillTest.kt
+++ src/test/kotlin/org/wfanet/measurement/duchy/daemon/mill/liquidlegionsv2/ReachOnlyLiquidLegionsV2MillTest.kt
@@ -1266,7 +1266,7 @@ class ReachOnlyLiquidLegionsV2MillTest {
     )
 
     // Stage 1. Process the above computation
-    nonAggregatorMill.pollAndProcessNextComputation()
+    aggregatorMill.pollAndProcessNextComputation()
 
     // Stage 2. Check the status of the computation
     assertThat(fakeComputationDb[LOCAL_ID])
@@ -1806,7 +1806,9 @@ class ReachOnlyLiquidLegionsV2MillTest {
     val inputBlobContext = ComputationBlobContext(GLOBAL_ID, EXECUTION_PHASE.toProtocolStage(), 0L)
     computationStore.writeString(inputBlobContext, "sketch" + NOISE_CIPHERTEXT)
     val cachedBlobContext = ComputationBlobContext(GLOBAL_ID, EXECUTION_PHASE.toProtocolStage(), 1L)
-    computationStore.writeString(cachedBlobContext, "cached result")
+    val testReach = 123L
+    var cryptoResult = completeReachOnlyExecutionPhaseAtAggregatorResponse { reach = testReach }
+    computationStore.writeString(cachedBlobContext, cryptoResult.toByteString().toStringUtf8())
     fakeComputationDb.addComputation(
       partialToken.localComputationId,
       partialToken.computationStage,
@@ -1820,7 +1822,7 @@ class ReachOnlyLiquidLegionsV2MillTest {
     )
 
     // Stage 1. Process the above computation
-    nonAggregatorMill.pollAndProcessNextComputation()
+    aggregatorMill.pollAndProcessNextComputation()
 
     // Stage 2. Check the status of the computation
     assertThat(fakeComputationDb[LOCAL_ID])
@@ -1897,16 +1899,16 @@ class ReachOnlyLiquidLegionsV2MillTest {
           attempt = 1
           computationStage = COMPLETE.toProtocolStage()
           version = 3 // claimTask + writeOutputBlob + transitionStage
-          computationDetails = computationDetails {
-            kingdomComputation = computationDetailsWithVidSamplingWidth.kingdomComputation
-            reachOnlyLiquidLegionsV2 =
-              computationDetailsWithVidSamplingWidth.reachOnlyLiquidLegionsV2
-            endingState = CompletedReason.SUCCEEDED
-          }
+          computationDetails =
+            computationDetailsWithVidSamplingWidth.copy {
+              reachOnlyLiquidLegionsV2 =
+                computationDetailsWithVidSamplingWidth.reachOnlyLiquidLegionsV2
+              endingState = CompletedReason.SUCCEEDED
+            }
           requisitions.addAll(REQUISITIONS)
         }
       )
-    assertThat(computationStore.get(blobKey)?.readToString()).isNotEmpty()
+    assertThat(computationStore.get(blobKey)?.readToString()).isEmpty()
 
     assertThat(systemComputationResult.name).isEqualTo("computations/$GLOBAL_ID")
     // The signature is non-deterministic, so we only verity the encryption is not empty.
@@ -1979,7 +1981,7 @@ class ReachOnlyLiquidLegionsV2MillTest {
     )
 
     // Stage 1. Process the above computation
-    nonAggregatorMill.pollAndProcessNextComputation()
+    aggregatorMill.pollAndProcessNextComputation()
 
     // Stage 2. Check the status of the computation
     assertThat(fakeComputationDb[LOCAL_ID]!!)
