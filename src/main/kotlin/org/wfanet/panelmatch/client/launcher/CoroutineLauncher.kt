b50389f3cab452f8d9db4aaaf7b77c4fff16750e
Steven Ware Jones <stevenwarejones@gmail.com>
2024-01-03 09:45:21 -0800

    add maxCoroutines parameter to CoroutineLauncher (#1383)
    The current CoroutineLauncher has not limit on the number of coroutines
    it launches. This works well on a platform like DataflowRunner but
    performs poorly with the DirectExecutor. This PR
    1. Adds a parameter to specify the max number of jobs that should be
    executed in parallel with the CoroutineLauncher.
    2. Sets the max number of jobs to 1 for the Aws example daemon.

--- src/main/kotlin/org/wfanet/panelmatch/client/launcher/CoroutineLauncher.kt
+++ src/main/kotlin/org/wfanet/panelmatch/client/launcher/CoroutineLauncher.kt
@@ -20,6 +20,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.plus
+import kotlinx.coroutines.sync.Semaphore
 import org.wfanet.measurement.api.v2alpha.CanonicalExchangeStepAttemptKey
 import org.wfanet.measurement.api.v2alpha.ExchangeStep
 import org.wfanet.panelmatch.client.launcher.ExchangeStepValidator.ValidatedExchangeStep
@@ -27,14 +28,19 @@ import org.wfanet.panelmatch.client.launcher.ExchangeStepValidator.ValidatedExch
 /** Executes an [ExchangeStep] in a new coroutine in [scope]. */
 class CoroutineLauncher(
   private val scope: CoroutineScope = CoroutineScope(Dispatchers.Default),
-  private val stepExecutor: ExchangeStepExecutor
+  private val stepExecutor: ExchangeStepExecutor,
+  maxCoroutines: Int? = null
 ) : JobLauncher {
+  private val semaphore = if (maxCoroutines !== null) Semaphore(maxCoroutines) else null
+
   override suspend fun execute(
     step: ValidatedExchangeStep,
     attemptKey: CanonicalExchangeStepAttemptKey
   ) {
     (scope + SupervisorJob()).launch(CoroutineName(attemptKey.toName())) {
+      if (semaphore !== null) semaphore.acquire()
       stepExecutor.execute(step, attemptKey)
+      if (semaphore !== null) semaphore.release()
     }
   }
 }
