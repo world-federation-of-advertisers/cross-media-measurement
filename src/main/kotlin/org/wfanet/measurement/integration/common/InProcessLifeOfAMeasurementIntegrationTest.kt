6351b2672e2a182777131b6d86cc89c51d6d81b9
Rieman <riemanli@google.com>
2024-01-24 11:18:00 -0800

    Fix reporting integration tests and EDP simulator tests (#1423)
    * Extracted the common code of sampling VIDs to a function `sampleVids`.
    Affected files:
        * src/main/kotlin/org/wfanet/measurement/loadtest/dataprovider/EdpSimulator.kt
        * src/main/kotlin/org/wfanet/measurement/loadtest/measurementconsumer/MeasurementConsumerSimulator.kt
        * src/main/kotlin/org/wfanet/measurement/integration/common/reporting/v2/InProcessLifeOfAReportIntegrationTest.kt
    * Computed expected values by VIDs instead of hardcoded values in EDP simulator tests.
    * Calculated tolerance for result check using metric variance in reporting integration tests.
    * Implemented impression metric result check in the reporting integration tests.
    * Fixed bug in reporting integration tests. Bug lists:
        * Use one event group to compute expected value when there are two in the reporting set.
        * The expected value of unique reach in a report is logically incorrect.
        * Report with cumulative doesn't set cumulative to true.
    * Added direct reach-only test to src/main/kotlin/org/wfanet/measurement/integration/common/InProcessLifeOfAMeasurementIntegrationTest.kt

--- src/main/kotlin/org/wfanet/measurement/integration/common/InProcessLifeOfAMeasurementIntegrationTest.kt
+++ src/main/kotlin/org/wfanet/measurement/integration/common/InProcessLifeOfAMeasurementIntegrationTest.kt
@@ -133,6 +133,13 @@ abstract class InProcessLifeOfAMeasurementIntegrationTest(
     }
 
   @Test
+  fun `create a direct reach-only measurement and check the result is equal to the expected result`() =
+    runBlocking {
+      // Use frontend simulator to create a direct reach-only measurement and verify its result.
+      mcSimulator.testDirectReachOnly("1234")
+    }
+
+  @Test
   fun `create a reach-only measurement and check the result is equal to the expected result`() =
     runBlocking {
       // Use frontend simulator to create a reach and frequency measurement and verify its result.
