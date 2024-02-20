world-federation-of-advertisers/cross-media-measurement

commit 532e4f259d4daefbcc9f0e435a90a5983224551d

cherry pick beam main shaded jar (#1490)
Cherry picks the following
93af154e04a55e389053634ff82b3856dd20571f
49b7059680c2a6503d2f9ba2fd77b555680df9a5
EMR Screenshot

![image](https://github.com/world-federation-of-advertisers/cross-media-measurement/assets/12742391/b18e77cd-9d15-4bf5-a3bd-6d3851da0c68)

---------

Co-authored-by: jonmolle <86323092+jonmolle@users.noreply.github.com>

WORKSPACE
@@ -111,6 +111,7 @@ MAVEN_ARTIFACTS_DICT = dict(common_jvm_maven_artifacts_dict().items() + {
     "com.google.cloud:google-cloud-security-private-ca": "2.3.1",
     "org.apache.beam:beam-runners-direct-java": APACHE_BEAM_VERSION,
     "org.apache.beam:beam-runners-google-cloud-dataflow-java": APACHE_BEAM_VERSION,
+    "org.apache.beam:beam-runners-spark": APACHE_BEAM_VERSION,
     "org.apache.beam:beam-sdks-java-io-google-cloud-platform": APACHE_BEAM_VERSION,
     "org.slf4j:slf4j-simple": "1.7.32",
     "software.amazon.awssdk:sts": AWS_SDK_VERSION,
@@ -118,7 +119,7 @@ MAVEN_ARTIFACTS_DICT = dict(common_jvm_maven_artifacts_dict().items() + {
     "software.amazon.awssdk:acmpca": AWS_SDK_VERSION,
 }.items())
 
-EXCLUDED_MAVEN_ARTIFACTS = [x for x in COMMON_JVM_EXCLUDED_ARTIFACTS if x != "org.slf4j:slf4j-log4j12"] + ["org.apache.beam:beam-sdks-java-io-kafka"]
+EXCLUDED_MAVEN_ARTIFACTS = ["org.apache.beam:beam-sdks-java-io-kafka"]
 
 maven_install(
     artifacts = artifacts.dict_to_list(MAVEN_ARTIFACTS_DICT),
@@ -160,3 +161,18 @@ go_repository(
 load("@wfa_common_jvm//build:common_jvm_extra_deps.bzl", "common_jvm_extra_deps")
 
 common_jvm_extra_deps()
+
+load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
+
+git_repository(
+    name = "com_github_johnynek_bazel_jar_jar",
+    commit = "4e7bf26da8bc8c955578fd8c8a2c763757d344df",  # Latest commit SHA as of 2023/10/31
+    remote = "https://github.com/johnynek/bazel_jar_jar.git",
+)
+
+load(
+    "@com_github_johnynek_bazel_jar_jar//:jar_jar.bzl",
+    "jar_jar_repositories",
+)
+
+jar_jar_repositories()

build/versions.bzl
@@ -21,7 +21,7 @@
 # * https://cloud.google.com/dataflow/docs/support/sdk-version-support-status#apache-beam-2.x-sdks
 # * https://beam.apache.org/documentation/runners/flink/#flink-version-compatibility
 # * https://docs.aws.amazon.com/kinesisanalytics/latest/java/earlier.html
-APACHE_BEAM_VERSION = "2.38.0"
+APACHE_BEAM_VERSION = "2.45.0"
 
 AWS_SDK_VERSION = "2.17.100"
 

imports/java/org/apache/beam/runners/spark/BUILD.bazel
@@ -1,0 +1,6 @@
+package(default_visibility = ["//visibility:public"])
+
+alias(
+    name = "spark",
+    actual = "@maven//:org_apache_beam_beam_runners_spark",
+)

src/main/kotlin/org/wfanet/panelmatch/client/deploy/example/aws/AwsExampleDaemon.kt
@@ -47,7 +47,7 @@ import software.amazon.awssdk.services.s3.S3AsyncClient
   name = "AwsExampleDaemon",
   description = ["Example daemon to execute ExchangeWorkflows on AWS"],
   mixinStandardHelpOptions = true,
-  showDefaultValues = true
+  showDefaultValues = true,
 )
 private class AwsExampleDaemon : ExampleDaemon() {
   @Mixin private lateinit var caFlags: CertificateAuthorityFlags
@@ -55,7 +55,7 @@ private class AwsExampleDaemon : ExampleDaemon() {
   @Option(
     names = ["--certificate-authority-arn"],
     description = ["AWS Certificate Authority ARN"],
-    required = true
+    required = true,
   )
   lateinit var certificateAuthorityArn: String
     private set
@@ -77,7 +77,7 @@ private class AwsExampleDaemon : ExampleDaemon() {
   @set:Option(
     names = ["--s3-from-beam"],
     description = ["Whether to configure s3 access from Apache Beam."],
-    defaultValue = "true"
+    defaultValue = "false",
   )
   private var s3FromBeam by Delegates.notNull<Boolean>()
 
@@ -137,11 +137,7 @@ private class AwsExampleDaemon : ExampleDaemon() {
     get() = defaults.sharedStorageInfo
 
   override val certificateAuthority by lazy {
-    CertificateAuthority(
-      caFlags.context,
-      certificateAuthorityArn,
-      PrivateCaClient(),
-    )
+    CertificateAuthority(caFlags.context, certificateAuthorityArn, PrivateCaClient())
   }
 }
 

src/main/kotlin/org/wfanet/panelmatch/client/storage/aws/s3/S3StorageFactory.kt
@@ -33,7 +33,7 @@ import software.amazon.awssdk.services.sts.model.AssumeRoleRequest
 /** [StorageFactory] for [S3StorageClient]. */
 class S3StorageFactory(
   private val storageDetails: StorageDetails,
-  private val exchangeDateKey: ExchangeDateKey
+  private val exchangeDateKey: ExchangeDateKey,
 ) : StorageFactory {
 
   override fun build(options: PipelineOptions?): StorageClient {
@@ -41,9 +41,12 @@ class S3StorageFactory(
       return build()
     }
     val beamOptions = options.`as`(BeamOptions::class.java)
-    val accessKey = beamOptions.awsAccessKey
-    val secretAccessKey = beamOptions.awsSecretAccessKey
-    val sessionToken = beamOptions.awsSessionToken
+    @Suppress("USELESS_ELVIS") // Beam returns String?
+    val accessKey = beamOptions.awsAccessKey ?: ""
+    @Suppress("USELESS_ELVIS") // Beam returns String?
+    val secretAccessKey = beamOptions.awsSecretAccessKey ?: ""
+    @Suppress("USELESS_ELVIS") // Beam returns String?
+    val sessionToken = beamOptions.awsSessionToken ?: ""
     if (accessKey.isEmpty() || secretAccessKey.isEmpty() || sessionToken.isEmpty()) {
       return build()
     }
@@ -53,7 +56,7 @@ class S3StorageFactory(
           .region(Region.of(storageDetails.aws.region))
           .credentialsProvider(StaticCredentialsProvider.create(builtCredentials))
           .build(),
-        storageDetails.aws.bucket
+        storageDetails.aws.bucket,
       )
       .withPrefix(exchangeDateKey.path)
   }
@@ -62,7 +65,7 @@ class S3StorageFactory(
     if (storageDetails.aws.role.roleArn.isEmpty()) {
       return S3StorageClient(
           S3AsyncClient.builder().region(Region.of(storageDetails.aws.region)).build(),
-          storageDetails.aws.bucket
+          storageDetails.aws.bucket,
         )
         .withPrefix(exchangeDateKey.path)
     } else {
@@ -88,7 +91,7 @@ class S3StorageFactory(
                 .build()
             )
             .build(),
-          storageDetails.aws.bucket
+          storageDetails.aws.bucket,
         )
         .withPrefix(exchangeDateKey.path)
     }

src/main/kotlin/org/wfanet/panelmatch/client/tools/BUILD.bazel
@@ -1,5 +1,9 @@
 load("@io_bazel_rules_kotlin//kotlin:jvm.bzl", "kt_jvm_library")
 load("@rules_java//java:defs.bzl", "java_binary")
+load(
+    "@com_github_johnynek_bazel_jar_jar//:jar_jar.bzl",
+    "jar_jar",
+)
 
 package(default_visibility = [
     "//src/main/kotlin/org/wfanet/panelmatch:__subpackages__",
@@ -70,6 +74,61 @@ java_binary(
     name = "SerializeExchangeWorkflow",
     main_class = "org.wfanet.panelmatch.client.tools.SerializeExchangeWorkflowKt",
     runtime_deps = [":serialize_exchange_workflow"],
+)
+
+kt_jvm_library(
+    name = "beam_jobs_main",
+    testonly = True,
+    srcs = ["BeamJobsMain.kt"],
+    deps = [
+        "//imports/java/org/apache/beam/runners/spark",
+        "//src/main/kotlin/org/wfanet/panelmatch/client/deploy",
+        "//src/main/kotlin/org/wfanet/panelmatch/client/deploy/example",
+        "//src/main/kotlin/org/wfanet/panelmatch/client/privatemembership",
+        "//src/main/kotlin/org/wfanet/panelmatch/client/storage",
+        "//src/main/kotlin/org/wfanet/panelmatch/client/storage/aws/s3",
+        "//src/main/kotlin/org/wfanet/panelmatch/client/storage/gcloud/gcs",
+        "//src/main/kotlin/org/wfanet/panelmatch/common",
+        "//src/main/kotlin/org/wfanet/panelmatch/common/certificates/aws",
+        "//src/main/kotlin/org/wfanet/panelmatch/common/certificates/gcloud",
+        "//src/main/kotlin/org/wfanet/panelmatch/common/certificates/testing",
+        "//src/main/proto/wfa/panelmatch/client/eventpreprocessing:combined_events_kt_jvm_proto",
+        "//src/main/proto/wfa/panelmatch/client/exchangetasks:join_key_exchange_kt_jvm_proto",
+        "//src/main/proto/wfa/panelmatch/client/privatemembership:decrypt_event_data_kt_jvm_proto",
+        "//src/main/proto/wfa/panelmatch/client/tools:data_provider_event_set_kt_jvm_proto",
+        "@wfa_common_jvm//imports/java/com/google/protobuf",
+        "@wfa_common_jvm//imports/java/picocli",
+        "@wfa_common_jvm//src/main/kotlin/org/wfanet/measurement/common",
+        "@wfa_common_jvm//src/main/kotlin/org/wfanet/measurement/common/crypto/testing",
+        "@wfa_common_jvm//src/main/kotlin/org/wfanet/measurement/storage:client",
+        "@wfa_common_jvm//src/main/kotlin/org/wfanet/measurement/storage/testing",
+        "@wfa_virtual_people_common//src/main/proto/wfa/virtual_people/common:event_kt_jvm_proto",
+    ],
+)
+
+java_binary(
+    name = "BeamJobsMain",
+    testonly = True,
+    main_class = "org.wfanet.panelmatch.client.tools.BeamJobsMainKt",
+    tags = ["manual"],
+    runtime_deps = [
+        ":beam_jobs_main",
+    ],
+)
+
+jar_jar(
+    name = "shaded_BeamJobsMain",
+    inline_rules = [
+        "rule com.google.protobuf.** shaded.com.google.protobuf.@1",
+        "rule com.google.common.** shaded.google.common.@1",
+        "rule com.fasterxml.jackson.** shaded.com.fasterxml.jackson.@1",
+        "rule org.bouncycastle.** shaded.org.bouncycastle.@1",
+        "rule org.apache.log4j.** shaded.org.apache.log4j.@1",
+        "rule org.slf4j.** shaded.org.slf4j.@1",
+        "rule org.apache.logging.log4j.** shaded.apache.logging.log4j.@1",
+        "rule org.apache.logging.slf4j.** shaded.apache.looging.slf4j.@1",
+    ],
+    input_jar = ":BeamJobsMain_deploy.jar",
 )
 
 kt_jvm_library(

src/main/kotlin/org/wfanet/panelmatch/client/tools/BeamJobsMain.kt
@@ -1,0 +1,255 @@
+// Copyright 2022 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.wfanet.panelmatch.client.tools
+
+import com.google.protobuf.ByteString
+import java.io.File
+import java.time.Clock
+import java.time.Duration
+import java.time.LocalDate
+import java.util.logging.Level
+import kotlin.properties.Delegates
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.runBlocking
+import org.apache.beam.runners.spark.SparkRunner
+import org.apache.beam.sdk.options.PipelineOptions
+import org.apache.beam.sdk.options.PipelineOptionsFactory
+import org.apache.beam.sdk.options.SdkHarnessOptions
+import org.wfanet.measurement.api.v2alpha.ExchangeStepAttemptKey
+import org.wfanet.measurement.api.v2alpha.ExchangeWorkflow
+import org.wfanet.measurement.aws.s3.S3Flags
+import org.wfanet.measurement.aws.s3.S3StorageClient
+import org.wfanet.measurement.common.commandLineMain
+import org.wfanet.measurement.common.throttler.MinimumIntervalThrottler
+import org.wfanet.measurement.gcloud.gcs.GcsFromFlags
+import org.wfanet.measurement.gcloud.gcs.GcsStorageClient
+import org.wfanet.measurement.storage.StorageClient
+import org.wfanet.measurement.storage.filesystem.FileSystemStorageClient
+import org.wfanet.panelmatch.client.common.ExchangeContext
+import org.wfanet.panelmatch.client.common.TaskParameters
+import org.wfanet.panelmatch.client.deploy.ProductionExchangeTaskMapper
+import org.wfanet.panelmatch.client.eventpreprocessing.PreprocessingParameters
+import org.wfanet.panelmatch.client.exchangetasks.ExchangeTask
+import org.wfanet.panelmatch.client.exchangetasks.ExchangeTaskMapper
+import org.wfanet.panelmatch.client.storage.FileSystemStorageFactory
+import org.wfanet.panelmatch.client.storage.PrivateStorageSelector
+import org.wfanet.panelmatch.client.storage.SharedStorageSelector
+import org.wfanet.panelmatch.client.storage.StorageDetails
+import org.wfanet.panelmatch.client.storage.StorageDetailsProvider
+import org.wfanet.panelmatch.client.storage.aws.s3.S3StorageFactory
+import org.wfanet.panelmatch.client.storage.gcloud.gcs.GcsStorageFactory
+import org.wfanet.panelmatch.common.ExchangeDateKey
+import org.wfanet.panelmatch.common.certificates.testing.TestCertificateManager
+import org.wfanet.panelmatch.common.loggerFor
+import org.wfanet.panelmatch.common.secrets.StorageClientSecretMap
+import org.wfanet.panelmatch.common.storage.StorageFactory
+import org.wfanet.panelmatch.common.storage.toByteString
+import org.wfanet.panelmatch.common.storage.withPrefix
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Model.CommandSpec
+import picocli.CommandLine.Option
+import picocli.CommandLine.ParameterException
+import picocli.CommandLine.Spec
+
+private const val BUFFER_SIZE_BYTES = 32 * 1024 * 1024 // 32MiB
+
+@Command(
+  name = "beam-jobs-main",
+  description = ["Runs a singleton Beam task produced by an exchange."],
+)
+class BeamJobsMain : Runnable {
+
+  @Spec lateinit var spec: CommandSpec
+
+  val commandLine: CommandLine
+    get() = spec.commandLine()
+
+  @CommandLine.Mixin private lateinit var gcsFlags: GcsFromFlags.Flags
+  @CommandLine.Mixin private lateinit var s3Flags: S3Flags
+
+  val clock: Clock = Clock.systemUTC()
+
+  @Option(
+    names = ["--root-directory"],
+    description =
+      [
+        "Root filesystem path to read from. If using a filesystem storage client, " +
+          "will also be the root private storage directory."
+      ],
+    required = false,
+  )
+  private lateinit var rootDirectory: File
+
+  @Option(
+    names = ["--exchange-workflow-blob-key"],
+    description =
+      [
+        "The decrypt exchange step to run. Can be made manually, or serialized from " +
+          "an existing workflow."
+      ],
+    required = true,
+  )
+  private lateinit var exchangeWorkflowBlobKey: String
+
+  @set:Option(names = ["--step-index"], description = ["Index of step."], required = true)
+  var stepIndex by Delegates.notNull<Int>()
+    private set
+
+  private val serializedExchangeWorkflow by lazy {
+    runBlocking { rootStorageClient.getBlob(exchangeWorkflowBlobKey)!!.toByteString() }
+  }
+
+  @Option(
+    names = ["--exchange-step-attempt-resource-id"],
+    description =
+      [
+        "Resource ID for the decrypt exchange step attempt. If not tied to an " +
+          "existing exchange, the only reason to set this is to keep track of your own " +
+          "attempt counts."
+      ],
+    required = true,
+  )
+  private lateinit var exchangeStepAttemptResourceId: String
+
+  @Option(
+    names = ["--exchange-date"],
+    description = ["Date in format of YYYY-MM-DD"],
+    required = true,
+  )
+  private lateinit var exchangeDate: String
+
+  @Option(
+    names = ["--storage-type"],
+    description = ["Type of destination storage: \${COMPLETION-CANDIDATES}"],
+    required = true,
+  )
+  private lateinit var storageType: StorageDetails.PlatformCase
+
+  private val rootStorageClient: StorageClient by lazy {
+    when (storageType) {
+      StorageDetails.PlatformCase.AWS -> S3StorageClient.fromFlags(s3Flags)
+      StorageDetails.PlatformCase.GCS -> GcsStorageClient.fromFlags(GcsFromFlags(gcsFlags))
+      StorageDetails.PlatformCase.FILE -> FileSystemStorageClient(rootDirectory)
+      else ->
+        throw ParameterException(
+          commandLine,
+          "Unsupported storage type. Must be one of: AWS, GCS, FILE",
+        )
+    }
+  }
+
+  private val privateStorageInfo: StorageDetailsProvider by lazy {
+    val storageClient = rootStorageClient.withPrefix("private-storage-info")
+    StorageDetailsProvider(StorageClientSecretMap(storageClient))
+  }
+
+  private fun makePipelineOptions(): PipelineOptions {
+    return PipelineOptionsFactory.`as`(SdkHarnessOptions::class.java).apply {
+      runner = SparkRunner::class.java
+      defaultSdkHarnessLogLevel = SdkHarnessOptions.LogLevel.TRACE
+    }
+  }
+
+  /** [PrivateStorageSelector] for writing to local (non-shared) storage. */
+  private val privateStorageSelector: PrivateStorageSelector by lazy {
+    PrivateStorageSelector(supportedStorageFactories, privateStorageInfo)
+  }
+
+  /** [SharedStorageSelector] for writing to shared storage. Not used, dummy declaration only. */
+  private val sharedStorageSelector: SharedStorageSelector by lazy {
+    SharedStorageSelector(TestCertificateManager, supportedStorageFactories, privateStorageInfo)
+  }
+
+  private val exchangeTaskMapper: ExchangeTaskMapper by lazy {
+    ProductionExchangeTaskMapper(
+      inputTaskThrottler = MinimumIntervalThrottler(clock, Duration.ofHours(24)), // Not used
+      privateStorageSelector = privateStorageSelector,
+      sharedStorageSelector = sharedStorageSelector, // Not used here.
+      certificateManager = TestCertificateManager, // Not used here.
+      makePipelineOptions = ::makePipelineOptions,
+      taskContext =
+        TaskParameters( // Not used. Set to defaults for consistency.
+          setOf(PreprocessingParameters(maxByteSize = 1000000, fileCount = 1000))
+        ),
+    )
+  }
+
+  override fun run() = runBlocking {
+    val exchangeWorkflow = ExchangeWorkflow.parseFrom(serializedExchangeWorkflow)
+    val step = exchangeWorkflow.getSteps(stepIndex)!!
+
+    require(
+      step.stepCase == ExchangeWorkflow.Step.StepCase.DECRYPT_PRIVATE_MEMBERSHIP_QUERY_RESULTS_STEP
+    ) {
+      "The only step type currently supported is DECRYPT_PRIVATE_MEMBERSHIP_QUERY_RESULTS_STEP"
+    }
+
+    val attemptKey = requireNotNull(ExchangeStepAttemptKey.fromName(exchangeStepAttemptResourceId))
+
+    val exchangeContext =
+      ExchangeContext(attemptKey, LocalDate.parse(exchangeDate), exchangeWorkflow, step)
+
+    val privateStorageClient: StorageClient =
+      privateStorageSelector.getStorageClient(exchangeContext.exchangeDateKey)
+
+    val exchangeTask: ExchangeTask = exchangeTaskMapper.getExchangeTaskForStep(exchangeContext)
+    logger.log(Level.INFO, "Reading Inputs")
+    val taskInput: Map<String, StorageClient.Blob> =
+      if (exchangeTask.skipReadInput()) emptyMap() else readInputs(step, privateStorageClient)
+    logger.log(Level.INFO, "Executing Exchange Task")
+    val taskOutput: Map<String, Flow<ByteString>> = exchangeTask.execute(taskInput)
+    logger.log(Level.INFO, "Writing Outputs")
+    writeOutputs(step, taskOutput, privateStorageClient)
+  }
+
+  private suspend fun readInputs(
+    step: ExchangeWorkflow.Step,
+    privateStorage: StorageClient,
+  ): Map<String, StorageClient.Blob> {
+    return step.inputLabelsMap.mapValues { (label, blobKey) ->
+      requireNotNull(privateStorage.getBlob(blobKey)) {
+        "Missing blob key '$blobKey' for input label '$label'"
+      }
+    }
+  }
+
+  private suspend fun writeOutputs(
+    step: ExchangeWorkflow.Step,
+    taskOutput: Map<String, Flow<ByteString>>,
+    privateStorage: StorageClient,
+  ) {
+    for ((genericLabel, flow) in taskOutput) {
+      val blobKey =
+        requireNotNull(step.outputLabelsMap[genericLabel]) {
+          "Missing $genericLabel in outputLabels for step: $step"
+        }
+      privateStorage.writeBlob(blobKey, flow)
+    }
+  }
+
+  companion object {
+    val supportedStorageFactories:
+      Map<StorageDetails.PlatformCase, (StorageDetails, ExchangeDateKey) -> StorageFactory> =
+      mapOf(
+        StorageDetails.PlatformCase.AWS to ::S3StorageFactory,
+        StorageDetails.PlatformCase.FILE to ::FileSystemStorageFactory,
+        StorageDetails.PlatformCase.GCS to ::GcsStorageFactory,
+      )
+    private val logger by loggerFor()
+  }
+}
+
+fun main(args: Array<String>) = commandLineMain(BeamJobsMain(), args)

src/test/kotlin/org/wfanet/panelmatch/client/eventpreprocessing/BatchingDoFnTest.kt
@@ -81,19 +81,22 @@ class BatchingDoFnTest : BeamTestBase() {
         mutableListOf("22"),
         mutableListOf("33"),
         mutableListOf("3", "3"),
-        mutableListOf("3")
+        mutableListOf("3"),
       )
   }
-  private fun makeParDo(maxByteSize: Long): ParDo.SingleOutput<String, MutableList<String>>? {
-    return ParDo.of(BatchingDoFn(maxByteSize, StringLengthSize))
+
+  private fun makeParDo(maxByteSize: Long): ParDo.SingleOutput<String, MutableList<String>> {
+    return ParDo.of(BatchingDoFn(maxByteSize, StringLengthSize))!!
   }
+
   private fun makeTestStream(): TestStream.Builder<String> {
     return TestStream.create(StringUtf8Coder.of())
   }
+
   private fun batchSingleBundle(
     batchSize: Long,
     item: String,
-    vararg items: String
+    vararg items: String,
   ): PCollection<MutableList<String>> {
     val testStream = makeTestStream().addElements(item, *items).advanceWatermarkToInfinity()
     return pipeline.apply(testStream).apply(makeParDo(batchSize))

commit 636ffefa45ef273c0155d537ef5d7f58f5fd1f8b

Fix test failures introduced in 532e4f259d4daefbcc9f0e435a90a5983224551d.
This fixes the following errors from an improper cherry pick in #1490:

* Dropped Maven artifact exclusions resulting in Spanner breakage.
* Used different version of bazel_jar_jar than in source revision.

WORKSPACE
@@ -1,5 +1,6 @@
 workspace(name = "wfa_measurement_system")
 
+load("//build:repositories.bzl", "wfa_measurement_system_repositories")
 load(
     "//build:versions.bzl",
     "APACHE_BEAM_VERSION",
@@ -7,7 +8,6 @@ load(
     "K8S_CLIENT_VERSION",
     "OPEN_TELEMETRY_SDK_VERSION",
 )
-load("//build:repositories.bzl", "wfa_measurement_system_repositories")
 
 wfa_measurement_system_repositories()
 
@@ -89,15 +89,19 @@ load("//build/grpc_health_probe:repo.bzl", "grpc_health_probe")
 
 grpc_health_probe()
 
+load("@rules_jvm_external//:defs.bzl", "maven_install")
+
 # Maven
 load(
     "@wfa_common_jvm//build:common_jvm_maven.bzl",
-    "COMMON_JVM_EXCLUDED_ARTIFACTS",
     "COMMON_JVM_MAVEN_OVERRIDE_TARGETS",
     "common_jvm_maven_artifacts_dict",
 )
-load("@rules_jvm_external//:defs.bzl", "maven_install")
 load("@wfa_common_jvm//build/maven:artifacts.bzl", "artifacts")
+load(
+    "@wfa_common_jvm//build/rules_proto:repo.bzl",
+    RULES_PROTO_EXCLUDED_ARTIFACTS = "EXCLUDED_ARTIFACTS",
+)
 
 MAVEN_ARTIFACTS_DICT = dict(common_jvm_maven_artifacts_dict().items() + {
     "software.aws.rds:aws-postgresql-jdbc": "0.1.0",
@@ -119,7 +123,7 @@ MAVEN_ARTIFACTS_DICT = dict(common_jvm_maven_artifacts_dict().items() + {
     "software.amazon.awssdk:acmpca": AWS_SDK_VERSION,
 }.items())
 
-EXCLUDED_MAVEN_ARTIFACTS = ["org.apache.beam:beam-sdks-java-io-kafka"]
+EXCLUDED_MAVEN_ARTIFACTS = RULES_PROTO_EXCLUDED_ARTIFACTS + ["org.apache.beam:beam-sdks-java-io-kafka"]
 
 maven_install(
     artifacts = artifacts.dict_to_list(MAVEN_ARTIFACTS_DICT),
@@ -161,14 +165,6 @@ go_repository(
 load("@wfa_common_jvm//build:common_jvm_extra_deps.bzl", "common_jvm_extra_deps")
 
 common_jvm_extra_deps()
-
-load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
-
-git_repository(
-    name = "com_github_johnynek_bazel_jar_jar",
-    commit = "4e7bf26da8bc8c955578fd8c8a2c763757d344df",  # Latest commit SHA as of 2023/10/31
-    remote = "https://github.com/johnynek/bazel_jar_jar.git",
-)
 
 load(
     "@com_github_johnynek_bazel_jar_jar//:jar_jar.bzl",

build/repositories.bzl
@@ -154,3 +154,10 @@ def wfa_measurement_system_repositories():
         strip_prefix = "rules_webpack-0.12.0",
         url = "https://github.com/aspect-build/rules_webpack/releases/download/v0.12.0/rules_webpack-v0.12.0.tar.gz",
     )
+
+    http_archive(
+        name = "com_github_johnynek_bazel_jar_jar",
+        sha256 = "f766a2143bf11f70a7de67798db6cd3851fedc77b7037162e052fee6207fa5ca",
+        strip_prefix = "bazel_jar_jar-0.1.0",
+        url = "https://github.com/bazeltools/bazel_jar_jar/releases/download/v0.1.0/bazel_jar_jar-v0.1.0.tar.gz",
+    )