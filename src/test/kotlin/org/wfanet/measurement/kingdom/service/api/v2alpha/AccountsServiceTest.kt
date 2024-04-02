1ea26ee9ce9c5e56b943bc31b5e57d9f925755ee
Joe Corilla <10494069+jcorilla@users.noreply.github.com>
2024-04-02 22:07:16 +0000

    Update test expectations

--- src/test/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/AccountsServiceTest.kt
+++ src/test/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/AccountsServiceTest.kt
@@ -528,10 +528,7 @@ class AccountsServiceTest {
 
     assertThat(exception.status.code).isEqualTo(Status.Code.PERMISSION_DENIED)
     assertThat(exception.errorInfo?.metadataMap)
-      .containsEntry(
-        "account_activation_state",
-        InternalAccount.ActivationState.ACTIVATED.toActivationState().toString(),
-      )
+      .containsEntry("account_activation_state", Account.ActivationState.ACTIVATED.toString())
     assertThat(exception.errorInfo?.metadataMap).containsEntry("account", ACCOUNT_NAME)
   }
 
