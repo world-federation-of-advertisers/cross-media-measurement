711b1afdfce8ce402d400a4d3ea1cf614f617717
Phi Hung Le <lephi@google.com>
2024-04-04 16:25:22 +0000

    Update comment about cryptographic hash collision.

--- src/main/kotlin/org/wfanet/measurement/loadtest/dataprovider/VidToIndexMapGenerator.kt
+++ src/main/kotlin/org/wfanet/measurement/loadtest/dataprovider/VidToIndexMapGenerator.kt
@@ -49,7 +49,9 @@ object VidToIndexMapGenerator {
       hashes.add(Pair(vid, hash))
     }
 
-    // Sorts by the hash values. There are no collisions due to the use of secure cryptography hash.
+    // Sorts by the hash values. There is negligible chance that collisions happen due to the use of
+    // a secure cryptography hash. Let n be the number of items, the chance to have at least 1
+    // collision is at most 2^{2log(n) - 256}.
     hashes.sortBy { it.second }
 
     // Maps the hash values to the unit interval and generates the vid to index and normalized hash
