847f89708ef056518c6147209468f8560184de1d
Tristan Vuong <85768771+tristanvuong2021@users.noreply.github.com>
2024-04-12 12:55:00 -0700

    Add index for update metric calculation spec reporting metrics in create metrics (#1538)

---
+++ src/main/resources/reporting/postgres/add-index-on-metric-calculation-spec-reporting-metrics-table-for-update.sql
@@ -0,0 +1,42 @@
+-- liquibase formatted sql
+
+-- Copyright 2024 The Cross-Media Measurement Authors
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-- Postgres database schema for the Reporting server.
+--
+-- Table hierarchy:
+--   Root
+--   └── MeasurementConsumers
+--       ├── EventGroups
+--       ├── ReportingSets
+--       │   ├── ReportingSetEventGroups
+--       │   ├── PrimitiveReportingSetBases
+--       │   │   └── PrimitiveReportingSetBasisFilters
+--       │   ├── SetExpressions
+--       │   └── WeightedSubsetUnions
+--       │       └── WeightedSubsetUnionPrimitiveReportingSetBases
+--       ├── Metrics
+--       │   └── MetricMeasurements
+--       ├── Measurements
+--       │   └── MeasurementPrimitiveReportingSetBases
+--       ├── MetricCalculationSpecs
+--       └── Reports
+--           ├── ReportTimeIntervals
+--           └── MetricCalculationSpecReportingMetrics
+
+-- changeset tristanvuong2021:add-metric-calculation-spec-reporting-metrics-update-index dbms:postgresl
+-- comment: index is for updating the rows in the table when inserting rows in Metrics
+CREATE INDEX metric_calculation_spec_reporting_metrics_create_metric_request_id
+  ON MetricCalculationSpecReportingMetrics (MeasurementConsumerId, CreateMetricRequestId);
