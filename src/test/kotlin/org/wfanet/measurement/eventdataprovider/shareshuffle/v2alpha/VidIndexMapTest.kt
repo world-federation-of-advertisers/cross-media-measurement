bcbf0833e7b4522e56b52da4db314d94bcf32e3c
kungfucraig <wrightcw@google.com>
2024-05-09 22:18:41 +0000

    Move shareshuffle files into a v2alpha subdirectory

--- src/test/kotlin/org/wfanet/measurement/eventdataprovider/shareshuffle/v2alpha/VidIndexMapTest.kt
+++ src/test/kotlin/org/wfanet/measurement/eventdataprovider/shareshuffle/v2alpha/VidIndexMapTest.kt
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.wfanet.measurement.eventdataprovider.shareshuffle
+package org.wfanet.measurement.eventdataprovider.shareshuffle.v2alpha
 
 import com.google.common.truth.Truth.assertThat
 import com.google.protobuf.ByteString
@@ -27,6 +27,7 @@ import org.wfanet.measurement.api.v2alpha.PopulationSpecValidationException.EndV
 import org.wfanet.measurement.api.v2alpha.PopulationSpecValidationException.VidRangeIndex
 import org.wfanet.measurement.api.v2alpha.populationSpec
 import org.wfanet.measurement.common.toLong
+import org.wfanet.measurement.eventdataprovider.shareshuffle.v2alpha.InMemoryVidIndexMap
 
 @RunWith(JUnit4::class)
 class VidIndexMapTest {
