615aca0fce4235300bd00150b1e48243b9ff605f
renjiez <renjiez@google.com>
2024-08-15 20:19:34 +0000

    Update based on PR comments.

--- src/main/kotlin/org/wfanet/measurement/duchy/service/system/v1alpha/ComputationControlService.kt
+++ src/main/kotlin/org/wfanet/measurement/duchy/service/system/v1alpha/ComputationControlService.kt
@@ -126,7 +126,7 @@ class ComputationControlService(
   override suspend fun getStage(request: GetStageRequest): Stage {
     val stageKey = grpcRequireNotNull(StageKey.fromName(request.name)) { "Invalid Stage name." }
     grpcRequire(stageKey.duchyId == duchyId) {
-      "Unmatched duchyId. request_duchy_id=${stageKey.duchyId}"
+      "Unmatched duchyId. request_duchy_id=${stageKey.duchyId}, service_duchy_id=${duchyId}"
     }
     val internalRequest = internalGetStageRequest { globalComputationId = stageKey.computationId }
     val stage =
