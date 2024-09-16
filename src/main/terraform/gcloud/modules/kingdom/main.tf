4a40851300a41e1b84506b59e29939947c9b59a5
Tristan Vuong <tristanvuong@google.com>
2024-09-16 21:52:55 +0000

    add missing data statement

--- src/main/terraform/gcloud/modules/kingdom/main.tf
+++ src/main/terraform/gcloud/modules/kingdom/main.tf
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+data "google_project" "project" {}
+
 module "kingdom_internal" {
   source = "../workload-identity-user"
 
