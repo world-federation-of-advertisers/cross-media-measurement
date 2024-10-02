5e14dba5286aab39b26787b2b545c3ff1e5edb04
Tristan Vuong <tristanvuong@google.com>
2024-10-02 23:56:19 +0000

    refactor: Improve performance of StreamRequisitions when using update_time

--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/queries/StreamRequisitions.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/queries/StreamRequisitions.kt
@@ -21,16 +21,29 @@ import org.wfanet.measurement.gcloud.spanner.bind
 import org.wfanet.measurement.internal.kingdom.StreamRequisitionsRequest
 import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.readers.RequisitionReader
 
-class StreamRequisitions(requestFilter: StreamRequisitionsRequest.Filter, limit: Int = 0) :
+class StreamRequisitions(request: StreamRequisitionsRequest) :
   SimpleSpannerQuery<RequisitionReader.Result>() {
 
   override val reader =
-    RequisitionReader().fillStatementBuilder {
-      appendWhereClause(requestFilter)
-      appendClause("ORDER BY ExternalDataProviderId ASC, ExternalRequisitionId ASC")
-      if (limit > 0) {
-        appendClause("LIMIT @$LIMIT")
-        bind(LIMIT to limit.toLong())
+    RequisitionReader().apply {
+      val orderByClause =
+        @Suppress("WHEN_ENUM_CAN_BE_NULL_IN_JAVA") // Proto enum fields are never null.
+        when (request.orderBy) {
+          StreamRequisitionsRequest.OrderBy.UPDATE_TIME ->
+            "ORDER BY UpdateTime ASC, ExternalDataProviderId ASC, ExternalRequisitionId ASC"
+          StreamRequisitionsRequest.OrderBy.DEFAULT,
+          StreamRequisitionsRequest.OrderBy.UNRECOGNIZED ->
+            "ORDER BY ExternalDataProviderId ASC, ExternalRequisitionId ASC"
+        }
+      this.orderByClause = orderByClause
+
+      fillStatementBuilder {
+        appendWhereClause(request.filter)
+        appendClause(orderByClause)
+        if (request.limit > 0) {
+          appendClause("LIMIT @$LIMIT")
+          bind(LIMIT to request.limit.toLong())
+        }
       }
     }
 
@@ -59,21 +72,46 @@ class StreamRequisitions(requestFilter: StreamRequisitionsRequest.Filter, limit:
     }
 
     if (filter.hasUpdatedAfter()) {
-      conjuncts.add("Requisitions.UpdateTime > @$UPDATED_AFTER")
-      bind(UPDATED_AFTER to filter.updatedAfter.toGcloudTimestamp())
-    }
-
-    if (filter.externalRequisitionIdAfter != 0L && filter.externalDataProviderIdAfter != 0L) {
-      conjuncts.add(
+      if (filter.externalRequisitionIdAfter != 0L && filter.externalDataProviderIdAfter != 0L) {
+        // CASE implements short-circuiting.
+        conjuncts.add(
+          """
+          CASE
+            WHEN Requisitions.UpdateTime > @$UPDATED_AFTER THEN TRUE
+            WHEN Requisitions.UpdateTime = @$UPDATED_AFTER
+              AND ExternalDataProviderId > @$EXTERNAL_DATA_PROVIDER_ID_AFTER THEN TRUE
+            WHEN Requisitions.UpdateTime = @$UPDATED_AFTER
+              AND ExternalDataProviderId = @$EXTERNAL_DATA_PROVIDER_ID_AFTER
+              AND ExternalRequisitionId > @$EXTERNAL_REQUISITION_ID_AFTER THEN TRUE
+            ELSE FALSE
+          END
         """
-          (ExternalDataProviderId > @$EXTERNAL_DATA_PROVIDER_ID_AFTER
-          OR (ExternalDataProviderId = @$EXTERNAL_DATA_PROVIDER_ID_AFTER
-          AND ExternalRequisitionId > @$EXTERNAL_REQUISITION_ID_AFTER))
+            .trimIndent()
+        )
+        bind(UPDATED_AFTER to filter.updatedAfter.toGcloudTimestamp())
+        bind(EXTERNAL_DATA_PROVIDER_ID_AFTER).to(filter.externalDataProviderIdAfter)
+        bind(EXTERNAL_REQUISITION_ID_AFTER).to(filter.externalRequisitionIdAfter)
+      } else {
+        conjuncts.add("Requisitions.UpdateTime > @$UPDATED_AFTER")
+        bind(UPDATED_AFTER to filter.updatedAfter.toGcloudTimestamp())
+      }
+    } else {
+      if (filter.externalRequisitionIdAfter != 0L && filter.externalDataProviderIdAfter != 0L) {
+        // CASE implements short-circuiting.
+        conjuncts.add(
+          """
+          CASE
+            WHEN ExternalDataProviderId > @$EXTERNAL_DATA_PROVIDER_ID_AFTER THEN TRUE
+            WHEN ExternalDataProviderId = @$EXTERNAL_DATA_PROVIDER_ID_AFTER
+              AND ExternalRequisitionId > @$EXTERNAL_REQUISITION_ID_AFTER THEN TRUE
+            ELSE FALSE
+          END
         """
-          .trimIndent()
-      )
-      bind(EXTERNAL_DATA_PROVIDER_ID_AFTER).to(filter.externalDataProviderIdAfter)
-      bind(EXTERNAL_REQUISITION_ID_AFTER).to(filter.externalRequisitionIdAfter)
+            .trimIndent()
+        )
+        bind(EXTERNAL_DATA_PROVIDER_ID_AFTER).to(filter.externalDataProviderIdAfter)
+        bind(EXTERNAL_REQUISITION_ID_AFTER).to(filter.externalRequisitionIdAfter)
+      }
     }
 
     if (conjuncts.isEmpty()) {
--- src/main/kotlin/org/wfanet/measurement/kingdom/service/internal/testing/RequisitionsServiceTest.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/service/internal/testing/RequisitionsServiceTest.kt
@@ -54,9 +54,11 @@ import org.wfanet.measurement.internal.kingdom.Requisition
 import org.wfanet.measurement.internal.kingdom.RequisitionKt.parentMeasurement
 import org.wfanet.measurement.internal.kingdom.RequisitionRefusal
 import org.wfanet.measurement.internal.kingdom.RequisitionsGrpcKt.RequisitionsCoroutineImplBase as RequisitionsCoroutineService
+import org.wfanet.measurement.internal.kingdom.StreamRequisitionsRequest
 import org.wfanet.measurement.internal.kingdom.StreamRequisitionsRequestKt.filter
 import org.wfanet.measurement.internal.kingdom.cancelMeasurementRequest
 import org.wfanet.measurement.internal.kingdom.fulfillRequisitionRequest
+import org.wfanet.measurement.internal.kingdom.getMeasurementByComputationIdRequest
 import org.wfanet.measurement.internal.kingdom.getMeasurementRequest
 import org.wfanet.measurement.internal.kingdom.getRequisitionRequest
 import org.wfanet.measurement.internal.kingdom.protocolConfig
@@ -517,6 +519,87 @@ abstract class RequisitionsServiceTest<T : RequisitionsCoroutineService> {
     assertThat(requisitions2[0].externalRequisitionId)
       .isGreaterThan(requisitions[0].externalRequisitionId)
   }
+
+  @Test
+  fun `streamRequisitions orders by update time when order_by set to UPDATE_TIME`(): Unit =
+    runBlocking {
+      val measurementConsumer =
+        population.createMeasurementConsumer(
+          dataServices.measurementConsumersService,
+          dataServices.accountsService,
+        )
+      val dataProvider = population.createDataProvider(dataServices.dataProvidersService)
+      val measurement =
+        population.createLlv2Measurement(
+          dataServices.measurementsService,
+          measurementConsumer,
+          "measurement 1",
+          dataProvider,
+        )
+
+      val measurement2 =
+        population.createLlv2Measurement(
+          dataServices.measurementsService,
+          measurementConsumer,
+          "measurement 2",
+          dataProvider,
+        )
+
+      val measurement3 =
+        population.createLlv2Measurement(
+          dataServices.measurementsService,
+          measurementConsumer,
+          "measurement 3",
+          dataProvider,
+        )
+
+      val requisition =
+        dataServices.measurementsService
+          .getMeasurementByComputationId(
+            getMeasurementByComputationIdRequest {
+              externalComputationId = measurement.externalComputationId
+            }
+          )
+          .requisitionsList[0]
+
+      val requisitions: List<Requisition> =
+        service
+          .streamRequisitions(
+            streamRequisitionsRequest {
+              filter = filter {
+                updatedAfter = requisition.updateTime
+                externalDataProviderIdAfter = requisition.externalDataProviderId
+                externalRequisitionIdAfter = requisition.externalRequisitionId
+              }
+              limit = 2
+              orderBy = StreamRequisitionsRequest.OrderBy.UPDATE_TIME
+            }
+          )
+          .toList()
+
+      val requisition2 =
+        dataServices.measurementsService
+          .getMeasurementByComputationId(
+            getMeasurementByComputationIdRequest {
+              externalComputationId = measurement2.externalComputationId
+            }
+          )
+          .requisitionsList[0]
+
+      val requisition3 =
+        dataServices.measurementsService
+          .getMeasurementByComputationId(
+            getMeasurementByComputationIdRequest {
+              externalComputationId = measurement3.externalComputationId
+            }
+          )
+          .requisitionsList[0]
+
+      assertThat(requisitions)
+        .comparingExpectedFieldsOnly()
+        .containsExactly(requisition2, requisition3)
+        .inOrder()
+    }
 
   @Test
   fun `getRequisition returns expected requisition`() = runBlocking {
