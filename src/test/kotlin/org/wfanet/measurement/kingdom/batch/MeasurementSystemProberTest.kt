9bd748d1a505df0f0e8826f4a5b6e0c989851df3
Lin <lindreamdeyi@google.com>
2024-10-17 19:22:53 +0000

    Implemented prober unit tests, update BUILD for both prober and prober tests, and make prober run return created measurement

--- src/test/kotlin/org/wfanet/measurement/kingdom/batch/MeasurementSystemProberTest.kt
+++ src/test/kotlin/org/wfanet/measurement/kingdom/batch/MeasurementSystemProberTest.kt
@@ -17,19 +17,19 @@
 package org.wfanet.measurement.kingdom.batch
 
 import com.google.common.truth.Truth
-import java.nio.file.Path
-import java.nio.file.Paths
-import java.time.Duration
+import com.google.protobuf.ByteString
+import com.google.protobuf.kotlin.toByteString
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import org.mockito.Mockito.any
 import org.mockito.Mockito.`when`
-import org.wfanet.measurement.api.v2alpha.DataProvider
+import org.mockito.kotlin.anyOrNull
 import org.wfanet.measurement.api.v2alpha.DataProvidersGrpcKt
+import org.wfanet.measurement.api.v2alpha.EncryptionPublicKey
+import org.wfanet.measurement.api.v2alpha.EventGroup
 import org.wfanet.measurement.api.v2alpha.EventGroupsGrpcKt
 import org.wfanet.measurement.api.v2alpha.ListEventGroupsResponse
 import org.wfanet.measurement.api.v2alpha.ListMeasurementsResponse
@@ -42,44 +42,55 @@ import org.wfanet.measurement.api.v2alpha.MeasurementsGrpcKt
 import org.wfanet.measurement.api.v2alpha.MeasurementsGrpcKt.MeasurementsCoroutineStub
 import org.wfanet.measurement.api.v2alpha.RequisitionsGrpcKt
 import org.wfanet.measurement.api.v2alpha.dataProvider
+import org.wfanet.measurement.api.v2alpha.eventGroup
 import org.wfanet.measurement.api.v2alpha.listEventGroupsResponse
 import org.wfanet.measurement.api.v2alpha.listMeasurementsResponse
 import org.wfanet.measurement.api.v2alpha.listRequisitionsResponse
 import org.wfanet.measurement.api.v2alpha.measurement
 import org.wfanet.measurement.api.v2alpha.measurementConsumer
+import org.wfanet.measurement.api.v2alpha.setMessage
+import org.wfanet.measurement.api.v2alpha.signedMessage
+import org.wfanet.measurement.common.crypto.SigningKeyHandle
+import org.wfanet.measurement.common.crypto.readCertificate
+import org.wfanet.measurement.common.crypto.testing.loadSigningKey
+import org.wfanet.measurement.common.crypto.tink.loadPublicKey
 import org.wfanet.measurement.common.getRuntimePath
 import org.wfanet.measurement.common.grpc.testing.GrpcTestServerRule
 import org.wfanet.measurement.common.grpc.testing.mockService
+import org.wfanet.measurement.common.pack
+import org.wfanet.measurement.common.readByteString
+import org.wfanet.measurement.common.toProtoTime
+import org.wfanet.measurement.consent.client.common.encryptMessage
+import org.wfanet.measurement.consent.client.common.toEncryptionPublicKey
+import org.wfanet.measurement.consent.client.common.toPublicKeyHandle
+import org.wfanet.measurement.consent.client.measurementconsumer.signEncryptionPublicKey
+import java.io.File
+import java.nio.file.Paths
+import java.time.Clock
+import java.time.Duration
 
 @RunWith(JUnit4::class)
 class MeasurementSystemProberTest {
-  private val measurementConsumerName = "measurementConsumers/some-mc"
-  private val dataProviderNames = listOf("dataProviders/some-dp")
-  private val apiAuthenticationKey = "some-api-key"
-  private val privateKeyDerFile = SECRET_FILES_PATH.resolve("${MC_ID}_cs_private.der").toFile()
-  private val measurementLookbackDuration = Duration.ofDays(1)
-  private val durationBetweenMeasurement = Duration.ofDays(1)
-
   private val measurementConsumersMock:
     MeasurementConsumersGrpcKt.MeasurementConsumersCoroutineImplBase =
     mockService {
-      onBlocking { getMeasurementConsumer(any()) }.thenReturn(MEASUREMENT_CONSUMER)
+      onBlocking { getMeasurementConsumer(anyOrNull()) }.thenReturn(MEASUREMENT_CONSUMER)
     }
 
   private val measurementsMock: MeasurementsGrpcKt.MeasurementsCoroutineImplBase = mockService {
-    onBlocking { listMeasurements(any()) }.thenReturn(LIST_MEASUREMENTS_RESPONSE)
+    onBlocking { listMeasurements(anyOrNull()) }.thenReturn(LIST_MEASUREMENTS_RESPONSE)
   }
 
   private val dataProvidersMock: DataProvidersGrpcKt.DataProvidersCoroutineImplBase = mockService {
-    onBlocking { getDataProvider(any()) }.thenReturn(DATA_PROVIDER)
+    onBlocking { getDataProvider(anyOrNull()) }.thenReturn(DATA_PROVIDER)
   }
 
   private val eventGroupsMock: EventGroupsGrpcKt.EventGroupsCoroutineImplBase = mockService {
-    onBlocking { listEventGroups(any()) }.thenReturn(LIST_EVENT_GROUPS_RESPONSE)
+    onBlocking { listEventGroups(anyOrNull()) }.thenReturn(LIST_EVENT_GROUPS_RESPONSE)
   }
 
   private val requisitionsMock: RequisitionsGrpcKt.RequisitionsCoroutineImplBase = mockService {
-    onBlocking { listRequisitions(any()) }.thenReturn(LIST_REQUISITIONS_RESPONSE)
+    onBlocking { listRequisitions(anyOrNull()) }.thenReturn(LIST_REQUISITIONS_RESPONSE)
   }
 
   private lateinit var measurementConsumersClient: MeasurementConsumersCoroutineStub
@@ -107,12 +118,12 @@ class MeasurementSystemProberTest {
     requisitionsClient = RequisitionsGrpcKt.RequisitionsCoroutineStub(grpcTestServerRule.channel)
     prober =
       MeasurementSystemProber(
-        measurementConsumerName,
-        dataProviderNames,
-        apiAuthenticationKey,
-        privateKeyDerFile,
-        measurementLookbackDuration,
-        durationBetweenMeasurement,
+        MEASUREMENT_CONSUMER_NAME,
+        DATA_PROVIDER_NAMES,
+        API_AUTHENTICATION_KEY,
+        PRIVATE_KEY_DER_FILE,
+        MEASUREMENT_LOOKBACK_DURATION,
+        DURATION_BETWEEN_MEASUREMENT,
         measurementConsumersClient,
         measurementsClient,
         dataProvidersClient,
@@ -122,18 +133,27 @@ class MeasurementSystemProberTest {
   }
 
   @Test
-  fun `run creates a new prober measurement when there are no previous prober measurement`(): Unit =
+  fun `run creates a new prober measurement when there is no previous prober measurement`(): Unit =
     runBlocking {
-      `when`(measurementsMock.listMeasurements(any())).thenReturn(listMeasurementsResponse {})
+      `when`(measurementsMock.listMeasurements(anyOrNull())).thenReturn(listMeasurementsResponse {})
       val measurement = prober.run()
       Truth.assertThat(measurement).isNotNull()
     }
 
   @Test
-  fun `run creates a new prober measurement when most recent issued prober measurement is finished`():
+  fun `run creates a new prober measurement when most recent issued prober measurement is finished a long time ago`():
     Unit = runBlocking {
-    `when`(measurementsMock.listMeasurements(any()))
-      .thenReturn(listMeasurementsResponse { measurements += FINISHED_MEASUREMENT })
+    `when`(measurementsMock.listMeasurements(anyOrNull()))
+      .thenReturn(listMeasurementsResponse { measurements += OLD_FINISHED_MEASUREMENT })
+    val measurement = prober.run()
+    Truth.assertThat(measurement).isNotNull()
+  }
+
+  @Test
+  fun `run creates a new prober measurement when most recent issued prober measurement is finished too recently`():
+    Unit = runBlocking {
+    `when`(measurementsMock.listMeasurements(anyOrNull()))
+      .thenReturn(listMeasurementsResponse { measurements += NEW_FINISHED_MEASUREMENT })
     val measurement = prober.run()
     Truth.assertThat(measurement).isNotNull()
   }
@@ -141,27 +161,90 @@ class MeasurementSystemProberTest {
   @Test
   fun `run does not create a new prober measurement when the most recent issued prober measurement is not finished`():
     Unit = runBlocking {
-    `when`(measurementsMock.listMeasurements(any()))
+    `when`(measurementsMock.listMeasurements(anyOrNull()))
       .thenReturn(listMeasurementsResponse { measurements += UNFINISHED_MEASUREMENT })
     val measurement = prober.run()
     Truth.assertThat(measurement).isNull()
   }
 
   companion object {
-    private const val MC_ID = "mc"
-    private val SECRET_FILES_PATH: Path =
-      checkNotNull(
-        getRuntimePath(
+    private val SECRETS_DIR: File =
+      getRuntimePath(
           Paths.get("wfa_measurement_system", "src", "main", "k8s", "testing", "secretfiles")
-        )
-      )
-    private val MEASUREMENT_CONSUMER: MeasurementConsumer = measurementConsumer {}
-    private val LIST_MEASUREMENTS_RESPONSE: ListMeasurementsResponse = listMeasurementsResponse {}
-    private val DATA_PROVIDER: DataProvider = dataProvider {}
-    private val LIST_EVENT_GROUPS_RESPONSE: ListEventGroupsResponse = listEventGroupsResponse {}
-    private val LIST_REQUISITIONS_RESPONSE: ListRequisitionsResponse = listRequisitionsResponse {}
+        )!!
+        .toFile()
 
-    private val FINISHED_MEASUREMENT = measurement { state = Measurement.State.SUCCEEDED }
+    private val API_AUTHENTICATION_KEY = "some-api-key"
+    private val PRIVATE_KEY_DER_FILE = SECRETS_DIR.resolve("mc_cs_private.der")
+    private val DURATION_BETWEEN_MEASUREMENT = Duration.ofDays(1)
+    private val MEASUREMENT_LOOKBACK_DURATION = Duration.ofDays(1)
+    private val CLOCK = Clock.systemUTC()
+
+    private const val MEASUREMENT_CONSUMER_NAME = "measurementConsumers/1"
+    private const val MEASUREMENT_CONSUMER_CERTIFICATE_NAME =
+      "measurementConsumers/1/certificates/1"
+    private val MC_CERTIFICATE_DER: ByteString =
+      SECRETS_DIR.resolve("mc_cs_cert.der").readByteString()
+    private val MC_ENCRYPTION_PUBLIC_KEY: EncryptionPublicKey =
+      loadPublicKey(SECRETS_DIR.resolve("mc_enc_public.tink")).toEncryptionPublicKey()
+    private val MEASUREMENT_CONSUMER: MeasurementConsumer = measurementConsumer {
+      name = MEASUREMENT_CONSUMER_NAME
+      certificate = MEASUREMENT_CONSUMER_CERTIFICATE_NAME
+      certificateDer = MC_CERTIFICATE_DER
+      publicKey = signedMessage { setMessage(MC_ENCRYPTION_PUBLIC_KEY.pack()) }
+    }
+    private val LIST_MEASUREMENTS_RESPONSE: ListMeasurementsResponse = listMeasurementsResponse {
+      measurements += measurement { measurementConsumer { MEASUREMENT_CONSUMER } }
+    }
+
+    private val OLD_FINISHED_MEASUREMENT = measurement {
+      state = Measurement.State.SUCCEEDED
+      updateTime = CLOCK.instant().minus(DURATION_BETWEEN_MEASUREMENT).toProtoTime()
+    }
+    private val NEW_FINISHED_MEASUREMENT = measurement {
+      state = Measurement.State.SUCCEEDED
+      updateTime =
+        CLOCK.instant().minus(DURATION_BETWEEN_MEASUREMENT - Duration.ofSeconds(1)).toProtoTime()
+    }
     private val UNFINISHED_MEASUREMENT = measurement { state = Measurement.State.COMPUTING }
+
+    private const val DATA_PROVIDER_NAME = "dataProviders/AAAAAAAAAHs"
+    private val DATA_PROVIDER_NAMES = listOf(DATA_PROVIDER_NAME)
+    private const val DATA_PROVIDER_CERTIFICATE_NAME =
+      "$DATA_PROVIDER_NAME/certificates/AAAAAAAAAHs"
+    private val DATA_PROVIDER_CERTIFICATE_DER: ByteString =
+      readCertificate(SECRETS_DIR.resolve("edp1_cs_cert.der").readByteString())
+        .encoded
+        .toByteString()
+    private val DATA_PROVIDER_SIGNING_KEY_HANDLE: SigningKeyHandle =
+      loadSigningKey(
+        SECRETS_DIR.resolve("edp1_cs_cert.der"),
+        SECRETS_DIR.resolve("edp1_cs_private.der"),
+      )
+    private val DATA_PROVIDER = dataProvider {
+      name = DATA_PROVIDER_NAME
+      certificate = DATA_PROVIDER_CERTIFICATE_NAME
+      certificateDer = DATA_PROVIDER_CERTIFICATE_DER
+      publicKey =
+        signEncryptionPublicKey(
+          loadPublicKey(SECRETS_DIR.resolve("edp1_enc_public.tink")).toEncryptionPublicKey(),
+          DATA_PROVIDER_SIGNING_KEY_HANDLE,
+        )
+    }
+
+    private const val EVENT_GROUP_NAME = "$DATA_PROVIDER_NAME/eventGroups/AAAAAAAAAHs"
+    private val LIST_EVENT_GROUPS_RESPONSE: ListEventGroupsResponse = listEventGroupsResponse {
+      eventGroups += eventGroup {
+        name = EVENT_GROUP_NAME
+        measurementConsumer = MEASUREMENT_CONSUMER_NAME
+        eventGroupReferenceId = "aaa"
+        measurementConsumerPublicKey = MEASUREMENT_CONSUMER.publicKey.message
+        encryptedMetadata =
+          MC_ENCRYPTION_PUBLIC_KEY.toPublicKeyHandle()
+            .encryptMessage(EventGroup.Metadata.getDefaultInstance().pack())
+      }
+    }
+
+    private val LIST_REQUISITIONS_RESPONSE: ListRequisitionsResponse = listRequisitionsResponse {}
   }
 }
