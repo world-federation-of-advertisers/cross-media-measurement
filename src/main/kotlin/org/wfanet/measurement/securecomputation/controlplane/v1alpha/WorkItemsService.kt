ab0c16a6234c2e7c5ff73a8ce6a03fc1604bfaa1
marcopremier <marcopremier@meta.com>
2024-10-23 05:12:31 -0700

    Init service class

--- src/main/kotlin/org/wfanet/measurement/securecomputation/controlplane/v1alpha/WorkItemsService.kt
+++ src/main/kotlin/org/wfanet/measurement/securecomputation/controlplane/v1alpha/WorkItemsService.kt
@@ -16,200 +16,271 @@
 
 package org.wfanet.measurement.securecomputation.controlplane.v1alpha
 
+import com.google.protobuf.Timestamp
+import com.google.protobuf.util.Timestamps
+import com.google.type.Date
+import com.google.type.Interval
+import com.google.type.copy
+import com.google.type.date
+import com.google.type.interval
 import io.grpc.Status
 import io.grpc.StatusException
-import java.io.IOException
-import org.wfanet.measurement.common.api.ResourceIds
+import io.grpc.StatusRuntimeException
+import java.time.DateTimeException
+import java.time.LocalDate
+import java.time.Period
+import java.time.temporal.ChronoField
+import java.time.temporal.ChronoUnit
+import java.time.temporal.Temporal
+import java.time.temporal.TemporalAdjusters
+import java.time.zone.ZoneRulesException
+import kotlin.math.min
+import kotlin.random.Random
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
+import org.projectnessie.cel.Env
+import org.wfanet.measurement.api.v2alpha.MeasurementConsumerKey
 import org.wfanet.measurement.common.base64UrlDecode
 import org.wfanet.measurement.common.base64UrlEncode
-import org.wfanet.measurement.internal.securecomputation.controlplane.ListWorkItemsPageToken
-import org.wfanet.measurement.internal.securecomputation.controlplane.ListWorkItemsResponse as InternalListWorkItemsResponse
-import org.wfanet.measurement.internal.securecomputation.controlplane.WorkItem as InternalWorkItem
-import org.wfanet.measurement.internal.securecomputation.controlplane.WorkItemsGrpcKt.WorkItemsCoroutineStub as InternalWorkItemsCoroutineStub
-import org.wfanet.measurement.internal.securecomputation.controlplane.createWorkItemRequest as internalCreateWorkItemRequest
-import org.wfanet.measurement.internal.securecomputation.controlplane.failWorkItemRequest as internalFailWorkItemRequest
-import org.wfanet.measurement.internal.securecomputation.controlplane.getWorkItemRequest as internalGetWorkItemRequest
-import org.wfanet.measurement.internal.securecomputation.controlplane.listWorkItemsRequest as internalListWorkItemsRequest
-import org.wfanet.measurement.internal.securecomputation.controlplane.workItem as internalWorkItem
-import org.wfanet.measurement.securecomputation.controlplane.v1alpha.WorkItemsGrpcKt.WorkItemsCoroutineImplBase
-import org.wfanet.measurement.securecomputation.service.InvalidFieldValueException
-import org.wfanet.measurement.securecomputation.service.RequiredFieldNotSetException
-import org.wfanet.measurement.securecomputation.service.WorkItemAlreadyExistsException
-import org.wfanet.measurement.securecomputation.service.WorkItemKey
-import org.wfanet.measurement.securecomputation.service.WorkItemNotFoundException
-import org.wfanet.measurement.securecomputation.service.internal.Errors as InternalErrors
+import org.wfanet.measurement.common.grpc.failGrpc
+import org.wfanet.measurement.common.grpc.grpcRequire
+import org.wfanet.measurement.common.grpc.grpcRequireNotNull
+import org.wfanet.measurement.common.toProtoTime
+import org.wfanet.measurement.config.reporting.MetricSpecConfig
+import org.wfanet.measurement.internal.reporting.v2.CreateReportRequest as InternalCreateReportRequest
+import org.wfanet.measurement.internal.reporting.v2.CreateReportRequestKt
+import org.wfanet.measurement.internal.reporting.v2.MetricCalculationSpec as InternalMetricCalculationSpec
+import org.wfanet.measurement.internal.reporting.v2.MetricCalculationSpec
+import org.wfanet.measurement.internal.reporting.v2.MetricCalculationSpecsGrpcKt.MetricCalculationSpecsCoroutineStub
+import org.wfanet.measurement.internal.reporting.v2.Report as InternalReport
+import org.wfanet.measurement.internal.reporting.v2.ReportKt as InternalReportKt
+import org.wfanet.measurement.internal.reporting.v2.ReportsGrpcKt.ReportsCoroutineStub
+import org.wfanet.measurement.internal.reporting.v2.StreamReportsRequest
+import org.wfanet.measurement.internal.reporting.v2.batchGetMetricCalculationSpecsRequest
+import org.wfanet.measurement.internal.reporting.v2.createReportRequest as internalCreateReportRequest
+import org.wfanet.measurement.internal.reporting.v2.getReportRequest as internalGetReportRequest
+import org.wfanet.measurement.internal.reporting.v2.report as internalReport
+import org.wfanet.measurement.reporting.service.api.submitBatchRequests
+import org.wfanet.measurement.reporting.service.api.v2alpha.MetadataPrincipalServerInterceptor.Companion.withPrincipalName
+import org.wfanet.measurement.reporting.service.api.v2alpha.ReportScheduleInfoServerInterceptor.Companion.reportScheduleInfoFromCurrentContext
+import org.wfanet.measurement.reporting.v2alpha.BatchCreateMetricsResponse
+import org.wfanet.measurement.reporting.v2alpha.BatchGetMetricsResponse
+import org.wfanet.measurement.reporting.v2alpha.CreateMetricRequest
+import org.wfanet.measurement.reporting.v2alpha.CreateReportRequest
+import org.wfanet.measurement.reporting.v2alpha.GetReportRequest
+import org.wfanet.measurement.reporting.v2alpha.ListReportsPageToken
+import org.wfanet.measurement.reporting.v2alpha.ListReportsPageTokenKt
+import org.wfanet.measurement.reporting.v2alpha.ListReportsRequest
+import org.wfanet.measurement.reporting.v2alpha.ListReportsResponse
+import org.wfanet.measurement.reporting.v2alpha.Metric
+import org.wfanet.measurement.reporting.v2alpha.MetricsGrpcKt.MetricsCoroutineStub
+import org.wfanet.measurement.reporting.v2alpha.Report
+import org.wfanet.measurement.reporting.v2alpha.ReportKt
+import org.wfanet.measurement.reporting.v2alpha.ReportsGrpcKt.ReportsCoroutineImplBase
+import org.wfanet.measurement.reporting.v2alpha.batchCreateMetricsRequest
+import org.wfanet.measurement.reporting.v2alpha.batchGetMetricsRequest
+import org.wfanet.measurement.reporting.v2alpha.copy
+import org.wfanet.measurement.reporting.v2alpha.listReportsPageToken
+import org.wfanet.measurement.reporting.v2alpha.listReportsResponse
+import org.wfanet.measurement.reporting.v2alpha.report
 
-class WorkItemsService(private val internalWorkItemsStub: InternalWorkItemsCoroutineStub) :
-  WorkItemsCoroutineImplBase() {
+import org.wfanet.measurement.common.grpc.grpcRequireNotNull
+import com.google.protobuf.Any
+import com.rabbitmq.client.Channel
+import com.rabbitmq.client.Connection
+import com.rabbitmq.client.ConnectionFactory
+import io.grpc.Status
+import io.grpc.StatusException
+import io.grpc.stub.StreamObserver
+import java.nio.charset.StandardCharsets
 
-  override suspend fun createWorkItem(request: CreateWorkItemRequest): WorkItem {
+class WorkItemsService(
+  private val internalReportsStub: ReportsCoroutineStub,
+  private val internalMetricCalculationSpecsStub: MetricCalculationSpecsCoroutineStub,
+  private val metricsStub: MetricsCoroutineStub,
+  private val metricSpecConfig: MetricSpecConfig,
+  private val secureRandom: Random,
+) : WorkItemsCoroutineImplBase() {
+  private data class CreateReportInfo(
+    val parent: String,
+    val requestId: String,
+    val timeIntervals: List<Interval>?,
+    val reportingInterval: Report.ReportingInterval?,
+  )
 
-    if (!request.hasWorkItem()) {
-      throw RequiredFieldNotSetException("work_item")
-        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
-    }
-    if (request.workItem.queue.isEmpty()) {
-      throw RequiredFieldNotSetException("queue")
-        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
-    }
-    if (request.workItemId.isEmpty()) {
-      throw RequiredFieldNotSetException("work_item_id")
-        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
-    }
-    if (!ResourceIds.RFC_1034_REGEX.matches(request.workItemId)) {
-      throw InvalidFieldValueException("work_item_id")
-        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+  override suspend fun listReports(request: ListReportsRequest): ListReportsResponse {
+    val parentKey: MeasurementConsumerKey =
+      grpcRequireNotNull(MeasurementConsumerKey.fromName(request.parent)) {
+        "Parent is either unspecified or invalid."
+      }
+    val listReportsPageToken = request.toListReportsPageToken()
+
+    val principal: ReportingPrincipal = principalFromCurrentContext
+    when (principal) {
+      is MeasurementConsumerPrincipal -> {
+        if (parentKey != principal.resourceKey) {
+          failGrpc(Status.PERMISSION_DENIED) {
+            "Cannot list Reports belonging to other MeasurementConsumers."
+          }
+        }
+      }
     }
 
-    val internalResponse: InternalWorkItem =
+    val streamInternalReportsRequest: StreamReportsRequest =
+      listReportsPageToken.toStreamReportsRequest()
+
+    val results: List<InternalReport> =
       try {
-        internalWorkItemsStub.createWorkItem(
-          internalCreateWorkItemRequest {
-            internalWorkItem {
-              queueResourceId = request.workItem.queue
-              workItemResourceId = request.workItemId
-              workItemParams = request.workItem.workItemParams
+        internalReportsStub.streamReports(streamInternalReportsRequest).toList()
+      } catch (e: StatusException) {
+        throw when (e.status.code) {
+          Status.Code.DEADLINE_EXCEEDED -> Status.DEADLINE_EXCEEDED
+          Status.Code.CANCELLED -> Status.CANCELLED
+          else -> Status.UNKNOWN
+        }
+          .withCause(e)
+          .withDescription("Unable to list Reports.")
+          .asRuntimeException()
+      }
+
+    if (results.isEmpty()) {
+      return ListReportsResponse.getDefaultInstance()
+    }
+
+    val nextPageToken: ListReportsPageToken? =
+      if (results.size > listReportsPageToken.pageSize) {
+        listReportsPageToken.copy {
+          lastReport =
+            ListReportsPageTokenKt.previousPageEnd {
+              cmmsMeasurementConsumerId = results[results.lastIndex - 1].cmmsMeasurementConsumerId
+              createTime = results[results.lastIndex - 1].createTime
+              externalReportId = results[results.lastIndex - 1].externalReportId
+            }
+        }
+      } else {
+        null
+      }
+
+    val subResults: List<InternalReport> =
+      results.subList(0, min(results.size, listReportsPageToken.pageSize))
+
+    // Get metrics.
+    val metricNames: Flow<String> = flow {
+      buildSet {
+        for (internalReport in subResults) {
+          for (reportingMetricEntry in internalReport.reportingMetricEntriesMap) {
+            for (metricCalculationSpecReportingMetrics in
+            reportingMetricEntry.value.metricCalculationSpecReportingMetricsList) {
+              for (reportingMetric in metricCalculationSpecReportingMetrics.reportingMetricsList) {
+                val name =
+                  MetricKey(
+                    internalReport.cmmsMeasurementConsumerId,
+                    reportingMetric.externalMetricId,
+                  )
+                    .toName()
+
+                if (!contains(name)) {
+                  emit(name)
+                  add(name)
+                }
+              }
             }
           }
-        )
-      } catch (e: StatusException) {
-        throw when (InternalErrors.getReason(e)) {
-          InternalErrors.Reason.WORK_ITEM_ALREADY_EXISTS ->
-            WorkItemAlreadyExistsException(request.workItem.name, e)
-              .asStatusRuntimeException(e.status.code)
-          InternalErrors.Reason.WORK_ITEM_ATTEMPT_ALREADY_EXISTS,
-          InternalErrors.Reason.REQUIRED_FIELD_NOT_SET,
-          InternalErrors.Reason.QUEUE_NOT_FOUND,
-          InternalErrors.Reason.QUEUE_NOT_FOUND_FOR_WORK_ITEM,
-          InternalErrors.Reason.INVALID_WORK_ITEM_STATE,
-          InternalErrors.Reason.WORK_ITEM_NOT_FOUND,
-          InternalErrors.Reason.WORK_ITEM_ATTEMPT_NOT_FOUND,
-          InternalErrors.Reason.INVALID_FIELD_VALUE,
-          InternalErrors.Reason.INVALID_WORK_ITEM_ATTEMPT_STATE,
-          null -> Status.INTERNAL.withCause(e).asRuntimeException()
         }
       }
-
-    return internalResponse.toWorkItem()
-  }
-
-  override suspend fun getWorkItem(request: GetWorkItemRequest): WorkItem {
-    if (request.name.isEmpty()) {
-      throw RequiredFieldNotSetException("name")
-        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
     }
 
-    val key =
-      WorkItemKey.fromName(request.name)
-        ?: throw InvalidFieldValueException("name")
-          .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
-
-    val internalResponse: InternalWorkItem =
-      try {
-        internalWorkItemsStub.getWorkItem(
-          internalGetWorkItemRequest { workItemResourceId = key.workItemId }
-        )
-      } catch (e: StatusException) {
-        throw when (InternalErrors.getReason(e)) {
-          InternalErrors.Reason.WORK_ITEM_NOT_FOUND ->
-            WorkItemNotFoundException(request.name, e).asStatusRuntimeException(e.status.code)
-          InternalErrors.Reason.REQUIRED_FIELD_NOT_SET,
-          InternalErrors.Reason.QUEUE_NOT_FOUND,
-          InternalErrors.Reason.QUEUE_NOT_FOUND_FOR_WORK_ITEM,
-          InternalErrors.Reason.INVALID_WORK_ITEM_STATE,
-          InternalErrors.Reason.WORK_ITEM_ATTEMPT_NOT_FOUND,
-          InternalErrors.Reason.INVALID_FIELD_VALUE,
-          InternalErrors.Reason.INVALID_WORK_ITEM_ATTEMPT_STATE,
-          InternalErrors.Reason.WORK_ITEM_ALREADY_EXISTS,
-          InternalErrors.Reason.WORK_ITEM_ATTEMPT_ALREADY_EXISTS,
-          null -> Status.INTERNAL.withCause(e).asRuntimeException()
-        }
-      }
-
-    return internalResponse.toWorkItem()
-  }
-
-  override suspend fun listWorkItems(request: ListWorkItemsRequest): ListWorkItemsResponse {
-    if (request.pageSize < 0) {
-      throw InvalidFieldValueException("page_size") { fieldName -> "$fieldName cannot be negative" }
-        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    val callRpc: suspend (List<String>) -> BatchGetMetricsResponse = { items ->
+      batchGetMetrics(principal.resourceKey.toName(), items)
     }
-
-    val pageSize =
-      when {
-        request.pageSize == 0 -> DEFAULT_PAGE_SIZE
-        request.pageSize > MAX_PAGE_SIZE -> MAX_PAGE_SIZE
-        else -> request.pageSize
+    val externalIdToMetricMap: Map<String, Metric> = buildMap {
+      submitBatchRequests(metricNames, BATCH_GET_METRICS_LIMIT, callRpc) { response ->
+        response.metricsList
       }
-
-    val internalPageToken: ListWorkItemsPageToken? =
-      if (request.pageToken.isEmpty()) {
-        null
-      } else {
-        try {
-          ListWorkItemsPageToken.parseFrom(request.pageToken.base64UrlDecode())
-        } catch (e: IOException) {
-          throw InvalidFieldValueException("page_token", e)
-            .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
-        }
-      }
-
-    val internalResponse: InternalListWorkItemsResponse =
-      internalWorkItemsStub.listWorkItems(
-        internalListWorkItemsRequest {
-          this.pageSize = pageSize
-          if (internalPageToken != null) {
-            pageToken = internalPageToken
+        .collect { metrics: List<Metric> ->
+          for (metric in metrics) {
+            computeIfAbsent(checkNotNull(MetricKey.fromName(metric.name)).metricId) { metric }
           }
         }
-      )
-
-    return listWorkItemsResponse {
-      workItems += internalResponse.workItemsList.map { it.toWorkItem() }
-      if (internalResponse.hasNextPageToken()) {
-        nextPageToken = internalResponse.nextPageToken.after.toByteString().base64UrlEncode()
-      }
-    }
-  }
-
-  override suspend fun failWorkItem(request: FailWorkItemRequest): WorkItem {
-    if (request.name.isEmpty()) {
-      throw RequiredFieldNotSetException("name")
-        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
     }
 
-    val key =
-      WorkItemKey.fromName(request.name)
-        ?: throw InvalidFieldValueException("name")
-          .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
-
-    val internalResponse: InternalWorkItem =
-      try {
-        internalWorkItemsStub.failWorkItem(
-          internalFailWorkItemRequest { workItemResourceId = key.workItemId }
+    return listReportsResponse {
+      reports +=
+        filterReports(
+          subResults.map { internalReport ->
+            convertInternalReportToPublic(internalReport, externalIdToMetricMap)
+          },
+          request.filter,
         )
-      } catch (e: StatusException) {
-        throw when (InternalErrors.getReason(e)) {
-          InternalErrors.Reason.WORK_ITEM_NOT_FOUND ->
-            WorkItemNotFoundException(request.name, e).asStatusRuntimeException(e.status.code)
-          InternalErrors.Reason.REQUIRED_FIELD_NOT_SET,
-          InternalErrors.Reason.QUEUE_NOT_FOUND,
-          InternalErrors.Reason.QUEUE_NOT_FOUND_FOR_WORK_ITEM,
-          InternalErrors.Reason.INVALID_WORK_ITEM_STATE,
-          InternalErrors.Reason.WORK_ITEM_ATTEMPT_NOT_FOUND,
-          InternalErrors.Reason.INVALID_FIELD_VALUE,
-          InternalErrors.Reason.INVALID_WORK_ITEM_ATTEMPT_STATE,
-          InternalErrors.Reason.WORK_ITEM_ALREADY_EXISTS,
-          InternalErrors.Reason.WORK_ITEM_ATTEMPT_ALREADY_EXISTS,
-          null -> Status.INTERNAL.withCause(e).asRuntimeException()
-        }
-      }
 
-    return internalResponse.toWorkItem()
+      if (nextPageToken != null) {
+        this.nextPageToken = nextPageToken.toByteString().base64UrlEncode()
+      }
+    }
   }
 
-  companion object {
-    private const val DEFAULT_PAGE_SIZE = 50
-    private const val MAX_PAGE_SIZE = 100
+
+}
+
+class WorkItemsService : WorkItemsCoroutineImplBase() {
+
+  private val rabbitMqHost: String = System.getenv("RABBIT_HOST") ?: "localhost"
+  private val connectionFactory: ConnectionFactory = ConnectionFactory().apply {
+    host = rabbitMqHost
+  }
+
+  override suspend fun createWorkItem(request: CreateWorkItemRequest): WorkItem {
+  override fun createWorkItem(
+    request: CreateWorkItemRequest,
+  ) {
+    try {
+
+      grpcRequireNotNull(ModelLineKey.fromName(request.parent)) {
+        "Parent is either unspecified or invalid"
+      }
+
+      // Validate the WorkItem request
+      val workItem = request.workItem
+      val queueName = workItem.queue
+      if (queueName.isEmpty()) {
+        throw StatusException(Status.INVALID_ARGUMENT.withDescription("Queue name is required."))
+      }
+
+      // Serialize the WorkItem if necessary (convert to byte array)
+      val serializedWorkItem = workItem.toByteArray()
+
+      // Enqueue the WorkItem into the RabbitMQ queue
+      enqueueToQueue(queueName, serializedWorkItem)
+
+
+    } catch (e: Exception) {
+
+    }
+  }
+
+  // Enqueues the serialized WorkItem to the specified RabbitMQ queue
+  private fun enqueueToQueue(queueName: String, serializedWorkItem: ByteArray) {
+    val connection: Connection = connectionFactory.newConnection()
+    val channel: Channel = connection.createChannel()
+
+    try {
+      // Check if the queue exists without creating it
+      channel.queueDeclarePassive(queueName)
+
+      // Publish the WorkItem to the queue
+      channel.basicPublish("", queueName, null, serializedWorkItem)
+    } catch (e: java.io.IOException) {
+      // Handle the case where the queue doesn't exist
+      throw StatusException(Status.NOT_FOUND.withDescription("Queue $queueName does not exist").withCause(e))
+    } finally {
+      // Ensure the channel and connection are closed
+      channel.close()
+      connection.close()
+    }
   }
 }
