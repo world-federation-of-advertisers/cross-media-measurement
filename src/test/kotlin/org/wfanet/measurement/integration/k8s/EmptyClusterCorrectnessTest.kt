85e50af33a21ba44500ebc2fb6ad419e992dbd22
Tristan Vuong <tristanvuong@google.com>
2024-11-13 16:08:31 +0000

    update config files

--- src/test/kotlin/org/wfanet/measurement/integration/k8s/EmptyClusterCorrectnessTest.kt
+++ src/test/kotlin/org/wfanet/measurement/integration/k8s/EmptyClusterCorrectnessTest.kt
@@ -190,7 +190,7 @@ class EmptyClusterCorrectnessTest : AbstractCorrectnessTest(measurementSystem) {
         override fun evaluate() {
           try {
             runBlocking {
-              withTimeout(Duration.ofMinutes(10)) {
+              withTimeout(Duration.ofMinutes(5)) {
                 val measurementConsumerData = populateCluster()
                 _testHarness = createTestHarness(measurementConsumerData)
                 _reportingTestHarness = createReportingUserSimulator(measurementConsumerData)
