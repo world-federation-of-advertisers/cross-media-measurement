18bd5eda21bd92fadeae61a99768b4f7d7a9987b
Tristan Vuong <tristanvuong@google.com>
2024-11-16 03:55:58 +0000

    add debug logging

--- src/test/kotlin/org/wfanet/panelmatch/integration/k8s/EmptyClusterPanelMatchCorrectnessTest.kt
+++ src/test/kotlin/org/wfanet/panelmatch/integration/k8s/EmptyClusterPanelMatchCorrectnessTest.kt
@@ -347,9 +347,10 @@ class EmptyClusterPanelMatchCorrectnessTest : AbstractPanelMatchCorrectnessTest(
 
           val configFilesDir = outputDir.toPath().resolve(CONFIG_FILES_PATH).toFile()
           logger.info("Copying $akidPrincipalMap to $CONFIG_FILES_PATH")
-          println("config files dir: ${configFilesDir.absolutePath}")
+          println("config files dir contents: ${configFilesDir.list()}")
           akidPrincipalMap.copyTo(configFilesDir.resolve(akidPrincipalMap.name))
 
+          println("akid copied")
           val configFile: File = outputDir.resolve("config.yaml")
           kustomize(
             outputDir
