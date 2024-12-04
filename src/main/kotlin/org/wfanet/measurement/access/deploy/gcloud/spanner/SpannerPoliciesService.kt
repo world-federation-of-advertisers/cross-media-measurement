76b238c02571ef776e8687b1c8abe567f4e53de7
Sanjay Vasandani <sanjayvas@google.com>
2024-12-03 17:04:24 -0800

    feat: Implement internal Access API services

---
+++ src/main/kotlin/org/wfanet/measurement/access/deploy/gcloud/spanner/SpannerPoliciesService.kt
@@ -0,0 +1,329 @@
+/*
+ * Copyright 2024 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.access.deploy.gcloud.spanner
+
+import com.google.cloud.spanner.ErrorCode
+import com.google.cloud.spanner.SpannerException
+import com.google.protobuf.Timestamp
+import io.grpc.Status
+import org.wfanet.measurement.access.common.TlsClientPrincipalMapping
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.deletePolicyBinding
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.getPolicyByProtectedResourceName
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.getPolicyByResourceId
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.getPrincipalIdsByResourceIds
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.getRoleIdByResourceId
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.getRoleIdsByResourceIds
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.insertPolicy
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.insertPolicyBinding
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.policyExists
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.updatePolicy
+import org.wfanet.measurement.access.service.internal.EtagMismatchException
+import org.wfanet.measurement.access.service.internal.IdGenerator
+import org.wfanet.measurement.access.service.internal.PolicyAlreadyExistsException
+import org.wfanet.measurement.access.service.internal.PolicyBindingMembershipAlreadyExistsException
+import org.wfanet.measurement.access.service.internal.PolicyBindingMembershipNotFoundException
+import org.wfanet.measurement.access.service.internal.PolicyNotFoundException
+import org.wfanet.measurement.access.service.internal.PolicyNotFoundForProtectedResourceException
+import org.wfanet.measurement.access.service.internal.PrincipalNotFoundException
+import org.wfanet.measurement.access.service.internal.PrincipalTypeNotSupportedException
+import org.wfanet.measurement.access.service.internal.RequiredFieldNotSetException
+import org.wfanet.measurement.access.service.internal.RoleNotFoundException
+import org.wfanet.measurement.access.service.internal.generateNewId
+import org.wfanet.measurement.common.api.ETags
+import org.wfanet.measurement.common.toInstant
+import org.wfanet.measurement.gcloud.spanner.AsyncDatabaseClient
+import org.wfanet.measurement.internal.access.AddPolicyBindingMembersRequest
+import org.wfanet.measurement.internal.access.GetPolicyRequest
+import org.wfanet.measurement.internal.access.LookupPolicyRequest
+import org.wfanet.measurement.internal.access.PoliciesGrpcKt
+import org.wfanet.measurement.internal.access.Policy
+import org.wfanet.measurement.internal.access.PolicyKt
+import org.wfanet.measurement.internal.access.Principal
+import org.wfanet.measurement.internal.access.RemovePolicyBindingMembersRequest
+import org.wfanet.measurement.internal.access.copy
+import org.wfanet.measurement.internal.access.policy
+
+class SpannerPoliciesService(
+  private val databaseClient: AsyncDatabaseClient,
+  private val tlsClientMapping: TlsClientPrincipalMapping,
+  private val idGenerator: IdGenerator = IdGenerator.Default,
+) : PoliciesGrpcKt.PoliciesCoroutineImplBase() {
+  override suspend fun getPolicy(request: GetPolicyRequest): Policy {
+    if (request.policyResourceId.isEmpty()) {
+      throw RequiredFieldNotSetException("policy_resource_id")
+        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    }
+
+    return try {
+      databaseClient.singleUse().use { txn ->
+        txn.getPolicyByResourceId(request.policyResourceId).policy
+      }
+    } catch (e: PolicyNotFoundException) {
+      throw e.asStatusRuntimeException(Status.Code.NOT_FOUND)
+    }
+  }
+
+  override suspend fun lookupPolicy(request: LookupPolicyRequest): Policy {
+    @Suppress("WHEN_ENUM_CAN_BE_NULL_IN_JAVA") // Protobuf oneof case enums cannot be null.
+    return when (request.lookupKeyCase) {
+      LookupPolicyRequest.LookupKeyCase.PROTECTED_RESOURCE_NAME -> {
+        try {
+          databaseClient.singleUse().use { txn ->
+            txn.getPolicyByProtectedResourceName(request.protectedResourceName).policy
+          }
+        } catch (e: PolicyNotFoundForProtectedResourceException) {
+          throw e.asStatusRuntimeException(Status.Code.NOT_FOUND)
+        }
+      }
+      LookupPolicyRequest.LookupKeyCase.LOOKUPKEY_NOT_SET ->
+        throw RequiredFieldNotSetException("lookup_key")
+          .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    }
+  }
+
+  override suspend fun createPolicy(request: Policy): Policy {
+    if (request.policyResourceId.isEmpty()) {
+      throw RequiredFieldNotSetException("policy_resource_id")
+        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    }
+    val bindings: Map<String, Set<String>> =
+      request.bindingsMap.mapValues { (_, value) ->
+        value.memberPrincipalResourceIdsList.toSet().also {
+          if (it.isEmpty())
+            throw RequiredFieldNotSetException("member_principal_resource_ids")
+              .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+        }
+      }
+    val principalResourceIds = buildSet { bindings.values.forEach { addAll(it) } }
+    try {
+      checkPrincipalTypes(principalResourceIds)
+    } catch (e: PrincipalTypeNotSupportedException) {
+      throw e.asStatusRuntimeException(Status.Code.FAILED_PRECONDITION)
+    }
+
+    val transactionRunner = databaseClient.readWriteTransaction()
+    return try {
+      transactionRunner.run { txn ->
+        val policyId = idGenerator.generateNewId { id -> txn.policyExists(id) }
+        txn.insertPolicy(policyId, request.policyResourceId, request.protectedResourceName)
+        val roleIdByResourceId: Map<String, Long> = txn.getRoleIdsByResourceIds(bindings.keys)
+        val principalIdByResourceId: Map<String, Long> =
+          txn.getPrincipalIdsByResourceIds(principalResourceIds)
+        for ((roleResourceId, memberPrincipalResourceIds) in bindings) {
+          val roleId = roleIdByResourceId.getValue(roleResourceId)
+          for (principalResourceId in memberPrincipalResourceIds) {
+            val principalId = principalIdByResourceId.getValue(principalResourceId)
+            txn.insertPolicyBinding(policyId, roleId, principalId)
+          }
+        }
+      }
+      val commitTimestamp: Timestamp = transactionRunner.getCommitTimestamp().toProto()
+
+      policy {
+        policyResourceId = request.policyResourceId
+        protectedResourceName = request.protectedResourceName
+        createTime = commitTimestamp
+        updateTime = commitTimestamp
+        etag = ETags.computeETag(updateTime.toInstant())
+
+        for ((roleResourceId, memberPrincipalResourceIds) in bindings) {
+          this.bindings[roleResourceId] =
+            PolicyKt.members { this.memberPrincipalResourceIds += memberPrincipalResourceIds }
+        }
+      }
+    } catch (e: SpannerException) {
+      if (e.errorCode == ErrorCode.ALREADY_EXISTS) {
+        throw PolicyAlreadyExistsException(e).asStatusRuntimeException(Status.Code.ALREADY_EXISTS)
+      } else {
+        throw e
+      }
+    } catch (e: RoleNotFoundException) {
+      throw e.asStatusRuntimeException(Status.Code.FAILED_PRECONDITION)
+    } catch (e: PrincipalNotFoundException) {
+      throw e.asStatusRuntimeException(Status.Code.FAILED_PRECONDITION)
+    }
+  }
+
+  override suspend fun addPolicyBindingMembers(request: AddPolicyBindingMembersRequest): Policy {
+    if (request.policyResourceId.isEmpty()) {
+      throw RequiredFieldNotSetException("policy_resource_id")
+        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    }
+    if (request.roleResourceId.isEmpty()) {
+      throw RequiredFieldNotSetException("role_resource_id")
+        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    }
+    if (request.memberPrincipalResourceIdsList.isEmpty()) {
+      throw RequiredFieldNotSetException("member_principal_resource_ids")
+        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    }
+    val memberPrincipalResourceIds: Set<String> = request.memberPrincipalResourceIdsList.toSet()
+    try {
+      checkPrincipalTypes(memberPrincipalResourceIds)
+    } catch (e: PrincipalTypeNotSupportedException) {
+      throw e.asStatusRuntimeException(Status.Code.FAILED_PRECONDITION)
+    }
+
+    val transactionRunner = databaseClient.readWriteTransaction()
+    return try {
+      val policy =
+        transactionRunner.run { txn ->
+          val (policyId, policy) = txn.getPolicyByResourceId(request.policyResourceId)
+          if (request.etag.isNotEmpty()) {
+            EtagMismatchException.check(request.etag, policy.etag)
+          }
+          val existingPrincipalResourceIds =
+            policy.bindingsMap
+              .getOrDefault(request.roleResourceId, Policy.Members.getDefaultInstance())
+              .memberPrincipalResourceIdsList
+              .toSet()
+          for (principalResourceId in memberPrincipalResourceIds) {
+            if (principalResourceId in existingPrincipalResourceIds) {
+              throw PolicyBindingMembershipAlreadyExistsException(
+                request.policyResourceId,
+                request.roleResourceId,
+                principalResourceId,
+              )
+            }
+          }
+
+          val roleId = txn.getRoleIdByResourceId(request.roleResourceId)
+          val principalIdByResourceId = txn.getPrincipalIdsByResourceIds(memberPrincipalResourceIds)
+          for (principalId in principalIdByResourceId.values) {
+            txn.insertPolicyBinding(policyId, roleId, principalId)
+          }
+          txn.updatePolicy(policyId)
+          policy
+        }
+      val commitTimestamp: Timestamp = transactionRunner.getCommitTimestamp().toProto()
+
+      policy.copy {
+        updateTime = commitTimestamp
+        etag = ETags.computeETag(updateTime.toInstant())
+        bindings[request.roleResourceId] =
+          bindings.getOrDefault(request.roleResourceId, Policy.Members.getDefaultInstance()).copy {
+            this.memberPrincipalResourceIds += memberPrincipalResourceIds
+          }
+      }
+    } catch (e: PolicyNotFoundException) {
+      throw e.asStatusRuntimeException(Status.Code.NOT_FOUND)
+    } catch (e: RoleNotFoundException) {
+      throw e.asStatusRuntimeException(Status.Code.FAILED_PRECONDITION)
+    } catch (e: PrincipalNotFoundException) {
+      throw e.asStatusRuntimeException(Status.Code.FAILED_PRECONDITION)
+    } catch (e: PolicyBindingMembershipAlreadyExistsException) {
+      throw e.asStatusRuntimeException(Status.Code.FAILED_PRECONDITION)
+    } catch (e: EtagMismatchException) {
+      throw e.asStatusRuntimeException(Status.Code.ABORTED)
+    }
+  }
+
+  override suspend fun removePolicyBindingMembers(
+    request: RemovePolicyBindingMembersRequest
+  ): Policy {
+    if (request.policyResourceId.isEmpty()) {
+      throw RequiredFieldNotSetException("policy_resource_id")
+        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    }
+    if (request.roleResourceId.isEmpty()) {
+      throw RequiredFieldNotSetException("role_resource_id")
+        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    }
+    if (request.memberPrincipalResourceIdsList.isEmpty()) {
+      throw RequiredFieldNotSetException("member_principal_resource_ids")
+        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    }
+    val memberPrincipalResourceIds: Set<String> = request.memberPrincipalResourceIdsList.toSet()
+
+    val transactionRunner = databaseClient.readWriteTransaction()
+    return try {
+      val policy: Policy =
+        transactionRunner.run { txn ->
+          val (policyId, policy) = txn.getPolicyByResourceId(request.policyResourceId)
+          if (request.etag.isNotEmpty()) {
+            EtagMismatchException.check(request.etag, policy.etag)
+          }
+          val existingPrincipalResourceIds =
+            policy.bindingsMap
+              .getOrDefault(request.roleResourceId, Policy.Members.getDefaultInstance())
+              .memberPrincipalResourceIdsList
+              .toSet()
+          for (principalResourceId in memberPrincipalResourceIds) {
+            if (principalResourceId !in existingPrincipalResourceIds) {
+              throw PolicyBindingMembershipNotFoundException(
+                request.policyResourceId,
+                request.roleResourceId,
+                principalResourceId,
+              )
+            }
+          }
+
+          val roleId = txn.getRoleIdByResourceId(request.roleResourceId)
+          val principalIdByResourceId = txn.getPrincipalIdsByResourceIds(memberPrincipalResourceIds)
+          for (principalId in principalIdByResourceId.values) {
+            txn.deletePolicyBinding(policyId, roleId, principalId)
+          }
+          txn.updatePolicy(policyId)
+          policy
+        }
+      val commitTimestamp: Timestamp = transactionRunner.getCommitTimestamp().toProto()
+
+      val updatedMemberPrincipalResourceIds: List<String> =
+        policy.bindingsMap.getValue(request.roleResourceId).memberPrincipalResourceIdsList.filter {
+          it !in memberPrincipalResourceIds
+        }
+      policy.copy {
+        updateTime = commitTimestamp
+        etag = ETags.computeETag(updateTime.toInstant())
+        if (updatedMemberPrincipalResourceIds.isEmpty()) {
+          bindings.remove(request.roleResourceId)
+        } else {
+          bindings[request.roleResourceId] =
+            PolicyKt.members {
+              this.memberPrincipalResourceIds += updatedMemberPrincipalResourceIds
+            }
+        }
+      }
+    } catch (e: PolicyNotFoundException) {
+      throw e.asStatusRuntimeException(Status.Code.NOT_FOUND)
+    } catch (e: RoleNotFoundException) {
+      throw e.asStatusRuntimeException(Status.Code.INTERNAL)
+    } catch (e: PrincipalNotFoundException) {
+      throw e.asStatusRuntimeException(Status.Code.INTERNAL)
+    } catch (e: PolicyBindingMembershipNotFoundException) {
+      throw e.asStatusRuntimeException(Status.Code.FAILED_PRECONDITION)
+    } catch (e: EtagMismatchException) {
+      throw e.asStatusRuntimeException(Status.Code.ABORTED)
+    }
+  }
+
+  /**
+   * Checks that each [Principal] represented by [principalResourceIds] has a supported type.
+   *
+   * @throws PrincipalTypeNotSupportedException
+   */
+  private fun checkPrincipalTypes(principalResourceIds: Iterable<String>) {
+    for (principalResourceId in principalResourceIds) {
+      if (tlsClientMapping.getByPrincipalResourceId(principalResourceId) != null) {
+        throw PrincipalTypeNotSupportedException(
+          principalResourceId,
+          Principal.IdentityCase.TLS_CLIENT,
+        )
+      }
+    }
+  }
+}
---
+++ src/main/kotlin/org/wfanet/measurement/access/service/internal/testing/PrincipalsServiceTest.kt
@@ -0,0 +1,259 @@
+/*
+ * Copyright 2024 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.access.service.internal.testing
+
+import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.extensions.proto.ProtoTruth.assertThat
+import com.google.rpc.errorInfo
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import java.time.Instant
+import kotlin.test.assertFailsWith
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.wfanet.measurement.access.common.TlsClientPrincipalMapping
+import org.wfanet.measurement.access.service.internal.Errors
+import org.wfanet.measurement.access.service.internal.IdGenerator
+import org.wfanet.measurement.common.grpc.errorInfo
+import org.wfanet.measurement.common.toInstant
+import org.wfanet.measurement.internal.access.Principal
+import org.wfanet.measurement.internal.access.PrincipalKt.oAuthUser
+import org.wfanet.measurement.internal.access.PrincipalKt.tlsClient
+import org.wfanet.measurement.internal.access.PrincipalsGrpcKt
+import org.wfanet.measurement.internal.access.copy
+import org.wfanet.measurement.internal.access.createUserPrincipalRequest
+import org.wfanet.measurement.internal.access.deletePrincipalRequest
+import org.wfanet.measurement.internal.access.getPrincipalRequest
+import org.wfanet.measurement.internal.access.lookupPrincipalRequest
+import org.wfanet.measurement.internal.access.principal
+
+@RunWith(JUnit4::class)
+abstract class PrincipalsServiceTest {
+  /** Initializes the service under test. */
+  abstract fun initService(
+    tlsClientMapping: TlsClientPrincipalMapping,
+    idGenerator: IdGenerator,
+  ): PrincipalsGrpcKt.PrincipalsCoroutineImplBase
+
+  private fun initService(idGenerator: IdGenerator = IdGenerator.Default) =
+    initService(TestConfig.TLS_CLIENT_MAPPING, idGenerator)
+
+  @Test
+  fun `getPrincipal returns TLS client principal`() {
+    val service = initService()
+    val request = getPrincipalRequest {
+      principalResourceId = TestConfig.TLS_CLIENT_PRINCIPAL_RESOURCE_ID
+    }
+
+    val response: Principal = runBlocking { service.getPrincipal(request) }
+
+    assertThat(response)
+      .isEqualTo(
+        principal {
+          principalResourceId = TestConfig.TLS_CLIENT_PRINCIPAL_RESOURCE_ID
+          tlsClient = tlsClient { authorityKeyIdentifier = TestConfig.AUTHORITY_KEY_IDENTIFIER }
+        }
+      )
+  }
+
+  @Test
+  fun `createUserPrincipal returns created principal`() = runBlocking {
+    val service = initService()
+    val request = createUserPrincipalRequest {
+      principalResourceId = "user-1"
+      user = oAuthUser {
+        issuer = "example-issuer"
+        subject = "user@example.com"
+      }
+    }
+
+    val principal: Principal = service.createUserPrincipal(request)
+
+    assertThat(principal)
+      .ignoringFields(Principal.CREATE_TIME_FIELD_NUMBER, Principal.UPDATE_TIME_FIELD_NUMBER)
+      .isEqualTo(
+        principal {
+          principalResourceId = request.principalResourceId
+          user = request.user
+        }
+      )
+    assertThat(principal.createTime.toInstant()).isGreaterThan(Instant.now().minusSeconds(10))
+    assertThat(principal.createTime).isEqualTo(principal.updateTime)
+    assertThat(principal)
+      .isEqualTo(
+        service.getPrincipal(
+          getPrincipalRequest { principalResourceId = request.principalResourceId }
+        )
+      )
+  }
+
+  @Test
+  fun `createUserPrincipal retries ID generation if ID already in use`(): Unit = runBlocking {
+    val principalId1 = 1234L
+    val principalId2 = 2345L
+    val idGeneratorMock =
+      mock<IdGenerator> { on { generateId() }.thenReturn(principalId1, principalId1, principalId2) }
+    val service = initService(idGeneratorMock)
+    service.createUserPrincipal(
+      createUserPrincipalRequest {
+        principalResourceId = "user-1"
+        user = oAuthUser {
+          issuer = "example-issuer"
+          subject = "user@example.com"
+        }
+      }
+    )
+
+    service.createUserPrincipal(
+      createUserPrincipalRequest {
+        principalResourceId = "user-2"
+        user = oAuthUser {
+          issuer = "example-issuer"
+          subject = "user2@example.com"
+        }
+      }
+    )
+
+    verify(idGeneratorMock, times(3)).generateId()
+  }
+
+  @Test
+  fun `createUserPrincipal throws ALREADY_EXISTS if Principal with resource ID already exists`() =
+    runBlocking {
+      val service = initService()
+      val request = createUserPrincipalRequest {
+        principalResourceId = "user-1"
+        user = oAuthUser {
+          issuer = "example-issuer"
+          subject = "user@example.com"
+        }
+      }
+      service.createUserPrincipal(request)
+
+      val exception =
+        assertFailsWith<StatusRuntimeException> {
+          service.createUserPrincipal(
+            request.copy { user = user.copy { subject = "user2@example.com" } }
+          )
+        }
+
+      assertThat(exception.status.code).isEqualTo(Status.Code.ALREADY_EXISTS)
+      assertThat(exception.errorInfo)
+        .isEqualTo(
+          errorInfo {
+            domain = Errors.DOMAIN
+            reason = Errors.Reason.PRINCIPAL_ALREADY_EXISTS.name
+          }
+        )
+    }
+
+  @Test
+  fun `lookupPrincipal returns TLS client principal`() {
+    val service = initService()
+    val request = lookupPrincipalRequest {
+      tlsClient = tlsClient { authorityKeyIdentifier = TestConfig.AUTHORITY_KEY_IDENTIFIER }
+    }
+
+    val response: Principal = runBlocking { service.lookupPrincipal(request) }
+
+    assertThat(response)
+      .isEqualTo(
+        principal {
+          principalResourceId = TestConfig.TLS_CLIENT_PRINCIPAL_RESOURCE_ID
+          tlsClient = request.tlsClient
+        }
+      )
+  }
+
+  @Test
+  fun `lookupPrincipal returns user principal`() = runBlocking {
+    val service = initService()
+    val principal =
+      service.createUserPrincipal(
+        createUserPrincipalRequest {
+          principalResourceId = "user-1"
+          user = oAuthUser {
+            issuer = "example-issuer"
+            subject = "user@example.com"
+          }
+        }
+      )
+    val request = lookupPrincipalRequest { user = principal.user }
+
+    val response: Principal = runBlocking { service.lookupPrincipal(request) }
+
+    assertThat(response).isEqualTo(principal)
+  }
+
+  @Test
+  fun `deletePrincipal deletes user Principal`() = runBlocking {
+    val service = initService()
+    val principal =
+      service.createUserPrincipal(
+        createUserPrincipalRequest {
+          principalResourceId = "user-1"
+          user = oAuthUser {
+            issuer = "example-issuer"
+            subject = "user@example.com"
+          }
+        }
+      )
+
+    service.deletePrincipal(
+      deletePrincipalRequest { principalResourceId = principal.principalResourceId }
+    )
+
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        service.getPrincipal(
+          getPrincipalRequest { principalResourceId = principal.principalResourceId }
+        )
+      }
+    assertThat(exception.status.code).isEqualTo(Status.Code.NOT_FOUND)
+  }
+
+  @Test
+  fun `deletePrincipal throws FAILED_PRECONDITION for TLS client Principal`() = runBlocking {
+    val service = initService()
+
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        service.deletePrincipal(
+          deletePrincipalRequest {
+            principalResourceId = TestConfig.TLS_CLIENT_PRINCIPAL_RESOURCE_ID
+          }
+        )
+      }
+
+    assertThat(exception.status.code).isEqualTo(Status.Code.FAILED_PRECONDITION)
+    assertThat(exception.errorInfo)
+      .isEqualTo(
+        errorInfo {
+          domain = Errors.DOMAIN
+          reason = Errors.Reason.PRINCIPAL_TYPE_NOT_SUPPORTED.name
+          metadata[Errors.Metadata.PRINCIPAL_TYPE.key] = "TLS_CLIENT"
+          metadata[Errors.Metadata.PRINCIPAL_RESOURCE_ID.key] =
+            TestConfig.TLS_CLIENT_PRINCIPAL_RESOURCE_ID
+        }
+      )
+  }
+}
