08a273696482d3d25c4e7f8b05afa788637c1a76
Sanjay Vasandani <sanjayvas@google.com>
2024-12-06 18:48:58 -0800

    feat: Add database schema for internal Access API (#1900)

---
+++ src/main/kotlin/org/wfanet/measurement/access/deploy/gcloud/spanner/testing/Schemata.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2024 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.access.deploy.gcloud.spanner.testing
+
+import java.nio.file.Path
+import org.wfanet.measurement.common.getJarResourcePath
+
+object Schemata {
+  private const val RESOURCE_PREFIX = "access/spanner"
+
+  private fun getResourcePath(fileName: String): Path {
+    val resourceName = "$RESOURCE_PREFIX/$fileName"
+    val classLoader = Schemata.javaClass.classLoader
+    return requireNotNull(classLoader.getJarResourcePath(resourceName)) {
+      "Resource $resourceName not found"
+    }
+  }
+
+  val ACCESS_CHANGELOG_PATH: Path = getResourcePath("changelog.yaml")
+}
