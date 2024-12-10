2dc095bc8b2172e78067d27829562baf859f7928
Sanjay Vasandani <sanjayvas@google.com>
2024-12-10 13:14:38 -0800

    feat: Implement internal Access API services (#1932)

---
+++ src/main/kotlin/org/wfanet/measurement/access/deploy/gcloud/spanner/SpannerPrincipalsService.kt
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2024 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.access.deploy.gcloud.spanner
+
+import com.google.cloud.spanner.ErrorCode
+import com.google.cloud.spanner.SpannerException
+import com.google.protobuf.ByteString
+import com.google.protobuf.Empty
+import com.google.protobuf.Timestamp
+import io.grpc.Status
+import org.wfanet.measurement.access.common.TlsClientPrincipalMapping
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.deletePrincipal
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.getPrincipalByResourceId
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.getPrincipalByUserKey
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.getPrincipalIdByResourceId
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.insertPrincipal
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.insertUserPrincipal
+import org.wfanet.measurement.access.deploy.gcloud.spanner.db.principalExists
+import org.wfanet.measurement.access.service.internal.IdGenerator
+import org.wfanet.measurement.access.service.internal.PrincipalAlreadyExistsException
+import org.wfanet.measurement.access.service.internal.PrincipalNotFoundException
+import org.wfanet.measurement.access.service.internal.PrincipalNotFoundForTlsClientException
+import org.wfanet.measurement.access.service.internal.PrincipalNotFoundForUserException
+import org.wfanet.measurement.access.service.internal.PrincipalTypeNotSupportedException
+import org.wfanet.measurement.access.service.internal.RequiredFieldNotSetException
+import org.wfanet.measurement.access.service.internal.generateNewId
+import org.wfanet.measurement.gcloud.spanner.AsyncDatabaseClient
+import org.wfanet.measurement.internal.access.CreateUserPrincipalRequest
+import org.wfanet.measurement.internal.access.DeletePrincipalRequest
+import org.wfanet.measurement.internal.access.GetPrincipalRequest
+import org.wfanet.measurement.internal.access.LookupPrincipalRequest
+import org.wfanet.measurement.internal.access.Principal
+import org.wfanet.measurement.internal.access.PrincipalKt.tlsClient
+import org.wfanet.measurement.internal.access.PrincipalsGrpcKt
+import org.wfanet.measurement.internal.access.principal
+
+class SpannerPrincipalsService(
+  private val databaseClient: AsyncDatabaseClient,
+  private val tlsClientMapping: TlsClientPrincipalMapping,
+  private val idGenerator: IdGenerator = IdGenerator.Default,
+) : PrincipalsGrpcKt.PrincipalsCoroutineImplBase() {
+  override suspend fun getPrincipal(request: GetPrincipalRequest): Principal {
+    val tlsClient = tlsClientMapping.getByPrincipalResourceId(request.principalResourceId)
+    if (tlsClient != null) {
+      return tlsClient.toPrincipal()
+    }
+
+    try {
+      databaseClient.singleUse().use { txn ->
+        return txn.getPrincipalByResourceId(request.principalResourceId).principal
+      }
+    } catch (e: PrincipalNotFoundException) {
+      throw e.asStatusRuntimeException(Status.Code.NOT_FOUND)
+    }
+  }
+
+  override suspend fun createUserPrincipal(request: CreateUserPrincipalRequest): Principal {
+    val runner: AsyncDatabaseClient.TransactionRunner = databaseClient.readWriteTransaction()
+    try {
+      runner.run { txn ->
+        val principalId: Long = idGenerator.generateNewId { id -> txn.principalExists(id) }
+        txn.insertPrincipal(principalId, request.principalResourceId)
+        txn.insertUserPrincipal(principalId, request.user.issuer, request.user.subject)
+      }
+      val commitTimestamp: Timestamp = runner.getCommitTimestamp().toProto()
+      return principal {
+        principalResourceId = request.principalResourceId
+        user = request.user
+        createTime = commitTimestamp
+        updateTime = commitTimestamp
+      }
+    } catch (e: SpannerException) {
+      if (e.errorCode == ErrorCode.ALREADY_EXISTS) {
+        throw PrincipalAlreadyExistsException(e)
+          .asStatusRuntimeException(Status.Code.ALREADY_EXISTS)
+      } else {
+        throw e
+      }
+    }
+  }
+
+  override suspend fun deletePrincipal(request: DeletePrincipalRequest): Empty {
+    if (request.principalResourceId.isEmpty()) {
+      throw RequiredFieldNotSetException("principal_resource_id")
+        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    }
+    if (tlsClientMapping.getByPrincipalResourceId(request.principalResourceId) != null) {
+      throw PrincipalTypeNotSupportedException(
+          request.principalResourceId,
+          Principal.IdentityCase.TLS_CLIENT,
+        )
+        .asStatusRuntimeException(Status.Code.FAILED_PRECONDITION)
+    }
+
+    try {
+      databaseClient.readWriteTransaction().run { txn ->
+        val principalId: Long = txn.getPrincipalIdByResourceId(request.principalResourceId)
+        txn.deletePrincipal(principalId)
+      }
+    } catch (e: PrincipalNotFoundException) {
+      throw e.asStatusRuntimeException(Status.Code.NOT_FOUND)
+    }
+
+    return Empty.getDefaultInstance()
+  }
+
+  override suspend fun lookupPrincipal(request: LookupPrincipalRequest): Principal {
+    @Suppress("WHEN_ENUM_CAN_BE_NULL_IN_JAVA") // Protobuf case enums cannot be null.
+    return when (request.lookupKeyCase) {
+      LookupPrincipalRequest.LookupKeyCase.TLS_CLIENT -> {
+        val authorityKeyIdentifier: ByteString = request.tlsClient.authorityKeyIdentifier
+        try {
+          lookupTlsClientPrincipal(authorityKeyIdentifier)
+        } catch (e: PrincipalNotFoundForTlsClientException) {
+          throw e.asStatusRuntimeException(Status.Code.NOT_FOUND)
+        }
+      }
+      LookupPrincipalRequest.LookupKeyCase.USER -> {
+        try {
+          return databaseClient.singleUse().use { txn ->
+            txn.getPrincipalByUserKey(request.user.issuer, request.user.subject).principal
+          }
+        } catch (e: PrincipalNotFoundForUserException) {
+          throw e.asStatusRuntimeException(Status.Code.NOT_FOUND)
+        }
+      }
+      LookupPrincipalRequest.LookupKeyCase.LOOKUPKEY_NOT_SET ->
+        throw RequiredFieldNotSetException("lookup_key")
+          .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    }
+  }
+
+  /**
+   * Looks up a TLS client [Principal] by [authorityKeyIdentifier].
+   *
+   * @throws PrincipalNotFoundForTlsClientException
+   */
+  private fun lookupTlsClientPrincipal(authorityKeyIdentifier: ByteString): Principal {
+    val tlsClient: TlsClientPrincipalMapping.TlsClient =
+      tlsClientMapping.getByAuthorityKeyIdentifier(authorityKeyIdentifier)
+        ?: throw PrincipalNotFoundForTlsClientException(authorityKeyIdentifier)
+    return tlsClient.toPrincipal()
+  }
+}
+
+private fun TlsClientPrincipalMapping.TlsClient.toPrincipal(): Principal {
+  val source = this
+  return principal {
+    principalResourceId = source.principalResourceId
+    tlsClient = tlsClient { authorityKeyIdentifier = source.authorityKeyIdentifier }
+  }
+}
---
+++ src/main/kotlin/org/wfanet/measurement/access/service/internal/testing/PermissionsServiceTest.kt
@@ -0,0 +1,264 @@
+/*
+ * Copyright 2024 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.access.service.internal.testing
+
+import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.extensions.proto.ProtoTruth.assertThat
+import com.google.protobuf.Descriptors
+import com.google.rpc.errorInfo
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import kotlin.test.assertFailsWith
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.wfanet.measurement.access.common.TlsClientPrincipalMapping
+import org.wfanet.measurement.access.service.internal.Errors
+import org.wfanet.measurement.access.service.internal.IdGenerator
+import org.wfanet.measurement.access.service.internal.PermissionMapping
+import org.wfanet.measurement.access.service.internal.toPermission
+import org.wfanet.measurement.common.grpc.errorInfo
+import org.wfanet.measurement.internal.access.CheckPermissionsResponse
+import org.wfanet.measurement.internal.access.ListPermissionsPageTokenKt
+import org.wfanet.measurement.internal.access.ListPermissionsRequest
+import org.wfanet.measurement.internal.access.ListPermissionsResponse
+import org.wfanet.measurement.internal.access.Permission
+import org.wfanet.measurement.internal.access.PermissionsGrpcKt
+import org.wfanet.measurement.internal.access.PoliciesGrpcKt
+import org.wfanet.measurement.internal.access.PolicyKt
+import org.wfanet.measurement.internal.access.PrincipalKt.oAuthUser
+import org.wfanet.measurement.internal.access.PrincipalsGrpcKt
+import org.wfanet.measurement.internal.access.RolesGrpcKt
+import org.wfanet.measurement.internal.access.checkPermissionsRequest
+import org.wfanet.measurement.internal.access.checkPermissionsResponse
+import org.wfanet.measurement.internal.access.createUserPrincipalRequest
+import org.wfanet.measurement.internal.access.getPermissionRequest
+import org.wfanet.measurement.internal.access.listPermissionsPageToken
+import org.wfanet.measurement.internal.access.listPermissionsRequest
+import org.wfanet.measurement.internal.access.listPermissionsResponse
+import org.wfanet.measurement.internal.access.policy
+import org.wfanet.measurement.internal.access.role
+
+@RunWith(JUnit4::class)
+abstract class PermissionsServiceTest {
+  protected data class Services(
+    /** Service under test. */
+    val service: PermissionsGrpcKt.PermissionsCoroutineImplBase,
+    val principalsService: PrincipalsGrpcKt.PrincipalsCoroutineImplBase,
+    val rolesServices: RolesGrpcKt.RolesCoroutineImplBase,
+    val policiesService: PoliciesGrpcKt.PoliciesCoroutineImplBase,
+  )
+
+  protected abstract fun initServices(
+    permissionMapping: PermissionMapping,
+    tlsClientMapping: TlsClientPrincipalMapping,
+    idGenerator: IdGenerator,
+  ): Services
+
+  private fun initServices(idGenerator: IdGenerator = IdGenerator.Default) =
+    initServices(TestConfig.PERMISSION_MAPPING, TestConfig.TLS_CLIENT_MAPPING, idGenerator)
+
+  @Test
+  fun `getPermission returns Permission`() = runBlocking {
+    val service = initServices().service
+
+    val response: Permission =
+      service.getPermission(
+        getPermissionRequest { permissionResourceId = PERMISSIONS.first().permissionResourceId }
+      )
+
+    assertThat(response)
+      .ignoringRepeatedFieldOrderOfFieldDescriptors(RESOURCE_TYPES_FIELD)
+      .isEqualTo(PERMISSIONS.first())
+  }
+
+  @Test
+  fun `getPermission throws NOT_FOUND when Permission not found`() = runBlocking {
+    val service = initServices().service
+
+    val request = getPermissionRequest { permissionResourceId = "not-found" }
+    val exception = assertFailsWith<StatusRuntimeException> { service.getPermission(request) }
+
+    assertThat(exception.status.code).isEqualTo(Status.Code.NOT_FOUND)
+    assertThat(exception.errorInfo)
+      .isEqualTo(
+        errorInfo {
+          domain = Errors.DOMAIN
+          reason = Errors.Reason.PERMISSION_NOT_FOUND.name
+          metadata[Errors.Metadata.PERMISSION_RESOURCE_ID.key] = request.permissionResourceId
+        }
+      )
+  }
+
+  @Test
+  fun `listPermissions returns Permissions ordered by resource ID`() {
+    runBlocking {
+      val service = initServices().service
+
+      val response: ListPermissionsResponse =
+        service.listPermissions(ListPermissionsRequest.getDefaultInstance())
+
+      assertThat(response)
+        .ignoringRepeatedFieldOrderOfFieldDescriptors(RESOURCE_TYPES_FIELD)
+        .isEqualTo(listPermissionsResponse { permissions += PERMISSIONS })
+    }
+  }
+
+  @Test
+  fun `listPermissions returns Permissions when page size is specified`() = runBlocking {
+    val service = initServices().service
+
+    val response: ListPermissionsResponse =
+      service.listPermissions(listPermissionsRequest { pageSize = PERMISSIONS.size })
+
+    assertThat(response)
+      .ignoringRepeatedFieldOrderOfFieldDescriptors(RESOURCE_TYPES_FIELD)
+      .isEqualTo(listPermissionsResponse { permissions += PERMISSIONS })
+  }
+
+  @Test
+  fun `listPermissions returns next page token when there are more results`() = runBlocking {
+    val service = initServices().service
+
+    val response: ListPermissionsResponse =
+      service.listPermissions(listPermissionsRequest { pageSize = 2 })
+
+    assertThat(response)
+      .ignoringRepeatedFieldOrderOfFieldDescriptors(RESOURCE_TYPES_FIELD)
+      .isEqualTo(
+        listPermissionsResponse {
+          permissions += PERMISSIONS.take(2)
+          nextPageToken = listPermissionsPageToken {
+            after =
+              ListPermissionsPageTokenKt.after {
+                permissionResourceId =
+                  this@listPermissionsResponse.permissions.last().permissionResourceId
+              }
+          }
+        }
+      )
+  }
+
+  @Test
+  fun `listPermissions returns Permissions after page token`() = runBlocking {
+    val service = initServices().service
+
+    val response: ListPermissionsResponse =
+      service.listPermissions(
+        listPermissionsRequest {
+          pageToken = listPermissionsPageToken {
+            after =
+              ListPermissionsPageTokenKt.after {
+                permissionResourceId = PERMISSIONS[1].permissionResourceId
+              }
+          }
+        }
+      )
+
+    assertThat(response)
+      .ignoringRepeatedFieldOrderOfFieldDescriptors(RESOURCE_TYPES_FIELD)
+      .isEqualTo(listPermissionsResponse { permissions += PERMISSIONS.drop(2) })
+  }
+
+  @Test
+  fun `checkPermissions returns all requested permissions for TLS client principal`() {
+    val service = initServices().service
+    val request = checkPermissionsRequest {
+      protectedResourceName = TestConfig.TLS_CLIENT_PROTECTED_RESOURCE_NAME
+      principalResourceId = TestConfig.TLS_CLIENT_PRINCIPAL_RESOURCE_ID
+      permissionResourceIds += TestConfig.PermissionResourceId.BOOKS_GET
+      permissionResourceIds += TestConfig.PermissionResourceId.BOOKS_CREATE
+    }
+
+    val response: CheckPermissionsResponse = runBlocking { service.checkPermissions(request) }
+
+    assertThat(response)
+      .isEqualTo(
+        checkPermissionsResponse { permissionResourceIds += request.permissionResourceIdsList }
+      )
+  }
+
+  @Test
+  fun `checkPermissions returns no permissions for TLS client principal with wrong protected resource`() {
+    val service = initServices().service
+    val request = checkPermissionsRequest {
+      protectedResourceName = "shelves/404"
+      principalResourceId = TestConfig.TLS_CLIENT_PRINCIPAL_RESOURCE_ID
+      permissionResourceIds += TestConfig.PermissionResourceId.BOOKS_GET
+      permissionResourceIds += TestConfig.PermissionResourceId.BOOKS_CREATE
+    }
+
+    val response: CheckPermissionsResponse = runBlocking { service.checkPermissions(request) }
+
+    assertThat(response).isEqualTo(CheckPermissionsResponse.getDefaultInstance())
+  }
+
+  @Test
+  fun `checkPermissions returns permissions for user Principal`(): Unit = runBlocking {
+    val (service, principalsService, rolesService, policiesService) = initServices()
+    val principal =
+      principalsService.createUserPrincipal(
+        createUserPrincipalRequest {
+          principalResourceId = "user-1"
+          user = oAuthUser {
+            issuer = "example-issuer"
+            subject = "user@example.com"
+          }
+        }
+      )
+    val role =
+      rolesService.createRole(
+        role {
+          roleResourceId = "shelfBookReader"
+          resourceTypes += TestConfig.ResourceType.SHELF
+          permissionResourceIds += TestConfig.PermissionResourceId.BOOKS_GET
+          permissionResourceIds += TestConfig.PermissionResourceId.BOOKS_LIST
+        }
+      )
+    val policy =
+      policiesService.createPolicy(
+        policy {
+          policyResourceId = "fantasy-shelf-policy"
+          protectedResourceName = "shelves/fantasy"
+          bindings[role.roleResourceId] =
+            PolicyKt.members { memberPrincipalResourceIds += principal.principalResourceId }
+        }
+      )
+
+    val response =
+      service.checkPermissions(
+        checkPermissionsRequest {
+          protectedResourceName = policy.protectedResourceName
+          principalResourceId = principal.principalResourceId
+          permissionResourceIds += TestConfig.PermissionResourceId.BOOKS_LIST
+          permissionResourceIds += TestConfig.PermissionResourceId.BOOKS_CREATE
+        }
+      )
+
+    assertThat(response.permissionResourceIdsList)
+      .containsExactly(TestConfig.PermissionResourceId.BOOKS_LIST)
+  }
+
+  companion object {
+    private val RESOURCE_TYPES_FIELD: Descriptors.FieldDescriptor =
+      Permission.getDescriptor().findFieldByNumber(Permission.RESOURCE_TYPES_FIELD_NUMBER)
+
+    private val PERMISSIONS: List<Permission> =
+      TestConfig.PERMISSION_MAPPING.permissions.map { it.toPermission() }
+  }
+}
