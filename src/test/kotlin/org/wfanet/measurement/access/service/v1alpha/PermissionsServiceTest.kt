64b7cc601c2e3335c2612ff1942ed77fa4d4bf6f
Lin <lindreamdeyi@google.com>
2025-01-02 23:32:07 +0000

    fix: add license headers

--- src/test/kotlin/org/wfanet/measurement/access/service/v1alpha/PermissionsServiceTest.kt
+++ src/test/kotlin/org/wfanet/measurement/access/service/v1alpha/PermissionsServiceTest.kt
@@ -1,3 +1,17 @@
+// Copyright 2024 The Cross-Media Measurement Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+g
 package org.wfanet.measurement.access.service.v1alpha
 
 import com.google.common.truth.Truth.assertThat
@@ -5,7 +19,6 @@ import com.google.common.truth.extensions.proto.ProtoTruth.assertThat
 import com.google.rpc.errorInfo
 import io.grpc.Status
 import io.grpc.StatusRuntimeException
-import kotlin.test.assertFailsWith
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
@@ -35,9 +48,10 @@ import org.wfanet.measurement.common.grpc.errorInfo
 import org.wfanet.measurement.common.grpc.testing.GrpcTestServerRule
 import org.wfanet.measurement.common.grpc.testing.mockService
 import org.wfanet.measurement.common.testing.verifyProtoArgument
+import org.wfanet.measurement.internal.access.PermissionsGrpcKt
+import kotlin.test.assertFailsWith
 import org.wfanet.measurement.internal.access.ListPermissionsPageTokenKt as InternalListPermissionsPageTokenKt
 import org.wfanet.measurement.internal.access.PermissionsGrpcKt as InternalPermissionsGrpcKt
-import org.wfanet.measurement.internal.access.PermissionsGrpcKt
 import org.wfanet.measurement.internal.access.checkPermissionsRequest as internalCheckPermissionsRequest
 import org.wfanet.measurement.internal.access.checkPermissionsResponse as internalCheckPermissionsResponse
 import org.wfanet.measurement.internal.access.getPermissionRequest as internalGetPermissionRequest
