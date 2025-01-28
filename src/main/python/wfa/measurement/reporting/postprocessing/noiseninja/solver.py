0f6ca78c34d01441d94454acd88249a8a479ec11
Phi Hung Le <lephi@google.com>
2025-01-28 16:04:59 +0000

    fix: Noise correction variables are all non-negative.

--- src/main/python/wfa/measurement/reporting/postprocessing/noiseninja/solver.py
+++ src/main/python/wfa/measurement/reporting/postprocessing/noiseninja/solver.py
@@ -49,6 +49,7 @@ class Solver:
     self._add_subsets(set_measurement_spec, variable_index_by_set_id)
     self._add_measurement_targets(set_measurement_spec,
                                   variable_index_by_set_id)
+    self._add_lower_bounds()
     self._init_base_value(set_measurement_spec, variable_index_by_set_id)
 
     self.variable_map = dict(
@@ -131,6 +132,14 @@ class Solver:
         self._add_cover_set_constraint(
             list(variable_index_by_set_id[i] for i in cover),
             variable_index_by_set_id[measured_set])
+
+  # Enforces that all the variables are non-negative.
+  def _add_lower_bounds(self):
+    logging.info("Adding lower bounds constraints.")
+    for i in range(self.num_variables):
+      variables = np.zeros(self.num_variables)
+      variables[i] = -1
+      self._add_gt_term(variables)
 
   def _add_cover_set_constraint(self, cover_variables: set[int],
       set_variable: int):
--- src/test/python/wfa/measurement/reporting/postprocessing/noiseninja/test_solver.py
+++ src/test/python/wfa/measurement/reporting/postprocessing/noiseninja/test_solver.py
@@ -12,12 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import numpy as np
 import unittest
-
-from noiseninja.noised_measurements import SetMeasurementsSpec, Measurement
+from noiseninja.noised_measurements import Measurement
+from noiseninja.noised_measurements import SetMeasurementsSpec
 from noiseninja.solver import Solver
-from qpsolvers import Solution, Problem
+from qpsolvers import Solution
 from unittest.mock import MagicMock
 
 HIGHS_SOLVER = "highs"
@@ -34,7 +33,6 @@ class SolverTest(unittest.TestCase):
     spec.add_measurement(2, Measurement(48, 0, "measurement_02"))
     spec.add_measurement(3, Measurement(1, 1, "measurement_03"))
     spec.add_measurement(4, Measurement(51, 1, "measurement_04"))
-
 
     solver = Solver(spec)
 
@@ -62,10 +60,10 @@ class SolverTest(unittest.TestCase):
     solution = solver.solve_and_translate()
 
     # Verifies that a valid solution is obtained.
-    self.assertAlmostEqual(solution[1], 50.000, places=3, msg=solution)
-    self.assertAlmostEqual(solution[2], 48.000, places=3, msg=solution)
-    self.assertAlmostEqual(solution[3], 2.000, places=3, msg=solution)
-    self.assertAlmostEqual(solution[1], solution[4], places=3, msg=solution)
+    self.assertAlmostEqual(solution[1], 50.000, places=2, msg=solution)
+    self.assertAlmostEqual(solution[2], 48.000, places=2, msg=solution)
+    self.assertAlmostEqual(solution[3], 2.000, places=2, msg=solution)
+    self.assertAlmostEqual(solution[1], solution[4], places=2, msg=solution)
 
   def test_solve_same_sigma_one_constraint(self):
     spec = SetMeasurementsSpec()
@@ -99,6 +97,24 @@ class SolverTest(unittest.TestCase):
     # set 3 has very small sigma, therefore should not change much.
     self.assertAlmostEqual(solution[3], 1, places=4, msg=solution)
     self.assertTrue(solution[1] <= solution[2] + solution[3])
+
+  def test_solve_solution_does_not_have_negative_values(self):
+    spec = SetMeasurementsSpec()
+    spec.add_subset_relation(1, 2)
+    spec.add_subset_relation(1, 3)
+    spec.add_cover(1, [2, 3])
+    spec.add_equal_relation(1, 4)
+    # The measurement values are set to meet the above constraints.
+    spec.add_measurement(1, Measurement(-1, 1, "measurement_01"))
+    spec.add_measurement(2, Measurement(-1, 1, "measurement_02"))
+    spec.add_measurement(3, Measurement(-1, 1, "measurement_03"))
+    spec.add_measurement(4, Measurement(-1, 1, "measurement_04"))
+    solution = Solver(spec).solve_and_translate()
+    # Solutions are all capped to 0 due to the lower bound constraints.
+    self.assertGreaterEqual(solution[1], 0.0)
+    self.assertGreaterEqual(solution[2], 0.0)
+    self.assertGreaterEqual(solution[3], 0.0)
+    self.assertGreaterEqual(solution[4], 0.0)
 
 
 if __name__ == "__main__":
