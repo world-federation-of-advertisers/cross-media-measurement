5746f39ebe6dd66085027b0c75f307f7c69e0d91
Sanjay Vasandani <sanjay@sanjayvas.com>
2025-02-04 08:49:18 -0800

    Revert "fix: Update population spec converter (#2036)"
    This reverts commit 06bb4ded8458cc4db68fdfbcef5acbf243e4e55d.

--- src/main/kotlin/org/wfanet/measurement/loadtest/dataprovider/PopulationSpecConverter.kt
+++ src/main/kotlin/org/wfanet/measurement/loadtest/dataprovider/PopulationSpecConverter.kt
@@ -20,10 +20,7 @@ import org.wfanet.measurement.api.v2alpha.PopulationSpec
 import org.wfanet.measurement.api.v2alpha.PopulationSpecKt.subPopulation
 import org.wfanet.measurement.api.v2alpha.PopulationSpecKt.vidRange
 import org.wfanet.measurement.api.v2alpha.event_group_metadata.testing.SyntheticPopulationSpec
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.Person
-import org.wfanet.measurement.api.v2alpha.event_templates.testing.person
 import org.wfanet.measurement.api.v2alpha.populationSpec
-import org.wfanet.measurement.common.pack
 
 fun SyntheticPopulationSpec.toPopulationSpec(): PopulationSpec {
   return populationSpec {
@@ -34,23 +31,6 @@ fun SyntheticPopulationSpec.toPopulationSpec(): PopulationSpec {
             startVid = it.vidSubRange.start
             endVidInclusive = (it.vidSubRange.endExclusive - 1)
           }
-          attributes +=
-            person {
-                gender =
-                  Person.Gender.forNumber(
-                    checkNotNull(it.populationFieldsValuesMap["person.gender"]).enumValue
-                  )
-                ageGroup =
-                  Person.AgeGroup.forNumber(
-                    checkNotNull(it.populationFieldsValuesMap["person.age_group"]).enumValue
-                  )
-                socialGradeGroup =
-                  Person.SocialGradeGroup.forNumber(
-                    checkNotNull(it.populationFieldsValuesMap["person.social_grade_group"])
-                      .enumValue
-                  )
-              }
-              .pack()
         }
       }
   }
