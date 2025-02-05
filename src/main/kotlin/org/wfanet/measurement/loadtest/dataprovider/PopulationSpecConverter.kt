adce18f88738eea129bac56bc38c80aa03e7ed3a
jojijac0b <jojijac0b@github.com>
2025-02-04 16:11:53 -0800

    Add a function to PopulationSpecConverter so that it can later be used in the KIND test for the PopulationRequisitionFulfiller.

--- src/main/kotlin/org/wfanet/measurement/loadtest/dataprovider/PopulationSpecConverter.kt
+++ src/main/kotlin/org/wfanet/measurement/loadtest/dataprovider/PopulationSpecConverter.kt
@@ -20,7 +20,10 @@ import org.wfanet.measurement.api.v2alpha.PopulationSpec
 import org.wfanet.measurement.api.v2alpha.PopulationSpecKt.subPopulation
 import org.wfanet.measurement.api.v2alpha.PopulationSpecKt.vidRange
 import org.wfanet.measurement.api.v2alpha.event_group_metadata.testing.SyntheticPopulationSpec
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.Person
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.person
 import org.wfanet.measurement.api.v2alpha.populationSpec
+import org.wfanet.measurement.common.pack
 
 fun SyntheticPopulationSpec.toPopulationSpec(): PopulationSpec {
   return populationSpec {
@@ -35,3 +38,35 @@ fun SyntheticPopulationSpec.toPopulationSpec(): PopulationSpec {
       }
   }
 }
+
+fun SyntheticPopulationSpec.toPopulationSpecWithAttributes(): PopulationSpec {
+  return populationSpec {
+    subpopulations +=
+      subPopulationsList.map { it ->
+        subPopulation {
+          vidRanges += vidRange {
+            startVid = it.vidSubRange.start
+            endVidInclusive = (it.vidSubRange.endExclusive - 1)
+          }
+          attributes +=
+            person {
+              gender =
+                Person.Gender.forNumber(
+                  checkNotNull(it.populationFieldsValuesMap["person.gender"]).enumValue
+                )
+              ageGroup =
+                Person.AgeGroup.forNumber(
+                  checkNotNull(it.populationFieldsValuesMap["person.age_group"]).enumValue
+                )
+              socialGradeGroup =
+                Person.SocialGradeGroup.forNumber(
+                  checkNotNull(it.populationFieldsValuesMap["person.social_grade_group"])
+                    .enumValue
+                )
+            }
+              .pack()
+        }
+      }
+  }
+}
+
