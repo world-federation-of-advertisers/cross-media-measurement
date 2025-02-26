b944330a1dff80d4b68ab0388f23657fb266c3ae
marcopremier <marcopremier@meta.com>
2025-02-26 07:56:36 -0800

    feat: wip createWorkItemAttempts internal service

---
+++ src/main/kotlin/org/wfanet/measurement/securecomputation/deploy/gcloud/spanner/SpannerWorkItemAttemptsService.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2025 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.securecomputation.deploy.gcloud.spanner
+
+import io.grpc.Status
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import org.wfanet.measurement.common.grpc.grpcRequire
+import org.wfanet.measurement.common.identity.ExternalId
+import org.wfanet.measurement.common.identity.IdGenerator
+import org.wfanet.measurement.gcloud.spanner.AsyncDatabaseClient
+import org.wfanet.measurement.internal.securecomputation.controlplane.v1alpha.CompleteWorkItemAttemptRequest
+import org.wfanet.measurement.internal.securecomputation.controlplane.v1alpha.CreateWorkItemAttemptRequest
+import org.wfanet.measurement.internal.securecomputation.controlplane.v1alpha.FailWorkItemAttemptRequest
+import org.wfanet.measurement.internal.securecomputation.controlplane.v1alpha.GetWorkItemAttemptRequest
+import org.wfanet.measurement.internal.securecomputation.controlplane.v1alpha.StreamWorkItemAttemptsRequest
+import org.wfanet.measurement.internal.securecomputation.controlplane.v1alpha.WorkItemAttemptsGrpcKt.WorkItemAttemptsCoroutineImplBase
+import org.wfanet.measurement.internal.securecomputation.controlplane.v1alpha.WorkItemAttempt
+import org.wfanet.measurement.securecomputation.deploy.gcloud.spanner.common.WorkItemNotFoundException
+import org.wfanet.measurement.securecomputation.deploy.gcloud.spanner.queries.StreamWorkItems
+import org.wfanet.measurement.securecomputation.deploy.gcloud.spanner.readers.WorkItemReader
+import org.wfanet.measurement.securecomputation.deploy.gcloud.spanner.writers.FailWorkItem
+
+class SpannerWorkItemAttemptsService(
+  private val idGenerator: IdGenerator,
+  private val client: AsyncDatabaseClient,
+) : WorkItemAttemptsCoroutineImplBase() {
+
+  override suspend fun createWorkItemAttempt(request: CreateWorkItemAttemptRequest): WorkItemAttempt {
+//    return CreateWorkItem(request.workItem).execute(client, idGenerator)
+  }
+
+  override suspend fun getWorkItemAttempt(request: GetWorkItemAttemptRequest): WorkItemAttempt {
+    val externalWorkItemId = ExternalId(request.externalWorkItemId)
+    return WorkItemReader()
+      .readByExternalId(
+        client.singleUse(),
+        externalWorkItemId,
+      )
+      ?.workItem
+      ?: throw WorkItemNotFoundException(
+        externalWorkItemId,
+      )
+        .asStatusRuntimeException(Status.Code.NOT_FOUND, "WorkItem not found.")
+  }
+
+  override fun streamWorkItemAttempts(request: StreamWorkItemAttemptsRequest): Flow<WorkItemAttempt> {
+    grpcRequire(request.limit >= 0) { "Limit cannot be less than 0" }
+    return StreamWorkItems(request.filter, request.limit).execute(client.singleUse()).map {
+      it.workItem
+    }
+  }
+
+  override suspend fun failWorkItemAttempt(request: FailWorkItemAttemptRequest): WorkItemAttempt {
+    grpcRequire(request.externalWorkItemId != 0L) {
+      "external_work_item_id not specified"
+    }
+    try {
+      return FailWorkItem(request).execute(client, idGenerator)
+    } catch (e: ModelLineNotFoundException) {
+      throw e.asStatusRuntimeException(Status.Code.NOT_FOUND, e.message ?: "ModelLine not found.")
+    } catch (e: ModelLineTypeIllegalException) {
+      throw e.asStatusRuntimeException(
+        Status.Code.INVALID_ARGUMENT,
+        e.message
+          ?: "Only ModelLines with type equal to 'PROD' can have a HoldbackModelLine having type equal to 'HOLDBACK'.",
+      )
+    }
+  }
+
+  override suspend fun completeWorkItemAttempt(request: CompleteWorkItemAttemptRequest): WorkItemAttempt {
+
+  }
+
+}
