b7cb2d2d3e56d7175c283b158a1541679c8472f8
Lin <lindreamdeyi@google.com>
2025-03-04 21:11:27 +0000

    feat: add a duration flag that defines recent updated measurement time window for prober, add two prober unit test cases, use mock clock and cancelMeasurement in prober integration test

--- src/main/kotlin/org/wfanet/measurement/integration/common/InProcessMeasurementSystemProberIntegrationTest.kt
+++ src/main/kotlin/org/wfanet/measurement/integration/common/InProcessMeasurementSystemProberIntegrationTest.kt
@@ -20,8 +20,8 @@ import com.google.common.truth.Truth.assertThat
 import io.grpc.StatusException
 import java.io.File
 import java.nio.file.Paths
-import java.time.Clock
 import java.time.Duration
+import java.time.Instant
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.toList
@@ -37,6 +37,7 @@ import org.wfanet.measurement.api.v2alpha.Measurement
 import org.wfanet.measurement.api.v2alpha.MeasurementConsumersGrpcKt.MeasurementConsumersCoroutineStub
 import org.wfanet.measurement.api.v2alpha.MeasurementsGrpcKt.MeasurementsCoroutineStub
 import org.wfanet.measurement.api.v2alpha.RequisitionsGrpcKt.RequisitionsCoroutineStub
+import org.wfanet.measurement.api.v2alpha.cancelMeasurementRequest
 import org.wfanet.measurement.api.v2alpha.listMeasurementsRequest
 import org.wfanet.measurement.api.withAuthenticationKey
 import org.wfanet.measurement.common.api.grpc.ResourceList
@@ -45,6 +46,7 @@ import org.wfanet.measurement.common.api.grpc.listResources
 import org.wfanet.measurement.common.getRuntimePath
 import org.wfanet.measurement.common.identity.withPrincipalName
 import org.wfanet.measurement.common.testing.ProviderRule
+import org.wfanet.measurement.common.testing.TestClockWithNamedInstants
 import org.wfanet.measurement.kingdom.batch.MeasurementSystemProber
 import org.wfanet.measurement.kingdom.deploy.common.service.DataServices
 import org.wfanet.measurement.system.v1alpha.ComputationLogEntriesGrpcKt.ComputationLogEntriesCoroutineStub
@@ -80,6 +82,8 @@ abstract class InProcessMeasurementSystemProberIntegrationTest(
     RequisitionsCoroutineStub(inProcessCmmsComponents.kingdom.publicApiChannel)
   }
 
+  private lateinit var clock: TestClockWithNamedInstants
+
   private lateinit var prober: MeasurementSystemProber
 
   @Before
@@ -99,7 +103,8 @@ abstract class InProcessMeasurementSystemProberIntegrationTest(
     val privateKeyDerFile = secretsDir.resolve("${MC_DISPLAY_NAME}_cs_private.der")
     val durationBetweenMeasurement: Duration = Duration.ofSeconds(5)
     val measurementLookBackDuration = Duration.ofDays(1)
-    val clock = Clock.systemUTC()
+    val recentUpdatedMeasurementWindow = Duration.ofHours(2)
+    clock = TestClockWithNamedInstants(Instant.now())
     prober =
       MeasurementSystemProber(
         measurementConsumerData.name,
@@ -108,6 +113,7 @@ abstract class InProcessMeasurementSystemProberIntegrationTest(
         privateKeyDerFile,
         measurementLookBackDuration,
         durationBetweenMeasurement,
+        recentUpdatedMeasurementWindow,
         publicMeasurementConsumersClient,
         publicMeasurementsClient,
         publicDataProvidersClient,
@@ -151,10 +157,22 @@ abstract class InProcessMeasurementSystemProberIntegrationTest(
     var measurements = listMeasurements()
     assertThat(measurements.size).isEqualTo(1)
 
-    Thread.sleep(10000)
+    clock.tickSeconds(
+      "Time buffer to allow the first prober measurement to finish",
+      Duration.ofSeconds(5).toSeconds(),
+    )
+
+    try {
+      publicMeasurementsClient
+        .withAuthenticationKey(
+          inProcessCmmsComponents.getMeasurementConsumerData().apiAuthenticationKey
+        )
+        .cancelMeasurement(cancelMeasurementRequest { name = measurements.single().name })
+    } catch (e: StatusException) {
+      throw Exception("Unable to cancel measurement ${measurements.single().name}")
+    }
 
     prober.run()
-    measurements = listMeasurements()
     assertThat(measurements.size).isEqualTo(2)
   }
 
