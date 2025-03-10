d63bed88f4c963a3ea4db587157e1afcc3af7cc2
Steven Ware Jones <stevenwarejones@gmail.com>
2025-03-10 09:44:48 -0400

    copy code from jojis cloud function pr on setting up mlts from env vars

--- src/main/kotlin/org/wfanet/measurement/securecomputation/datawatcher/DataWatcher.kt
+++ src/main/kotlin/org/wfanet/measurement/securecomputation/datawatcher/DataWatcher.kt
@@ -32,11 +32,53 @@ import com.google.protobuf.InvalidProtocolBufferException
 import com.google.protobuf.util.JsonFormat
 import java.util.UUID
 import kotlinx.coroutines.runBlocking
+import org.wfanet.measurement.common.pack
+import org.wfanet.measurement.securecomputation.controlplane.v1alpha.GooglePubSubWorkItemsService
+import org.wfanet.measurement.securecomputation.controlplane.v1alpha.createWorkItemRequest
+import org.wfanet.measurement.securecomputation.controlplane.v1alpha.workItem
+import org.wfanet.measurement.securecomputation.controlplane.v1alpha.workItemConfig
+import org.wfanet.measurement.securecomputation.datawatcher.v1alpha.DataWatcherConfig
+import kotlin.text.matches
+import kotlin.io.path.Path
+import org.wfanet.measurement.common.crypto.SigningCerts
 
 class DataWatcher(
   private val workItemsService: GooglePubSubWorkItemsService,
   private val dataWatcherConfigs: List<DataWatcherConfig>
 ) : CloudEventsFunction {
+
+  private fun getClientCerts(): SigningCerts {
+    return SigningCerts.fromPemFiles(
+      certificateFile = Path(System.getenv("CERT_FILE_PATH")).toFile(),
+      privateKeyFile = Path(System.getenv("PRIVATE_KEY_FILE_PATH")).toFile(),
+      trustedCertCollectionFile = Path(System.getenv("CERT_COLLECTION_FILE_PATH")).toFile(),
+    )
+  }
+
+override fun service(request: HttpRequest, response: HttpResponse) {
+  response.getWriter().write("OK")
+  if (true) {
+    val publicChannel =
+      buildMutualTlsChannel(System.getenv("TARGET"), getClientCerts(), System.getenv("CERT_HOST"))
+
+    val requisitionsStub = RequisitionsCoroutineStub(publicChannel)
+    val requisitionsStorageClient =
+      GcsStorageClient(
+        StorageOptions.newBuilder()
+          .setProjectId(System.getenv("REQUISITIONS_GCS_PROJECT_ID"))
+          .build()
+          .service,
+        System.getenv("REQUISITIONS_GCS_BUCKET"),
+      )
+    val requisitionFetcher =
+      RequisitionFetcher(
+        requisitionsStub,
+        requisitionsStorageClient,
+        System.getenv("DATAPROVIDER_NAME"),
+        System.getenv("PAGE_SIZE").toInt(),
+        System.getenv("STORAGE_PATH_PREFIX"),
+      )
+    runBlocking { requisitionFetcher.fetchAndStoreRequisitions() }
 
   override fun accept(event: CloudEvent) {
     val cloudEventData = String(event.getData().toBytes(), StandardCharsets.UTF_8)
