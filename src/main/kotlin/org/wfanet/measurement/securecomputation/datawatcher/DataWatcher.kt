d07eeb03cd1661a7ce26be4799d79ec77a48a998
Steven Ware Jones <stevenwarejones@gmail.com>
2025-03-19 20:17:33 -0400

    add separate DataWatcherTest

--- src/main/kotlin/org/wfanet/measurement/securecomputation/datawatcher/DataWatcher.kt
+++ src/main/kotlin/org/wfanet/measurement/securecomputation/datawatcher/DataWatcher.kt
@@ -16,21 +16,8 @@
 
 package org.wfanet.measurement.securecomputation.datawatcher
 
-import org.wfanet.measurement.securecomputation.controlplane.v1alpha.WorkItemsService
-import com.google.events.cloud.storage.v1.StorageObjectData
-import org.wfanet.measurement.securecomputation.DataWatcherConfig.DiscoveredWork
-import org.wfanet.measurement.securecomputation.DataWatcherConfig
-import com.google.cloud.functions.CloudEventsFunction
-import org.wfanet.measurement.securecomputation.controlplane.v1alpha.GooglePubSubWorkItemsService
-import java.nio.charset.StandardCharsets
-import org.wfanet.measurement.securecomputation.controlplane.v1alpha.WorkItem
-import org.wfanet.measurement.securecomputation.controlplane.v1alpha.CreateWorkItemRequest
-import org.wfanet.measurement.common.pack
-import com.google.protobuf.Any
-import io.cloudevents.CloudEvent
-import com.google.protobuf.InvalidProtocolBufferException
-import com.google.protobuf.util.JsonFormat
 import java.util.UUID
+import kotlin.text.matches
 import kotlinx.coroutines.runBlocking
 import org.wfanet.measurement.common.pack
 import org.wfanet.measurement.securecomputation.controlplane.v1alpha.GooglePubSubWorkItemsService
@@ -38,67 +25,28 @@ import org.wfanet.measurement.securecomputation.controlplane.v1alpha.createWorkI
 import org.wfanet.measurement.securecomputation.controlplane.v1alpha.workItem
 import org.wfanet.measurement.securecomputation.controlplane.v1alpha.workItemConfig
 import org.wfanet.measurement.securecomputation.datawatcher.v1alpha.DataWatcherConfig
-import kotlin.text.matches
-import kotlin.io.path.Path
-import org.wfanet.measurement.common.crypto.SigningCerts
 
+/*
+ * The DataWatcher calls various sinks to with the data it has received.
+ * @param workItemsService - the Google Pub Sub Sink to call
+ * @param dataWatcherConfigs - a list of [DataWatcherConfig]
+ */
 class DataWatcher(
   private val workItemsService: GooglePubSubWorkItemsService,
-  private val dataWatcherConfigs: List<DataWatcherConfig>
-) : CloudEventsFunction {
-
-  private fun getClientCerts(): SigningCerts {
-    return SigningCerts.fromPemFiles(
-      certificateFile = Path(System.getenv("CERT_FILE_PATH")).toFile(),
-      privateKeyFile = Path(System.getenv("PRIVATE_KEY_FILE_PATH")).toFile(),
-      trustedCertCollectionFile = Path(System.getenv("CERT_COLLECTION_FILE_PATH")).toFile(),
-    )
-  }
-
-override fun service(request: HttpRequest, response: HttpResponse) {
-  response.getWriter().write("OK")
-  if (true) {
-    val publicChannel =
-      buildMutualTlsChannel(System.getenv("TARGET"), getClientCerts(), System.getenv("CERT_HOST"))
-
-    val requisitionsStub = RequisitionsCoroutineStub(publicChannel)
-    val requisitionsStorageClient =
-      GcsStorageClient(
-        StorageOptions.newBuilder()
-          .setProjectId(System.getenv("REQUISITIONS_GCS_PROJECT_ID"))
-          .build()
-          .service,
-        System.getenv("REQUISITIONS_GCS_BUCKET"),
-      )
-    val requisitionFetcher =
-      RequisitionFetcher(
-        requisitionsStub,
-        requisitionsStorageClient,
-        System.getenv("DATAPROVIDER_NAME"),
-        System.getenv("PAGE_SIZE").toInt(),
-        System.getenv("STORAGE_PATH_PREFIX"),
-      )
-    runBlocking { requisitionFetcher.fetchAndStoreRequisitions() }
-
-  override fun accept(event: CloudEvent) {
-    val cloudEventData = String(event.getData().toBytes(), StandardCharsets.UTF_8)
-    val builder = StorageObjectData.newBuilder()
-    JsonFormat.parser().merge(cloudEventData, builder)
-    val data = builder.build()
-    val bucket = data.getBucket()
-    val blobKey = data.getName()
-    dataWatcherConfigs.forEach { config ->
+  private val dataWatcherConfigs: List<DataWatcherConfig>,
+) {
+  fun receivePath(path: String) {
+    for (config in dataWatcherConfigs) {
       val regex = config.sourcePathRegex.toRegex()
-      if (regex.containsMatchIn(blobKey)) {
-        @Suppress("WHEN_ENUM_CAN_BE_NULL_IN_JAVA")
+      if (regex.matches(path)) {
         when (config.sinkConfigCase) {
           DataWatcherConfig.SinkConfigCase.CONTROL_PLANE_CONFIG -> {
             val queueConfig = config.controlPlaneConfig
             val workItemId = UUID.randomUUID().toString()
             val workItemParams =
-              triggeredApp {
+              workItemConfig {
                   this.config = queueConfig.appConfig
-                  this.path = path
+                  this.dataPath = path
                 }
                 .pack()
             val request = createWorkItemRequest {
