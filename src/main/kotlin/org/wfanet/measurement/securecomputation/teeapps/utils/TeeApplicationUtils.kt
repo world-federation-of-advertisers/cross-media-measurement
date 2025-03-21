01c10e1ef525f125e230e4aca7c7e5d21876fce5
jojijac0b <jojijac0b@github.com>
2025-03-20 20:07:04 -0700

    Update logic to remove FieldMapper interface

--- src/main/kotlin/org/wfanet/measurement/securecomputation/teeapps/utils/TeeApplicationUtils.kt
+++ src/main/kotlin/org/wfanet/measurement/securecomputation/teeapps/utils/TeeApplicationUtils.kt
@@ -6,138 +6,190 @@ import com.google.protobuf.Descriptors.Descriptor
 import com.google.protobuf.Descriptors.FieldDescriptor
 import com.google.protobuf.DynamicMessage
 import com.google.protobuf.Message
-
-
-
-interface FieldMapper {
-    fun getMappedValue(sourceValue: Any): Any
-}
-
-data class FieldDescriptorAndMapper(
-    val fieldDescriptor: FieldDescriptor,
-    val mapper: FieldMapper,
-)
+import com.google.protobuf.util.JsonFormat
 
 class TeeApplicationUtils {
+  companion object {
     fun convertMessage(
-        originalMessage: Message,
-        newMessageDescriptor: Descriptor,
-        fieldMapping: Map<String, FieldDescriptorAndMapper>
+      originalMessage: Message,
+      newMessageDescriptor: Descriptor,
+      fieldNameMap: Map<String, String>,
+      fieldValueMap: Map<String, String>,
     ): DynamicMessage {
-        val builder = DynamicMessage.newBuilder(newMessageDescriptor)
+      val builder = DynamicMessage.newBuilder(newMessageDescriptor)
 
-        for (field in originalMessage.allFields) {
-            val fieldName = field.key.name
-            // TODO: dont just continue if these values arent there. throw an error because this is not expected. the values should be present
-            val (newMessageFieldDescriptor, fieldMapper) = fieldMapping[fieldName] ?: continue
-            val newValue = fieldMapper.getMappedValue(field.value)
-            val convertedValue = convertValueToTargetType(newValue, newMessageFieldDescriptor) ?:
-                throw IllegalArgumentException("Failed to convert value for field ${fieldName} from ${newValue.javaClass} to ${newMessageFieldDescriptor.javaType}")
-            builder.setField(newMessageFieldDescriptor, convertedValue)
+      for (originalMessageFieldDescriptor in originalMessage.descriptorForType.fields) {
+        val originalMessageFieldName = originalMessageFieldDescriptor.name
+        val originalMessageFieldValueString = originalMessage.getField(originalMessageFieldDescriptor).toString()
+
+        val newMessageFieldName = fieldNameMap[originalMessageFieldName]
+        val newMessageFieldValueString = fieldValueMap[originalMessageFieldValueString]!!
+        val newMessageFieldDescriptor = newMessageDescriptor.findFieldByName(newMessageFieldName)
+
+        when (newMessageFieldDescriptor.type) {
+          // Integer types
+          FieldDescriptor.Type.INT32, FieldDescriptor.Type.SINT32, FieldDescriptor.Type.FIXED32, FieldDescriptor.Type.SFIXED32 ->
+            builder.setField(newMessageFieldDescriptor, newMessageFieldValueString.toInt())
+
+          // Long types
+          FieldDescriptor.Type.INT64, FieldDescriptor.Type.SINT64, FieldDescriptor.Type.FIXED64, FieldDescriptor.Type.SFIXED64 ->
+            builder.setField(newMessageFieldDescriptor, newMessageFieldValueString.toLong())
+
+          // Float and Double
+          FieldDescriptor.Type.FLOAT ->
+            builder.setField(newMessageFieldDescriptor, newMessageFieldValueString.toFloat())
+
+          FieldDescriptor.Type.DOUBLE ->
+            builder.setField(newMessageFieldDescriptor, newMessageFieldValueString.toDouble())
+
+          // Boolean
+          FieldDescriptor.Type.BOOL ->
+            builder.setField(newMessageFieldDescriptor, newMessageFieldValueString.lowercase() == "true")
+
+          // String - direct use
+          FieldDescriptor.Type.STRING ->
+            builder.setField(newMessageFieldDescriptor, newMessageFieldValueString)
+
+          // Bytes - convert string to ByteString
+          FieldDescriptor.Type.BYTES ->
+            builder.setField(newMessageFieldDescriptor, ByteString.copyFrom(newMessageFieldValueString.toByteArray()))
+
+          // Enum types - find enum value by name
+          FieldDescriptor.Type.ENUM -> {
+            val enumType = newMessageFieldDescriptor.enumType
+            val enumValue = enumType.findValueByName(newMessageFieldValueString)
+              ?: throw IllegalArgumentException(
+                "Invalid enum value '$newMessageFieldValueString' for enum type ${enumType.fullName}"
+              )
+            builder.setField(newMessageFieldDescriptor, enumValue)
+          }
+
+          // Message types - parse from JSON
+          FieldDescriptor.Type.MESSAGE -> {
+            try {
+              // This assumes newMessageFieldValueString is a valid JSON representation of the message
+              val jsonParser =JsonFormat.parser()
+              val fieldMessageBuilder = DynamicMessage.newBuilder(newMessageFieldDescriptor.messageType)
+              jsonParser.merge(newMessageFieldValueString, fieldMessageBuilder)
+              builder.setField(newMessageFieldDescriptor, fieldMessageBuilder.build())
+            } catch (e: Exception) {
+              throw IllegalArgumentException(
+                "Failed to parse message value '$newMessageFieldValueString' for message type ${newMessageFieldDescriptor.messageType.fullName}",
+                e
+              )
+            }
+          }
+
+          // For other types, throw an exception
+          else -> throw IllegalArgumentException(
+            "Unsupported field type: ${newMessageFieldDescriptor.type} for field ${newMessageFieldDescriptor.fullName}"
+          )
         }
-
-        return builder.build()
+      }
+      return builder.build()
     }
 
     /**
      * Converts a value to the appropriate type for the target field.
      */
     private fun convertValueToTargetType(value: Any, fieldDescriptor: FieldDescriptor): Any? {
-        return when (fieldDescriptor.javaType) {
-            FieldDescriptor.JavaType.MESSAGE -> {
-                when (value) {
-                    // If value is already a Message, check if it's compatible
-                    is Message -> {
-                        if (value.descriptorForType == fieldDescriptor.messageType) {
-                            // Already the correct type
-                            value
-                        } else {
-                            // Would need custom conversion between different message types
-                            null
-                        }
-                    }
-                    else -> null
-                }
-            }
-
-            FieldDescriptor.JavaType.ENUM -> {
-                when (value) {
-                    // If value is already an EnumValueDescriptor, check if it belongs to the right enum type
-                    is EnumValueDescriptor -> {
-                        if (value.type == fieldDescriptor.enumType) value else null
-                    }
-                    // If value is a Java/Kotlin enum
-                    is Enum<*> -> fieldDescriptor.enumType.findValueByName(value.name)
-                    // If value is a string, try to find corresponding enum value
-                    is String -> fieldDescriptor.enumType.findValueByName(value)
-                    // If value is an integer, try to find corresponding enum value by number
-                    is Int -> fieldDescriptor.enumType.findValueByNumber(value)
-                    // If nothing else works, try toString and find by name
-                    else -> fieldDescriptor.enumType.findValueByName(value.toString())
-                }
-            }
-
-            FieldDescriptor.JavaType.INT -> {
-                when (value) {
-                    is Int -> value
-                    is Number -> value.toInt()
-                    is String -> value.toIntOrNull()
-                    is Boolean -> if (value) 1 else 0
-                    else -> null
-                }
-            }
-
-            FieldDescriptor.JavaType.LONG -> {
-                when (value) {
-                    is Long -> value
-                    is Number -> value.toLong()
-                    is String -> value.toLongOrNull()
-                    is Boolean -> if (value) 1L else 0L
-                    else -> null
-                }
-            }
-
-            FieldDescriptor.JavaType.FLOAT -> {
-                when (value) {
-                    is Float -> value
-                    is Number -> value.toFloat()
-                    is String -> value.toFloatOrNull()
-                    is Boolean -> if (value) 1f else 0f
-                    else -> null
-                }
-            }
-
-            FieldDescriptor.JavaType.DOUBLE -> {
-                when (value) {
-                    is Double -> value
-                    is Number -> value.toDouble()
-                    is String -> value.toDoubleOrNull()
-                    is Boolean -> if (value) 1.0 else 0.0
-                    else -> null
-                }
-            }
-
-            FieldDescriptor.JavaType.BOOLEAN -> {
-                when (value) {
-                    is Boolean -> value
-                    is String -> value.toBoolean()
-                    is Number -> value.toInt() != 0
-                    else -> null
-                }
-            }
-
-            FieldDescriptor.JavaType.STRING -> value.toString()
-
-            FieldDescriptor.JavaType.BYTE_STRING -> {
-                when (value) {
-                    is ByteString -> value
-                    is ByteArray -> ByteString.copyFrom(value)
-                    is String -> ByteString.copyFromUtf8(value)
-                    else -> null
-                }
+      return when (fieldDescriptor.javaType) {
+        FieldDescriptor.JavaType.MESSAGE -> {
+          when (value) {
+            // If value is already a Message, check if it's compatible
+            is Message -> {
+              if (value.descriptorForType == fieldDescriptor.messageType) {
+                // Already the correct type
+                value
+              } else {
+                // Would need custom conversion between different message types
+                null
+              }
             }
 
             else -> null
+          }
         }
+
+        FieldDescriptor.JavaType.ENUM -> {
+          when (value) {
+            // If value is already an EnumValueDescriptor, check if it belongs to the right enum type
+            is EnumValueDescriptor -> {
+              if (value.type == fieldDescriptor.enumType) value else null
+            }
+            // If value is a Java/Kotlin enum
+            is Enum<*> -> fieldDescriptor.enumType.findValueByName(value.name)
+            // If value is a string, try to find corresponding enum value
+            is String -> fieldDescriptor.enumType.findValueByName(value)
+            // If value is an integer, try to find corresponding enum value by number
+            is Int -> fieldDescriptor.enumType.findValueByNumber(value)
+            // If nothing else works, try toString and find by name
+            else -> fieldDescriptor.enumType.findValueByName(value.toString())
+          }
+        }
+
+        FieldDescriptor.JavaType.INT -> {
+          when (value) {
+            is Int -> value
+            is Number -> value.toInt()
+            is String -> value.toIntOrNull()
+            is Boolean -> if (value) 1 else 0
+            else -> null
+          }
+        }
+
+        FieldDescriptor.JavaType.LONG -> {
+          when (value) {
+            is Long -> value
+            is Number -> value.toLong()
+            is String -> value.toLongOrNull()
+            is Boolean -> if (value) 1L else 0L
+            else -> null
+          }
+        }
+
+        FieldDescriptor.JavaType.FLOAT -> {
+          when (value) {
+            is Float -> value
+            is Number -> value.toFloat()
+            is String -> value.toFloatOrNull()
+            is Boolean -> if (value) 1f else 0f
+            else -> null
+          }
+        }
+
+        FieldDescriptor.JavaType.DOUBLE -> {
+          when (value) {
+            is Double -> value
+            is Number -> value.toDouble()
+            is String -> value.toDoubleOrNull()
+            is Boolean -> if (value) 1.0 else 0.0
+            else -> null
+          }
+        }
+
+        FieldDescriptor.JavaType.BOOLEAN -> {
+          when (value) {
+            is Boolean -> value
+            is String -> value.toBoolean()
+            is Number -> value.toInt() != 0
+            else -> null
+          }
+        }
+
+        FieldDescriptor.JavaType.STRING -> value.toString()
+
+        FieldDescriptor.JavaType.BYTE_STRING -> {
+          when (value) {
+            is ByteString -> value
+            is ByteArray -> ByteString.copyFrom(value)
+            is String -> ByteString.copyFromUtf8(value)
+            else -> null
+          }
+        }
+
+        else -> null
+      }
     }
+  }
 }
--- src/test/kotlin/org/wfanet/measurement/securecomputation/teeapps/utils/TeeApplicationUtilsTest.kt
+++ src/test/kotlin/org/wfanet/measurement/securecomputation/teeapps/utils/TeeApplicationUtilsTest.kt
@@ -3,19 +3,13 @@ package org.wfanet.measurement.securecomputation.teeapps.utils
 import org.junit.Before
 import org.junit.Test
 import org.wfanet.measurement.api.v2alpha.event_templates.testing.Person
+import org.wfanet.panelmatch.common.beam.testing.assertThat
 import org.wfanet.virtualpeople.common.AgeRange
 import org.wfanet.virtualpeople.common.Gender
 import org.wfanet.virtualpeople.common.ageRange
 import org.wfanet.virtualpeople.common.demoBucket
 
 class TeeApplicationUtilsTest {
-  private lateinit var utils: TeeApplicationUtils
-
-  @Before
-  fun setup() {
-    utils = TeeApplicationUtils()
-  }
-
   @Test
   fun `mapMessage should correctly map fields from source to target message`() {
     val originalMessage = demoBucket {
@@ -26,52 +20,27 @@ class TeeApplicationUtilsTest {
       }
     }
 
+    val fieldNameMap = mapOf(
+      "gender" to "gender",
+      "age" to "age_group"
+    )
+
+    val fieldValueMap = mapOf(
+      "GENDER_MALE" to "MALE",
+      "GENDER_FEMALE" to "FEMALE",
+    )
+
     val newMessageDescriptor = Person.getDescriptor()
 
-    val fieldMapping = mapOf(
-      "gender" to FieldDescriptorAndMapper(
-        Person.getDescriptor().findFieldByName("gender"),
-        object : FieldMapper {
-          override fun getMappedValue(sourceValue: Any): Any {
-            val genderEnumValue = sourceValue as Int
-            return when (genderEnumValue) {
-              0 -> Person.Gender.GENDER_UNSPECIFIED
-              1 -> Person.Gender.MALE
-              2 -> Person.Gender.FEMALE
-              else -> throw IllegalArgumentException("Illegal enum value for Gender")
-            }
-          }
-        }
-      ),
-      "age" to FieldDescriptorAndMapper(
-        Person.getDescriptor().findFieldByName("age_group"),
-        object : FieldMapper {
-          override fun getMappedValue(sourceValue: Any): Any {
-            return try {
-              val ageRange = sourceValue as AgeRange
-              val minAge = ageRange.minAge
-              val maxAge = ageRange.maxAge
-
-              // TODO: When is age group unspecified. When is Person.AgeGroup.AGE_GROUP_UNSPECIFIED returned
-              if (minAge >= 18 && maxAge <= 34) {
-                Person.AgeGroup.YEARS_18_TO_34
-              } else if (minAge >= 35 && maxAge <= 54) {
-                Person.AgeGroup.YEARS_35_TO_54
-              } else if (minAge >= 55) {
-                Person.AgeGroup.YEARS_18_TO_34_VALUE
-              } else Person.AgeGroup.UNRECOGNIZED
-            } catch (e: IllegalArgumentException) {
-              println("Error: The value is not an AgeRange: $e")
-            }
-          }
-        }
-      )
-    )
-
-    utils.convertMessage(
+    val a = TeeApplicationUtils.convertMessage(
       originalMessage,
       newMessageDescriptor,
-      fieldMapping
+      fieldNameMap,
+      fieldValueMap
     )
+
+    println("JOJI $a")
+
+    assert(1==2)
   }
 }
