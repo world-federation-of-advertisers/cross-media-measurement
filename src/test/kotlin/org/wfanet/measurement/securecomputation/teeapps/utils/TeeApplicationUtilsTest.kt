b51d8e7f49f704f10fae30d5e1ea7117fa35c034
jojijac0b <jojijac0b@github.com>
2025-03-21 08:19:35 -0700

    Update test

--- src/test/kotlin/org/wfanet/measurement/securecomputation/teeapps/utils/TeeApplicationUtilsTest.kt
+++ src/test/kotlin/org/wfanet/measurement/securecomputation/teeapps/utils/TeeApplicationUtilsTest.kt
@@ -1,24 +1,34 @@
 package org.wfanet.measurement.securecomputation.teeapps.utils
 
-import org.junit.Before
 import org.junit.Test
+import com.google.common.truth.Truth.assertThat
 import org.wfanet.measurement.api.v2alpha.event_templates.testing.Person
-import org.wfanet.panelmatch.common.beam.testing.assertThat
-import org.wfanet.virtualpeople.common.AgeRange
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.person
 import org.wfanet.virtualpeople.common.Gender
 import org.wfanet.virtualpeople.common.ageRange
 import org.wfanet.virtualpeople.common.demoBucket
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.Person.Gender as PersonGender
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.Person.AgeGroup
 
 class TeeApplicationUtilsTest {
   @Test
   fun `mapMessage should correctly map fields from source to target message`() {
+//    val originalMessage = common {
+//      gender = Gender.GENDER_MALE
+//      age = ageRange {
+//        minAge = 18
+//        maxAge = 24
+//      }
+//    }
+
     val originalMessage = demoBucket {
       gender = Gender.GENDER_MALE
       age = ageRange {
         minAge = 18
-        maxAge = 24
+        maxAge = 34
       }
     }
+
 
     val fieldNameMap = mapOf(
       "gender" to "gender",
@@ -28,19 +38,25 @@ class TeeApplicationUtilsTest {
     val fieldValueMap = mapOf(
       "GENDER_MALE" to "MALE",
       "GENDER_FEMALE" to "FEMALE",
+       originalMessage.age.toString() to "YEARS_18_TO_34"
     )
 
     val newMessageDescriptor = Person.getDescriptor()
 
-    val a = TeeApplicationUtils.convertMessage(
+    val newMessage = TeeApplicationUtils.convertMessage(
       originalMessage,
       newMessageDescriptor,
       fieldNameMap,
       fieldValueMap
     )
 
-    println("JOJI $a")
+    val expectedMessage = person {
+      gender = PersonGender.MALE
+      ageGroup = AgeGroup.YEARS_18_TO_34
+    }
 
-    assert(1==2)
+
+    assertThat(newMessage.descriptorForType == expectedMessage.descriptorForType)
+    assertThat(newMessage.toByteString() == expectedMessage.toByteString())
   }
 }
