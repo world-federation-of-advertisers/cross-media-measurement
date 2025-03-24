fa19fc917286fad52d44be58f0c7ed1b25447cc7
Lin <lindreamdeyi@google.com>
2025-03-24 22:27:38 +0000

    feat: add the internal iqf service to the internal reporting server
    # Conflicts:
    #	src/main/k8s/reporting_v2.cue
    #	src/main/kotlin/org/wfanet/measurement/reporting/deploy/v2/common/server/InternalReportingServer.kt
    #	src/main/kotlin/org/wfanet/measurement/reporting/deploy/v2/common/service/DataServices.kt
    
    # Conflicts:
    #	src/main/k8s/reporting_v2.cue
    #	src/main/kotlin/org/wfanet/measurement/reporting/deploy/v2/common/server/InternalReportingServer.kt
    #	src/main/kotlin/org/wfanet/measurement/reporting/deploy/v2/common/service/DataServices.kt
    
    # Conflicts:
    #	src/main/kotlin/org/wfanet/measurement/reporting/deploy/v2/common/server/InternalReportingServer.kt
    #	src/main/kotlin/org/wfanet/measurement/reporting/deploy/v2/common/service/DataServices.kt

--- src/main/kotlin/org/wfanet/measurement/reporting/deploy/v2/common/service/DataServices.kt
+++ src/main/kotlin/org/wfanet/measurement/reporting/deploy/v2/common/service/DataServices.kt
@@ -20,6 +20,7 @@ import org.wfanet.measurement.common.db.r2dbc.DatabaseClient
 import org.wfanet.measurement.common.identity.IdGenerator
 import org.wfanet.measurement.gcloud.spanner.AsyncDatabaseClient
 import org.wfanet.measurement.internal.reporting.v2.BasicReportsGrpcKt
+import org.wfanet.measurement.internal.reporting.v2.ImpressionQualificationFiltersGrpcKt
 import org.wfanet.measurement.internal.reporting.v2.MeasurementConsumersGrpcKt
 import org.wfanet.measurement.internal.reporting.v2.MeasurementsGrpcKt
 import org.wfanet.measurement.internal.reporting.v2.MetricCalculationSpecsGrpcKt
@@ -37,9 +38,12 @@ import org.wfanet.measurement.reporting.deploy.v2.postgres.PostgresReportSchedul
 import org.wfanet.measurement.reporting.deploy.v2.postgres.PostgresReportSchedulesService
 import org.wfanet.measurement.reporting.deploy.v2.postgres.PostgresReportingSetsService
 import org.wfanet.measurement.reporting.deploy.v2.postgres.PostgresReportsService
+import org.wfanet.measurement.reporting.service.internal.ImpressionQualificationFilterMapping
 
 data class Services(
   val basicReportsService: BasicReportsGrpcKt.BasicReportsCoroutineImplBase?,
+  val impressionQualificationFiltersService:
+    ImpressionQualificationFiltersGrpcKt.ImpressionQualificationFiltersCoroutineImplBase?,
   val measurementConsumersService: MeasurementConsumersGrpcKt.MeasurementConsumersCoroutineImplBase,
   val measurementsService: MeasurementsGrpcKt.MeasurementsCoroutineImplBase,
   val metricsService: MetricsGrpcKt.MetricsCoroutineImplBase,
@@ -58,14 +62,22 @@ object DataServices {
     idGenerator: IdGenerator,
     postgresClient: DatabaseClient,
     spannerClient: AsyncDatabaseClient?,
+    impressionQualificationFilterMapping: ImpressionQualificationFilterMapping?,
   ): Services {
     val basicReportsService: BasicReportsGrpcKt.BasicReportsCoroutineImplBase? =
       if (spannerClient != null) {
         SpannerBasicReportsService(spannerClient, postgresClient)
       } else null
 
+    val impressionQualificationFiltersService:
+      ImpressionQualificationFiltersGrpcKt.ImpressionQualificationFiltersCoroutineImplBase? =
+      if (impressionQualificationFilterMapping != null) {
+        ImpressionQualificationFiltersService(impressionQualificationFilterMapping)
+      } else null
+
     return Services(
       basicReportsService = basicReportsService,
+      impressionQualificationFiltersService = impressionQualificationFiltersService,
       measurementConsumersService =
         PostgresMeasurementConsumersService(idGenerator, postgresClient),
       measurementsService = PostgresMeasurementsService(idGenerator, postgresClient),
