0558d7776fe9508d7cd34ef8a96d34bb040e48ba
Lin <lindreamdeyi@google.com>
2025-03-26 23:06:43 +0000

    feat: move iqf config creation into integration common Configs

--- src/test/kotlin/org/wfanet/measurement/integration/deploy/gcloud/GCloudInProcessLifeOfAReportV2IntegrationTest.kt
+++ src/test/kotlin/org/wfanet/measurement/integration/deploy/gcloud/GCloudInProcessLifeOfAReportV2IntegrationTest.kt
@@ -21,11 +21,10 @@ import org.wfanet.measurement.common.db.r2dbc.postgres.testing.PostgresDatabaseP
 import org.wfanet.measurement.duchy.deploy.common.postgres.testing.Schemata.DUCHY_CHANGELOG_PATH
 import org.wfanet.measurement.gcloud.spanner.testing.SpannerEmulatorRule
 import org.wfanet.measurement.integration.common.ALL_DUCHY_NAMES
-import org.wfanet.measurement.integration.common.IMPRESSION_QUALIFICATION_FILTERS_CONFIG
+import org.wfanet.measurement.integration.common.IMPRESSION_QUALIFICATION_FILTER_MAPPING
 import org.wfanet.measurement.integration.common.reporting.v2.InProcessLifeOfAReportIntegrationTest
 import org.wfanet.measurement.integration.deploy.common.postgres.PostgresDuchyDependencyProviderRule
 import org.wfanet.measurement.reporting.deploy.v2.postgres.testing.Schemata.REPORTING_CHANGELOG_PATH as POSTGRES_REPORTING_CHANGELOG_PATH
-import org.wfanet.measurement.reporting.service.internal.ImpressionQualificationFilterMapping
 
 /** Implementation of [InProcessLifeOfAReportIntegrationTest] for Google Cloud. */
 class GCloudInProcessLifeOfAReportV2IntegrationTest :
@@ -36,7 +35,7 @@ class GCloudInProcessLifeOfAReportV2IntegrationTest :
     InternalReportingServicesProviderRule(
       spannerEmulator,
       reportingPostgresDatabaseProvider,
-      impressionQualificationFilterMapping,
+      IMPRESSION_QUALIFICATION_FILTER_MAPPING,
     ),
   ) {
   companion object {
@@ -50,9 +49,5 @@ class GCloudInProcessLifeOfAReportV2IntegrationTest :
     @get:ClassRule
     @JvmStatic
     val duchyDatabaseProvider = PostgresDatabaseProviderRule(DUCHY_CHANGELOG_PATH)
-
-    @JvmStatic
-    val impressionQualificationFilterMapping =
-      ImpressionQualificationFilterMapping(IMPRESSION_QUALIFICATION_FILTERS_CONFIG)
   }
 }
