96cc3cf701d5ab585a267abcedc4b818f7273665
jojijac0b <jojijac0b@github.com>
2025-03-31 17:37:39 -0700

    Update logic to handle nested messages.

--- src/main/kotlin/org/wfanet/measurement/securecomputation/teeapps/utils/TeeApplicationUtils.kt
+++ src/main/kotlin/org/wfanet/measurement/securecomputation/teeapps/utils/TeeApplicationUtils.kt
@@ -1,12 +1,13 @@
 package org.wfanet.measurement.securecomputation.teeapps.utils
 
 import com.google.protobuf.ByteString
+import com.google.protobuf.Descriptors
 import com.google.protobuf.Descriptors.EnumValueDescriptor
 import com.google.protobuf.Descriptors.Descriptor
 import com.google.protobuf.Descriptors.FieldDescriptor
 import com.google.protobuf.DynamicMessage
 import com.google.protobuf.Message
-import com.google.protobuf.util.JsonFormat
+import com.google.protobuf.kotlin.toByteStringUtf8
 
 class TeeApplicationUtils {
   companion object {
@@ -14,79 +15,65 @@ class TeeApplicationUtils {
       originalMessage: Message,
       newMessageDescriptor: Descriptor,
       fieldNameMap: Map<String, String>,
-      fieldValueMap: Map<String, String>,
+      fieldValueMap: Map<String, String>
     ): DynamicMessage {
       val builder = DynamicMessage.newBuilder(newMessageDescriptor)
-
-      for (originalMessageFieldDescriptor in originalMessage.descriptorForType.fields) {
-        val originalMessageFieldName = originalMessageFieldDescriptor.name
-        val originalMessageFieldValueString = originalMessage.getField(originalMessageFieldDescriptor).toString()
-
-        val newMessageFieldName = fieldNameMap[originalMessageFieldName]
-        val newMessageFieldValueString = fieldValueMap[originalMessageFieldValueString]!!
-        val newMessageFieldDescriptor = newMessageDescriptor.findFieldByName(newMessageFieldName)
-
-        when (newMessageFieldDescriptor.type) {
-          // Integer types
-          FieldDescriptor.Type.INT32, FieldDescriptor.Type.SINT32, FieldDescriptor.Type.FIXED32, FieldDescriptor.Type.SFIXED32 ->
-            builder.setField(newMessageFieldDescriptor, newMessageFieldValueString.toInt())
-
-          // Long types
-          FieldDescriptor.Type.INT64, FieldDescriptor.Type.SINT64, FieldDescriptor.Type.FIXED64, FieldDescriptor.Type.SFIXED64 ->
-            builder.setField(newMessageFieldDescriptor, newMessageFieldValueString.toLong())
-
-          // Float and Double
-          FieldDescriptor.Type.FLOAT ->
-            builder.setField(newMessageFieldDescriptor, newMessageFieldValueString.toFloat())
-
-          FieldDescriptor.Type.DOUBLE ->
-            builder.setField(newMessageFieldDescriptor, newMessageFieldValueString.toDouble())
-
-          // Boolean
-          FieldDescriptor.Type.BOOL ->
-            builder.setField(newMessageFieldDescriptor, newMessageFieldValueString.lowercase() == "true")
-
-          // String - direct use
-          FieldDescriptor.Type.STRING ->
-            builder.setField(newMessageFieldDescriptor, newMessageFieldValueString)
-
-          // Bytes - convert string to ByteString
-          FieldDescriptor.Type.BYTES ->
-            builder.setField(newMessageFieldDescriptor, ByteString.copyFrom(newMessageFieldValueString.toByteArray()))
-
-          // Enum types - find enum value by name
-          FieldDescriptor.Type.ENUM -> {
-            val enumType = newMessageFieldDescriptor.enumType
-            val enumValue = enumType.findValueByName(newMessageFieldValueString)
-              ?: throw IllegalArgumentException(
-                "Invalid enum value '$newMessageFieldValueString' for enum type ${enumType.fullName}"
-              )
-            builder.setField(newMessageFieldDescriptor, enumValue)
-          }
-
-          // Message types - parse from JSON
-          FieldDescriptor.Type.MESSAGE -> {
-            try {
-              // This assumes newMessageFieldValueString is a valid JSON representation of the message
-              val jsonParser =JsonFormat.parser()
-              val fieldMessageBuilder = DynamicMessage.newBuilder(newMessageFieldDescriptor.messageType)
-              jsonParser.merge(newMessageFieldValueString, fieldMessageBuilder)
-              builder.setField(newMessageFieldDescriptor, fieldMessageBuilder.build())
-            } catch (e: Exception) {
-              throw IllegalArgumentException(
-                "Failed to parse message value '$newMessageFieldValueString' for message type ${newMessageFieldDescriptor.messageType.fullName}",
-                e
-              )
-            }
-          }
-
-          // For other types, throw an exception
-          else -> throw IllegalArgumentException(
-            "Unsupported field type: ${newMessageFieldDescriptor.type} for field ${newMessageFieldDescriptor.fullName}"
-          )
+      for ((originalFieldPath, newFieldPath) in fieldNameMap) {
+        val originalFieldValue = getFieldValueByPath(originalMessage, originalFieldPath)
+        val newFieldDescriptor = getFieldDescriptorByPath(newMessageDescriptor, newFieldPath)
+        if (originalFieldValue != null && newFieldDescriptor != null) {
+          val convertedValue = convertValue(originalFieldValue, newFieldDescriptor, fieldValueMap, originalFieldPath)
+          builder.setField(newFieldDescriptor, convertedValue)
         }
       }
       return builder.build()
+    }
+    private fun getFieldValueByPath(message: Message, fieldPath: String): Any? {
+      val fieldNames = fieldPath.split(".")
+      var currentMessage: Message = message
+      for (fieldName in fieldNames.dropLast(1)) {
+        val fieldDescriptor = currentMessage.descriptorForType.findFieldByName(fieldName)
+        if (fieldDescriptor != null && fieldDescriptor.type == FieldDescriptor.Type.MESSAGE) {
+          currentMessage = currentMessage.getField(fieldDescriptor) as Message
+        } else {
+          return null
+        }
+      }
+      val finalFieldDescriptor = currentMessage.descriptorForType.findFieldByName(fieldNames.last())
+      return finalFieldDescriptor?.let { currentMessage.getField(it) }
+    }
+    private fun getFieldDescriptorByPath(descriptor: Descriptor, fieldPath: String): FieldDescriptor? {
+      val fieldNames = fieldPath.split(".")
+      var currentDescriptor: Descriptor = descriptor
+      for (fieldName in fieldNames.dropLast(1)) {
+        val fieldDescriptor = currentDescriptor.findFieldByName(fieldName)
+        if (fieldDescriptor != null && fieldDescriptor.type == FieldDescriptor.Type.MESSAGE) {
+          currentDescriptor = fieldDescriptor.messageType
+        } else {
+          return null
+        }
+      }
+      return currentDescriptor.findFieldByName(fieldNames.last())
+    }
+    private fun convertValue(
+      originalValue: Any,
+      newFieldDescriptor: FieldDescriptor,
+      fieldValueMap: Map<String, String>,
+      originalFieldPath: String
+    ): Any {
+      val originalValueString = originalValue.toString()
+      val mappedValueKey = "$originalFieldPath.$originalValueString"
+      val mappedValueString = fieldValueMap[mappedValueKey] ?: originalValueString
+      return when (newFieldDescriptor.type) {
+        FieldDescriptor.Type.INT32 -> mappedValueString.toInt()
+        FieldDescriptor.Type.INT64 -> mappedValueString.toLong()
+        FieldDescriptor.Type.FLOAT -> mappedValueString.toFloat()
+        FieldDescriptor.Type.DOUBLE -> mappedValueString.toDouble()
+        FieldDescriptor.Type.BOOL -> mappedValueString.toBoolean()
+        FieldDescriptor.Type.STRING -> mappedValueString
+        FieldDescriptor.Type.ENUM -> newFieldDescriptor.enumType.findValueByName(mappedValueString)
+        else -> originalValue // Handle other types as needed
+      }
     }
   }
 }
--- src/test/kotlin/org/wfanet/measurement/securecomputation/teeapps/utils/TeeApplicationUtilsTest.kt
+++ src/test/kotlin/org/wfanet/measurement/securecomputation/teeapps/utils/TeeApplicationUtilsTest.kt
@@ -1,6 +1,7 @@
 package org.wfanet.measurement.securecomputation.teeapps.utils
 
 import org.junit.Test
+import com.google.common.truth.extensions.proto.ProtoTruth.assertThat
 import com.google.common.truth.Truth.assertThat
 import org.wfanet.measurement.api.v2alpha.event_templates.testing.Person
 import org.wfanet.measurement.api.v2alpha.event_templates.testing.person
@@ -8,19 +9,16 @@ import org.wfanet.virtualpeople.common.Gender
 import org.wfanet.virtualpeople.common.ageRange
 import org.wfanet.virtualpeople.common.demoBucket
 import org.wfanet.measurement.api.v2alpha.event_templates.testing.Person.Gender as PersonGender
+import com.google.protobuf.ExtensionRegistry
+import com.google.protobuf.TypeRegistry
 import org.wfanet.measurement.api.v2alpha.event_templates.testing.Person.AgeGroup
+import org.wfanet.virtualpeople.common.demoInfo
+import com.google.protobuf.Any
+
 
 class TeeApplicationUtilsTest {
   @Test
   fun `mapMessage should correctly map fields from source to target message`() {
-//    val originalMessage = common {
-//      gender = Gender.GENDER_MALE
-//      age = ageRange {
-//        minAge = 18
-//        maxAge = 24
-//      }
-//    }
-
     val originalMessage = demoBucket {
       gender = Gender.GENDER_MALE
       age = ageRange {
@@ -32,13 +30,62 @@ class TeeApplicationUtilsTest {
 
     val fieldNameMap = mapOf(
       "gender" to "gender",
-      "age" to "age_group"
+      "age.min_age" to "age_group",
+      "age.max_age" to "age_group"
+
     )
 
     val fieldValueMap = mapOf(
-      "GENDER_MALE" to "MALE",
-      "GENDER_FEMALE" to "FEMALE",
-       originalMessage.age.toString() to "YEARS_18_TO_34"
+      "gender.GENDER_MALE" to "MALE",
+      "gender.GENDER_FEMALE" to "FEMALE",
+      "age.min_age.18" to "YEARS_18_TO_34",
+      "age.max_age.34" to "YEARS_18_TO_34",
+    )
+
+    val newMessageDescriptor = Person.getDescriptor()
+
+    val newMessage = TeeApplicationUtils.convertMessage(
+      originalMessage,
+      newMessageDescriptor,
+      fieldNameMap,
+      fieldValueMap
+    )
+
+    val expectedMessage = person {
+      gender = PersonGender.MALE
+      ageGroup = AgeGroup.YEARS_18_TO_34
+    }
+    val typeRegistry = TypeRegistry.newBuilder().add(Person.getDescriptor()).build()
+    assertThat(newMessage.descriptorForType).isEqualTo(expectedMessage.descriptorForType)
+    assertThat(Any.pack(newMessage)).unpackingAnyUsing(typeRegistry, ExtensionRegistry.getEmptyRegistry()).isEqualTo(Any.pack(expectedMessage))
+    assertThat(newMessage.toByteString() == expectedMessage.toByteString())
+  }
+
+  @Test
+  fun `mapMessage should correctly map fields from source to target message with nested messages`() {
+    val originalMessage = demoInfo {
+      demoBucket = demoBucket {
+        gender = Gender.GENDER_MALE
+        age = ageRange {
+          minAge = 18
+          maxAge = 34
+        }
+      }
+    }
+
+
+    val fieldNameMap = mapOf(
+      "demo_bucket.gender" to "gender",
+      "demo_bucket.age.min_age" to "age_group",
+      "demo_bucket.age.max_age" to "age_group"
+
+    )
+
+    val fieldValueMap = mapOf(
+      "demo_bucket.gender.GENDER_MALE" to "MALE",
+      "demo_bucket.gender.GENDER_FEMALE" to "FEMALE",
+      "demo_bucket.age.min_age.18" to "YEARS_18_TO_34",
+      "demo_bucket.age.max_age.34" to "YEARS_18_TO_34",
     )
 
     val newMessageDescriptor = Person.getDescriptor()
@@ -55,8 +102,9 @@ class TeeApplicationUtilsTest {
       ageGroup = AgeGroup.YEARS_18_TO_34
     }
 
-
-    assertThat(newMessage.descriptorForType == expectedMessage.descriptorForType)
+    val typeRegistry = TypeRegistry.newBuilder().add(Person.getDescriptor()).build()
+    assertThat(newMessage.descriptorForType).isEqualTo(expectedMessage.descriptorForType)
+    assertThat(Any.pack(newMessage)).unpackingAnyUsing(typeRegistry, ExtensionRegistry.getEmptyRegistry()).isEqualTo(Any.pack(expectedMessage))
     assertThat(newMessage.toByteString() == expectedMessage.toByteString())
   }
 }
