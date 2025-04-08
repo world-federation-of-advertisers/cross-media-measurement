06cb3eb2763b47ea78377708af518c56aca78b21
Phi Hung Le <lephi@google.com>
2025-04-08 20:21:46 +0000

    Fix Lint.

--- src/test/python/wfa/measurement/reporting/postprocessing/noiseninja/test_solver.py
+++ src/test/python/wfa/measurement/reporting/postprocessing/noiseninja/test_solver.py
@@ -13,13 +13,15 @@
 # limitations under the License.
 
 import unittest
+from unittest.mock import MagicMock
+
+from qpsolvers import Solution
+
 from noiseninja.noised_measurements import Measurement
 from noiseninja.noised_measurements import SetMeasurementsSpec
 from noiseninja.solver import Solver
-from qpsolvers import Solution
 from src.main.proto.wfa.measurement.reporting.postprocessing.v2alpha import \
   report_post_processor_result_pb2
-from unittest.mock import MagicMock
 
 ReportPostProcessorErrorCode = report_post_processor_result_pb2.ReportPostProcessorErrorCode
 ReportPostProcessorStatus = report_post_processor_result_pb2.ReportPostProcessorStatus
