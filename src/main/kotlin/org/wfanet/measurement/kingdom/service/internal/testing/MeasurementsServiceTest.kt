143996d34d1ccb37206c6c9118a394785b10bc5a
Lin <lindreamdeyi@google.com>
2025-04-21 21:19:06 +0000

    fix: undo RequisitionReader changes, address protocol_not_set issue in RequisitionsService instead

--- src/main/kotlin/org/wfanet/measurement/kingdom/service/internal/testing/MeasurementsServiceTest.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/service/internal/testing/MeasurementsServiceTest.kt
@@ -989,6 +989,12 @@ abstract class MeasurementsServiceTest<T : MeasurementsCoroutineImplBase> {
               dataProviderPublicKeySignatureAlgorithmOid =
                 dataProviderValue.dataProviderPublicKeySignatureAlgorithmOid
             }
+            duchies[Population.AGGREGATOR_DUCHY.externalDuchyId] =
+              Requisition.DuchyValue.getDefaultInstance()
+            duchies[Population.WORKER1_DUCHY.externalDuchyId] =
+              Requisition.DuchyValue.getDefaultInstance()
+            duchies[Population.WORKER2_DUCHY.externalDuchyId] =
+              Requisition.DuchyValue.getDefaultInstance()
           }
         )
 
