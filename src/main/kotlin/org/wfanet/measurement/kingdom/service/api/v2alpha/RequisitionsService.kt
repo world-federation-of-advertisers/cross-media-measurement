4c9218620487c1da3e35a1ef7433a01894fa3a1e
Lin <lindreamdeyi@google.com>
2025-04-21 22:29:48 +0000

    fix: undo changes in RequisitionsService, and make prober only update lastTerminalRequisition when measurement is not cancelled

--- src/main/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/RequisitionsService.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/RequisitionsService.kt
@@ -387,7 +387,7 @@ private fun DuchyValue.availableForFulfillment(): Boolean {
     DuchyValue.ProtocolCase.HONEST_MAJORITY_SHARE_SHUFFLE -> {
       !honestMajorityShareShuffle.tinkPublicKey.isEmpty
     }
-    DuchyValue.ProtocolCase.PROTOCOL_NOT_SET -> false
+    DuchyValue.ProtocolCase.PROTOCOL_NOT_SET -> error("protocol not set")
   }
 }
 
@@ -460,7 +460,7 @@ private fun DuchyValue.toDuchyEntryValue(
             DuchyEntry.HonestMajorityShareShuffle.getDefaultInstance()
           }
       }
-      DuchyValue.ProtocolCase.PROTOCOL_NOT_SET -> Unit
+      DuchyValue.ProtocolCase.PROTOCOL_NOT_SET -> error("protocol not set")
     }
   }
 }
