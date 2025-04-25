b5c7c5a2551d77f4f90156523f257b209fe4b576
Phi Hung Le <lephi@google.com>
2025-04-25 01:32:08 +0000

    Update code.

--- src/main/kotlin/org/wfanet/measurement/reporting/service/api/v2alpha/ProtoConversions.kt
+++ src/main/kotlin/org/wfanet/measurement/reporting/service/api/v2alpha/ProtoConversions.kt
@@ -707,17 +707,7 @@ private fun Measurement.Result.Frequency.toInternal(
           .first { it.hasHonestMajorityShareShuffle() }
           .honestMajorityShareShuffle
       noiseMechanism = cmmsProtocol.noiseMechanism.toInternal()
-      @Suppress("WHEN_ENUM_CAN_BE_NULL_IN_JAVA")
-      when (source.methodologyCase) {
-        Measurement.Result.Frequency.MethodologyCase.HONEST_MAJORITY_SHARE_SHUFFLE -> {
-          honestMajorityShareShuffle = source.honestMajorityShareShuffle.toInternal()
-        }
-        Measurement.Result.Frequency.MethodologyCase.CUSTOM_DIRECT_METHODOLOGY,
-        Measurement.Result.Frequency.MethodologyCase.DETERMINISTIC_DISTRIBUTION,
-        Measurement.Result.Frequency.MethodologyCase.LIQUID_LEGIONS_DISTRIBUTION,
-        Measurement.Result.Frequency.MethodologyCase.LIQUID_LEGIONS_V2,
-        Measurement.Result.Frequency.MethodologyCase.METHODOLOGY_NOT_SET -> {}
-      }
+      honestMajorityShareShuffle = source.honestMajorityShareShuffle.toInternal()
     } else {
       error("Measurement protocol is not set or not supported.")
     }
@@ -769,18 +759,7 @@ private fun Measurement.Result.Reach.toInternal(
           .first { it.hasHonestMajorityShareShuffle() }
           .honestMajorityShareShuffle
       noiseMechanism = cmmsProtocol.noiseMechanism.toInternal()
-      @Suppress("WHEN_ENUM_CAN_BE_NULL_IN_JAVA")
-      when (source.methodologyCase) {
-        Measurement.Result.Reach.MethodologyCase.HONEST_MAJORITY_SHARE_SHUFFLE -> {
-          honestMajorityShareShuffle = source.honestMajorityShareShuffle.toInternal()
-        }
-        Measurement.Result.Reach.MethodologyCase.CUSTOM_DIRECT_METHODOLOGY,
-        Measurement.Result.Reach.MethodologyCase.DETERMINISTIC_COUNT_DISTINCT,
-        Measurement.Result.Reach.MethodologyCase.LIQUID_LEGIONS_COUNT_DISTINCT,
-        Measurement.Result.Reach.MethodologyCase.LIQUID_LEGIONS_V2,
-        Measurement.Result.Reach.MethodologyCase.REACH_ONLY_LIQUID_LEGIONS_V2,
-        Measurement.Result.Reach.MethodologyCase.METHODOLOGY_NOT_SET -> {}
-      }
+      honestMajorityShareShuffle = source.honestMajorityShareShuffle.toInternal()
     } else {
       error("Measurement protocol is not set or not supported.")
     }
