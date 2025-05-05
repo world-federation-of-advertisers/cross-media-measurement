682f58eba71c29f2f88aefd1ade4d5e005a722b3
jojijac0b <jojijac0b@github.com>
2025-05-05 07:34:48 -0700

    Remove unused method

--- src/main/kotlin/org/wfanet/measurement/edpaggregator/resultsfulfiller/MeasurementHelper.kt
+++ src/main/kotlin/org/wfanet/measurement/edpaggregator/resultsfulfiller/MeasurementHelper.kt
@@ -301,25 +301,6 @@ open class MeasurementHelper(
     }
   }
 
-  /**
-   * Converts a [NoiseMechanism] to a nullable [DirectNoiseMechanism].
-   *
-   * @return [DirectNoiseMechanism] when there is a matched, otherwise null.
-   */
-  private fun NoiseMechanism.toDirectNoiseMechanism(): DirectNoiseMechanism? {
-    return when (this) {
-      NoiseMechanism.NONE -> DirectNoiseMechanism.NONE
-      NoiseMechanism.CONTINUOUS_LAPLACE -> DirectNoiseMechanism.CONTINUOUS_LAPLACE
-      NoiseMechanism.CONTINUOUS_GAUSSIAN -> DirectNoiseMechanism.CONTINUOUS_GAUSSIAN
-      NoiseMechanism.NOISE_MECHANISM_UNSPECIFIED,
-      NoiseMechanism.GEOMETRIC,
-      NoiseMechanism.DISCRETE_GAUSSIAN,
-      NoiseMechanism.UNRECOGNIZED -> {
-        null
-      }
-    }
-  }
-
   protected open class RequisitionRefusalException(
     val justification: Requisition.Refusal.Justification,
     message: String,
