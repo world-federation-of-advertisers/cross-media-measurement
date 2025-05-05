3f884f4bdd366d08f5ab5a80d75b6031112da86c
marcopremier <marcopremier@meta.com>
2025-05-05 02:49:55 -0700

    fix: resource name

--- src/main/terraform/gcloud/modules/secret/main.tf
+++ src/main/terraform/gcloud/modules/secret/main.tf
@@ -20,6 +20,6 @@ resource "google_secret_manager_secret" "secret" {
 }
 
 resource "google_secret_manager_secret_version" "secret_version" {
-  secret      = google_secret_manager_secret.data_watcher_tls.id
-  secret_data = file("${path.module}/${var.secret_path}")
+  secret      = google_secret_manager_secret.secret.id
+  secret_data = file("${path.root}/${var.secret_path}")
 }
