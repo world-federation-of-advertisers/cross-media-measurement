63cdd7241435361eb7bf6ca006ec7243d3606183
Sanjay Vasandani <sanjayvas@google.com>
2025-05-08 19:07:02 +0000

    feat: Add IntegerIds CLI tool for debugging

---
+++ src/main/kotlin/org/wfanet/measurement/common/api/tools/IntegerIds.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2025 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.common.api.tools
+
+import org.wfanet.measurement.common.commandLineMain
+import org.wfanet.measurement.common.identity.ApiId
+import org.wfanet.measurement.common.identity.ExternalId
+import picocli.CommandLine
+
+@CommandLine.Command(
+  name = "IntegerIds",
+  description = ["Tool for integer-based IDs"],
+  subcommands = [CommandLine.HelpCommand::class],
+)
+class IntegerIds : Runnable {
+  override fun run() {
+    CommandLine.usage(this, System.err)
+  }
+
+  @CommandLine.Command(
+    name = "to-api-id",
+    description = ["Print the API ID for an external ID value"],
+  )
+  fun toApiId(
+    @CommandLine.Parameters(description = ["External ID value"], paramLabel = "EXTERNAL_ID")
+    externalIdValue: Long
+  ) {
+    print(ExternalId(externalIdValue).apiId.value)
+  }
+
+  @CommandLine.Command(
+    name = "to-external-id",
+    description = ["Print the external ID value for an API ID"],
+  )
+  fun toExternalId(
+    @CommandLine.Parameters(description = ["API ID value"], paramLabel = "API_ID")
+    apiIdValue: String
+  ) {
+    print(ApiId(apiIdValue).externalId.value)
+  }
+
+  companion object {
+    @JvmStatic fun main(args: Array<String>) = commandLineMain(IntegerIds(), args)
+  }
+}
