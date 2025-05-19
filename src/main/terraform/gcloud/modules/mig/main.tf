117db5580c5bdf2e64560fe711153795d6ebe848
marcopremier <marcopremier@meta.com>
2025-05-19 03:18:16 -0700

    redeploy mig

--- src/main/terraform/gcloud/modules/mig/main.tf
+++ src/main/terraform/gcloud/modules/mig/main.tf
@@ -46,22 +46,6 @@ resource "google_secret_manager_secret_iam_member" "mig_sa_secret_accessor" {
   member    = "serviceAccount:${google_service_account.mig_service_account.email}"
 }
 
-resource "google_compute_firewall" "allow-egress-all" {
-  name    = "allow-egress-all"
-  network = "default"
-
-  direction = "EGRESS"
-  allow {
-    protocol = "tcp"
-    ports    = ["443", "80"]
-  }
-
-  destination_ranges = ["0.0.0.0/0"]
-  priority           = 1000
-
-  target_service_accounts = [google_service_account.mig_service_account.email]
-}
-
 resource "google_compute_instance_template" "confidential_vm_template" {
   machine_type = var.machine_type
 
@@ -79,8 +63,11 @@ resource "google_compute_instance_template" "confidential_vm_template" {
     create_before_destroy = true
   }
 
+#   disk {
+#     source_image = "projects/cos-cloud/global/images/family/cos-stable"
+#   }
   disk {
-    source_image = "projects/cos-cloud/global/images/family/cos-stable"
+    source_image = "ubuntu-os-cloud/ubuntu-2004-lts"
   }
 
   network_interface {
@@ -88,67 +75,106 @@ resource "google_compute_instance_template" "confidential_vm_template" {
     access_config { }
   }
 
-
   metadata = merge(
-      {
-        "google-logging-enabled"    = "true"
-        "google-monitoring-enabled" = "true"
-      },
-      {
-        # 1) at boot, loop over each secret and write it out:
-        "startup-script" = <<-EOT
-          #!/bin/bash
-          set -euo pipefail
-          %{ for s in var.secrets_to_mount }
-          # get an access token from the metadata server
-          TOKEN=$(curl -s -H "Metadata-Flavor: Google" \
-            http://metadata/computeMetadata/v1/instance/service-accounts/default/token \
-            | jq -r .access_token)
+    {
+      "google-logging-enabled"    = "true"
+      "google-monitoring-enabled" = "true"
+      "startup-script" = <<-EOT
+        #!/bin/bash
+        set -euo pipefail
 
-          curl -s -H "Authorization: Bearer $TOKEN" \
-            "https://secretmanager.googleapis.com/v1/projects/${data.google_project.project.name}/secrets/${var.secrets[s.secret_key].secret_id}/versions/${s.version}:access" \
-            | jq -r .payload.data \
-            | base64 --decode > ${s.mount_path}
-          chmod 644 ${s.mount_path}
-          %{ endfor }
-        EOT
-      },
-      {
-        # 2) append a --<flag>=<path> for each mount into the container args
-        "gce-container-declaration" = <<-EOT
-  spec:
-    containers:
-      - name: subscriber
-        image: ${var.docker_image}
-        stdin: false
-        tty: false
-        args: ${jsonencode(
-          concat(
-            var.app_args,
-            [ for s in var.secrets_to_mount :
-              "${s.flag_name}=${s.mount_path}"
-              if s.flag_name != null
-            ]
-          )
-        )}
-        env:
-          - name: GRPC_TRACE
-            value: "all"
-          - name: GRPC_VERBOSITY
-            value: "DEBUG"
-        volumeMounts:
-          - name: ssl-secrets
-            mountPath: /etc/ssl
-            readOnly: true
-    restartPolicy: Always
-    volumes:
-      - name: ssl-secrets
-        hostPath:
-          path: /etc/ssl
-          type: DirectoryOrCreate
-  EOT
-      }
-    )
+        echo "Installing Docker..."
+        apt-get update
+        apt-get install -y docker.io
+        systemctl start docker
+        systemctl enable docker
+
+        echo "Retrieving secrets..."
+        %{ for s in var.secrets_to_mount }
+        TOKEN=$(curl -s -H "Metadata-Flavor: Google" \
+          http://metadata/computeMetadata/v1/instance/service-accounts/default/token \
+          | jq -r .access_token)
+
+        curl -s -H "Authorization: Bearer $TOKEN" \
+          "https://secretmanager.googleapis.com/v1/projects/${data.google_project.project.name}/secrets/${var.secrets[s.secret_key].secret_id}/versions/${s.version}:access" \
+          | jq -r .payload.data \
+          | base64 --decode > ${s.mount_path}
+        chmod 644 ${s.mount_path}
+        %{ endfor }
+
+        echo "Running container..."
+        docker run -d --name=subscriber --restart=always \\
+          --network=host \\
+          -v /etc/ssl:/etc/ssl:ro \\
+          ${join(" ", concat(
+            [for s in var.secrets_to_mount : "--env ${s.flag_name}=${s.mount_path}" if s.flag_name != null],
+            [var.docker_image],
+            var.app_args
+          ))}
+      EOT
+    }
+  )
+
+#   metadata = merge(
+#       {
+#         "google-logging-enabled"    = "true"
+#         "google-monitoring-enabled" = "true"
+#       },
+#       {
+#         # 1) at boot, loop over each secret and write it out:
+#         "startup-script" = <<-EOT
+#           #!/bin/bash
+#           set -euo pipefail
+#           %{ for s in var.secrets_to_mount }
+#           # get an access token from the metadata server
+#           TOKEN=$(curl -s -H "Metadata-Flavor: Google" \
+#             http://metadata/computeMetadata/v1/instance/service-accounts/default/token \
+#             | jq -r .access_token)
+#
+#           curl -s -H "Authorization: Bearer $TOKEN" \
+#             "https://secretmanager.googleapis.com/v1/projects/${data.google_project.project.name}/secrets/${var.secrets[s.secret_key].secret_id}/versions/${s.version}:access" \
+#             | jq -r .payload.data \
+#             | base64 --decode > ${s.mount_path}
+#           chmod 644 ${s.mount_path}
+#           %{ endfor }
+#         EOT
+#       },
+#       {
+#         # 2) append a --<flag>=<path> for each mount into the container args
+#         "gce-container-declaration" = <<-EOT
+#   spec:
+#     containers:
+#       - name: subscriber
+#         image: ${var.docker_image}
+#         stdin: false
+#         tty: false
+#         args: ${jsonencode(
+#           concat(
+#             var.app_args,
+#             [ for s in var.secrets_to_mount :
+#               "${s.flag_name}=${s.mount_path}"
+#               if s.flag_name != null
+#             ]
+#           )
+#         )}
+#         env:
+#           - name: GRPC_TRACE
+#             value: "all"
+#           - name: GRPC_VERBOSITY
+#             value: "DEBUG"
+#         volumeMounts:
+#           - name: ssl-secrets
+#             mountPath: /etc/ssl
+#             readOnly: true
+#     restartPolicy: Always
+#     volumes:
+#       - name: ssl-secrets
+#         hostPath:
+#           path: /etc/ssl
+#           type: DirectoryOrCreate
+#   EOT
+#       }
+#     )
 
   service_account {
     email = google_service_account.mig_service_account.email
