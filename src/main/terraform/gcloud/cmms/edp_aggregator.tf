847e69da31423e9e58d684af186c5f6b70d3c7b0
marcopremier <marcopremier@meta.com>
2025-05-19 02:10:30 -0700

    redeploy mig

--- src/main/terraform/gcloud/cmms/edp_aggregator.tf
+++ src/main/terraform/gcloud/cmms/edp_aggregator.tf
@@ -116,7 +116,7 @@ locals {
           "--google-pub-sub-project-id=halo-cmm-dev"
         ]
         machine_type                = "n2d-standard-2"
-        docker_image                = "ghcr.io/world-federation-of-advertisers/edp-aggregator/results_fulfiller:e44f8f4ab34685859332082e8ce8293afe945682"
+        docker_image                = "ghcr.io/world-federation-of-advertisers/edp-aggregator/results_fulfiller:035ffad2af86e4fa11378ce60e4618fb88b9fe23"
         secrets_to_mount            = [
           {
             secret_key              = "edpa_tee_app_tls_key"
