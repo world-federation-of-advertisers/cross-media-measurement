e30570aacaef20bbcbadbf3abe5cfabf93476355
ugur47 <ugur47@users.noreply.github.com>
2025-05-20 08:46:09 +0000

    getting there

---
+++ src/test/kotlin/org/wfanet/measurement/privacybudgetmanager/LandscapeUtilsTest.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2023 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.privacybudgetmanager
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.wfanet.measurement.api.v2alpha.event_templates.testing.TestEvent
+
+@RunWith(JUnit4::class)
+class LandscapeUtilsTest {
+  @Test
+  fun `getBuckets works as expected`() {
+
+    val desctiptors = listOf(TestEvent.getDescriptor().file)
+    val landscape = privacyLandscape {}
+    val landscapeMasks = listOf(eventGroupLandscapeMask {})
+    LandscapeUtils.getBuckets("mcid", landscapeMasks, landscape, desctiptors)
+  }
+}
