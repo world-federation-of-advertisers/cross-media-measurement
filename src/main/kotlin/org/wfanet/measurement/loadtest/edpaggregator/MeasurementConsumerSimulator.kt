43bfd3c97dc335f0b790f57779d5f14e5277e090
marcopremier <marcopremier@meta.com>
2025-05-21 10:59:44 -0700

    run tests

--- src/main/kotlin/org/wfanet/measurement/loadtest/edpaggregator/MeasurementConsumerSimulator.kt
+++ src/main/kotlin/org/wfanet/measurement/loadtest/edpaggregator/MeasurementConsumerSimulator.kt
@@ -125,7 +125,6 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
-import org.wfanet.measurement.loadtest.measurementconsumer.MeasurementConsumerSimulator
 
 data class MeasurementConsumerData(
   // The MC's public API resource name
