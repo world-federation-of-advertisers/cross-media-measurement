eb72d95b7e8df5a16231b8eab73979960db4e899
Steven Ware Jones <stevenwarejones@gmail.com>
2025-05-22 14:18:19 -0400

    refactor

--- src/main/kotlin/org/wfanet/measurement/edpaggregator/requisitionfetcher/RequisitionGrouper.kt
+++ src/main/kotlin/org/wfanet/measurement/edpaggregator/requisitionfetcher/RequisitionGrouper.kt
@@ -16,11 +16,96 @@
 
 package org.wfanet.measurement.edpaggregator.requisitionfetcher
 
+import com.google.protobuf.Any
+import com.google.protobuf.InvalidProtocolBufferException
+import com.google.type.Interval
+import java.security.GeneralSecurityException
+import java.util.logging.Logger
+import kotlinx.coroutines.runBlocking
+import org.wfanet.measurement.api.v2alpha.EventGroup
+import org.wfanet.measurement.api.v2alpha.EventGroupsGrpcKt.EventGroupsCoroutineStub
+import org.wfanet.measurement.api.v2alpha.MeasurementSpec
 import org.wfanet.measurement.api.v2alpha.Requisition
+import org.wfanet.measurement.api.v2alpha.RequisitionSpec
+import org.wfanet.measurement.api.v2alpha.getEventGroupRequest
+import org.wfanet.measurement.api.v2alpha.unpack
+import org.wfanet.measurement.common.crypto.PrivateKeyHandle
+import org.wfanet.measurement.consent.client.dataprovider.decryptRequisitionSpec
 import org.wfanet.measurement.edpaggregator.v1alpha.GroupedRequisitions
+import org.wfanet.measurement.edpaggregator.v1alpha.groupedRequisitions
 
 /** An interface to group a list of requisitions. */
-interface RequisitionGrouper {
+abstract class RequisitionGrouper(
+  /** The DataProvider's decryption key. */
+  private val privateEncryptionKey: PrivateKeyHandle,
+  private val eventGroupsClient: EventGroupsCoroutineStub,
+) {
 
-  fun groupRequisitions(requisitions: List<Requisition>): List<GroupedRequisitions>
+  fun groupRequisitions(requisitions: List<Requisition>): List<GroupedRequisitions> {
+    val mappedRequisitions = mapRequisitions(requisitions)
+    return combineGroupedRequisitions(mappedRequisitions)
+  }
+
+  protected abstract fun combineGroupedRequisitions(
+    groupedRequisitions: List<GroupedRequisitions>
+  ): List<GroupedRequisitions>
+
+  private fun mapRequisitions(requisitions: List<Requisition>): List<GroupedRequisitions> {
+    return requisitions.mapNotNull { requisition ->
+      val measurementSpec: MeasurementSpec? =
+        try {
+          requisition.measurementSpec.unpack()
+        } catch (e: InvalidProtocolBufferException) {
+          logger.info("Unable to parse measurement spec for ${requisition.name}: ${e.message}")
+          null
+        }
+      if (measurementSpec == null) {
+        null
+      }
+      val requisitionSpec: RequisitionSpec? =
+        try {
+          decryptRequisitionSpec(requisition.encryptedRequisitionSpec, privateEncryptionKey)
+            .unpack()
+        } catch (e: GeneralSecurityException) {
+          logger.info("RequisitionSpec decryption failed for ${requisition.name}: ${e.message}")
+          null
+        } catch (e: InvalidProtocolBufferException) {
+          logger.info("Unable to parse requisition spec for ${requisition.name}: ${e.message}")
+          null
+        }
+      if (requisitionSpec == null) {
+        null
+      }
+      val eventGroupMap = mutableMapOf<String, String>()
+      val collectionIntervalsMap = mutableMapOf<String, Interval>()
+      for (eventGroupEntry in requisitionSpec!!.events.eventGroupsList) {
+        val eventGroupName = eventGroupEntry.key
+        val eventGroupReferenceId = runBlocking {
+          getEventGroup(eventGroupName).eventGroupReferenceId
+        }
+        eventGroupMap[eventGroupName] = eventGroupReferenceId
+        if (eventGroupName in collectionIntervalsMap) {
+          logger.info(
+            "Identical event group names not allowed in same requisition: ${requisition.name}"
+          )
+          null
+        }
+        collectionIntervalsMap[eventGroupReferenceId] = eventGroupEntry.value.collectionInterval
+      }
+      groupedRequisitions {
+        modelLine = measurementSpec!!.modelLine
+        this.requisitions += Any.pack(requisition)
+        this.eventGroupMap.putAll(eventGroupMap)
+        collectionIntervals.putAll(collectionIntervalsMap)
+      }
+    }
+  }
+
+  private suspend fun getEventGroup(name: String): EventGroup {
+    return eventGroupsClient.getEventGroup(getEventGroupRequest { this.name = name })
+  }
+
+  companion object {
+    private val logger: Logger = Logger.getLogger(this::class.java.name)
+  }
 }
--- src/main/kotlin/org/wfanet/measurement/edpaggregator/requisitionfetcher/SingleRequisitionGrouper.kt
+++ src/main/kotlin/org/wfanet/measurement/edpaggregator/requisitionfetcher/SingleRequisitionGrouper.kt
@@ -16,87 +16,25 @@
 
 package org.wfanet.measurement.edpaggregator.requisitionfetcher
 
-import com.google.protobuf.Any
-import com.google.protobuf.InvalidProtocolBufferException
-import com.google.type.Interval
-import java.security.GeneralSecurityException
 import java.util.logging.Logger
-import kotlinx.coroutines.runBlocking
-import org.wfanet.measurement.api.v2alpha.EventGroup
 import org.wfanet.measurement.api.v2alpha.EventGroupsGrpcKt.EventGroupsCoroutineStub
-import org.wfanet.measurement.api.v2alpha.MeasurementSpec
-import org.wfanet.measurement.api.v2alpha.Requisition
-import org.wfanet.measurement.api.v2alpha.RequisitionSpec
-import org.wfanet.measurement.api.v2alpha.getEventGroupRequest
-import org.wfanet.measurement.api.v2alpha.unpack
 import org.wfanet.measurement.common.crypto.PrivateKeyHandle
-import org.wfanet.measurement.consent.client.dataprovider.decryptRequisitionSpec
 import org.wfanet.measurement.edpaggregator.v1alpha.GroupedRequisitions
-import org.wfanet.measurement.edpaggregator.v1alpha.groupedRequisitions
 
 /**
- * Naively returns each requisition as their own group. Generally not recommended for production use
+ * Naively does not combine a set of requisition. Generally not recommended for production use
  * cases.
  */
 class SingleRequisitionGrouper(
   /** The DataProvider's decryption key. */
-  private val privateEncryptionKey: PrivateKeyHandle,
-  private val eventGroupsClient: EventGroupsCoroutineStub,
-) : RequisitionGrouper {
+  privateEncryptionKey: PrivateKeyHandle,
+  eventGroupsClient: EventGroupsCoroutineStub,
+) : RequisitionGrouper(privateEncryptionKey, eventGroupsClient) {
 
-  override fun groupRequisitions(requisitions: List<Requisition>): List<GroupedRequisitions> {
-    return requisitions.mapNotNull { requisition ->
-      val measurementSpec: MeasurementSpec? =
-        try {
-          requisition.measurementSpec.unpack()
-        } catch (e: InvalidProtocolBufferException) {
-          logger.info("Unable to parse measurement spec for ${requisition.name}: ${e.message}")
-          null
-        }
-      if (measurementSpec == null) {
-        null
-      }
-      val requisitionSpec: RequisitionSpec? =
-        try {
-          decryptRequisitionSpec(requisition.encryptedRequisitionSpec, privateEncryptionKey)
-            .unpack()
-        } catch (e: GeneralSecurityException) {
-          logger.info("RequisitionSpec decryption failed for ${requisition.name}: ${e.message}")
-          null
-        } catch (e: InvalidProtocolBufferException) {
-          logger.info("Unable to parse requisition spec for ${requisition.name}: ${e.message}")
-          null
-        }
-      if (requisitionSpec == null) {
-        null
-      }
-      val eventGroupMap = mutableMapOf<String, String>()
-      val collectionIntervalsMap = mutableMapOf<String, Interval>()
-      for (eventGroupEntry in requisitionSpec!!.events.eventGroupsList) {
-        val eventGroupName = eventGroupEntry.key
-        val eventGroupReferenceId = runBlocking {
-          getEventGroup(eventGroupName).eventGroupReferenceId
-        }
-        eventGroupMap[eventGroupName] = eventGroupReferenceId
-        if (eventGroupName in collectionIntervalsMap) {
-          logger.info(
-            "Identical event group names not allowed in same requisition: ${requisition.name}"
-          )
-          null
-        }
-        collectionIntervalsMap[eventGroupReferenceId] = eventGroupEntry.value.collectionInterval
-      }
-      groupedRequisitions {
-        modelLine = measurementSpec!!.modelLine
-        this.requisitions += Any.pack(requisition)
-        this.eventGroupMap.putAll(eventGroupMap)
-        collectionIntervals.putAll(collectionIntervalsMap)
-      }
-    }
-  }
-
-  private suspend fun getEventGroup(name: String): EventGroup {
-    return eventGroupsClient.getEventGroup(getEventGroupRequest { this.name = name })
+  override fun combineGroupedRequisitions(
+    groupedRequisitions: List<GroupedRequisitions>
+  ): List<GroupedRequisitions> {
+    return groupedRequisitions
   }
 
   companion object {
