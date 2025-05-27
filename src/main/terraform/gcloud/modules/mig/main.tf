d054f7b6eefcd333c6f6d63b09628cc007811032
marcopremier <marcopremier@meta.com>
2025-05-27 06:59:27 -0700

    style: remove comments

--- src/main/terraform/gcloud/modules/mig/main.tf
+++ src/main/terraform/gcloud/modules/mig/main.tf
@@ -28,8 +28,7 @@ resource "google_service_account_iam_member" "allow_terraform_to_use_mig_sa" {
 
 resource "google_pubsub_subscription_iam_member" "mig_subscriber" {
   subscription  = var.subscription_id
-#   role          = "roles/pubsub.subscriber"
-  role          = "roles/pubsub.admin"
+  role          = "roles/pubsub.subscriber"
   member        = "serviceAccount:${google_service_account.mig_service_account.email}"
 }
 
@@ -46,23 +45,6 @@ resource "google_secret_manager_secret_iam_member" "mig_sa_secret_accessor" {
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${google_service_account.mig_service_account.email}"
 }
-
-# resource "google_compute_firewall" "allow-egress-all" {
-#   name    = "allow-egress-all"
-#   network = "default"
-#
-#   direction = "EGRESS"
-#
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["443", "80"]
-#   }
-#
-#   destination_ranges = ["0.0.0.0/0"]
-#   priority           = 1000
-#
-#   target_service_accounts = [google_service_account.mig_service_account.email]
-# }
 
 resource "google_compute_instance_template" "confidential_vm_template" {
   machine_type = var.machine_type
@@ -84,54 +66,11 @@ resource "google_compute_instance_template" "confidential_vm_template" {
   disk {
     source_image = "projects/cos-cloud/global/images/family/cos-stable"
   }
-#   disk {
-#     source_image = "ubuntu-os-cloud/ubuntu-2004-lts"
-#   }
 
   network_interface {
     network = "default"
     access_config { }
   }
-
-#   metadata = merge(
-#     {
-#       "google-logging-enabled"    = "true"
-#       "google-monitoring-enabled" = "true"
-#       "startup-script" = <<-EOT
-#         #!/bin/bash
-#         set -euo pipefail
-#
-#         echo "Installing Docker..."
-#         apt-get update
-#         apt-get install -y docker.io
-#         systemctl start docker
-#         systemctl enable docker
-#
-#         echo "Retrieving secrets..."
-#         %{ for s in var.secrets_to_mount }
-#         TOKEN=$(curl -s -H "Metadata-Flavor: Google" \
-#           http://metadata/computeMetadata/v1/instance/service-accounts/default/token \
-#           | jq -r .access_token)
-#
-#         curl -s -H "Authorization: Bearer $TOKEN" \
-#           "https://secretmanager.googleapis.com/v1/projects/${data.google_project.project.name}/secrets/${var.secrets[s.secret_key].secret_id}/versions/${s.version}:access" \
-#           | jq -r .payload.data \
-#           | base64 --decode > ${s.mount_path}
-#         chmod 644 ${s.mount_path}
-#         %{ endfor }
-#
-#         echo "Running container..."
-#         docker run -d --name=subscriber --restart=always \\
-#           --network=host \\
-#           -v /etc/ssl:/etc/ssl:ro \\
-#           ${join(" ", concat(
-#             [for s in var.secrets_to_mount : "--env ${s.flag_name}=${s.mount_path}" if s.flag_name != null],
-#             [var.docker_image],
-#             var.app_args
-#           ))}
-#       EOT
-#     }
-#   )
 
   metadata = merge(
       {
@@ -193,11 +132,6 @@ resource "google_compute_instance_template" "confidential_vm_template" {
   EOT
       }
     )
-#     - name: GRPC_DEFAULT_SSL_ROOTS_FILE_PATH
-#                 value: "/etc/ssl/ca-certificates.crt"
-#           - name: JAVA_TOOL_OPTIONS
-#             value: "-Djavax.net.ssl.trustStore=/etc/ssl/ca-certificates.crt \
-#                     -Djavax.net.ssl.trustStorePassword=changeit"
   service_account {
     email = google_service_account.mig_service_account.email
     scopes = [
