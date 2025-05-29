ae15329902e725eb6ef5089a6951a6c2b7f6191a
Lin <lindreamdeyi@google.com>
2025-05-29 21:58:35 +0000

    feat: implement ListModelProviders

--- src/test/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/ModelProvidersServiceTest.kt
+++ src/test/kotlin/org/wfanet/measurement/kingdom/service/api/v2alpha/ModelProvidersServiceTest.kt
@@ -34,17 +34,24 @@ import org.wfanet.measurement.api.v2alpha.GetModelProviderRequest
 import org.wfanet.measurement.api.v2alpha.ModelProvider
 import org.wfanet.measurement.api.v2alpha.ModelProviderKey
 import org.wfanet.measurement.api.v2alpha.getModelProviderRequest
+import org.wfanet.measurement.api.v2alpha.listModelProvidersRequest
+import org.wfanet.measurement.api.v2alpha.listModelProvidersResponse
 import org.wfanet.measurement.api.v2alpha.modelProvider
 import org.wfanet.measurement.api.v2alpha.withDataProviderPrincipal
 import org.wfanet.measurement.api.v2alpha.withDuchyPrincipal
 import org.wfanet.measurement.api.v2alpha.withMeasurementConsumerPrincipal
 import org.wfanet.measurement.api.v2alpha.withModelProviderPrincipal
+import org.wfanet.measurement.common.base64UrlEncode
 import org.wfanet.measurement.common.grpc.testing.GrpcTestServerRule
 import org.wfanet.measurement.common.grpc.testing.mockService
 import org.wfanet.measurement.common.identity.apiIdToExternalId
 import org.wfanet.measurement.common.testing.verifyProtoArgument
+import org.wfanet.measurement.internal.kingdom.ListModelProvidersPageTokenKt
 import org.wfanet.measurement.internal.kingdom.ModelProvidersGrpcKt as InternalModelProvidersGrpcKt
 import org.wfanet.measurement.internal.kingdom.getModelProviderRequest as internalGetModelProviderRequest
+import org.wfanet.measurement.internal.kingdom.listModelProvidersPageToken as internalListModelProvidersPageToken
+import org.wfanet.measurement.internal.kingdom.listModelProvidersRequest as internalListModelProvidersRequest
+import org.wfanet.measurement.internal.kingdom.listModelProvidersResponse as internalListModelProvidersResponse
 import org.wfanet.measurement.internal.kingdom.modelProvider as internalModelProvider
 
 @RunWith(JUnit4::class)
@@ -75,9 +82,7 @@ class ModelProvidersServiceTest {
 
     val request = getModelProviderRequest { name = MODEL_PROVIDER_NAME }
     val result =
-      withModelProviderPrincipal(MODEL_PROVIDER_NAME) {
-        runBlocking { service.getModelProvider(request) }
-      }
+      withModelProviderPrincipal(MODEL_PROVIDER_NAME) { service.getModelProvider(request) }
 
     verifyProtoArgument(
         internalModelProvidersMock,
@@ -90,7 +95,7 @@ class ModelProvidersServiceTest {
   }
 
   @Test
-  fun `getModelProvider returns ModelProvider when DataProvider caller is found`() {
+  fun `getModelProvider returns ModelProvider when DataProvider caller is found`() = runBlocking {
     val internalModelProvider = internalModelProvider {
       externalModelProviderId = EXTERNAL_MODEL_PROVIDER_ID
     }
@@ -99,10 +104,7 @@ class ModelProvidersServiceTest {
     }
 
     val request = getModelProviderRequest { name = MODEL_PROVIDER_NAME }
-    val result =
-      withDataProviderPrincipal(DATA_PROVIDER_NAME) {
-        runBlocking { service.getModelProvider(request) }
-      }
+    val result = withDataProviderPrincipal(DATA_PROVIDER_NAME) { service.getModelProvider(request) }
 
     verifyProtoArgument(
         internalModelProvidersMock,
@@ -115,70 +117,141 @@ class ModelProvidersServiceTest {
   }
 
   @Test
-  fun `getModelProvider throws PERMISSION_DENIED when Principal is duchy`() {
+  fun `getModelProvider throws PERMISSION_DENIED when Principal is duchy`() = runBlocking {
     val request = getModelProviderRequest { name = MODEL_PROVIDER_NAME }
 
     val exception =
       assertFailsWith<StatusRuntimeException> {
-        withDuchyPrincipal(DUCHY_NAME) { runBlocking { service.getModelProvider(request) } }
+        withDuchyPrincipal(DUCHY_NAME) { service.getModelProvider(request) }
       }
     assertThat(exception.status.code).isEqualTo(Status.Code.PERMISSION_DENIED)
   }
 
   @Test
-  fun `getModelProvider throws PERMISSION_DENIED when Principal is MeasurementConsumer`() {
-    val request = getModelProviderRequest { name = MODEL_PROVIDER_NAME }
+  fun `getModelProvider throws PERMISSION_DENIED when Principal is MeasurementConsumer`() =
+    runBlocking {
+      val request = getModelProviderRequest { name = MODEL_PROVIDER_NAME }
 
-    val exception =
-      assertFailsWith<StatusRuntimeException> {
-        withMeasurementConsumerPrincipal(MEASUREMENT_CONSUMER_NAME) {
-          runBlocking { service.getModelProvider(request) }
+      val exception =
+        assertFailsWith<StatusRuntimeException> {
+          withMeasurementConsumerPrincipal(MEASUREMENT_CONSUMER_NAME) {
+            service.getModelProvider(request)
+          }
         }
-      }
-    assertThat(exception.status.code).isEqualTo(Status.Code.PERMISSION_DENIED)
-  }
+      assertThat(exception.status.code).isEqualTo(Status.Code.PERMISSION_DENIED)
+    }
 
   @Test
-  fun `getModelProvider throws PERMISSION_DENIED when ModelProvider caller doesn't match`() {
-    val request = getModelProviderRequest { name = MODEL_PROVIDER_NAME }
+  fun `getModelProvider throws PERMISSION_DENIED when ModelProvider caller doesn't match`() =
+    runBlocking {
+      val request = getModelProviderRequest { name = MODEL_PROVIDER_NAME }
 
-    val exception =
-      assertFailsWith<StatusRuntimeException> {
-        withModelProviderPrincipal(MODEL_PROVIDER_NAME_2) {
-          runBlocking { service.getModelProvider(request) }
+      val exception =
+        assertFailsWith<StatusRuntimeException> {
+          withModelProviderPrincipal(MODEL_PROVIDER_NAME_2) { service.getModelProvider(request) }
         }
-      }
-    assertThat(exception.status.code).isEqualTo(Status.Code.PERMISSION_DENIED)
-  }
+      assertThat(exception.status.code).isEqualTo(Status.Code.PERMISSION_DENIED)
+    }
 
   @Test
-  fun `getModelProvider throws UNAUTHENTICATED when no Principal is found`() {
+  fun `getModelProvider throws UNAUTHENTICATED when no Principal is found`() = runBlocking {
     val request = getModelProviderRequest { name = MODEL_PROVIDER_NAME }
 
-    val exception =
-      assertFailsWith<StatusRuntimeException> { runBlocking { service.getModelProvider(request) } }
+    val exception = assertFailsWith<StatusRuntimeException> { service.getModelProvider(request) }
     assertThat(exception.status.code).isEqualTo(Status.Code.UNAUTHENTICATED)
   }
 
   @Test
-  fun `getModelProvider throws INVALID_ARGUMENT when name is missing`() {
+  fun `getModelProvider throws INVALID_ARGUMENT when name is missing`() = runBlocking {
     val exception =
       assertFailsWith<StatusRuntimeException> {
         withModelProviderPrincipal(MODEL_PROVIDER_NAME) {
-          runBlocking { service.getModelProvider(GetModelProviderRequest.getDefaultInstance()) }
+          service.getModelProvider(GetModelProviderRequest.getDefaultInstance())
         }
       }
     assertThat(exception.status.code).isEqualTo(Status.Code.INVALID_ARGUMENT)
   }
 
+  @Test
+  fun `listModelProviders with page size and page token returns ModelProviders`() = runBlocking {
+    val internalModelProvider = internalModelProvider {
+      externalModelProviderId = EXTERNAL_MODEL_PROVIDER_ID_2
+    }
+    val internalListModelProvidersResponse = internalListModelProvidersResponse {
+      modelProviders += internalModelProvider
+      nextPageToken = internalListModelProvidersPageToken {
+        after =
+          ListModelProvidersPageTokenKt.after {
+            externalModelProviderId = EXTERNAL_MODEL_PROVIDER_ID_3
+          }
+      }
+    }
+    internalModelProvidersMock.stub {
+      onBlocking { listModelProviders(any()) } doReturn internalListModelProvidersResponse
+    }
+
+    val internalPagetoken = internalListModelProvidersPageToken {
+      after =
+        ListModelProvidersPageTokenKt.after { externalModelProviderId = EXTERNAL_MODEL_PROVIDER_ID }
+    }
+    val request = listModelProvidersRequest {
+      pageSize = 1
+      pageToken = internalPagetoken.toByteString().base64UrlEncode()
+    }
+    val result = service.listModelProviders(request)
+
+    verifyProtoArgument(
+        internalModelProvidersMock,
+        InternalModelProvidersGrpcKt.ModelProvidersCoroutineImplBase::listModelProviders,
+      )
+      .isEqualTo(
+        internalListModelProvidersRequest {
+          pageSize = 1
+          pageToken = internalPagetoken
+        }
+      )
+    assertThat(result)
+      .isEqualTo(
+        listModelProvidersResponse {
+          modelProviders += internalModelProvider.toModelProvider()
+          nextPageToken =
+            internalListModelProvidersResponse.nextPageToken.toByteString().base64UrlEncode()
+        }
+      )
+  }
+
+  @Test
+  fun `listModelProviders throws INVALID_ARGUMENT when page size is negative`() = runBlocking {
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        service.listModelProviders(listModelProvidersRequest { pageSize = -1 })
+      }
+
+    assertThat(exception.status.code).isEqualTo(Status.Code.INVALID_ARGUMENT)
+  }
+
+  @Test
+  fun `listModelProviders throws INVALID_ARGUMENT when page token is invalid`() = runBlocking {
+    val exception =
+      assertFailsWith<StatusRuntimeException> {
+        service.listModelProviders(listModelProvidersRequest { pageToken = "betty black" })
+      }
+    assertThat(exception.status.code).isEqualTo(Status.Code.INVALID_ARGUMENT)
+  }
+
   companion object {
-    private const val DUCHY_NAME = "duchies/AAAAAAAAAHs"
     private const val DATA_PROVIDER_NAME = "dataProviders/AAAAAAAwAAHs"
+    private const val DUCHY_NAME = "duchies/AAAAAAAAAHs"
+    private const val MEASUREMENT_CONSUMER_NAME = "measurementConsumers/AAAAAAAAAHs"
     private const val MODEL_PROVIDER_NAME = "modelProviders/AAAAAAAAAHs"
     private const val MODEL_PROVIDER_NAME_2 = "modelProviders/AAAAAAAAAJs"
-    private const val MEASUREMENT_CONSUMER_NAME = "measurementConsumers/AAAAAAAAAHs"
+    private const val MODEL_PROVIDER_NAME_3 = "modelProviders/AAAAAAAABJs"
     private val EXTERNAL_MODEL_PROVIDER_ID =
       apiIdToExternalId(ModelProviderKey.fromName(MODEL_PROVIDER_NAME)!!.modelProviderId)
+    private val EXTERNAL_MODEL_PROVIDER_ID_2 =
+      apiIdToExternalId(ModelProviderKey.fromName(MODEL_PROVIDER_NAME_2)!!.modelProviderId)
+    private val EXTERNAL_MODEL_PROVIDER_ID_3 =
+      apiIdToExternalId(ModelProviderKey.fromName(MODEL_PROVIDER_NAME_3)!!.modelProviderId)
 
     private val MODEL_PROVIDER: ModelProvider = modelProvider { name = MODEL_PROVIDER_NAME }
   }
