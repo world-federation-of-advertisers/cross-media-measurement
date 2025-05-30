a042c809a51bfa49394c8e08565ca58d3b059e1f
Lin <lindreamdeyi@google.com>
2025-05-30 20:27:02 +0000

    draft: model provider cli and tests

--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/tools/ModelRepository.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/tools/ModelRepository.kt
@@ -19,10 +19,14 @@ package org.wfanet.measurement.kingdom.deploy.tools
 import io.grpc.ManagedChannel
 import java.time.Duration
 import kotlinx.coroutines.runBlocking
+import org.wfanet.measurement.api.v2alpha.ListModelProvidersResponse
 import org.wfanet.measurement.api.v2alpha.ListModelSuitesResponse
+import org.wfanet.measurement.api.v2alpha.ModelProvidersGrpcKt.ModelProvidersCoroutineStub
 import org.wfanet.measurement.api.v2alpha.ModelSuitesGrpcKt.ModelSuitesCoroutineStub
 import org.wfanet.measurement.api.v2alpha.createModelSuiteRequest
+import org.wfanet.measurement.api.v2alpha.getModelProviderRequest
 import org.wfanet.measurement.api.v2alpha.getModelSuiteRequest
+import org.wfanet.measurement.api.v2alpha.listModelProvidersRequest
 import org.wfanet.measurement.api.v2alpha.listModelSuitesRequest
 import org.wfanet.measurement.api.v2alpha.modelSuite
 import org.wfanet.measurement.common.commandLineMain
@@ -41,7 +45,7 @@ private val CHANNEL_SHUTDOWN_TIMEOUT = Duration.ofSeconds(30)
 @Command(
   name = "model-repository",
   description = ["Manages all Model Repository artifacts"],
-  subcommands = [CommandLine.HelpCommand::class, ModelSuites::class],
+  subcommands = [CommandLine.HelpCommand::class, ModelProviders::class, ModelSuites::class],
 )
 class ModelRepository private constructor() : Runnable {
   @Mixin private lateinit var tlsFlags: TlsFlags
@@ -76,6 +80,55 @@ class ModelRepository private constructor() : Runnable {
 
   companion object {
     @JvmStatic fun main(args: Array<String>) = commandLineMain(ModelRepository(), args)
+  }
+}
+
+@Command(
+  name = "model-providers",
+  subcommands = [CommandLine.HelpCommand::class, GetModelProvider::class, ListModelProviders::class],
+)
+private class ModelProviders {
+  @ParentCommand private lateinit var parentCommand: ModelRepository
+
+  val modelProvidersClient: ModelProvidersCoroutineStub by lazy {
+    ModelProvidersCoroutineStub(parentCommand.channel)
+  }
+}
+
+@Command(name = "get", description = ["Get a ModelProvider"])
+class GetModelProvider : Runnable {
+  @ParentCommand private lateinit var parentCommand: ModelProviders
+
+  @Parameters(index = "0", description = ["API resource name of the ModelProvider"], arity = "1")
+  private lateinit var modelProviderName: String
+
+  override fun run() {
+    val modelProvider = runBlocking {
+      parentCommand.modelProvidersClient.getModelProvider(
+        getModelProviderRequest { name = modelProviderName }
+      )
+    }
+    println(modelProvider)
+  }
+}
+
+@Command(name = "list", description = ["List ModelProviders"])
+class ListModelProviders : Runnable {
+  @ParentCommand private lateinit var parentCommand: ModelProviders
+
+  @Mixin private lateinit var pageParams: PageParams
+
+  override fun run() {
+    val response: ListModelProvidersResponse = runBlocking {
+      parentCommand.modelProvidersClient.listModelProviders(
+        listModelProvidersRequest {
+          pageSize = pageParams.pageSize
+          pageToken = pageParams.pageToken
+        }
+      )
+    }
+
+    println(response)
   }
 }
 
