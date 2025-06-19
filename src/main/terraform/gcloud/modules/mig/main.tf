c5b55943ae8476eb79392c1cf63dab539f9b2f15
marcopremier <marcopremier@meta.com>
2025-06-18 22:12:55 -0700

    debugging mig

--- src/main/terraform/gcloud/modules/mig/main.tf
+++ src/main/terraform/gcloud/modules/mig/main.tf
@@ -47,6 +47,7 @@ resource "google_secret_manager_secret_iam_member" "mig_sa_secret_accessor" {
 }
 
 resource "google_compute_instance_template" "confidential_vm_template" {
+  name_prefix  = "nonconfidential-vm-template-"
   machine_type = var.machine_type
 
 #   confidential_instance_config {
