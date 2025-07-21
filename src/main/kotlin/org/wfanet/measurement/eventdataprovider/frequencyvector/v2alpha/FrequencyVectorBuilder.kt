7d7f735091b65098306b4e0ac22fbc1a999bf32b
Lin <lindreamdeyi@google.com>
2025-07-21 16:38:20 +0000

    feat: refactor eventdataprovider folder structure, and implement TrusTee RequisitionRequestBuilder

--- src/main/kotlin/org/wfanet/measurement/eventdataprovider/frequencyvector/v2alpha/FrequencyVectorBuilder.kt
+++ src/main/kotlin/org/wfanet/measurement/eventdataprovider/frequencyvector/v2alpha/FrequencyVectorBuilder.kt
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.wfanet.measurement.eventdataprovider.shareshuffle.v2alpha
+package org.wfanet.measurement.eventdataprovider.frequencyvector.v2alpha
 
 import org.wfanet.frequencycount.FrequencyVector
 import org.wfanet.frequencycount.frequencyVector
--- src/main/kotlin/org/wfanet/measurement/loadtest/measurementconsumer/MeasurementConsumerSimulator.kt
+++ src/main/kotlin/org/wfanet/measurement/loadtest/measurementconsumer/MeasurementConsumerSimulator.kt
@@ -809,6 +809,9 @@ abstract class MeasurementConsumerSimulator(
           protocol.honestMajorityShareShuffle.noiseMechanism,
         )
       }
+      ProtocolConfig.Protocol.ProtocolCase.TRUS_TEE -> {
+        error("Pending implementation.")
+      }
       ProtocolConfig.Protocol.ProtocolCase.PROTOCOL_NOT_SET -> {
         error("Protocol is not set.")
       }
