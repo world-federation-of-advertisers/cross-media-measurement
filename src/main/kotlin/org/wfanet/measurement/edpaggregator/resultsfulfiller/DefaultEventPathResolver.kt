20e7bdaa5fda641900576b6be04cd812deed13d0
Matthias Georgi <mmg@meta.com>
2025-07-25 13:12:12 +0000

    Orchestrator

---
+++ src/main/kotlin/org/wfanet/measurement/edpaggregator/resultsfulfiller/DefaultEventPathResolver.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2025 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.edpaggregator.resultsfulfiller
+
+import java.time.LocalDate
+
+/**
+ * Default implementation using the current path convention.
+ */
+class DefaultEventPathResolver(
+  private val impressionsMetadataBucketUri: String
+) : EventPathResolver {
+
+  override suspend fun resolvePaths(
+    date: LocalDate,
+    eventGroupReferenceId: String
+  ): EventPathResolver.EventPaths {
+    val metadataPath =
+      "$impressionsMetadataBucketUri/ds/$date/event-group-reference-id/$eventGroupReferenceId/metadata"
+
+    return EventPathResolver.EventPaths(
+      metadataPath = metadataPath,
+      eventGroupReferenceId = eventGroupReferenceId
+    )
+  }
+}
