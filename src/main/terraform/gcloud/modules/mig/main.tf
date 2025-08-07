ed8a5eb91105f0f4718b1564994af0c6b2282839
marcopremier <marcopremier@meta.com>
2025-08-07 05:54:08 -0700

    wip

--- src/main/terraform/gcloud/modules/mig/main.tf
+++ src/main/terraform/gcloud/modules/mig/main.tf
@@ -133,6 +133,7 @@ resource "google_compute_region_instance_group_manager" "mig" {
   distribution_policy_zones = var.mig_distribution_policy_zones
   lifecycle {
     create_before_destroy = true
+    replace_triggered_by  = [terraform_data.image_change_trigger]
   }
 }
 
