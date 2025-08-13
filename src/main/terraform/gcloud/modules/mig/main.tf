51e9e08c9225eef7fc669ed495d24ff46754d1c5
marcopremier <marcopremier@meta.com>
2025-08-13 05:29:37 -0700

    refactor: add _snp from VM instance type

--- src/main/terraform/gcloud/modules/mig/main.tf
+++ src/main/terraform/gcloud/modules/mig/main.tf
@@ -76,7 +76,7 @@ resource "google_compute_instance_template" "confidential_vm_template" {
 
   confidential_instance_config {
     enable_confidential_compute = true
-    confidential_instance_type  = "SEV"
+    confidential_instance_type  = "SEV_SNP"
   }
 
   scheduling {
