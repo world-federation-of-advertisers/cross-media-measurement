d3801cfeb10cf9ddcb3167ccbeb6e93b2f356223
kungfucraig <wrightcw@google.com>
2025-09-01 21:39:17 +0000

    Schema now compiles.

--- src/main/resources/reporting/spanner/create-report-results-schema.sql
+++ src/main/resources/reporting/spanner/create-report-results-schema.sql
@@ -14,131 +14,153 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
--- changeset
--- comment: Create ReportResults tables and make them usable for BasicReport
-
--- TODO: Add proto bundle
+--changeset kungfucraig:2 dbms:cloudspanner
+-- comment: Create ReportResults tables and make them usable for BasicReport results
 
 START BATCH DDL;
 
--- reference this table in basic report. If this table is referenced then the details should NOT be
--- set
+-- There is one in memory table that provides an GroupingDimensionFingerprint
+-- for use in the tables below as a join key.
+--
+-- This field is a 64-bit integer that represents a combination of grouping dimensions.
+-- For example if we have age bucket and gender dimensions each combination of their
+-- values is assigned a unique integer. For example the combination (AGE_18_24, F)
+-- is assigned a unique integer as does (AGE_18_24, null), in which case this includes
+-- al values of the gender dimension in the grouping.
+-- This allows for two things:
+ --   1. a more efficient join of results
+ --   2. a way to easily track versions of Event Templates
+ -- The fingerprint considers the combination of dimension values as well as the Event Template
+ -- version.
 
--- TODO: code should be written to generate the EventTemplateDimensions table at report creation time if
--- any referenced dimensions do not exist.
--- it should have a column per groupable dimension. And it should have one column for any filter applied.
-
--- Create a place holder for supporting multiple EventTemplates concurrently
-CREATE TABLE EventTemplates (
-  EventTemplateId INT64 NOT NULL)
-PRIMARY KEY (EventTemplateId)
-
--- This table should be defined/altered as part of configuring the system and/or updating
--- the EventTemplate version.
--- Alternatively this table could just be kept in memory in the internal server
--- If kept in memory the above table won't be necessary. Preference would be to keep this in
--- memory to start with and if it gets too big to keep it in a DB.
-CREATE TABLE EventTemplateDimensions (
-  EventTemplateId INT64 NOT NULL,
-  -- This should be globally unique so we can use it as a join key on ReportResultDimensions
-  EventTemplateDimensionId INT64 NOT NULL,
-
-  EventTemplateVersion INT64 NOT NULL,
-
-  -- Create a column per event template dimension that can be grouped by. It's type is "string" and represents all
-  -- of the values the event template field can take on.
-  ) PRIMARY KEY (EventTemplateId, EventTemplateDimension)
-
-CREATE TABLE ReportResultStaticDimensions (
-  ReportStaticDimensionId INT64 NOT NULL,
-    -- The IQF for which these results were computed. In the future this
-    -- column could be nullable if we decide to make IQFs optional and/or
-    -- deprecate them.
-    ImpressionQualificationFilterId INT64 NOT NULL
-    -- Corresponds to the enum report_results.proto
-    MetricFrequencyType INT64 NOT NULL;
-    -- Corresponds to the enum report_results.proto.
-    VennDiagramRegionType INT64 NOT NULL;
-    -- Describes the event template dimensions that the results are filtered to
-  ) PRIMARY KEY (ReportStaticDimensionId);
-  -- create unique index on three values
-
+-- This table is the root for all of the Results associated with a Basic Report and in the
+-- future an Advanced Report or Report Cube.
 CREATE TABLE ReportResults (
   MeasurementConsumerId INT64 NOT NULL,
   ReportResultId INT64 NOT NULL,
   -- This is the start time of the set of results. Dates in subtables
   -- are with respect to the time defined by this timestamp. This is also
   -- used to indicate the start date for cumulative results in the child tables
-  ReportIntervalStartTime TIMESTAMP NOT NULL
-
+  ReportIntervalStartTime TIMESTAMP NOT NULL,
+  -- The time that this row was created.
   CreateTime TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true),
   FOREIGN KEY (MeasurementConsumerId) REFERENCES MeasurementConsumers (MeasurementConsumerId)
-) PRIMARY KEY (MeasurementConsumerId, ReportResultsId),
+) PRIMARY KEY (MeasurementConsumerId, ReportResultId),
   INTERLEAVE IN PARENT MeasurementConsumers ON DELETE CASCADE;
 
--- This being a child table of the above ensures that results for all ReportingSets for a given
--- ReportResult are physically nearby.
-CREATE TABLE ReportResultDimensions (
+-- Each row of this table represents the Results for a particular ReportingSet Venn diagram
+-- region that is entailed by the Report.
+--
+-- This being a child table of the above ensures that results for the entire Report are
+-- physically nearby.
+CREATE TABLE ReportingSetResults(
   MeasurementConsumerId INT64 NOT NULL,
   ReportResultId INT64 NOT NULL,
-  ReportDimensionId INT64 NOT NULL,
-  -- ReportingSetId is the Postgres ReportingSetId
-  -- This corresponds to the ReportingSet for which the results are computed.
+  ReportingSetResultId INT64 NOT NULL,
+  -- A reference to the Postgres ReportingSetId
+  -- Corresponds to the ReportingSet for which the results are computed.
   -- We expect that all ReportingSets referenced here represent only unions of
   -- EventGroups.
   ReportingSetId INT64 NOT NULL,
-  ReportResultStaticDimensionId INT64 NOT NULL,
-  EventTemplateDimensionId INT64 NOT NULL;
+  -- Corresponds to the enum VennDiagramRegionType in report_results.proto.
+  -- The VennDiagram region type may only be PRIMITIVE when the ReportingSet was
+  -- passed in by the user, or in the case of BasicReport, when it corresponds to
+  -- exactly one DataProvider. ReportingSets created as a side effect of Report creation
+  -- should always be type UNION.
+  VennDiagramRegionType INT64 NOT NULL,
+  -- The ID of ImpressionQualificationFilter for which the set of results was computed.
+  -- In the future this column could be nullable if we decide to make IQFs optional and/or
+  -- deprecate them. We limit the use of Custom IQFs to a single filter. When the filter is
+  -- custom the value of this column will be -1.
+  ImpressionQualificationFilterName STRING(MAX) NOT NULL,
+  -- Corresponds to the MetricFrequencyType enum in report_results.proto (e.g. WEEKLY)
+  MetricFrequencyType INT64 NOT NULL,
+  -- The grouping dimension.
+  -- See the comment at the top of this file related to this field.
+  GroupingDimensionFingerprint INT64 NOT NULL,
+ -- The fingerprint of the normalized EventFilter proto that was used to create
+ -- the results. It does not include any filters implied by the IQF.
+ -- This field is NULL if no filters were applied.
+ FilterFingerprint INT64,
+ -- The creation time of this row.
+ CreateTime TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true),
+) PRIMARY KEY (MeasurementConsumerId, ReportResultId, ReportingSetResultId),
+  INTERLEAVE IN PARENT ReportResults ON DELETE CASCADE;
+-- Ensure non-primary key dimensions are unique with respect to the parent.
+CREATE UNIQUE INDEX ReportingSetResultsByDimensions
+    ON ReportingSetResults(
+    MeasurementConsumerId, ReportResultId,
+    ReportingSetId, VennDiagramRegionType,
+    ImpressionQualificationFilterName, MetricFrequencyType,
+    GroupingDimensionFingerprint, FilterFingerprint),
+ INTERLEAVE IN ReportResults;
+
+-- Represents the results for each reporting window associated with each
+-- ReportingSetResult.
+CREATE TABLE ReportingWindowResults(
+  MeasurementConsumerId INT64 NOT NULL,
+  ReportResultId INT64 NOT NULL,
+  ReportingSetResultId INT64 NOT NULL,
+  ReportingWindowResultId INT64 NOT NULL,
   -- The start date for non-cumulative results in this row
   ReportingWindowStartDate DATE NOT NULL,
   -- The end date for results in this row
   ReportingWindowEndDate DATE NOT NULL,
-  -- This is the normalized filter proto. We expect to be able to do comparisions on this column. It is NULL
-  -- if no filters are applied.
-  Filter FilterProto `path.to.filter.proto`,
-  CreateTime TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true),
-  FOREIGN KEY (MeasurementConsumerId) REFERENCES MeasurementConsumers (MeasurementConsumerId)
-  FOREIGN KEY (ReportResultId) REFERENCES ReportResults (ReportResultId)
-) PRIMARY KEY (MeasurementConsumerId, ReportResultsId, ReportDimensionId),
-  INTERLEAVE IN PARENT ReportResults ON DELETE CASCADE;
--- Create an index on the non-primary key dimensions as those should be unique as a group
+ -- The creation time of this row.
+ CreateTime TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true),
+ ) PRIMARY KEY (MeasurementConsumerId, ReportResultId,
+               ReportingSetResultId, ReportingWindowResultId),
+   INTERLEAVE IN PARENT ReportingSetResults ON DELETE CASCADE;
+-- Ensure non-primary key dimensions are unique with respect to the parent.
+CREATE UNIQUE INDEX ReportingWindowResultsByDates
+     ON ReportingWindowResults(
+     MeasurementConsumerId, ReportResultId,
+     ReportingSetResultId, ReportingWindowStartDate,
+     ReportingWindowEndDate),
+  INTERLEAVE IN ReportingSetResults;
 
+-- Contains denoised values. We expect all derived metrics to be present in the cumulative
+-- and non-cumulative columns regardless of whether they were explicitly requested via the
+-- API. Both CumulativeResults and NonCumulativeResults must not be null.
 CREATE TABLE ReportResultValues (
   MeasurementConsumerId INT64 NOT NULL,
   ReportResultId INT64 NOT NULL,
-  ReportDimensionId INT64 NOT NULL,
+  ReportingSetResultId INT64 NOT NULL,
+  ReportingWindowResultId INT64 NOT NULL,
   ReportResultValueId INT64 NOT NULL,
-
-  CumulativeResults
-    BasicReportResultDetails `wfa.measurement.internal.reporting.v2.ResultGroup.MetricSet.BasicMetricSet`,
-
-  NonCumulativeResults
-    BasicReportResultDetails `wfa.measurement.internal.reporting.v2.ResultGroup.MetricSet.BasicMetricSet`,
-
+  -- Denoised cumulative results for the window represented by the parent table
+  CumulativeResults `wfa.measurement.internal.reporting.v2.ResultGroup.MetricSet.BasicMetricSet`,
+  -- Denoised non-cumulative results for the window represented by the parent table.
+  NonCumulativeResults `wfa.measurement.internal.reporting.v2.ResultGroup.MetricSet.BasicMetricSet`,
+  -- The creation time of this row.
   CreateTime TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true),
-) PRIMARY KEY (MeasurementConsumerId, ReportResultsId, ReportDimensionId, ReportResultValueId),
-  INTERLEAVE IN PARENT ReportResultDimensions ON DELETE CASCADE;
+) PRIMARY KEY (MeasurementConsumerId, ReportResultId, ReportingSetResultId,
+               ReportingWindowResultId, ReportResultValueId),
+  INTERLEAVE IN PARENT ReportingWindowResults ON DELETE CASCADE;
 
-
+-- Contains noisy values. We do not expected this table to contain any derived metrics.
+-- Both CumulativeResults and NonCumulativeResults must not be null.
 CREATE TABLE NoisyReportResultValues (
   MeasurementConsumerId INT64 NOT NULL,
   ReportResultId INT64 NOT NULL,
-  ReportDimensionId INT64 NOT NULL,
+  ReportingSetResultId INT64 NOT NULL,
+  ReportingWindowResultId INT64 NOT NULL,
   NoisyReportResultValueId INT64 NOT NULL,
-
-  CumulativeResults
-    BasicReportResultDetails `wfa.measurement.internal.reporting.v2.ReportResults.BaseMetricSet.BasicBaseMetricSet`,
-
-  NonCumulativeResults
-    BasicReportResultDetails `wfa.measurement.internal.reporting.v2.ReportResults.BaseMetricSet.BaseBasicBaseMetricSet`,
-
+  -- Noisy cumulative results for the window represented by the parent table.
+  -- Only non-drived metrics will be present.
+  CumulativeResults `wfa.measurement.internal.reporting.v2.ResultGroup.MetricSet.BasicMetricSet`,
+  -- Noisy non-cumulative results for the window represented by the parent table.
+  -- Only non-drived metrics will be present.
+  NonCumulativeResults `wfa.measurement.internal.reporting.v2.ResultGroup.MetricSet.BasicMetricSet`,
+  -- The creation time of this row.
   CreateTime TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp = true),
-) PRIMARY KEY (MeasurementConsumerId, ReportResultsId, ReportDimensionId, NoisyReportResultValueId),
-  INTERLEAVE IN PARENT ReportResultDimensions ON DELETE CASCADE;
+) PRIMARY KEY (MeasurementConsumerId, ReportResultId, ReportingSetResultId,
+                ReportingWindowResultId, NoisyReportResultValueId),
+  INTERLEAVE IN PARENT ReportingWindowResults ON DELETE CASCADE;
 
 ALTER TABLE BasicReports ADD COLUMN ReportResultId INT64;
 ALTER TABLE BasicReports ALTER COLUMN BasicReportResultDetails
- `wfa.measurement.internal.reporting.v2.BasicReportResultDetails` NULL;
+ `wfa.measurement.internal.reporting.v2.BasicReportResultDetails`;
 
 RUN BATCH;
 
