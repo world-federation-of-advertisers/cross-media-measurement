5e0c6f7c9b3a9d32c6fd8abb32f209a5f3ce8d9d
marcopremier <marcopremier@meta.com>
2025-09-03 06:40:13 -0700

    test all direct with new req function

--- src/test/kotlin/org/wfanet/measurement/integration/k8s/AbstractEdpAggregatorCorrectnessTest.kt
+++ src/test/kotlin/org/wfanet/measurement/integration/k8s/AbstractEdpAggregatorCorrectnessTest.kt
@@ -63,7 +63,7 @@ abstract class AbstractEdpAggregatorCorrectnessTest(
       // Use frontend simulator to create an impression measurement and verify its
       // result.
       mcSimulator.testImpression("1234", eventGroupFilter = EVENT_GROUP_FILTERING_LAMBDA_DIRECT_MEASUREMENTS)
-//    }
+    }
 
 //  @Test
 //  fun `create a Hmss reach-only measurement and check the result is equal to the expected result`() =
