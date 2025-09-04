77f1dbf7969d23f6dc0ec35553ac54fdf7cadbed
marcopremier <marcopremier@meta.com>
2025-09-04 04:20:47 -0700

    redeploy results fulfiller

--- src/test/kotlin/org/wfanet/measurement/integration/k8s/EdpAggregatorCorrectnessTest.kt
+++ src/test/kotlin/org/wfanet/measurement/integration/k8s/EdpAggregatorCorrectnessTest.kt
@@ -363,9 +363,9 @@ class EdpAggregatorCorrectnessTest : AbstractEdpAggregatorCorrectnessTest(measur
         "dataprovider",
       )
 
-    private const val GROUP_REFERENCE_ID_EDPA_EDP1 = "edpa-eg-reference-id-1"
-//    private const val GROUP_REFERENCE_ID_EDPA_EDP2 = "edpa-eg-reference-id-2"
-    private const val GROUP_REFERENCE_ID_EDPA_EDP2 = "edpa-eg-reference-id-meta"
+//    private const val GROUP_REFERENCE_ID_EDPA_EDP1 = "edpa-eg-reference-id-1"
+    private const val GROUP_REFERENCE_ID_EDPA_EDP1 = "edpa-eg-reference-id-meta"
+    private const val GROUP_REFERENCE_ID_EDPA_EDP2 = "edpa-eg-reference-id-2"
 
     private val TEST_DATA_RUNTIME_PATH =
       org.wfanet.measurement.common.getRuntimePath(TEST_DATA_PATH)!!
