01117cd19a49de100993254d874c4e426fdcb805
marcopremier <marcopremier@meta.com>
2025-09-10 08:08:52 -0700

    refactor: temporary add debug image

--- src/main/terraform/gcloud/cmms/edp_aggregator.tf
+++ src/main/terraform/gcloud/cmms/edp_aggregator.tf
@@ -198,5 +198,6 @@ module "edp_aggregator" {
   trusted_root_ca_collection                = local.trusted_root_ca_collection
   edps_certs                                = local.edps_certs
   cloud_function_configs                    = local.cloud_function_configs
-  results_fulfiller_disk_image_family       = "confidential-space"
+  # DO_NOT_SUBMIT (Replace confidential-space-debug with confidential-space)
+  results_fulfiller_disk_image_family       = "confidential-space-debug"
 }
