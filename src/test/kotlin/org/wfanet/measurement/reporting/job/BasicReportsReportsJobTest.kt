028934379b10fd99eccdae7382c4118929473594
Tristan Vuong <tristanvuong@google.com>
2025-09-10 18:19:48 +0000

    change test name

--- src/test/kotlin/org/wfanet/measurement/reporting/job/BasicReportsReportsJobTest.kt
+++ src/test/kotlin/org/wfanet/measurement/reporting/job/BasicReportsReportsJobTest.kt
@@ -87,36 +87,37 @@ class BasicReportsReportsJobTest {
   }
 
   @Test
-  fun `execute gets report when report for basic report is SUCCEEDED`(): Unit = runBlocking {
-    whenever(reportsMock.getReport(any()))
-      .thenReturn(REPORT.copy { state = Report.State.SUCCEEDED })
+  fun `execute transforms and persists results when report for basic report is SUCCEEDED`(): Unit =
+    runBlocking {
+      whenever(reportsMock.getReport(any()))
+        .thenReturn(REPORT.copy { state = Report.State.SUCCEEDED })
 
-    job.execute()
+      job.execute()
 
-    verifyProtoArgument(basicReportsMock, BasicReportsCoroutineImplBase::listBasicReports)
-      .isEqualTo(
-        listBasicReportsRequest {
-          filter =
-            ListBasicReportsRequestKt.filter {
-              cmmsMeasurementConsumerId = CMMS_MEASUREMENT_CONSUMER_ID
-              state = BasicReport.State.REPORT_CREATED
-            }
-          pageSize = BATCH_SIZE
-        }
-      )
+      verifyProtoArgument(basicReportsMock, BasicReportsCoroutineImplBase::listBasicReports)
+        .isEqualTo(
+          listBasicReportsRequest {
+            filter =
+              ListBasicReportsRequestKt.filter {
+                cmmsMeasurementConsumerId = CMMS_MEASUREMENT_CONSUMER_ID
+                state = BasicReport.State.REPORT_CREATED
+              }
+            pageSize = BATCH_SIZE
+          }
+        )
 
-    verifyProtoArgument(reportsMock, ReportsCoroutineImplBase::getReport)
-      .isEqualTo(
-        getReportRequest {
-          name =
-            ReportKey(CMMS_MEASUREMENT_CONSUMER_ID, INTERNAL_BASIC_REPORT.externalReportId).toName()
-        }
-      )
+      verifyProtoArgument(reportsMock, ReportsCoroutineImplBase::getReport)
+        .isEqualTo(
+          getReportRequest {
+            name =
+              ReportKey(CMMS_MEASUREMENT_CONSUMER_ID, INTERNAL_BASIC_REPORT.externalReportId).toName()
+          }
+        )
 
-    verify(basicReportsMock, times(0)).failBasicReport(any())
+      verify(basicReportsMock, times(0)).failBasicReport(any())
 
-    // TODO(world-federation-of-advertisers/cross-media-measurement#2607): Verify transformation of Report Results and persistence in Spanner
-  }
+      // TODO(world-federation-of-advertisers/cross-media-measurement#2607): Verify transformation of Report Results and persistence in Spanner
+    }
 
   @Test
   fun `execute only gets report when report for basic report is RUNNING`(): Unit = runBlocking {
