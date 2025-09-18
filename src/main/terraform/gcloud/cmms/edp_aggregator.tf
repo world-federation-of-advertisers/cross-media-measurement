0632f8155d158051bc85ad47e03fb73fe7cff435
marcopremier <marcopremier@meta.com>
2025-09-18 03:49:22 -0700

    refactor: temporary enable debug image

--- src/main/terraform/gcloud/cmms/edp_aggregator.tf
+++ src/main/terraform/gcloud/cmms/edp_aggregator.tf
@@ -198,6 +198,6 @@ module "edp_aggregator" {
   trusted_root_ca_collection                = local.trusted_root_ca_collection
   edps_certs                                = local.edps_certs
   cloud_function_configs                    = local.cloud_function_configs
-  results_fulfiller_disk_image_family       = "confidential-space"
+  results_fulfiller_disk_image_family       = "confidential-space-debug"
   private_network_location                  = data.google_client_config.default.region
 }
