6739e562137afd90fc6bb9c9494d13401d39a483
Lin <lindreamdeyi@google.com>
2025-09-19 06:16:28 +0000

    fix: fix error message

--- src/main/kotlin/org/wfanet/measurement/edpaggregator/service/v1alpha/ImpressionMetadataService.kt
+++ src/main/kotlin/org/wfanet/measurement/edpaggregator/service/v1alpha/ImpressionMetadataService.kt
@@ -67,7 +67,7 @@ class ImpressionMetadataService(
       } catch (e: StatusException) {
         throw when (InternalErrors.getReason(e)) {
           InternalErrors.Reason.IMPRESSION_METADATA_NOT_FOUND ->
-            ImpressionMetadataNotFoundException(request.name, e)
+            ImpressionMetadataNotFoundException(key.dataProviderId, key.impressionMetadataId, e)
               .asStatusRuntimeException(Status.Code.NOT_FOUND)
           InternalErrors.Reason.REQUISITION_METADATA_NOT_FOUND,
           InternalErrors.Reason.IMPRESSION_METADATA_ALREADY_EXISTS,
@@ -113,7 +113,7 @@ class ImpressionMetadataService(
       }
 
     ModelLineKey.fromName(request.impressionMetadata.modelLine)
-      ?: throw InvalidFieldValueException("model_line")
+      ?: throw InvalidFieldValueException("impression_metadata.model_line")
         .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
 
     val internalCreateRequest = internalCreateImpressionMetadataRequest {
@@ -165,7 +165,7 @@ class ImpressionMetadataService(
     } catch (e: StatusException) {
       throw when (InternalErrors.getReason(e)) {
         InternalErrors.Reason.IMPRESSION_METADATA_NOT_FOUND ->
-          ImpressionMetadataNotFoundException(request.name, e)
+          ImpressionMetadataNotFoundException(key.dataProviderId, key.impressionMetadataId, e)
             .asStatusRuntimeException(e.status.code)
         InternalErrors.Reason.IMPRESSION_METADATA_ALREADY_EXISTS,
         InternalErrors.Reason.REQUISITION_METADATA_NOT_FOUND,
