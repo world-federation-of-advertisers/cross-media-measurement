f220b31e87a16c5989c7c489239ef8bc2516234f
marcopremier <marcopremier@meta.com>
2025-09-19 06:08:45 -0700

    refactor: remove debug image

--- src/main/terraform/gcloud/cmms/edp_aggregator.tf
+++ src/main/terraform/gcloud/cmms/edp_aggregator.tf
@@ -198,6 +198,6 @@ module "edp_aggregator" {
   trusted_root_ca_collection                = local.trusted_root_ca_collection
   edps_certs                                = local.edps_certs
   cloud_function_configs                    = local.cloud_function_configs
-  results_fulfiller_disk_image_family       = "confidential-space-debug"
+  results_fulfiller_disk_image_family       = "confidential-space"
   dns_managed_zone_name                     = "googleapis-private"
 }
