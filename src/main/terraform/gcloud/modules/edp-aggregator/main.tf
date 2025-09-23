227a1ac5b2590d131f51c6b91abc7d35f5ef6da0
marcopremier <marcopremier@meta.com>
2025-09-23 02:05:33 -0700

    refactor: re-enable deletion protection for sql db

--- src/main/terraform/gcloud/modules/edp-aggregator/main.tf
+++ src/main/terraform/gcloud/modules/edp-aggregator/main.tf
@@ -367,7 +367,6 @@ resource "google_spanner_database" "edp_aggregator" {
   instance         = var.spanner_instance.name
   name             = var.spanner_database_name
   database_dialect = "GOOGLE_STANDARD_SQL"
-  deletion_protection = false
 }
 
 resource "google_spanner_database_iam_member" "edp_aggregator_internal" {
