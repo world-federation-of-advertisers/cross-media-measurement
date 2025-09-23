166ab84d2f05dd3a2e191e53fde87a947a7af5f1
Sanjay Vasandani <sanjay@sanjayvas.com>
2025-09-23 16:30:56 -0700

    Revert "feat!: add private network  (#2391)"
    This reverts commit 18e327887f5f23074a9913f1ca9c1301db8491ed.

--- src/main/terraform/gcloud/modules/mig/variables.tf
+++ src/main/terraform/gcloud/modules/mig/variables.tf
@@ -108,12 +108,6 @@ variable "config_storage_bucket" {
   type        = string
 }
 
-variable "subnetwork_name" {
-  description = "The name of the subnetwork for the MIG instances."
-  type        = string
-  nullable    = false
-}
-
 variable "edpa_tee_signed_image_repo" {
   description = "Trusted container image repository for Confidential Space attestation."
   type        = string
