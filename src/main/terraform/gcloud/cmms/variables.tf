c9d45207513b503fc7b847235de55bbc3cadbfdf
Sanjay Vasandani <sanjayvas@google.com>
2025-09-23 23:39:52 +0000

    Revert #2934
    This reverts 4c77effb3930c2bdae542b9cebfbdc7e2cb359b9

--- src/main/terraform/gcloud/cmms/variables.tf
+++ src/main/terraform/gcloud/cmms/variables.tf
@@ -165,12 +165,6 @@ variable "event_group_sync_function_name" {
   nullable    = false
 }
 
-variable "data_availability_sync_function_name" {
-  description = "Name of the DataAvailabilitySync function"
-  type        = string
-  nullable    = false
-}
-
 variable "data_watcher_env_var" {
   description = "DataWatcher extra env variables"
   type        = string
@@ -201,16 +195,6 @@ variable "event_group_secret_mapping" {
   type        = string
 }
 
-variable "data_availability_env_var" {
-  description = "DataAvailabilitySync extra env variables"
-  type        = string
-}
-
-variable "data_availability_secret_mapping" {
-  description = "DataAvailabilitySync secret mapping"
-  type        = string
-}
-
 variable "data_watcher_uber_jar_path" {
   description = "Path to DataWatcher uber jar."
   type = string
@@ -223,11 +207,6 @@ variable "requisition_fetcher_uber_jar_path" {
 
 variable "event_group_uber_jar_path" {
   description = "Path to EventGroupSync uber jar."
-  type = string
-}
-
-variable "data_availability_uber_jar_path" {
-  description = "Path to DataAvailability uber jar."
   type = string
 }
 
