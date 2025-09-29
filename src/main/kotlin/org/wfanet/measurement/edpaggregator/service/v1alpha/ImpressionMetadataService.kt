d7bceedddecfe9be9126d8578a856d13493dc468
Lin <lindreamdeyi@google.com>
2025-09-29 22:25:02 +0000

    feat: Implement system computeModelLineBounds (#2977)

--- src/main/kotlin/org/wfanet/measurement/edpaggregator/service/v1alpha/ImpressionMetadataService.kt
+++ src/main/kotlin/org/wfanet/measurement/edpaggregator/service/v1alpha/ImpressionMetadataService.kt
@@ -29,6 +29,9 @@ import org.wfanet.measurement.edpaggregator.service.ImpressionMetadataNotFoundEx
 import org.wfanet.measurement.edpaggregator.service.InvalidFieldValueException
 import org.wfanet.measurement.edpaggregator.service.RequiredFieldNotSetException
 import org.wfanet.measurement.edpaggregator.service.internal.Errors as InternalErrors
+import org.wfanet.measurement.edpaggregator.v1alpha.ComputeModelLineBoundsRequest
+import org.wfanet.measurement.edpaggregator.v1alpha.ComputeModelLineBoundsResponse
+import org.wfanet.measurement.edpaggregator.v1alpha.ComputeModelLineBoundsResponseKt.modelLineBoundMapEntry
 import org.wfanet.measurement.edpaggregator.v1alpha.CreateImpressionMetadataRequest
 import org.wfanet.measurement.edpaggregator.v1alpha.DeleteImpressionMetadataRequest
 import org.wfanet.measurement.edpaggregator.v1alpha.GetImpressionMetadataRequest
@@ -36,8 +39,10 @@ import org.wfanet.measurement.edpaggregator.v1alpha.ImpressionMetadata
 import org.wfanet.measurement.edpaggregator.v1alpha.ImpressionMetadataServiceGrpcKt.ImpressionMetadataServiceCoroutineImplBase
 import org.wfanet.measurement.edpaggregator.v1alpha.ListImpressionMetadataRequest
 import org.wfanet.measurement.edpaggregator.v1alpha.ListImpressionMetadataResponse
+import org.wfanet.measurement.edpaggregator.v1alpha.computeModelLineBoundsResponse
 import org.wfanet.measurement.edpaggregator.v1alpha.impressionMetadata
 import org.wfanet.measurement.edpaggregator.v1alpha.listImpressionMetadataResponse
+import org.wfanet.measurement.internal.edpaggregator.ComputeModelLineBoundsResponse as InternalComputeModelLineBoundsResponse
 import org.wfanet.measurement.internal.edpaggregator.ImpressionMetadata as InternalImpressionMetadata
 import org.wfanet.measurement.internal.edpaggregator.ImpressionMetadataServiceGrpcKt.ImpressionMetadataServiceCoroutineStub as InternalImpressionMetadataServiceCoroutineStub
 import org.wfanet.measurement.internal.edpaggregator.ImpressionMetadataState as InternalImpressionMetadataState
@@ -45,6 +50,7 @@ import org.wfanet.measurement.internal.edpaggregator.ListImpressionMetadataPageT
 import org.wfanet.measurement.internal.edpaggregator.ListImpressionMetadataRequest as InternalListImpressionMetadataRequest
 import org.wfanet.measurement.internal.edpaggregator.ListImpressionMetadataRequestKt.filter as internalListImpressionMetadataRequestFilter
 import org.wfanet.measurement.internal.edpaggregator.ListImpressionMetadataResponse as InternalListImpressionMetadataResponse
+import org.wfanet.measurement.internal.edpaggregator.computeModelLineBoundsRequest as internalComputeModelLineBoundsRequest
 import org.wfanet.measurement.internal.edpaggregator.createImpressionMetadataRequest as internalCreateImpressionMetadataRequest
 import org.wfanet.measurement.internal.edpaggregator.deleteImpressionMetadataRequest as internalDeleteImpressionMetadataRequest
 import org.wfanet.measurement.internal.edpaggregator.getImpressionMetadataRequest as internalGetImpressionMetadataRequest
@@ -285,6 +291,64 @@ class ImpressionMetadataService(
     }
   }
 
+  override suspend fun computeModelLineBounds(
+    request: ComputeModelLineBoundsRequest
+  ): ComputeModelLineBoundsResponse {
+    if (request.parent.isEmpty()) {
+      throw RequiredFieldNotSetException("parent")
+        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    }
+    val dataProviderKey =
+      DataProviderKey.fromName(request.parent)
+        ?: throw InvalidFieldValueException("parent")
+          .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+
+    if (request.modelLinesList.isEmpty()) {
+      throw RequiredFieldNotSetException("model_lines")
+        .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    }
+
+    request.modelLinesList.forEachIndexed { index, modelLine ->
+      ModelLineKey.fromName(modelLine)
+        ?: throw InvalidFieldValueException("model_lines.$index")
+          .asStatusRuntimeException(Status.Code.INVALID_ARGUMENT)
+    }
+
+    val internalResponse: InternalComputeModelLineBoundsResponse =
+      try {
+        internalImpressionMetadataStub.computeModelLineBounds(
+          internalComputeModelLineBoundsRequest {
+            dataProviderResourceId = dataProviderKey.dataProviderId
+            cmmsModelLine += request.modelLinesList
+          }
+        )
+      } catch (e: StatusException) {
+        throw when (InternalErrors.getReason(e)) {
+          InternalErrors.Reason.IMPRESSION_METADATA_NOT_FOUND,
+          InternalErrors.Reason.IMPRESSION_METADATA_ALREADY_EXISTS,
+          InternalErrors.Reason.IMPRESSION_METADATA_STATE_INVALID,
+          InternalErrors.Reason.REQUISITION_METADATA_NOT_FOUND,
+          InternalErrors.Reason.REQUISITION_METADATA_NOT_FOUND_BY_BLOB_URI,
+          InternalErrors.Reason.REQUISITION_METADATA_NOT_FOUND_BY_CMMS_REQUISITION,
+          InternalErrors.Reason.REQUISITION_METADATA_ALREADY_EXISTS,
+          InternalErrors.Reason.REQUISITION_METADATA_STATE_INVALID,
+          InternalErrors.Reason.REQUIRED_FIELD_NOT_SET,
+          InternalErrors.Reason.INVALID_FIELD_VALUE,
+          InternalErrors.Reason.ETAG_MISMATCH,
+          null -> Status.INTERNAL.withCause(e).asRuntimeException()
+        }
+      }
+
+    return computeModelLineBoundsResponse {
+      for ((key, value) in internalResponse.modelLineBoundsMap) {
+        modelLineBounds += modelLineBoundMapEntry {
+          this.key = key
+          this.value = value
+        }
+      }
+    }
+  }
+
   companion object {
     private const val DEFAULT_PAGE_SIZE = 50
     private const val MAX_PAGE_SIZE = 100
@@ -342,5 +406,19 @@ internal fun InternalImpressionMetadataState.toState(): ImpressionMetadata.State
     InternalImpressionMetadataState.UNRECOGNIZED,
     InternalImpressionMetadataState.IMPRESSION_METADATA_STATE_UNSPECIFIED ->
       error("Unrecognized state")
+  }
+}
+
+/**
+ * Converts a public [ImpressionMetadata.State] to an internal [InternalImpressionMetadataState].
+ */
+internal fun ImpressionMetadata.State.toInternal(): InternalImpressionMetadataState {
+  return when (this) {
+    ImpressionMetadata.State.ACTIVE ->
+      InternalImpressionMetadataState.IMPRESSION_METADATA_STATE_ACTIVE
+    ImpressionMetadata.State.DELETED ->
+      InternalImpressionMetadataState.IMPRESSION_METADATA_STATE_DELETED
+    ImpressionMetadata.State.UNRECOGNIZED,
+    ImpressionMetadata.State.STATE_UNSPECIFIED -> error("Unrecognized state")
   }
 }
