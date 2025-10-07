6e7c20f96604d03266353c4fbf072a3d0d1f7e0e
Tristan Vuong <tristanvuong@google.com>
2025-10-07 19:29:45 +0000

    Add skeleton

---
+++ src/main/kotlin/org/wfanet/measurement/reporting/deploy/v2/gcloud/spanner/db/BasicReportNoiseCorrectedResultsTransformation.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2025 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.reporting.deploy.v2.gcloud.spanner.db
+
+import org.wfanet.measurement.internal.reporting.v2.BasicReport
+import org.wfanet.measurement.internal.reporting.v2.ReportResult
+import org.wfanet.measurement.internal.reporting.v2.ResultGroup
+
+fun buildResultGroups(basicReport: BasicReport, reportResult: ReportResult): List<ResultGroup> {
+  return buildList {
+
+  }
+}
