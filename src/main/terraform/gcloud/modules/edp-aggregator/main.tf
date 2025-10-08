2a071b6954cde912c6bd870cd0e5f98e9f766b90
marcopremier <marcopremier@meta.com>
2025-10-08 08:26:33 -0700

    refactor: remove terraform changes

--- src/main/terraform/gcloud/modules/edp-aggregator/main.tf
+++ src/main/terraform/gcloud/modules/edp-aggregator/main.tf
@@ -38,10 +38,6 @@ locals {
       secret_id  = var.trusted_root_ca_collection.secret_id
       version    = "latest"
     },
-    {
-      secret_id  = var.metadata_storage_root_ca.secret_id
-      version    = "latest"
-    },
   ]
 
   edp_secrets_to_access = flatten([
@@ -86,13 +82,11 @@ locals {
     { data_watcher_tls_key                          = var.data_watcher_tls_key },
     { data_watcher_tls_pem                          = var.data_watcher_tls_pem },
     { secure_computation_root_ca                    = var.secure_computation_root_ca },
-    { metadata_storage_root_ca                      = var.metadata_storage_root_ca },
     { trusted_root_ca_collection                    = var.trusted_root_ca_collection },
     local.edps_secrets
   )
 
   data_watcher_secrets_access = [
-    "metadata_storage_root_ca",
     "secure_computation_root_ca",
     "data_watcher_tls_key",
     "data_watcher_tls_pem",
