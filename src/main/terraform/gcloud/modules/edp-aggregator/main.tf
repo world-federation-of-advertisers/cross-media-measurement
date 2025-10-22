91569abe53852ac543308b4d1ae97208e58e0d57
marcopremier <marcopremier@meta.com>
2025-10-22 04:06:57 -0700

    fix: var project id

--- src/main/terraform/gcloud/modules/edp-aggregator/main.tf
+++ src/main/terraform/gcloud/modules/edp-aggregator/main.tf
@@ -157,7 +157,6 @@ locals {
     "event_group_sync"          = module.event_group_sync_cloud_function.cloud_function_service_account.email
     "data_availability_sync"    = module.data_availability_sync_cloud_function.cloud_function_service_account.email
   }
-
 }
 
 module "edp_aggregator_bucket" {
@@ -500,21 +499,21 @@ resource "google_compute_address" "edp_aggregator_api_server" {
 
 resource "google_project_iam_member" "telemetry_log_writer" {
   for_each = local.service_accounts
-  project  = var.project_id
+  project  = data.google_project.project.project_id
   role     = "roles/logging.logWriter"
   member   = "serviceAccount:${each.value}"
 }
 
 resource "google_project_iam_member" "telemetry_metric_writer" {
   for_each = local.service_accounts
-  project  = var.project_id
+  project  = data.google_project.project.project_id
   role     = "roles/monitoring.metricWriter"
   member   = "serviceAccount:${each.value}"
 }
 
 resource "google_project_iam_member" "telemetry_trace_agent" {
   for_each = local.service_accounts
-  project  = var.project_id
+  project  = data.google_project.project.project_id
   role     = "roles/cloudtrace.agent"
   member   = "serviceAccount:${each.value}"
 }
