e50a0aa0027ac20149a49a92d2d3d7e09dfaf5ff
Tristan Vuong <85768771+tristanvuong2021@users.noreply.github.com>
2025-11-19 01:29:04 -0800

    Update add-model-line-boolean-to-basic-report.sql
    Update comment to increment change number

--- src/main/resources/reporting/spanner/add-model-line-boolean-to-basic-report.sql
+++ src/main/resources/reporting/spanner/add-model-line-boolean-to-basic-report.sql
@@ -14,7 +14,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
--- changeset tristanvuong2021:11 dbms:cloudspanner
+-- changeset tristanvuong2021:12 dbms:cloudspanner
 -- comment: Add ModelLine Boolean to BasicReport
 
 ALTER TABLE BasicReports
