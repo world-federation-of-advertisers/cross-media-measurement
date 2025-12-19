25c57d8ef68d745945d711a81b00f2302412ad66
Lin <lindreamdeyi@google.com>
2025-12-19 00:03:19 +0000

    fix: Improve comments and naming

--- src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/SpannerEventGroupActivitiesService.kt
+++ src/main/kotlin/org/wfanet/measurement/kingdom/deploy/gcloud/spanner/SpannerEventGroupActivitiesService.kt
@@ -14,16 +14,13 @@
 
 package org.wfanet.measurement.kingdom.deploy.gcloud.spanner
 
-import com.google.protobuf.Empty
 import io.grpc.Status
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 import org.wfanet.measurement.common.identity.IdGenerator
 import org.wfanet.measurement.gcloud.spanner.AsyncDatabaseClient
-import org.wfanet.measurement.internal.kingdom.BatchDeleteEventGroupActivitiesRequest
 import org.wfanet.measurement.internal.kingdom.BatchUpdateEventGroupActivitiesRequest
 import org.wfanet.measurement.internal.kingdom.BatchUpdateEventGroupActivitiesResponse
-import org.wfanet.measurement.internal.kingdom.DeleteEventGroupActivityRequest
 import org.wfanet.measurement.internal.kingdom.EventGroupActivitiesGrpcKt.EventGroupActivitiesCoroutineImplBase
 import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.common.DataProviderNotFoundException
 import org.wfanet.measurement.kingdom.deploy.gcloud.spanner.common.EventGroupNotFoundException
@@ -96,15 +93,5 @@ class SpannerEventGroupActivitiesService(
     } catch (e: KingdomInternalException) {
       throw e.asStatusRuntimeException(Status.Code.INTERNAL, "Unexpected internal error.")
     }
-  }
-
-  override suspend fun deleteEventGroupActivity(request: DeleteEventGroupActivityRequest): Empty {
-    return super.deleteEventGroupActivity(request)
-  }
-
-  override suspend fun batchDeleteEventGroupActivities(
-    request: BatchDeleteEventGroupActivitiesRequest
-  ): Empty {
-    return super.batchDeleteEventGroupActivities(request)
   }
 }
