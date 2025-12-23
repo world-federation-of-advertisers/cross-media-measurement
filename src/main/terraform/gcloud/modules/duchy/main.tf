e418dab33afd59cff07a03346c03ef2b40e6d764
renjiez <renjiez@google.com>
2025-12-23 00:03:42 +0000

    Bring back trustee mill again

--- src/main/terraform/gcloud/modules/duchy/main.tf
+++ src/main/terraform/gcloud/modules/duchy/main.tf
@@ -107,13 +107,13 @@ resource "google_storage_bucket_iam_member" "storage" {
   member = module.storage_user.iam_service_account.member
 }
 
-# resource "google_storage_bucket_iam_member" "trustee_mill" {
-#   count  = var.trustee_config != null ? 1 : 0
-#
-#   bucket = var.storage_bucket.name
-#   role   = "roles/storage.objectAdmin"
-#   member = module.trustee_mill[0].mig_service_account.member
-# }
+resource "google_storage_bucket_iam_member" "trustee_mill" {
+  count  = var.trustee_config != null ? 1 : 0
+
+  bucket = var.storage_bucket.name
+  role   = "roles/storage.objectAdmin"
+  member = module.trustee_mill[0].mig_service_account.member
+}
 
 
 resource "google_compute_address" "v2alpha" {
@@ -140,78 +140,78 @@ resource "google_monitoring_dashboard" "dashboards" {
   })
 }
 
-# resource "google_compute_subnetwork" "trustee_mill_subnetwork" {
-#   count = var.trustee_config != null ? 1 : 0
-#
-#   name          = "${var.name}-trustee-mill-compute-subnetwork"
-#   region        = data.google_client_config.default.region
-#   network       = var.trustee_mill_subnetwork_network
-#   ip_cidr_range = var.trustee_mill_subnetwork_cidr_range
-#   private_ip_google_access = true
-# }
-#
-# # Cloud Router for NAT gateway
-# resource "google_compute_router" "trustee_mill_router" {
-#   count   = var.trustee_config != null ? 1 : 0
-#
-#   name    = "${var.name}-trustee-mill-compute-router"
-#   region  = data.google_client_config.default.region
-#   network = var.trustee_mill_subnetwork_network
-# }
-#
-# # Cloud NAT configuration
-# resource "google_compute_router_nat" "trustee_mill_nat" {
-#   count = var.trustee_config != null ? 1 : 0
-#
-#   name                               = "${var.name}-trustee-mill-compute-router-nat"
-#   router                             = google_compute_router.trustee_mill_router[0].name
-#   region                             = google_compute_router.trustee_mill_router[0].region
-#   nat_ip_allocate_option             = "AUTO_ONLY"
-#   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
-#
-#   subnetwork {
-#     name                    = google_compute_subnetwork.trustee_mill_subnetwork[0].self_link
-#     source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
-#   }
-#
-#   enable_endpoint_independent_mapping = true
-#
-#   log_config {
-#     enable = true
-#     filter = "ERRORS_ONLY"
-#   }
-# }
-#
-# module "trustee_mill" {
-#   count = var.trustee_config != null ? 1 : 0
-#
-#   source   = "../mig"
-#
-#   depends_on = [module.secrets]
-#
-#   instance_template_name        = var.trustee_config.instance_template_name
-#   base_instance_name            = var.trustee_config.base_instance_name
-#   managed_instance_group_name   = var.trustee_config.managed_instance_group_name
-#   mig_service_account_name      = var.trustee_config.mig_service_account_name
-#   min_replicas                  = var.trustee_config.replicas
-#   max_replicas                  = var.trustee_config.replicas
-#   machine_type                  = var.trustee_config.machine_type
-#   docker_image                  = var.trustee_config.docker_image
-#   tee_signed_image_repo         = var.trustee_config.signed_image_repo
-#   mig_distribution_policy_zones = var.trustee_config.mig_distribution_policy_zones
-#   terraform_service_account     = var.trustee_config.terraform_service_account
-#   disk_image_family             = var.trustee_config.disk_image_family
-#   tee_cmd                       = var.trustee_config.app_flags
-#   secrets_to_access             = local.trustee_secrets_to_access
-#   subnetwork_name               = google_compute_subnetwork.trustee_mill_subnetwork[0].name
-# }
-#
-# module "secrets" {
-#   source            = "../secret"
-#
-#   for_each          = local.all_secrets
-#   secret_id         = each.value.secret_id
-#   secret_path       = each.value.secret_local_path
-#   is_binary_format  = each.value.is_binary_format
-# }
-#
+resource "google_compute_subnetwork" "trustee_mill_subnetwork" {
+  count = var.trustee_config != null ? 1 : 0
+
+  name          = "${var.name}-trustee-mill-compute-subnetwork"
+  region        = data.google_client_config.default.region
+  network       = var.trustee_mill_subnetwork_network
+  ip_cidr_range = var.trustee_mill_subnetwork_cidr_range
+  private_ip_google_access = true
+}
+
+# Cloud Router for NAT gateway
+resource "google_compute_router" "trustee_mill_router" {
+  count   = var.trustee_config != null ? 1 : 0
+
+  name    = "${var.name}-trustee-mill-compute-router"
+  region  = data.google_client_config.default.region
+  network = var.trustee_mill_subnetwork_network
+}
+
+# Cloud NAT configuration
+resource "google_compute_router_nat" "trustee_mill_nat" {
+  count = var.trustee_config != null ? 1 : 0
+
+  name                               = "${var.name}-trustee-mill-compute-router-nat"
+  router                             = google_compute_router.trustee_mill_router[0].name
+  region                             = google_compute_router.trustee_mill_router[0].region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.trustee_mill_subnetwork[0].self_link
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  enable_endpoint_independent_mapping = true
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
+
+module "trustee_mill" {
+  count = var.trustee_config != null ? 1 : 0
+
+  source   = "../mig"
+
+  depends_on = [module.secrets]
+
+  instance_template_name        = var.trustee_config.instance_template_name
+  base_instance_name            = var.trustee_config.base_instance_name
+  managed_instance_group_name   = var.trustee_config.managed_instance_group_name
+  mig_service_account_name      = var.trustee_config.mig_service_account_name
+  min_replicas                  = var.trustee_config.replicas
+  max_replicas                  = var.trustee_config.replicas
+  machine_type                  = var.trustee_config.machine_type
+  docker_image                  = var.trustee_config.docker_image
+  tee_signed_image_repo         = var.trustee_config.signed_image_repo
+  mig_distribution_policy_zones = var.trustee_config.mig_distribution_policy_zones
+  terraform_service_account     = var.trustee_config.terraform_service_account
+  disk_image_family             = var.trustee_config.disk_image_family
+  tee_cmd                       = var.trustee_config.app_flags
+  secrets_to_access             = local.trustee_secrets_to_access
+  subnetwork_name               = google_compute_subnetwork.trustee_mill_subnetwork[0].name
+}
+
+module "secrets" {
+  source            = "../secret"
+
+  for_each          = local.all_secrets
+  secret_id         = each.value.secret_id
+  secret_path       = each.value.secret_local_path
+  is_binary_format  = each.value.is_binary_format
+}
+
