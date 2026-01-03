6016e9d49a8a4bed5ce3402065100f5bdec2f77d
Tristan Vuong <tristanvuong@google.com>
2026-01-03 17:15:43 +0000

    fix build

--- src/main/go/reporting/grpcgateway.go
+++ src/main/go/reporting/grpcgateway.go
@@ -25,6 +25,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
 
 	"github.com/world-federation-of-advertisers/cross-media-measurement/cmms/apiv2alpha/cmmspb"
