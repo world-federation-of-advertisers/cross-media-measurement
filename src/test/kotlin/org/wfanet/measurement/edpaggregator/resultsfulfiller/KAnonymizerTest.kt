481e6b5341b7208880f463e000b18507b1f9f97a
dawn-wang22 <mpd0622@yahoo.com>
2026-01-20 19:37:56 +0000

    refactor: Move KAnonymizer to edpaggregator.resultsfulfiller namespace
    Address PR review comment to avoid computation depending on requisition
    package. This moves KAnonymizer from computation namespace to
    edpaggregator.resultsfulfiller namespace.
    
    Changes:
    - Move KAnonymizer.kt to resultsfulfiller package
    - Move KAnonymizerTest.kt to resultsfulfiller test package
    - Update BUILD files and dependencies
    - Remove computation visibility from requisition/v2alpha/common BUILD
    - Update imports in HMShuffleMeasurementFulfiller and TrusTeeMeasurementFulfiller

--- src/test/kotlin/org/wfanet/measurement/edpaggregator/resultsfulfiller/KAnonymizerTest.kt
+++ src/test/kotlin/org/wfanet/measurement/edpaggregator/resultsfulfiller/KAnonymizerTest.kt
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.wfanet.measurement.computation
+package org.wfanet.measurement.edpaggregator.resultsfulfiller
 
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
@@ -23,6 +23,7 @@ import org.wfanet.measurement.api.v2alpha.PopulationSpecKt
 import org.wfanet.measurement.api.v2alpha.measurementSpec
 import org.wfanet.measurement.api.v2alpha.populationSpec
 import org.wfanet.measurement.eventdataprovider.requisition.v2alpha.common.FrequencyVectorBuilder
+import org.wfanet.measurement.computation.KAnonymityParams
 
 @RunWith(JUnit4::class)
 class KAnonymizerTest {
