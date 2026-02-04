b1bb31e2a98bfd8a10bce2fc27f094bd99f44889
dawn-wang22 <mpd0622@yahoo.com>
2026-02-04 23:46:31 +0000

    Format code with ktfmt

--- src/main/kotlin/org/wfanet/measurement/integration/common/InProcessEdpAggregatorComponents.kt
+++ src/main/kotlin/org/wfanet/measurement/integration/common/InProcessEdpAggregatorComponents.kt
@@ -206,7 +206,8 @@ class InProcessEdpAggregatorComponents(
       getImpressionsStorageConfig = getStorageConfig,
       getRequisitionsStorageConfig = getStorageConfig,
       trusTeeConfigs =
-        emptyMap(), // TODO(world-federation-of-advertisers/cross-media-measurement#3394): Test TrusTee protocol in integration tests.
+        emptyMap(), // TODO(world-federation-of-advertisers/cross-media-measurement#3394): Test
+                    // TrusTee protocol in integration tests.
       modelLineInfoMap = modelLineInfoMap,
       metrics = ResultsFulfillerMetrics.create(),
     )
