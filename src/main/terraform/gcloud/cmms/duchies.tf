d77c0a08c5877556a9dba246e8ac2c727818b2b5
Sanjay Vasandani <sanjayvas@google.com>
2026-02-13 01:18:51 +0000

    Trim cmms Terraform module to minimal configuration to restore dev

--- src/main/terraform/gcloud/cmms/duchies.tf
+++ src/main/terraform/gcloud/cmms/duchies.tf
@@ -42,56 +42,6 @@ locals {
     secret_local_path = abspath("${path.root}/../../../k8s/testing/secretfiles/aggregator_cs_private.der"),
     is_binary_format  = true
   }
-
-  aggregator_trustee_config = {
-    instance_template_name        = "trustee-mill-template"
-    base_instance_name            = "trustee-mill"
-    managed_instance_group_name   = "trustee-mill-mig"
-    mig_service_account_name      = "trustee-mill-mig-sa"
-    replicas                      = 1
-    machine_type                  = "c4d-standard-2"
-    docker_image                  = "ghcr.io/world-federation-of-advertisers/duchy/trus-tee-mill:${var.image_tag}"
-    signed_image_repo             = "ghcr.io/world-federation-of-advertisers/duchy/trus-tee-mill"
-    mig_distribution_policy_zones = ["us-central1-a"]
-    disk_image_family             = "confidential-space"
-
-    aggregator_tls_cert        = local.aggregator_tls_cert
-    aggregator_tls_key         = local.aggregator_tls_key
-    aggregator_cert_collection = local.aggregator_cert_collection
-    aggregator_cs_cert         = local.aggregator_cs_cert
-    aggregator_cs_private      = local.aggregator_cs_private
-    terraform_service_account  = var.terraform_service_account
-
-    app_flags = [
-      "--google-project-id", data.google_client_config.default.project,
-      "--tls-cert-secret-id", local.aggregator_tls_cert.secret_id,
-      "--tls-key-secret-id", local.aggregator_tls_key.secret_id,
-      "--cert-collection-secret-id", local.aggregator_cert_collection.secret_id,
-      "--cs-cert-secret-id", local.aggregator_cs_cert.secret_id,
-      "--cs-private-key-secret-id", local.aggregator_cs_private.secret_id,
-
-      "--tls-cert-file", "/tmp/secrets/aggregator_tls.pem",
-      "--tls-key-file", "/tmp/secrets/aggregator_tls.key",
-      "--cert-collection-file", "/tmp/secrets/all_root_certs.pem",
-      "--consent-signaling-certificate-der-file", "/tmp/secrets/aggregator_cs_cert.der",
-      "--consent-signaling-private-key-der-file", "/tmp/secrets/aggregator_cs_private.der",
-      "--attestation-token-file", "/run/container_launcher/attestation_verifier_claims_token",
-
-      "--computations-service-target", "${var.duchy_aggregator_computations_service_target}",
-      "--computations-service-cert-host", "localhost",
-      "--kingdom-system-api-target", "${var.kingdom_system_api_target}",
-      "--kingdom-system-api-cert-host", "localhost",
-
-      "--duchy-name", "aggregator",
-      "--work-lock-duration", "10m",
-      "--polling-interval", "5s",
-
-      "--consent-signaling-certificate-resource-name", "duchies/aggregator/certificates/${var.duchy_aggregator_cert_id}",
-
-      "--google-cloud-storage-project", data.google_client_config.default.project,
-      "--google-cloud-storage-bucket", "${var.storage_bucket_name}",
-    ]
-  }
 }
 
 module "clusters" {
@@ -145,9 +95,6 @@ module "aggregator_duchy" {
   database_name    = "aggregator_duchy_computations"
   spanner_instance = google_spanner_instance.spanner_instance
   storage_bucket   = module.storage.storage_bucket
-
-  # TrusTEE MIG configurations
-  trustee_config = local.aggregator_trustee_config
 }
 
 module "worker1_duchy" {
