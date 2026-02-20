bd9e025622f8fac349d285fb13b5eb071bffc12f
jojijac0b <jojijacob@bazel-server.us-east1-b.c.xmm-test-ppp9j.internal>
2026-02-20 15:16:23 +0000

    Rename spanner table. Remove FileIndex field. Add UploadDate field. Update indexes

--- src/main/resources/edpaggregator/spanner/create-raw-impression-metadata-schema.sql
+++ src/main/resources/edpaggregator/spanner/create-raw-impression-metadata-schema.sql
@@ -19,7 +19,11 @@
 
 -- Set protobuf FileDescriptorSet as a base64 string.
 SET PROTO_DESCRIPTORS =
+<<<<<<< HEAD
 'Ct0CCkZ3ZmEvbWVhc3VyZW1lbnQvaW50ZXJuYWwvZWRwYWdncmVnYXRvci9pbXByZXNzaW9uX21ldGFkYXRhX3N0YXRlLnByb3RvEiZ3ZmEubWVhc3VyZW1lbnQuaW50ZXJuYWwuZWRwYWdncmVnYXRvciqRAQoXSW1wcmVzc2lvbk1ldGFkYXRhU3RhdGUSKQolSU1QUkVTU0lPTl9NRVRBREFUQV9TVEFURV9VTlNQRUNJRklFRBAAEiQKIElNUFJFU1NJT05fTUVUQURBVEFfU1RBVEVfQUNUSVZFEAESJQohSU1QUkVTU0lPTl9NRVRBREFUQV9TVEFURV9ERUxFVEVEEAJCTwotb3JnLndmYW5ldC5tZWFzdXJlbWVudC5pbnRlcm5hbC5lZHBhZ2dyZWdhdG9yQhxJbXByZXNzaW9uTWV0YWRhdGFTdGF0ZVByb3RvUAFiBnByb3RvMwqJBApHd2ZhL21lYXN1cmVtZW50L2ludGVybmFsL2VkcGFnZ3JlZ2F0b3IvcmVxdWlzaXRpb25fbWV0YWRhdGFfc3RhdGUucHJvdG8SJndmYS5tZWFzdXJlbWVudC5pbnRlcm5hbC5lZHBhZ2dyZWdhdG9yKrsCChhSZXF1aXNpdGlvbk1ldGFkYXRhU3RhdGUSKgomUkVRVUlTSVRJT05fTUVUQURBVEFfU1RBVEVfVU5TUEVDSUZJRUQQABIlCiFSRVFVSVNJVElPTl9NRVRBREFUQV9TVEFURV9TVE9SRUQQARIlCiFSRVFVSVNJVElPTl9NRVRBREFUQV9TVEFURV9RVUVVRUQQAhIpCiVSRVFVSVNJVElPTl9NRVRBREFUQV9TVEFURV9QUk9DRVNTSU5HEAMSKAokUkVRVUlTSVRJT05fTUVUQURBVEFfU1RBVEVfRlVMRklMTEVEEAQSJgoiUkVRVUlTSVRJT05fTUVUQURBVEFfU1RBVEVfUkVGVVNFRBAFEigKJFJFUVVJU0lUSU9OX01FVEFEQVRBX1NUQVRFX1dJVEhEUkFXThAGQlAKLW9yZy53ZmFuZXQubWVhc3VyZW1lbnQuaW50ZXJuYWwuZWRwYWdncmVnYXRvckIdUmVxdWlzaXRpb25NZXRhZGF0YVN0YXRlUHJvdG9QAWIGcHJvdG8zCosDCkd3ZmEvbWVhc3VyZW1lbnQvaW50ZXJuYWwvZWRwYWdncmVnYXRvci9yYXdfaW1wcmVzc2lvbl9iYXRjaF9zdGF0ZS5wcm90bxImd2ZhLm1lYXN1cmVtZW50LmludGVybmFsLmVkcGFnZ3JlZ2F0b3IqvgEKF1Jhd0ltcHJlc3Npb25CYXRjaFN0YXRlEioKJlJBV19JTVBSRVNTSU9OX0JBVENIX1NUQVRFX1VOU1BFQ0lGSUVEEAASJgoiUkFXX0lNUFJFU1NJT05fQkFUQ0hfU1RBVEVfQ1JFQVRFRBABEigKJFJBV19JTVBSRVNTSU9OX0JBVENIX1NUQVRFX1BST0NFU1NFRBACEiUKIVJBV19JTVBSRVNTSU9OX0JBVENIX1NUQVRFX0ZBSUxFRBADQk8KLW9yZy53ZmFuZXQubWVhc3VyZW1lbnQuaW50ZXJuYWwuZWRwYWdncmVnYXRvckIcUmF3SW1wcmVzc2lvbkJhdGNoU3RhdGVQcm90b1ABYgZwcm90bzMK4QIKQXdmYS9tZWFzdXJlbWVudC9pbnRlcm5hbC9lZHBhZ2dyZWdhdG9yL3Jhd19pbXByZXNzaW9uX3N0YXRlLnByb3RvEiZ3ZmEubWVhc3VyZW1lbnQuaW50ZXJuYWwuZWRwYWdncmVnYXRvciqfAQoSUmF3SW1wcmVzc2lvblN0YXRlEiQKIFJBV19JTVBSRVNTSU9OX1NUQVRFX1VOU1BFQ0lGSUVEEAASHwobUkFXX0lNUFJFU1NJT05fU1RBVEVfQUNUSVZFEAESIAocUkFXX0lNUFJFU1NJT05fU1RBVEVfREVMRVRFRBACEiAKHFJBV19JTVBSRVNTSU9OX1NUQVRFX0lOVkFMSUQQA0JKCi1vcmcud2ZhbmV0Lm1lYXN1cmVtZW50LmludGVybmFsLmVkcGFnZ3JlZ2F0b3JCF1Jhd0ltcHJlc3Npb25TdGF0ZVByb3RvUAFiBnByb3RvMw==';
+=======
+'CosDCkd3ZmEvbWVhc3VyZW1lbnQvaW50ZXJuYWwvZWRwYWdncmVnYXRvci9yYXdfaW1wcmVzc2lvbl9iYXRjaF9zdGF0ZS5wcm90bxImd2ZhLm1lYXN1cmVtZW50LmludGVybmFsLmVkcGFnZ3JlZ2F0b3IqvgEKF1Jhd0ltcHJlc3Npb25CYXRjaFN0YXRlEioKJlJBV19JTVBSRVNTSU9OX0JBVENIX1NUQVRFX1VOU1BFQ0lGSUVEEAASJgoiUkFXX0lNUFJFU1NJT05fQkFUQ0hfU1RBVEVfQ1JFQVRFRBABEigKJFJBV19JTVBSRVNTSU9OX0JBVENIX1NUQVRFX1BST0NFU1NFRBACEiUKIVJBV19JTVBSRVNTSU9OX0JBVENIX1NUQVRFX0ZBSUxFRBADQk8KLW9yZy53ZmFuZXQubWVhc3VyZW1lbnQuaW50ZXJuYWwuZWRwYWdncmVnYXRvckIcUmF3SW1wcmVzc2lvbkJhdGNoU3RhdGVQcm90b1ABYgZwcm90bzMKvgIKQXdmYS9tZWFzdXJlbWVudC9pbnRlcm5hbC9lZHBhZ2dyZWdhdG9yL3Jhd19pbXByZXNzaW9uX3N0YXRlLnByb3RvEiZ3ZmEubWVhc3VyZW1lbnQuaW50ZXJuYWwuZWRwYWdncmVnYXRvcip9ChJSYXdJbXByZXNzaW9uU3RhdGUSJAogUkFXX0lNUFJFU1NJT05fU1RBVEVfVU5TUEVDSUZJRUQQABIfChtSQVdfSU1QUkVTU0lPTl9TVEFURV9BQ1RJVkUQARIgChxSQVdfSU1QUkVTU0lPTl9TVEFURV9ERUxFVEVEEAJCSgotb3JnLndmYW5ldC5tZWFzdXJlbWVudC5pbnRlcm5hbC5lZHBhZ2dyZWdhdG9yQhdSYXdJbXByZXNzaW9uU3RhdGVQcm90b1ABYgZwcm90bzM=';
+>>>>>>> c136a5bc4 (Rename spanner table. Remove FileIndex field. Add UploadDate field. Update indexes)
 
 START BATCH DDL;
 
@@ -30,6 +34,7 @@ ALTER PROTO BUNDLE INSERT (
 
 -- Tracks raw impression files grouped into batches for VID labeling.
 -- Hierarchy: DataProvider -> Upload -> Batch -> File
+<<<<<<< HEAD
 -- Resource pattern: dataProviders/{data_provider}/rawImpressionUploads/{raw_impression_upload}/batches/{batch}/files/{file}
 CREATE TABLE RawImpressionMetadata (
   -- External API resource ID of the DataProvider that owns this file.
@@ -52,6 +57,18 @@ CREATE TABLE RawImpressionMetadata (
   -- sessions, as well as for partitioning and soft delete functionality.
   UploadDate DATE NOT NULL,
   -- The URI of the raw impressions blob. Used for actual blob lookups.
+=======
+CREATE TABLE RawImpressionMetadata (
+  -- The globally unique resource ID of the DataProvider that owns this file.
+  DataProviderResourceId STRING(63) NOT NULL,
+  -- Identifier for the upload session that produced this file.
+  UploadId STRING(63) NOT NULL,
+  -- Zero-based index of the batch within the upload.
+  BatchIndex INT64 NOT NULL,
+  -- The date this upload covers.
+  UploadDate DATE NOT NULL,
+  -- The URI of the raw impressions blob.
+>>>>>>> c136a5bc4 (Rename spanner table. Remove FileIndex field. Add UploadDate field. Update indexes)
   BlobUri STRING(MAX) NOT NULL,
   -- The processing state of the batch containing this file.
   BatchState `wfa.measurement.internal.edpaggregator.RawImpressionBatchState` NOT NULL,
@@ -61,6 +78,7 @@ CREATE TABLE RawImpressionMetadata (
   CreateTime TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp=true),
   -- The time this record was last updated in this database.
   UpdateTime TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp=true),
+<<<<<<< HEAD
 ) PRIMARY KEY (DataProviderResourceId, UploadId, BatchIndex, FileId);
 
 -- Supports looking up all file rows for a given external upload resource ID.
@@ -71,6 +89,9 @@ CREATE INDEX RawImpressionMetadataByUploadResourceId
 -- looking up a file row by its external resource ID.
 CREATE UNIQUE INDEX RawImpressionMetadataByFileResourceId
   ON RawImpressionMetadata(DataProviderResourceId, UploadResourceId, BatchIndex, FileResourceId);
+=======
+) PRIMARY KEY (DataProviderResourceId, UploadId, BatchIndex);
+>>>>>>> c136a5bc4 (Rename spanner table. Remove FileIndex field. Add UploadDate field. Update indexes)
 
 -- Enforces uniqueness of BlobUri per DataProvider and enables idempotency
 -- checks before inserting a new file record.
@@ -82,4 +103,8 @@ CREATE UNIQUE INDEX RawImpressionMetadataByBlobUri
 CREATE INDEX RawImpressionMetadataByUploadDateAndState
   ON RawImpressionMetadata(DataProviderResourceId, UploadDate, State);
 
+<<<<<<< HEAD
 RUN BATCH;
+=======
+RUN BATCH;
+>>>>>>> c136a5bc4 (Rename spanner table. Remove FileIndex field. Add UploadDate field. Update indexes)
