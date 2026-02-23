9eb183d11095e51847e83ba30a7483fc6464692e
Sanjay Vasandani <sanjayvas@google.com>
2026-02-23 11:26:30 -0800

    fix: Only use documented attributes of google_project TF data source (#3567)
    `project_id` and `name` are documented as arguments on the resource but not as attributes. Therefore they are not guaranteed to be available on the data source.

--- src/main/terraform/gcloud/modules/mig/main.tf
+++ src/main/terraform/gcloud/modules/mig/main.tf
@@ -18,13 +18,13 @@ locals {
 
   metadata_map = merge(
     {
-      "tee-signed-image-repos"                        = var.tee_signed_image_repo
-      "tee-image-reference"                           = var.docker_image
-      "tee-cmd"                                       = jsonencode(var.tee_cmd)
+      "tee-signed-image-repos" = var.tee_signed_image_repo
+      "tee-image-reference"    = var.docker_image
+      "tee-cmd"                = jsonencode(var.tee_cmd)
 
-      "google-logging-enabled"                        = "true"
-      "google-monitoring-enabled"                     = "true"
-      "tee-container-log-redirect"                    = "true"
+      "google-logging-enabled"     = "true"
+      "google-monitoring-enabled"  = "true"
+      "tee-container-log-redirect" = "true"
     },
     var.config_storage_bucket == null ? {} : {
       "tee-env-EDPA_CONFIG_STORAGE_BUCKET" = "gs://${var.config_storage_bucket}"
@@ -51,9 +51,9 @@ resource "google_service_account_iam_member" "allow_terraform_to_use_mig_sa" {
 resource "google_pubsub_subscription_iam_member" "mig_subscriber" {
   count = var.subscription_id != null ? 1 : 0
 
-  subscription  = var.subscription_id
-  role          = "roles/pubsub.subscriber"
-  member        = "serviceAccount:${google_service_account.mig_service_account.email}"
+  subscription = var.subscription_id
+  role         = "roles/pubsub.subscriber"
+  member       = "serviceAccount:${google_service_account.mig_service_account.email}"
 }
 
 resource "google_secret_manager_secret_iam_member" "mig_sa_secret_accessor" {
@@ -65,31 +65,31 @@ resource "google_secret_manager_secret_iam_member" "mig_sa_secret_accessor" {
 }
 
 resource "google_project_iam_member" "mig_sa_user" {
-  project = data.google_project.project.name
+  project = data.google_project.project.id
   role    = "roles/iam.serviceAccountUser"
   member  = "serviceAccount:${google_service_account.mig_service_account.email}"
 }
 
 resource "google_project_iam_member" "confidential_workload_user" {
-  project  = data.google_project.project.name
-  role     = "roles/confidentialcomputing.workloadUser"
-  member   = "serviceAccount:${google_service_account.mig_service_account.email}"
+  project = data.google_project.project.id
+  role    = "roles/confidentialcomputing.workloadUser"
+  member  = "serviceAccount:${google_service_account.mig_service_account.email}"
 }
 
 resource "google_project_iam_member" "mig_log_writer" {
-  project = data.google_project.project.name
+  project = data.google_project.project.id
   role    = "roles/logging.logWriter"
   member  = "serviceAccount:${google_service_account.mig_service_account.email}"
 }
 
 resource "google_project_iam_member" "mig_metric_writer" {
-  project = data.google_project.project.name
+  project = data.google_project.project.id
   role    = "roles/monitoring.metricWriter"
   member  = "serviceAccount:${google_service_account.mig_service_account.email}"
 }
 
 resource "google_project_iam_member" "mig_trace_agent" {
-  project = data.google_project.project.name
+  project = data.google_project.project.id
   role    = "roles/cloudtrace.agent"
   member  = "serviceAccount:${google_service_account.mig_service_account.email}"
 }
@@ -137,15 +137,15 @@ resource "google_compute_instance_template" "confidential_vm_template" {
   service_account {
     email = google_service_account.mig_service_account.email
     scopes = [
-        "https://www.googleapis.com/auth/cloud-platform",
-        "https://www.googleapis.com/auth/pubsub"
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/pubsub"
     ]
   }
 }
 
 resource "google_compute_region_instance_group_manager" "mig" {
-  name                    = var.managed_instance_group_name
-  base_instance_name      = var.base_instance_name
+  name               = var.managed_instance_group_name
+  base_instance_name = var.base_instance_name
   version {
     instance_template = google_compute_instance_template.confidential_vm_template.id
   }
