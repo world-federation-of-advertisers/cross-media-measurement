1e806140fea965cfbae94e8f9585f22999db983c
dawn-wang22 <mpd0622@yahoo.com>
2026-02-25 18:01:08 +0000

    Move ThrowingKmsClient to crypto.testing package and restore requiredDuchies

--- src/main/kotlin/org/wfanet/measurement/integration/crypto/testing/ThrowingKmsClient.kt
+++ src/main/kotlin/org/wfanet/measurement/integration/crypto/testing/ThrowingKmsClient.kt
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.wfanet.measurement.integration.common
+package org.wfanet.measurement.integration.crypto.testing
 
 import com.google.crypto.tink.Aead
 import com.google.crypto.tink.KmsClient
