5b857595311579485453dc53f7f1808f6a850745
getina <tinage@bazel-server.us-east1-b.c.xmm-test-ppp9j.internal>
2026-05-05 23:49:35 +0000

    feat: add BigQuery views and connections for reporting dashboard

---
+++ src/main/terraform/gcloud/cmms/sql/requisition_overview.sql
@@ -0,0 +1,56 @@
+SELECT
+  r.DataProviderResourceId,
+  r.Report,
+  REGEXP_EXTRACT(r.Report, r'reports/(.+)$') AS ReportId,
+  r.CmmsMeasurementConsumer,
+  CASE r.State
+    WHEN 0 THEN 'UNSPECIFIED'
+    WHEN 1 THEN 'STORED'
+    WHEN 2 THEN 'QUEUED'
+    WHEN 3 THEN 'PROCESSING'
+    WHEN 4 THEN 'FULFILLED'
+    WHEN 5 THEN 'REFUSED'
+    WHEN 6 THEN 'WITHDRAWN'
+    ELSE CAST(r.State AS STRING)
+  END AS RequisitionState,
+  r.CmmsCreateTime,
+  r.RequisitionCreateTime,
+  TIMESTAMP_DIFF(r.FulfilledTime, r.StoredTime, SECOND) AS FulfillmentDurationSeconds,
+  rpt.State AS ReportState,
+  rpt.CreateTime AS ReportCreateTime
+FROM (
+  SELECT * FROM EXTERNAL_QUERY(
+    'projects/${project_id}/locations/${region}/connections/edp-aggregator-conn',
+    '''SELECT
+      rm.DataProviderResourceId,
+      rm.Report,
+      REGEXP_EXTRACT(rm.Report, 'measurementConsumers/([^/]+)/') AS CmmsMeasurementConsumer,
+      CAST(rm.State AS INT64) AS State,
+      rm.CmmsCreateTime,
+      rm.CreateTime AS RequisitionCreateTime,
+      rma_stored.CreateTime AS StoredTime,
+      rma_fulfilled.CreateTime AS FulfilledTime
+    FROM RequisitionMetadata rm
+    LEFT JOIN RequisitionMetadataActions rma_stored
+      ON rm.DataProviderResourceId = rma_stored.DataProviderResourceId
+      AND rm.RequisitionMetadataId = rma_stored.RequisitionMetadataId
+      AND CAST(rma_stored.CurrentState AS INT64) = 1
+    LEFT JOIN RequisitionMetadataActions rma_fulfilled
+      ON rm.DataProviderResourceId = rma_fulfilled.DataProviderResourceId
+      AND rm.RequisitionMetadataId = rma_fulfilled.RequisitionMetadataId
+      AND CAST(rma_fulfilled.CurrentState AS INT64) = 4''')
+) r
+LEFT JOIN (
+  SELECT * FROM EXTERNAL_QUERY(
+    'projects/${project_id}/locations/${region}/connections/reporting-postgres-conn',
+    'SELECT
+      CAST("ExternalReportId" AS TEXT) AS "ExternalReportId",
+      "MeasurementConsumerId",
+      "State",
+      "CreateTime"
+    FROM "Reports"')
+) rpt
+  ON REGEXP_EXTRACT(r.Report, 'reports/(.+)$') = rpt.ExternalReportId
+%{ if data_provider_id != "" }
+WHERE r.DataProviderResourceId = '${data_provider_id}'
+%{ endif }
