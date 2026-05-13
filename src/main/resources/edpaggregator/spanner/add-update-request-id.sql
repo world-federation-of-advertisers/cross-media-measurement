4ea15c7b6417f41ead36d64f4364ca2ae6d20a03
stevenwarejones <stevenwarejones@gmail.com>
2026-05-13 23:02:21 +0000

    address requested changes

--- src/main/resources/edpaggregator/spanner/add-update-request-id.sql
+++ src/main/resources/edpaggregator/spanner/add-update-request-id.sql
@@ -14,7 +14,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
--- changeset stevenwarejones:add-update-request-id
+-- changeset stevenwarejones:10 dbms:cloudspanner
 
 START BATCH DDL;
 
