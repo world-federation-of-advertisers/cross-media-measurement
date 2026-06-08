9f9663d561109821b32abcdab23ea49153734c1f
autopilot <autopilot@local>
2026-06-08 07:35:08 +0000

    fix: missing liquibase formatted-SQL header

--- src/main/resources/kingdom/spanner/add-model-shard-memoized-vid-assignment-flag.sql
+++ src/main/resources/kingdom/spanner/add-model-shard-memoized-vid-assignment-flag.sql
@@ -1,3 +1,5 @@
+-- liquibase formatted sql
+
 -- Copyright 2026 The Cross-Media Measurement Authors
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,6 +13,9 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
+
+-- changeset marcopremier:35 dbms:cloudspanner
+-- comment: Add MemoizedVidAssignmentEnabled column to ModelShards.
 
 START BATCH DDL;
 
