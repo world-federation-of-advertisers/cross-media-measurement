ca8f0d3b36ee7ad85dd89c722628adcabe1ca866
getina <getina@google.com>
2026-08-04 19:35:01 +0000

    feat: add unlinked client accounts to the dashboard

---
+++ src/main/terraform/gcloud/cmms/sql/unlinked_accounts.sql
@@ -0,0 +1,48 @@
+-- Copyright 2026 The Cross-Media Measurement Authors
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+MERGE INTO `${project_id}.${dataset}.${table_name}` T
+USING (
+SELECT
+  `${project_id}.dashboard.externalIdToApiId`(dp.ExternalDataProviderId) AS CmmsDataProvider,
+  u.ClientAccountReferenceId,
+  u.Brands,
+  COALESCE(u.EventGroupReferenceId, CONCAT(u.EventGroupEntityKeyType, '/', u.EventGroupEntityKeyId)) AS ObservedEventGroup,
+  u.FirstObservedTime
+FROM (
+  SELECT * FROM EXTERNAL_QUERY(
+    'projects/${project_id}/locations/${region}/connections/kingdom-conn',
+    '''SELECT
+      u.DataProviderId,
+      u.ClientAccountReferenceId,
+      u.Brands,
+      u.EventGroupReferenceId,
+      u.EventGroupEntityKeyType,
+      u.EventGroupEntityKeyId,
+      u.FirstObservedTime
+    FROM UnlinkedClientAccounts u''')
+) u
+LEFT JOIN (
+  SELECT * FROM EXTERNAL_QUERY(
+    'projects/${project_id}/locations/${region}/connections/kingdom-conn',
+    '''SELECT
+      dp.DataProviderId,
+      dp.ExternalDataProviderId
+    FROM DataProviders dp''')
+) dp
+  ON u.DataProviderId = dp.DataProviderId
+) S
+ON FALSE
+WHEN NOT MATCHED THEN INSERT ROW
+WHEN NOT MATCHED BY SOURCE THEN DELETE;
