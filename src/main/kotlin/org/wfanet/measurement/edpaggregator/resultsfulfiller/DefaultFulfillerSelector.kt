1704a5669e9019bc40fc47bf6fccb50ebe7c6ced
Steven Jones <stevenwarejones@gmail.com>
2026-08-28 00:55:55 +0000

    Revert "feat: Add DYNAMIC impression cap mode for EDPA Direct (#4359)"
    This reverts commit 70a8c0c67a9fd7c5b94d34a026111fafccda2490.

--- src/main/kotlin/org/wfanet/measurement/edpaggregator/resultsfulfiller/DefaultFulfillerSelector.kt
+++ src/main/kotlin/org/wfanet/measurement/edpaggregator/resultsfulfiller/DefaultFulfillerSelector.kt
@@ -36,7 +36,6 @@ import org.wfanet.measurement.edpaggregator.resultsfulfiller.fulfillers.DirectMe
 import org.wfanet.measurement.edpaggregator.resultsfulfiller.fulfillers.HMShuffleMeasurementFulfiller
 import org.wfanet.measurement.edpaggregator.resultsfulfiller.fulfillers.MeasurementFulfiller
 import org.wfanet.measurement.edpaggregator.resultsfulfiller.fulfillers.TrusTeeMeasurementFulfiller
-import org.wfanet.measurement.edpaggregator.v1alpha.ResultsFulfillerParams.ImpressionCapMode
 import org.wfanet.measurement.eventdataprovider.requisition.v2alpha.common.FrequencyVectorBuilder
 import org.wfanet.measurement.eventdataprovider.requisition.v2alpha.trustee.FulfillRequisitionRequestBuilder as TrusteeFulfillRequisitionRequestBuilder
 
@@ -114,7 +113,6 @@ data class TrusTeeConfig(
  * @param noiserSelector strategy for selecting differential privacy mechanisms
  * @param resultMinimumThresholds optional small-cell suppression thresholds; null disables
  *   suppression
- * @param impressionCapMode how the per-user impression cap is chosen.
  * @param overrideImpressionMaxFrequencyPerUser optional frequency cap override; null or -1 means no
  *   capping and uses totalUncappedImpressions instead
  * @param supportedMultiPartyNoiseMechanisms set of [NoiseMechanism] values this EDP supports for
@@ -132,7 +130,6 @@ class DefaultFulfillerSelector(
   private val noiserSelector: NoiserSelector,
   private val resultMinimumThresholds: ResultMinimumThresholds?,
   private val overrideImpressionMaxFrequencyPerUser: Int?,
-  private val impressionCapMode: ImpressionCapMode = ImpressionCapMode.LEGACY_CAP_MODE,
   private val supportedMultiPartyNoiseMechanisms: Set<NoiseMechanism>,
   private val trusTeeConfig: TrusTeeConfig? = null,
   private val kekUriToKeyNameMap: Map<String, String> = emptyMap(),
@@ -336,7 +333,6 @@ class DefaultFulfillerSelector(
         resultMinimumThresholds = resultMinimumThresholds,
         impressionMaxFrequencyPerUser = overrideImpressionMaxFrequencyPerUser,
         totalUncappedImpressions = totalUncappedImpressions,
-        impressionCapMode = impressionCapMode,
       )
     return DirectMeasurementFulfiller(
       requisition.name,
