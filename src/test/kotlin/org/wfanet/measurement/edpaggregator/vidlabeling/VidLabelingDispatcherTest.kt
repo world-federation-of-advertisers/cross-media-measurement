40831086d881e0740c31e1c6d2ad37d1f6dbafae
marcopremier <1636171+Marco-Premier@users.noreply.github.com>
2026-09-02 13:47:36 +0000

    test(edpa): Preserve recovery redelivery coverage
    Issue: #4428

--- src/test/kotlin/org/wfanet/measurement/edpaggregator/vidlabeling/VidLabelingDispatcherTest.kt
+++ src/test/kotlin/org/wfanet/measurement/edpaggregator/vidlabeling/VidLabelingDispatcherTest.kt
@@ -591,6 +591,8 @@ class VidLabelingDispatcherTest {
         listRawImpressionUploadsResponse { rawImpressionUploads += registeredRecovery },
         listRawImpressionUploadsResponse { rawImpressionUploads += registeredRecovery },
         listRawImpressionUploadsResponse { rawImpressionUploads += registeredRecovery },
+        listRawImpressionUploadsResponse { rawImpressionUploads += registeredRecovery },
+        listRawImpressionUploadsResponse { rawImpressionUploads += registeredRecovery },
         listRawImpressionUploadsResponse {},
       )
     whenever(rawImpressionUploadModelLineService.listRawImpressionUploadModelLines(any()))
