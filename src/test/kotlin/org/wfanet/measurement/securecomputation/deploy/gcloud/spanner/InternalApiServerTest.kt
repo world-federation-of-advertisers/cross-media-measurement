d3e931035bdadb2c5bc456498af7aa757cca92ed
marcopremier <marcoprem@gmail.com>
2026-09-04 08:54:37 +0200

    feat(edpa): throttle VID recovery RPCs (#4429)
    Throttle dead-letter and manual recovery RPCs and document the
    deployment-wide rate budget.
    
    Issue: #4427
    
    ---------
    
    Co-authored-by: marcopremier <1636171+Marco-Premier@users.noreply.github.com>

---
+++ src/test/kotlin/org/wfanet/measurement/securecomputation/deploy/gcloud/spanner/InternalApiServerTest.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2026 The Cross-Media Measurement Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wfanet.measurement.securecomputation.deploy.gcloud.spanner
+
+import com.google.common.truth.Truth.assertThat
+import kotlin.test.assertFailsWith
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import picocli.CommandLine
+
+@RunWith(JUnit4::class)
+class InternalApiServerTest {
+  @Test
+  fun `command line rejects zero RPC interval`() {
+    val exception =
+      assertFailsWith<CommandLine.ParameterException> {
+        CommandLine(InternalApiServer()).parseArgs("--metadata-read-rpc-min-interval=0s")
+      }
+
+    assertThat(exception).hasMessageThat().contains("positive human-readable duration")
+  }
+
+  @Test
+  fun `command line rejects negative RPC interval`() {
+    val exception =
+      assertFailsWith<CommandLine.ParameterException> {
+        CommandLine(InternalApiServer()).parseArgs("--metadata-read-rpc-min-interval=-1s")
+      }
+
+    assertThat(exception).hasMessageThat().contains("complete human-readable duration")
+  }
+
+  @Test
+  fun `command line rejects partially malformed RPC interval`() {
+    val exception =
+      assertFailsWith<CommandLine.ParameterException> {
+        CommandLine(InternalApiServer()).parseArgs("--control-plane-rpc-min-interval=500msjunk")
+      }
+
+    assertThat(exception).hasMessageThat().contains("complete human-readable duration")
+  }
+}
