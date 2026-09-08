b16cf3fe393ed5614b310c8ce99a7355b72b3ed1
marcopremier <1636171+Marco-Premier@users.noreply.github.com>
2026-09-08 10:29:31 +0000

    fix(edpaggregator): Make batch undelete retry-safe
    Issue: #4428

--- src/test/kotlin/org/wfanet/measurement/edpaggregator/service/v1alpha/ImpressionMetadataServiceTest.kt
+++ src/test/kotlin/org/wfanet/measurement/edpaggregator/service/v1alpha/ImpressionMetadataServiceTest.kt
@@ -1416,6 +1416,7 @@ class ImpressionMetadataServiceTest {
 
     assertThat(response.impressionMetadataList.map { it.state })
       .containsExactly(ImpressionMetadata.State.ACTIVE, ImpressionMetadata.State.ACTIVE)
+      .inOrder()
   }
 
   @Test
