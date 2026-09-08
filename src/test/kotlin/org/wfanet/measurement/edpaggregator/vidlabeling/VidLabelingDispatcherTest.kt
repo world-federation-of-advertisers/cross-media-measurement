35850fce3d5c69bb3788c8a9e383f8bbac85fc72
marcopremier <1636171+Marco-Premier@users.noreply.github.com>
2026-09-08 13:44:15 +0000

    fix(edpaggregator): avoid ordering recovery generations
    Issue: #4428

--- src/test/kotlin/org/wfanet/measurement/edpaggregator/vidlabeling/VidLabelingDispatcherTest.kt
+++ src/test/kotlin/org/wfanet/measurement/edpaggregator/vidlabeling/VidLabelingDispatcherTest.kt
@@ -676,7 +676,8 @@ class VidLabelingDispatcherTest {
       name = sourceUploadName
       state = RawImpressionUpload.State.FAILED
       doneBlobUri = DONE_BLOB_PATH
-      doneBlobGeneration = DONE_BLOB_GENERATION - 1
+      doneBlobGeneration = DONE_BLOB_GENERATION + 100
+      doneBlobCreateTime = DONE_BLOB_CREATE_TIME.minusSeconds(1).toProtoTime()
     }
     val blob = createMockBlob("$FOLDER_PREFIX/file1.parquet")
     whenever(storageClient.listBlobs(any())).thenReturn(flowOf(blob))
@@ -713,6 +714,7 @@ class VidLabelingDispatcherTest {
           name = "$DATA_PROVIDER_NAME/rawImpressionUploads/$RAW_IMPRESSION_UPLOAD_ID"
           doneBlobUri = DONE_BLOB_PATH
           doneBlobGeneration = DONE_BLOB_GENERATION
+          doneBlobCreateTime = DONE_BLOB_CREATE_TIME.toProtoTime()
         }
       )
     whenever(rawImpressionUploadFileService.batchCreateRawImpressionUploadFiles(any()))
@@ -738,12 +740,14 @@ class VidLabelingDispatcherTest {
       name = sourceUploadName
       state = RawImpressionUpload.State.FAILED
       doneBlobUri = DONE_BLOB_PATH
-      doneBlobGeneration = DONE_BLOB_GENERATION - 1
+      doneBlobGeneration = DONE_BLOB_GENERATION + 100
+      doneBlobCreateTime = DONE_BLOB_CREATE_TIME.minusSeconds(1).toProtoTime()
     }
     val registeredRecovery = rawImpressionUpload {
       name = "$DATA_PROVIDER_NAME/rawImpressionUploads/$RAW_IMPRESSION_UPLOAD_ID"
       doneBlobUri = DONE_BLOB_PATH
       doneBlobGeneration = DONE_BLOB_GENERATION
+      doneBlobCreateTime = DONE_BLOB_CREATE_TIME.toProtoTime()
       replacesRawImpressionUpload = sourceUploadName
     }
     val blob = createMockBlob("$FOLDER_PREFIX/file1.parquet")
@@ -808,12 +812,14 @@ class VidLabelingDispatcherTest {
       name = sourceUploadName
       state = RawImpressionUpload.State.FAILED
       doneBlobUri = DONE_BLOB_PATH
-      doneBlobGeneration = DONE_BLOB_GENERATION - 1
+      doneBlobGeneration = DONE_BLOB_GENERATION + 100
+      doneBlobCreateTime = DONE_BLOB_CREATE_TIME.minusSeconds(1).toProtoTime()
     }
     val newerRecovery = rawImpressionUpload {
       name = "$DATA_PROVIDER_NAME/rawImpressionUploads/newer-recovery"
       doneBlobUri = DONE_BLOB_PATH
-      doneBlobGeneration = DONE_BLOB_GENERATION + 1
+      doneBlobGeneration = DONE_BLOB_GENERATION - 100
+      doneBlobCreateTime = DONE_BLOB_CREATE_TIME.plusSeconds(1).toProtoTime()
       replacesRawImpressionUpload = sourceUploadName
     }
     whenever(rawImpressionUploadService.getRawImpressionUpload(any())).thenReturn(source)
@@ -838,7 +844,8 @@ class VidLabelingDispatcherTest {
       name = sourceUploadName
       state = RawImpressionUpload.State.COMPLETED
       doneBlobUri = DONE_BLOB_PATH
-      doneBlobGeneration = DONE_BLOB_GENERATION - 1
+      doneBlobGeneration = DONE_BLOB_GENERATION + 100
+      doneBlobCreateTime = DONE_BLOB_CREATE_TIME.minusSeconds(1).toProtoTime()
     }
     whenever(rawImpressionUploadService.getRawImpressionUpload(any())).thenReturn(source)
     whenever(rawImpressionUploadService.listRawImpressionUploads(any()))
