35e84200c7c0bc5d35fa55020ba0dd90599503f3
Steven Jones <stevenwarejones@gmail.com>
2026-09-15 21:27:59 +0000

    feat(terraform): Add report trace operator identity

--- src/main/terraform/gcloud/modules/reporting/outputs.tf
+++ src/main/terraform/gcloud/modules/reporting/outputs.tf
@@ -12,17 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-output "work_item_tee_mig_names" {
-  description = "Names of Managed Instance Groups that consume Secure Computation WorkItems."
-  value       = module.edp_aggregator.work_item_tee_mig_names
-}
-
 output "report_trace_operator_service_account_email" {
   description = "Email of the read-only report-trace operator service account."
-  value       = module.reporting_v2.report_trace_operator_service_account_email
+  value       = google_service_account.report_trace_operator.email
 }
 
 output "report_trace_operator_postgres_user" {
   description = "Cloud SQL IAM database username for the report-trace operator."
-  value       = module.reporting_v2.report_trace_operator_postgres_user
+  value       = google_sql_user.report_trace_operator.name
 }
