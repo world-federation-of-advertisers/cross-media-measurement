1d5d652879099654018a9bc0e3a580e27b729d63
Steven Jones <stevenwarejones@gmail.com>
2026-09-16 13:44:17 +0000

    style(edpa): format dispatch validation changes

--- src/test/kotlin/org/wfanet/measurement/edpaggregator/requisitionfetcher/RequisitionFetcherTest.kt
+++ src/test/kotlin/org/wfanet/measurement/edpaggregator/requisitionfetcher/RequisitionFetcherTest.kt
@@ -656,9 +656,7 @@ class RequisitionFetcherTest {
       }
     whenever(requisitionsServiceMock.listRequisitions(any()))
       .thenReturn(
-        listRequisitionsResponse {
-          requisitions += listOf(existingRequisition, newRequisition)
-        }
+        listRequisitionsResponse { requisitions += listOf(existingRequisition, newRequisition) }
       )
     val invalidRows =
       listOf(
@@ -737,8 +735,7 @@ class RequisitionFetcherTest {
     val dispatchedGroupIds = mutableListOf<String>()
     val dispatcher =
       object : RequisitionWorkItemDispatcher {
-        override fun workItemName(groupId: String): String =
-          "workItems/results-fulfiller-$groupId"
+        override fun workItemName(groupId: String): String = "workItems/results-fulfiller-$groupId"
 
         override suspend fun dispatch(groupId: String, blobUri: String) {
           dispatchedGroupIds += groupId
